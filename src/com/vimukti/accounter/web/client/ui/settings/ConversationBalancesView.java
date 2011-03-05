75183c241402feae3468349cd570847227507acd
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-03-05 13:05:25 +0000

    Former-commit-id: 55f0b05d8b7c69ccdf6c076f5e46a0a74528b035

--- src/com/vimukti/accounter/web/client/ui/settings/ConversationBalancesView.java
+++ src/com/vimukti/accounter/web/client/ui/settings/ConversationBalancesView.java
@@ -1,10 +1,23 @@
 package com.vimukti.accounter.web.client.ui.settings;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.core.Utility;
 import com.vimukti.accounter.web.client.ui.AbstractBaseView;
 
 @SuppressWarnings("unchecked")
 public class ConversationBalancesView extends AbstractBaseView {
+	private HTML superHeaderHtml, headerHtml;
+	private VerticalPanel headerPanel, bodyPanel, mainPanel, tabBodyPanel;
+	private HorizontalPanel buttonPanel;
+	private Button addComparativeBalancesButton, conversionDateButton;
+	private DecoratedTabPanel tabPanel;
+
+	// private TabPanel
 
 	@Override
 	public void fitToSize(int height, int width) {
@@ -19,12 +32,49 @@ public class ConversationBalancesView extends AbstractBaseView {
 	}
 
 	private void createControls() {
+		headerPanel = new VerticalPanel();
+		bodyPanel = new VerticalPanel();
+		mainPanel = new VerticalPanel();
+		buttonPanel = new HorizontalPanel();
+
+		superHeaderHtml = new HTML("General Settings >");
+		headerHtml = new HTML("Conversation Balances");
+
+		addComparativeBalancesButton = new Button("Add Comparative Balances");
+		conversionDateButton = new Button("Conversion Date");
+
+		buttonPanel.add(addComparativeBalancesButton);
+		buttonPanel.add(conversionDateButton);
+
+		headerPanel.add(superHeaderHtml);
+		headerPanel.add(headerHtml);
+
+		bodyPanel.add(buttonPanel);
+		try {
+			tabPanel = new DecoratedTabPanel();
+			tabPanel.add(getBodyControls(), Utility
+					.getCurrentFiscalYearStartDate().toString()
+					+ " _ " + Utility.getCurrentFiscalYearEndDate().toString());
+
+			bodyPanel.add(tabPanel);
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		mainPanel.add(headerPanel);
+		mainPanel.add(bodyPanel);
+
+		add(mainPanel);
+	}
+
+	private VerticalPanel getBodyControls() {
+		tabBodyPanel = new VerticalPanel();
+		return tabBodyPanel;
 
 	}
 
 	@Override
 	public void initData() {
-		// TODO Auto-generated method stub
 		super.initData();
 	}
 
