f3a81f6f49a5815934560e6e85f14e245b1bd16d
kumar.k <kumar.k@vimukti.com>
2011-03-07 05:41:00 +0000

    Former-commit-id: cbc11094c66c5fc9a5d5a03c1913ffd0ad09cb21

--- src/com/vimukti/accounter/web/client/ui/Header.java
+++ src/com/vimukti/accounter/web/client/ui/Header.java
@@ -1,12 +1,14 @@
 package com.vimukti.accounter.web.client.ui;
 
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 
 public class Header extends HorizontalPanel {
 
 	private HorizontalPanel hpanel1;
-	private Label l1, l2, l3;
+	private Label l1, helpLabel, userName;
+	private HTML logout;
 
 	public Header() {
 		createControls();
@@ -19,13 +21,21 @@ public class Header extends HorizontalPanel {
 		l1 = new Label();
 		l1.setText("Demo Company (Global)");
 		l1.addStyleName("label2-style");
-		l2 = new Label("Logout");
-		l3 = new Label("Help");
+		userName = new Label();
+		userName.setText(FinanceApplication.getCompanyMessages().userName(
+				FinanceApplication.clientIdentity.getDisplayName()));
+		userName.addStyleName("userName-style");
+		logout = new HTML("<a href='/do/logout'>Logout</a>");
+		logout.addStyleName("logout-html");
+		helpLabel = new Label("Help");
+		helpLabel.addStyleName("help-style");
 		hpanel1.add(l1);
-		hpanel1.add(l2);
-		hpanel1.add(l3);
+		hpanel1.add(userName);
+		hpanel1.add(logout);
+		hpanel1.add(helpLabel);
 		this.add(hpanel1);
 		this.setWidth("100%");
 
 	}
+
 }
