8ffb9a89d21aa9c959d5db075cff43388248ee76
kumar.k <kumar.k@vimukti.com>
2011-03-08 06:28:39 +0000

    Former-commit-id: 856fb4eff966a454267e919cb8dc2ea75005a6c4

--- src/com/vimukti/accounter/web/client/ui/customers/InvoiceView.java
+++ src/com/vimukti/accounter/web/client/ui/customers/InvoiceView.java
@@ -3,6 +3,7 @@ package com.vimukti.accounter.web.client.ui.customers;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
