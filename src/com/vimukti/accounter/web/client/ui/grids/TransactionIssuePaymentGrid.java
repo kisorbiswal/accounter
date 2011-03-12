9d110e5622d1ba4011db114dd774d3a60bf1626e
kumar.k <kumar.k@vimukti.com>
2011-03-12 09:17:50 +0000

    Former-commit-id: 7d2243e6c6d6065ebe9dc910a9ae16b26e746dee

--- src/com/vimukti/accounter/web/client/ui/grids/TransactionIssuePaymentGrid.java
+++ src/com/vimukti/accounter/web/client/ui/grids/TransactionIssuePaymentGrid.java
@@ -33,8 +33,8 @@ public class TransactionIssuePaymentGrid extends
 				FinanceApplication.getVendorsMessages().number(),
 				FinanceApplication.getVendorsMessages().name(),
 				FinanceApplication.getVendorsMessages().memo(),
-				FinanceApplication.getVendorsMessages().Amount(),
-				FinanceApplication.getVendorsMessages().Paymentmethod() };
+				FinanceApplication.getBankingsMessages().amount(),
+				FinanceApplication.getVendorsMessages().paymentMethod() };
 	}
 
 	public boolean isSelected(ClientTransactionIssuePayment transactionList) {
