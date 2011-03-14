1b880ccea1ecd6736a07ca7eadba882e6feaa2f8
kumar.k <kumar.k@vimukti.com>
2011-03-14 10:11:44 +0000

    Former-commit-id: 8da37fa13e43e98eac90f8efcceb03553d400550

--- src/com/vimukti/accounter/web/client/Finance.java
+++ src/com/vimukti/accounter/web/client/Finance.java
@@ -1,18 +1,31 @@
 package com.vimukti.accounter.web.client;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.vimukti.accounter.web.client.data.ClientIdentity;
+import com.vimukti.accounter.web.client.services.IdentityService;
+import com.vimukti.accounter.web.client.services.IdentityServiceAsync;
+import com.vimukti.accounter.web.client.ui.FinanceApplication;
 import com.vimukti.accounter.web.client.ui.StartupDialog;
 import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
 import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallBack;
 
 public class Finance implements EntryPoint {
 
+	private static ClientIdentity identity;
+
 	public void onModuleLoad() {
-		
+
 		try {
 			myTestFlow();
 		} catch (Throwable e) {
-//			//UIUtils.logError("An Error Has Occured\n Please Refer Log..", e);
+			// //UIUtils.logError("An Error Has Occured\n Please Refer Log..",
+			// e);
 		}
 	}
 
@@ -20,17 +33,72 @@ public class Finance implements EntryPoint {
 		AccounterAsync.createAsync(new CreateViewAsyncCallBack() {
 
 			public void onCreateFailed(Throwable t) {
-//				UIUtils
-//						.logError("An Error Has Occured\n Please Refer Log..",
-//								t);
+				// UIUtils
+				// .logError("An Error Has Occured\n Please Refer Log..",
+				// t);
 			}
 
 			public void onCreated() {
-				new StartupDialog();
+				checkCookie();
+				// new StartupDialog();
 			}
 
 		});
 
 	}
 
+	private static void checkCookie() {
+		if (GWT.isScript()) {
+			ClientIdentity Clntidentity = getIdentity();
+			if (Clntidentity != null) {
+				initializeIdentity(Clntidentity);
+			}
+		} else {
+			new StartupDialog();
+		}
+	}
+
+	public static void initializeIdentity(ClientIdentity result) {
+		Finance.identity = result;
+	}
+
+	public static ClientIdentity getIdentity() {
+		if (identity == null) {
+			getIdentityFromServer();
+		}
+		return identity;
+	}
+
+	private static void getIdentityFromServer() {
+		IdentityServiceAsync service = GWT.create(IdentityService.class);
+		((ServiceDefTarget) service)
+				.setServiceEntryPoint("/do/bizantra/identity");
+
+		service.getClientIdentity(new Date().getTimezoneOffset(),
+				new AsyncCallback<ClientIdentity>() {
+
+					@Override
+					public void onSuccess(ClientIdentity result) {
+						initializeIdentity(result);
+						FinanceApplication application = new FinanceApplication(
+								"", result,
+								new ValueCallBack<FinanceApplication>() {
+
+									@Override
+									public void execute(FinanceApplication value) {
+										RootPanel.get().add(value);
+									}
+								});
+					}
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+	}
+
 }
+
+ 
