8decf36f8c840ee3136e225d242e8cc716619b4f
kumar.k <kumar.k@vimukti.com>
2011-03-21 07:11:18 +0000

    Former-commit-id: ebeadd93c2166faaea9da9705af99f909e27d4c3

--- src/com/vimukti/accounter/web/client/theme/ThemeImages.java
+++ src/com/vimukti/accounter/web/client/theme/ThemeImages.java
@@ -1,7 +1,25 @@
 package com.vimukti.accounter.web.client.theme;
 
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.MenuBar.Resources;
 
 public interface ThemeImages extends Resources {
+	@Source("menu_bar_left.png")
+	ImageResource menubar_left();
 
+	@Source("dropdown_indicator_5x3.png")
+	ImageResource drop_down_indicator();
+    
+	@Source("Menubar_Divider_2x30.jpg")
+	ImageResource menu_bar_devider();
+	
+	@Source("Button_left_1x27.png")
+	ImageResource button_right_green_image();
+	
+	@Source("gray_Button_left_1x27.png")
+	ImageResource button_right_gray_image();
+	
+	@Source("blue_Left_side.png")
+	ImageResource button_right_blue_image();
+	
 }
---
+++ src/com/vimukti/accounter/web/client/theme/ThemesUtil.java
@@ -0,0 +1,52 @@
+package com.vimukti.accounter.web.client.theme;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.MenuItem;
+import com.vimukti.accounter.web.client.ui.FinanceApplication;
+
+public class ThemesUtil {
+
+	public static void insertImageChildToMenuItem(MenuBar menubar,
+			MenuItem menuitem) {
+		Image child = new Image();
+		child.addStyleName("menu_arrow");
+		child.setUrl(FinanceApplication.getThemeImages().drop_down_indicator()
+				.getURL());
+		DOM.insertChild(menuitem.getElement(), child.getElement(), 0);
+		insertEmptyChildToMenuBar(menubar);
+	}
+
+	public static void insertEmptyChildToMenuBar(MenuBar menubar) {
+		MenuItem menuitem = menubar.addItem(new MenuItem("", new Command() {
+
+			@Override
+			public void execute() {
+
+			}
+		}));
+		Image child = new Image();
+		child.setUrl(FinanceApplication.getThemeImages().menu_bar_devider()
+				.getURL());
+		menuitem.setStyleName("menubar-devider");
+		DOM.insertChild(menuitem.getElement(), child.getElement(), 0);
+	}
+
+	public static void addDivToButton(Button button, ImageResource imgResource,
+			String styleName) {
+		Element btnEle = button.getElement();
+
+		Element divEle = DOM.createDiv();
+		divEle.addClassName(styleName);
+		divEle.getStyle().setBackgroundImage("url("+imgResource.getURL()+")");
+
+		btnEle.getParentElement().insertAfter(divEle, btnEle);
+
+	}
+
+}
