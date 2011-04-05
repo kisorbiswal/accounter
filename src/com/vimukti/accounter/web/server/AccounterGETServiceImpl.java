6cb0421aabfe8635ed8dd51e5bf11de2d9b54acd
kumar.k <kumar.k@vimukti.com>
2011-04-05 10:52:28 +0000

    Former-commit-id: 8f4b73809fdb2948e1591a44574b4c775ac4ba12

--- src/com/vimukti/accounter/web/server/AccounterGETServiceImpl.java
+++ src/com/vimukti/accounter/web/server/AccounterGETServiceImpl.java
@@ -1,5 +1,6 @@
 package com.vimukti.accounter.web.server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bizantra.server.utils.SecureUtils;
@@ -7,6 +8,7 @@ import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.web.client.IAccounterGETService;
 import com.vimukti.accounter.web.client.core.AccounterCoreType;
 import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.HelpLink;
 import com.vimukti.accounter.web.client.core.HrEmployee;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
 import com.vimukti.accounter.web.client.core.Lists.KeyFinancialIndicators;
@@ -105,5 +107,36 @@ public class AccounterGETServiceImpl extends AccounterRPCBaseServiceImpl
 	public List<HrEmployee> getHREmployees() throws InvalidSessionException {
 		FinanceTool tool = (FinanceTool) getFinanceTool();
 		return tool.getHREmployees();
+	}
+
+	public List<HelpLink> getHelpLinks(int type) {
+
+		HelpLink link = new HelpLink(
+				"How to EnterBill transaction?",
+				"Click on the Enter Bill action or in the Supplier drop down select Enter Bill.");
+		HelpLink link1 = new HelpLink(
+				"How to Invoice transaction?",
+				"Click on the New Invoice action or in the Customer drop down select New and then New Invoice");
+		HelpLink link2 = new HelpLink(
+				"How to Quote transaction?",
+				"Click on the Quote action or in the Customer drop down select New and then New Quote");
+		HelpLink link3 = new HelpLink("How to Paybill transaction?",
+				"Click on the PayBill action or in the Supplier drop down select PayBill");
+		HelpLink link4 = new HelpLink("How to CustomerPayment transaction?",
+				"In the Customer drop down select Customer payment");
+		HelpLink link5 = new HelpLink("How to Makedeposit transaction?",
+				"In the Banking drop down select Deposit/Transfer Funds");
+
+		List<HelpLink> helpLinks = new ArrayList<HelpLink>();
+
+		helpLinks.add(link);
+		helpLinks.add(link1);
+		helpLinks.add(link2);
+		helpLinks.add(link3);
+		helpLinks.add(link4);
+		helpLinks.add(link5);
+
+		return helpLinks;
+
 	}
 }
