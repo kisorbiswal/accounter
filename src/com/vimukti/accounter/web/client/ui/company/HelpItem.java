55a6daf1cbd1949ff075c49bc5ac720c4943d0b6
kumar.k <kumar.k@vimukti.com>
2011-04-05 10:55:08 +0000

    Former-commit-id: 677ab3c0449fc9e224eb7336d570c1cff69d4bfc

---
+++ src/com/vimukti/accounter/web/client/ui/company/HelpItem.java
@@ -0,0 +1,69 @@
+package com.vimukti.accounter.web.client.ui.company;
+
+import java.util.List;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.CaptionPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.vimukti.accounter.web.client.core.HelpLink;
+import com.vimukti.accounter.web.client.ui.forms.CustomDialog;
+
+public class HelpItem extends HorizontalPanel {
+	public FlexTable table;
+	public int currentrow, currentcolumn;
+
+	public HelpItem() {
+		createControls();
+	}
+
+	private void createControls() {
+		CaptionPanel panel = new CaptionPanel();
+		panel.setCaptionText("Help Links");
+		panel.setStyleName("help-links1");
+		table = new FlexTable();
+		panel.add(table);
+		add(panel);
+		this.setCellHorizontalAlignment(panel, ALIGN_LEFT);
+		this.setSize("100%", "100%");
+
+	}
+
+	public void addLinks(List<HelpLink> helplinks) {
+		for (HelpLink helpLink : helplinks) {
+			addLink(helpLink);
+		}
+	}
+
+	public void addLink(final HelpLink helpLink) {
+		final Anchor hyperlink = new Anchor();
+		hyperlink.setText(helpLink.getTitle());
+		hyperlink.addClickHandler(new ClickHandler() {
+
+			@SuppressWarnings("deprecation")
+			@Override
+			public void onClick(ClickEvent event) {
+
+				CustomDialog dialog = new CustomDialog(false, true);
+				HTML data = new HTML("<p>" + helpLink.getMessage() + "</p");
+				dialog.add(data);
+				dialog.setSize("350px", "350px");
+				dialog.setPopupPosition(hyperlink.getAbsoluteLeft(), hyperlink
+						.getAbsoluteTop() - 350);
+				dialog.show();
+
+			}
+		});
+		table.setWidget(currentrow, currentcolumn, hyperlink);
+		table.setWidth("100%");
+		currentcolumn++;
+		if (currentcolumn > 1) {
+			currentrow++;
+			currentcolumn = 0;
+		}
+	}
+
+}
