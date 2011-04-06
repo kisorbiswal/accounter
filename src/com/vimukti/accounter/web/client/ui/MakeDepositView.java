b01bc722e795f879c3fcda8aa9c1e1f36c1564cd
kumar.k <kumar.k@vimukti.com>
2011-04-06 11:11:24 +0000

    Former-commit-id: 78324516490704bb85a577283867f9d971d4b6e0

--- src/com/vimukti/accounter/web/client/ui/MakeDepositView.java
+++ src/com/vimukti/accounter/web/client/ui/MakeDepositView.java
@@ -777,7 +777,7 @@ public class MakeDepositView extends
 		Label lab = new Label(FinanceApplication.getFinanceUIConstants()
 				.makeDeposit());
 		lab.addStyleName("lable-title");
-		lab.setHeight("50px");
+//		lab.setHeight("50px");
 		date = UIUtils.date(bankingConstants.date());
 
 		// set the transactionDate while creation
