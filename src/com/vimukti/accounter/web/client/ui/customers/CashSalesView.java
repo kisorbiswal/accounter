9520976ed5447bd1f2542322b02ad904165a41ef
kumar.k <kumar.k@vimukti.com>
2011-04-06 11:11:50 +0000

    Former-commit-id: 5f72f46e211022daf2486b3f04741819feb4b2b8

--- src/com/vimukti/accounter/web/client/ui/customers/CashSalesView.java
+++ src/com/vimukti/accounter/web/client/ui/customers/CashSalesView.java
@@ -83,7 +83,7 @@ public class CashSalesView extends
 				.newcashsale());
 		lab1.setStyleName(FinanceApplication.getCustomersMessages()
 				.lableTitle());
-		lab1.setHeight("35px");
+//		lab1.setHeight("35px");
 		transactionDateItem = createTransactionDateItem();
 		transactionDateItem
 				.addDateValueChangeHandler(new DateValueChangeHandler() {
