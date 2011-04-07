ff1c79451a9b981e46e026e3f5cc84eb7d8ba239
kumar.k <kumar.k@vimukti.com>
2011-04-07 07:28:26 +0000

    Former-commit-id: 05398238d6b6a32b66722e4f17b3484ea1cedd5d

---
+++ src/com/vimukti/accounter/web/client/ui/SalesTaxItemsGrid.java
@@ -0,0 +1,62 @@
+package com.vimukti.accounter.web.client.ui;
+
+import com.vimukti.accounter.web.client.core.ClientTAXItem;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.ui.core.InvalidTransactionEntryException;
+import com.vimukti.accounter.web.client.ui.grids.BaseListGrid;
+import com.vimukti.accounter.web.client.ui.grids.ListGrid;
+
+public class SalesTaxItemsGrid extends BaseListGrid<ClientTAXItem> {
+
+	public SalesTaxItemsGrid(boolean isMultiSelectionEnable) {
+		super(isMultiSelectionEnable);
+	}
+
+	@Override
+	protected void executeDelete(ClientTAXItem object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected int[] setColTypes() {
+		return new int[] { ListGrid.COLUMN_TYPE_TEXT,
+				ListGrid.COLUMN_TYPE_DECIMAL_TEXT };
+	}
+
+	@Override
+	protected Object getColumnValue(ClientTAXItem obj, int index) {
+		switch (index) {
+		case 0:
+			return obj.getName() != null ? obj.getName() : "";
+		case 1:
+			return obj.getTaxRate() + "%";
+		}
+		return null;
+	}
+
+	@Override
+	public void onDoubleClick(ClientTAXItem obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean validateGrid() throws InvalidTransactionEntryException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected String[] getColumns() {
+		return new String[] { FinanceApplication.getCustomersMessages().name(),
+				FinanceApplication.getCustomersMessages().currentRate() };
+	}
+
+	@Override
+	public void processupdateView(IAccounterCore core, int command) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
