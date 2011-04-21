dc2486cd89bfb0c6f269f0b2cafa5b585c05ae41
Varakumar Pjd <varakumar.pjd@vimukti.com>
2011-04-21 05:06:12 +0000

    Former-commit-id: ed7bf424e061355464874fa1a1dd3789d37161ce

---
+++ src/com/vimukti/accounter/web/client/ui/forms/CustomComboItem.java
@@ -0,0 +1,240 @@
+package com.vimukti.accounter.web.client.ui.forms;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.ui.FocusWidget;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.vimukti.accounter.web.client.ui.core.Accounter;
+import com.vimukti.accounter.web.client.ui.forms.TextBoxItem.KeyPressListener;
+
+public class CustomComboItem extends FormItem {
+
+	public TextBoxItem textBox;
+	private CustomFocusWidget customFocusWidget;
+	public ClickHandler clickHandler;
+	public KeyPressHandler keyPressHandler;
+	public KeyDownHandler keyDownHandler;
+	public BlurHandler blurHandler;
+
+	// TextBoxItem textBoxItem;
+	public CustomComboItem() {
+
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+
+		textBox = new TextBoxItem() {
+			protected void onAttach() {
+				super.onAttach();
+				CustomComboItem.this.onAttach();
+			};
+		};
+
+		horizontalPanel.add(textBox);
+		SimplePanel downarrowpanel = new SimplePanel();
+		downarrowpanel.addStyleName("downarrow-button");
+		horizontalPanel.add(downarrowpanel);
+		// textBoxItem= new TextBoxItem();
+		// @Override
+		// public void sinkEvents(int eventBitsToAdd) {
+		// super.sinkEvents(Event.ONFOCUS);
+		// }
+		//
+		// @Override
+		// public void onBrowserEvent(Event event) {
+		// TextItem.this.showValidated();
+		// super.onBrowserEvent(event);
+		// }
+		// };
+
+		this.textBox.addBlurHandler(new BlurHandler() {
+
+			@Override
+			public void onBlur(BlurEvent event) {
+
+				CustomComboItem.this.showValidated();
+
+			}
+		});
+
+		customFocusWidget = new CustomFocusWidget(horizontalPanel.getElement());
+
+		ClickHandler clickHandler = new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				CustomComboItem.this.clickHandler.onClick(event);
+			}
+		};
+
+		customFocusWidget.addClickHandler(clickHandler);
+
+		KeyPressHandler keyPressHandler = new KeyPressHandler() {
+
+			@Override
+			public void onKeyPress(KeyPressEvent event) {
+				if (CustomComboItem.this.keyPressHandler != null)
+					CustomComboItem.this.keyPressHandler.onKeyPress(event);
+			}
+		};
+
+		customFocusWidget.addKeyPressHandler(keyPressHandler);
+
+		KeyDownHandler keyDownHandler = new KeyDownHandler() {
+
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (CustomComboItem.this.keyDownHandler != null)
+					CustomComboItem.this.keyDownHandler.onKeyDown(event);
+			}
+		};
+
+		customFocusWidget.addKeyDownHandler(keyDownHandler);
+
+		BlurHandler blurHandler = new BlurHandler() {
+
+			@Override
+			public void onBlur(BlurEvent event) {
+				if (CustomComboItem.this.blurHandler != null)
+					CustomComboItem.this.blurHandler.onBlur(event);
+			}
+		};
+
+		customFocusWidget.addBlurHandler(blurHandler);
+		// if(!validate()){
+		// parent.setNumCols(3);
+		// addLabelWidget(parent);
+		// parent.add(getMainWidget(), columnSpan);
+		// Label label = new Label("Error");
+		// label.setStyleName("ErrorLabel");
+		// parent.add(label,1);
+		// }
+		// else{
+
+	}
+
+	protected void onAttach() {
+
+	}
+
+	public CustomComboItem(String title) {
+		this();
+		setTitle(title);
+	}
+
+	@Override
+	public Object getValue() {
+		if (textBox.getText() == null)
+			return "";
+		return textBox.getText();
+	}
+
+	@Override
+	public String getDisplayValue() {
+		if (textBox.getText() == null)
+			return "";
+		return textBox.getText();
+
+	}
+
+	@Override
+	public void setValue(Object value) {
+		if (value != null)
+			this.textBox.setValue(value.toString());
+
+	}
+
+	public void setKeyPressHandler(KeyPressListener keyPressListener) {
+		this.textBox.setKeyPressHandler(keyPressListener);
+	}
+
+	@Override
+	public void addBlurHandler(BlurHandler blurHandler) {
+		textBox.addBlurHandler(blurHandler);
+		this.blurHandler = blurHandler;
+	}
+
+	public void setKeyBoardHandler(KeyPressHandler keyPressHandler) {
+		textBox.addKeyPressHandler(keyPressHandler);
+		this.keyPressHandler = keyPressHandler;
+	}
+
+	public void setKeyDownHandler(KeyDownHandler keyDownHandler) {
+		textBox.addKeyDownHandler(keyDownHandler);
+		this.keyDownHandler = keyDownHandler;
+	}
+
+	@Override
+	public void addChangeHandler(ChangeHandler changeHandler) {
+		textBox.addChangeHandler(changeHandler);
+	}
+
+	@Override
+	public void addClickHandler(ClickHandler handler) {
+		// textBox.addClickHandler(handler);
+		this.clickHandler = handler;
+	}
+
+	public void setDefaultValue(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// void helpimformationsetposition(){
+	//
+	// }
+
+	public void setHint(String string) {
+
+	}
+
+	public void focusInItem() {
+		this.textBox.setFocus(true);
+
+	}
+
+	@SuppressWarnings("unused")
+	private void setBorder() {
+
+	}
+
+	@Override
+	public Widget getMainWidget() {
+		return customFocusWidget;
+	}
+
+	@Override
+	public void setDisabled(boolean b) {
+		if (b)
+			this.textBox.addStyleName("disable-TextField");
+		this.textBox.setEnabled(!b);
+
+	}
+
+	public void addStyleName(String style) {
+		textBox.addStyleName(style);
+	}
+
+	public void removeStyleName(String style) {
+		textBox.removeStyleName(style);
+	}
+
+	class CustomFocusWidget extends FocusWidget {
+		public CustomFocusWidget() {
+			super();
+		}
+
+		public CustomFocusWidget(Element element) {
+			super(element);
+		}
+	}
+
+}
