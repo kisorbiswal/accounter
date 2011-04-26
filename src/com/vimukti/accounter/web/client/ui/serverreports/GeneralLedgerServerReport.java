b8d10d7b5e84960b463f019d46b35f44457e5aa5
kumar.k <kumar.k@vimukti.com>
2011-04-26 04:54:48 +0000

    Former-commit-id: 59d5d8fa41d5710bbd228056290d1260b75d6ee5

--- src/com/vimukti/accounter/web/client/ui/serverreports/GeneralLedgerServerReport.java
+++ src/com/vimukti/accounter/web/client/ui/serverreports/GeneralLedgerServerReport.java
@@ -1,15 +1,22 @@
 package com.vimukti.accounter.web.client.ui.serverreports;
 
+import com.vimukti.accounter.web.client.core.reports.TransactionDetailByAccount;
+import com.vimukti.accounter.web.client.ui.reports.IFinanceReport;
+
 /**
  * GL Report is an TransactionDetailByAccountReport only
  * 
  * @author kumar kasimala
  * 
  */
-public class GeneralLedgerServerReport extends TransactionDetailByAccountServerReport {
+public class GeneralLedgerServerReport extends
+		TransactionDetailByAccountServerReport {
 
-	public GeneralLedgerServerReport(long startDate,long endDate,int generationType) {
+	public GeneralLedgerServerReport(long startDate, long endDate,
+			int generationType,
+			IFinanceReport<TransactionDetailByAccount> reportView) {
 		super(startDate, endDate, generationType);
+		this.reportView = reportView;
 	}
 
 	@Override
