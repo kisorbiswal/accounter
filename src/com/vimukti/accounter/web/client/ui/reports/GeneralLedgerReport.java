7b117b895fd2ac1b65a5e06b9dc91ccda5aabc42
kumar.k <kumar.k@vimukti.com>
2011-04-26 04:47:54 +0000

    Former-commit-id: 30feeae73bd5b914e023edb2c1088fb4733a0be5

--- src/com/vimukti/accounter/web/client/ui/reports/GeneralLedgerReport.java
+++ src/com/vimukti/accounter/web/client/ui/reports/GeneralLedgerReport.java
@@ -1,5 +1,6 @@
 package com.vimukti.accounter.web.client.ui.reports;
 
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.ui.serverreports.GeneralLedgerServerReport;
 
 /**
@@ -11,8 +12,8 @@ import com.vimukti.accounter.web.client.ui.serverreports.GeneralLedgerServerRepo
 public class GeneralLedgerReport extends TransactionDetailByAccountReport {
 
 	public GeneralLedgerReport() {
-		this.serverReport = new GeneralLedgerServerReport(this.startDate
-				.getTime(), this.endDate.getTime(), 0);
+		this.serverReport = new GeneralLedgerServerReport(new ClientFinanceDate()
+				.getTime(), new ClientFinanceDate().getTime(), 0, this);
 	}
 
 }
