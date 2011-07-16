21e986ac7b2b0fb9409abe4a5876106e0864dd4b
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-07-16 06:02:23 +0000

    Former-commit-id: 3c610ce48fcbeaf7bfb2b022c222548aa80aa86b

---
+++ src/com/vimukti/accounter/web/client/ui/combo/EmployeeCombo.java
@@ -0,0 +1,43 @@
+package com.vimukti.accounter.web.client.ui.combo;
+
+import com.vimukti.accounter.web.client.core.ClientUser;
+import com.vimukti.accounter.web.client.ui.FinanceApplication;
+
+public class EmployeeCombo extends CustomCombo<ClientUser> {
+
+	public EmployeeCombo(String title) {
+		super(title);
+		initCombo(FinanceApplication.getCompany().getUsersList());
+	}
+
+	@Override
+	public SelectItemType getSelectItemType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDisplayName(ClientUser object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getColumnData(ClientUser object, int row, int col) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDefaultAddNewCaption() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onAddNew() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
