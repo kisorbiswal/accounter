5138bc8271f6538810c8159c0a3030d1114a1294
Amrit Mishra <amrit.mishra@vimukti.com>
2011-07-25 10:42:51 +0000

    Company initialzer class is created.
    Former-commit-id: 30b500325a5a4023648df651e0ff7aebef8ac59f

---
+++ src/com/vimukti/accounter/company/initialize/CompanyInitializedFactory.java
@@ -0,0 +1,18 @@
+package com.vimukti.accounter.company.initialize;
+
+
+import com.vimukti.accounter.core.Company;
+
+
+public class CompanyInitializedFactory {
+
+	
+	/**
+	 * this is use to get the initializer for the company
+	 * @param companyName
+	 */
+	public void getInitializer(Company companyName){
+		
+	}
+	
+}
---
+++ src/com/vimukti/accounter/company/initialize/CompanyInitializer.java
@@ -0,0 +1,13 @@
+package com.vimukti.accounter.company.initialize;
+
+public abstract class CompanyInitializer {
+
+	
+	/**
+	 * this is used to set the company type
+	 */
+	public void setCompany(){
+		
+	}
+	
+}
