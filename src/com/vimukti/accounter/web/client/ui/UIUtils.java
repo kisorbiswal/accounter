9ae42b54a53e71e55272cac69575ed9d98afb250
Nagaraju P <nagaraju.p@vimukti.com>
2011-07-25 10:31:16 +0000

    Former-commit-id: 603423e62ef07c17fb915c95b26da7e55bce6292

--- src/com/vimukti/accounter/web/client/ui/UIUtils.java
+++ src/com/vimukti/accounter/web/client/ui/UIUtils.java
@@ -1910,7 +1910,8 @@ public class UIUtils {
 	}-*/;
 
 	public static native void generateReportPDF(int start, int end,
-			int reportType, String name, String dateRangeHtml, String status)/*-{
+			int reportType, String name, String dateRangeHtml, long status)/*-{
+				//FIXME changed status String to long
 		try {
 			var frame = document.createElement("IFRAME");
 			frame.setAttribute("src",
