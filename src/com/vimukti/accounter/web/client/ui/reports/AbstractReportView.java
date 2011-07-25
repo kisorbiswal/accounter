093150d8d02356381ca1cfc3fe6fa8be610ab544
Lingarao R <lingarao.r@vimukti.com>
2011-07-25 11:45:50 +0000

    Former-commit-id: ff032c6ebf978eb5c50711a3e0ceb8aadebf3b0c

--- src/com/vimukti/accounter/web/client/ui/reports/AbstractReportView.java
+++ src/com/vimukti/accounter/web/client/ui/reports/AbstractReportView.java
@@ -13,7 +13,6 @@ import com.vimukti.accounter.web.client.core.ClientCompany;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
 import com.vimukti.accounter.web.client.core.ISorting;
-import com.vimukti.accounter.web.client.core.Utility;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.UIUtils;
 import com.vimukti.accounter.web.client.ui.core.AccounterDOM;
