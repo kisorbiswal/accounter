6c8bd1e0bc0af59622976f3bef1925f90fddf2d8
Nagaraju P <nagaraju.p@vimukti.com>
2011-07-25 12:31:53 +0000

    Former-commit-id: f3754836ea6418c4c9db2ce2060ea7b3242fc0b2

--- src/com/vimukti/accounter/company/initialize/CompanyInitializedFactory.java
+++ src/com/vimukti/accounter/company/initialize/CompanyInitializedFactory.java
@@ -1,21 +1,16 @@
 package com.vimukti.accounter.company.initialize;
 
-
 import com.vimukti.accounter.core.Company;
-
 
 public class CompanyInitializedFactory {
 
-	public static void main(String args[]){
-		
-	}
 	/**
 	 * this is use to get the initializer for the company
+	 * 
 	 * @param companyName
 	 */
-	public void getInitializer(Company companyName){
-		
-		
+	public void getInitializer(Company company) {
+
 	}
-	
+
 }
