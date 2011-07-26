175951d0d43db4035c96563ba3188b4838c0089d
Umasree V <umasree.v@vimukti.com>
2011-07-26 07:14:29 +0000

    Former-commit-id: 8438348f5f72662d12808dec05c6dbd30d2c051f

---
+++ src/com/vimukti/accounter/web/client/ui/core/ClientWarehouse.java
@@ -0,0 +1,94 @@
+package com.vimukti.accounter.web.client.ui.core;
+
+import java.util.Set;
+
+import com.vimukti.accounter.core.Address;
+import com.vimukti.accounter.core.Contact;
+import com.vimukti.accounter.core.FinanceDate;
+import com.vimukti.accounter.core.ItemStatus;
+import com.vimukti.accounter.web.client.core.AccounterCoreType;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+
+public class ClientWarehouse implements IAccounterCore {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 32420869218969185L;
+	private String stringID;
+	private Address address;
+	private Set<ItemStatus> itemStatuses;
+
+	private String name;
+	private Contact contact;
+
+	String createdBy;
+	String lastModifier;
+	FinanceDate createdDate;
+	FinanceDate lastModifiedDate;
+
+	public ClientWarehouse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public Set<ItemStatus> getItemStatuses() {
+		return itemStatuses;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public void setItemStatuses(Set<ItemStatus> itemStatuses) {
+		this.itemStatuses = itemStatuses;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
+
+	public Contact getContact() {
+		return contact;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String getDisplayName() {
+		return getName();
+	}
+
+	@Override
+	public AccounterCoreType getObjectType() {
+		return AccounterCoreType.WAREHOUSE;
+	}
+
+	@Override
+	public String getClientClassSimpleName() {
+		return "ClientWarehouse";
+	}
+
+	@Override
+	public void setID(long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public long getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
