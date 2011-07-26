428f5cb1e3c8c5abbf36a930b7c2ac29d8649b5d
Amrit Mishra <amrit.mishra@vimukti.com>
2011-07-26 11:01:56 +0000

    Former-commit-id: 671b5c4ee5f09158371bcce8b185ad10e2ca72a7

--- src/com/vimukti/accounter/company/initialize/CompanyInitializer.java
+++ src/com/vimukti/accounter/company/initialize/CompanyInitializer.java
@@ -10,4 +10,90 @@ public abstract class CompanyInitializer {
 	public abstract void init();
 	public abstract Company getCompany();
 	
+	
+	
+	public void advertising_and_marketing() {
+	}
+	
+	public void consulting_and_accounting(){
+		
+	}
+	public void deprecation(){
+		
+	}
+	public void meals_and_entertainment(){
+		
+	}
+	public void general_expenses(){
+		
+	}
+	public void insurance(){
+		
+	}
+	public void interest_expense(){
+		
+	}
+	public void legal_expense(){
+		
+	}
+	
+	public abstract void office_expense();
+	
+	public void printing_and_stationary(){
+		
+	}
+	public void freight_and_courier(){
+		
+	}
+	public void repair_and_maintainance(){
+		
+	}
+	public void telephone_and_internet(){
+		
+	}
+	public void rent(){
+		
+	}
+	public abstract void motor_veichel_expense();
+	
+	public void veichel_fuel(){
+		
+	}
+	public void gas_expense(){
+		
+	}
+	public void veichle_lease_expense(){
+		
+	}
+	public void veichle_repair_maintance(){
+		
+	}
+	
+	public void wages_and_salaries(){
+		
+	}
+	public void superannuation(){
+		
+	}
+	public void subscription(){
+		
+	}
+	public void bank_revaluations(){
+		
+	}
+	public void unrealised_currency_gain(){
+		
+	}
+	public void incometax_expense(){
+		
+	}
+	public void bad_debit(){
+		
+	}
+	public void bank_fee_charge(){
+		
+	}
+	public void credit_card_charge(){
+		
+	}
 }
