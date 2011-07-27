0bf8e2e640fca348b42482fe0b01b228f7eb0c99
sravanthi.v <sravanthi.v@vimukti.com>
2011-07-27 05:31:01 +0000

    Former-commit-id: fe27efc51159ed0051f88eb441e1968a46fb9eee

--- src/com/vimukti/accounter/core/trigger/TriggerEstimate.java
+++ src/com/vimukti/accounter/core/trigger/TriggerEstimate.java
@@ -4,28 +4,26 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.h2.api.Trigger;
+
 /**
  * 
  * @author Devesh Satwani
- *
+ * 
  */
 public class TriggerEstimate implements Trigger {
 
 	@Override
 	public void fire(Connection arg0, Object[] arg1, Object[] arg2)
 			throws SQLException {
-		// TODO Auto-generated method stub
+		// currently not using anywhere in the project.
 
 	}
-
 
 	@Override
-
 	public void init(Connection arg0, String arg1, String arg2, String arg3,
 			boolean arg4, int arg5) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
+		// currently not using anywhere in the project.
 
+	}
 
 }
