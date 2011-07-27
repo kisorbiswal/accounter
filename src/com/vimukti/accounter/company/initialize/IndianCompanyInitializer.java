cbb6032ece2bf00948c6ff736ac8fea1839d8ac7
sravanthi.v <sravanthi.v@vimukti.com>
2011-07-27 12:24:37 +0000

    Former-commit-id: 94b4201033b9a567129f40785c734816656469fc

--- src/com/vimukti/accounter/company/initialize/IndianCompanyInitializer.java
+++ src/com/vimukti/accounter/company/initialize/IndianCompanyInitializer.java
@@ -24,40 +24,31 @@ public class IndianCompanyInitializer extends CompanyInitializer {
 		return null;
 	}
 
-	/*@Override
-	public void office_expense() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void motor_veichel_expense() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void travel_expenses() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void other_expenses() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void Cost_of_good_sold() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void other_direct_cost() {
-		// TODO Auto-generated method stub
-
-	}*/
+	/*
+	 * @Override public void office_expense() { // Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void motor_veichel_expense() { // Auto-generated method
+	 * stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void travel_expenses() { // Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void other_expenses() { // Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void Cost_of_good_sold() { // Auto-generated method stub
+	 * 
+	 * }
+	 * 
+	 * @Override public void other_direct_cost() { // Auto-generated method stub
+	 * 
+	 * }
+	 */
 
 }
