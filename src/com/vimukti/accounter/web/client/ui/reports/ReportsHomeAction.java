fa8ad13a0edc887ac6bae153b81b1a5897ad035a
sravanthi.v <sravanthi.v@vimukti.com>
2011-07-27 06:58:41 +0000

    Former-commit-id: 2b1102277553eac1c630913d578e0d33c37b6377

--- src/com/vimukti/accounter/web/client/ui/reports/ReportsHomeAction.java
+++ src/com/vimukti/accounter/web/client/ui/reports/ReportsHomeAction.java
@@ -25,7 +25,7 @@ public class ReportsHomeAction extends Action {
 	@SuppressWarnings("unchecked")
 	@Override
 	public ParentCanvas getView() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
