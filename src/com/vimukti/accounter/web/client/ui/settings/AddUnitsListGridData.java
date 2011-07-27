e01f33171a726c4a7471008fd666fba43f37d2b8
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-07-27 14:09:02 +0000

    Former-commit-id: e6f0b05ba59cd4e3c3efb0126966ed52382c6f08

---
+++ src/com/vimukti/accounter/web/client/ui/settings/AddUnitsListGridData.java
@@ -0,0 +1,33 @@
+package com.vimukti.accounter.web.client.ui.settings;
+
+public class AddUnitsListGridData {
+
+	private String name = "";
+	private String decription = "";
+	private String units = "";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDecription() {
+		return decription;
+	}
+
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+
+	public String getUnits() {
+		return units;
+	}
+
+	public void setUnits(String units) {
+		this.units = units;
+	}
+
+}
