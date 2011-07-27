c3a666b4adf5e2a497f89d580ad80853ebcda848
Lingarao R <lingarao.r@vimukti.com>
2011-07-27 05:29:18 +0000

    Former-commit-id: 7db2e9355f22af85c0d844f07686a31e3e2d553f

--- src/com/vimukti/accounter/web/client/ui/grids/ConversionBalancesGrid.java
+++ src/com/vimukti/accounter/web/client/ui/grids/ConversionBalancesGrid.java
@@ -25,61 +25,58 @@ public class ConversionBalancesGrid extends ListGrid {
 
 	@Override
 	protected Object getColumnValue(Object obj, int index) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	protected String[] getSelectValues(Object obj, int index) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	protected boolean isEditable(Object obj, int row, int index) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	protected void onClick(Object obj, int row, int index) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void onDoubleClick(Object obj) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void onValueChange(Object obj, int index, Object value) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected int sort(Object obj1, Object obj2, int index) {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
 	@Override
 	public boolean validateGrid() throws InvalidTransactionEntryException {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	protected int getCellWidth(int index) {
-		// TODO Auto-generated method stub
+
 		return 0;
 	}
 
 	@Override
 	protected String[] getColumns() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
