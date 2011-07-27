09b126ea2e6a58c35b64e7fe2a1bc5cfeaf59f71
sravanthi.v <sravanthi.v@vimukti.com>
2011-07-27 06:45:45 +0000

    Former-commit-id: 49b251c8301c83b3830ca92fa5feb56e506be874

--- src/com/vimukti/accounter/core/TAXItemGroup.java
+++ src/com/vimukti/accounter/core/TAXItemGroup.java
@@ -113,13 +113,12 @@ public class TAXItemGroup implements IAccounterServerCore, Lifecycle {
 
 	@Override
 	public boolean onDelete(Session arg0) throws CallbackException {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public void onLoad(Session arg0, Serializable arg1) {
-		// TODO Auto-generated method stub
 
 	}
 
