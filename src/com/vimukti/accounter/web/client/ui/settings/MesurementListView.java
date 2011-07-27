b7ff7708042633b34a1f08253bbd958df9261653
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-07-27 14:09:20 +0000

    Former-commit-id: 2d69a7b308d0abdca0ec77e76095badd74ac8539

---
+++ src/com/vimukti/accounter/web/client/ui/settings/MesurementListView.java
@@ -0,0 +1,119 @@
+package com.vimukti.accounter.web.client.ui.settings;
+
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.ui.core.AccounterButton;
+import com.vimukti.accounter.web.client.ui.core.BaseView;
+
+public class MesurementListView extends BaseView {
+
+	private UnitsListGird grid;
+	private VerticalPanel mesurementPanel;
+	private SettingsMessages settingsMessages = GWT
+			.create(SettingsMessages.class);
+
+	@Override
+	public void deleteFailed(Throwable caught) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteSuccess(Boolean result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processupdateView(IAccounterCore core, int command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getViewTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fitToSize(int height, int width) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEdit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printPreview() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init() {
+		super.init();
+		mesurementPanel = new VerticalPanel();
+		mesurementPanel.setSpacing(20);
+		setSize("100%", "100%");
+		createMeasurementListView();
+
+	}
+
+	private void createMeasurementListView() {
+		HorizontalPanel hPanel = new HorizontalPanel();
+		AccounterButton addMeasurementButton = new AccounterButton();
+		addMeasurementButton.setText(settingsMessages.addMeasurementButton());
+		mesurementPanel = new VerticalPanel();
+		mesurementPanel.add(addMeasurementButton);
+		addMeasurementButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				SettingsActionFactory.getAddMeasurementAction()
+						.run(null, false);
+				;
+			}
+		});
+		initGrid();
+		mesurementPanel.add(grid);
+		mainPanel.add(mesurementPanel);
+	}
+
+	private void initGrid() {
+		grid = new UnitsListGird(false);
+		grid.isEnable = false;
+		grid.init();
+		grid.setView(this);
+		grid.setHeight("600px");
+		grid.setSize("100%", "100%");
+		setExistingListToView();
+	}
+
+	@Override
+	public List getForms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void setExistingListToView() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
