2ab9f209339c47d8555e46ca570ed02da4bd1ce9
Syamalamba P <syamalamba.p@vimukti.com>
2011-07-27 06:45:37 +0000

    Former-commit-id: 0b5db8b7acc14b1fb3a6409e9e6d37034747c370

--- src/com/vimukti/accounter/web/client/ui/vendors/NewCheckAction.java
+++ src/com/vimukti/accounter/web/client/ui/vendors/NewCheckAction.java
@@ -77,13 +77,11 @@ public class NewCheckAction extends Action {
 
 	@Override
 	public String getImageUrl() {
-		// TODO Auto-generated method stub
 		return "/images/new_check.png";
 	}
 
 	@Override
 	public String getHistoryToken() {
-		// TODO Auto-generated method stub
 		return "check";
 	}
 }
