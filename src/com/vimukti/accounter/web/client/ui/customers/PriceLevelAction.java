4dbbb474081f483452c41e12bdcbd58a1ae20ab6
Syamalamba P <syamalamba.p@vimukti.com>
2011-07-27 08:53:25 +0000

    Former-commit-id: a9bb7c356ffb9986000babfdd3ea08487c00c9d9

--- src/com/vimukti/accounter/web/client/ui/customers/PriceLevelAction.java
+++ src/com/vimukti/accounter/web/client/ui/customers/PriceLevelAction.java
@@ -25,7 +25,7 @@ public class PriceLevelAction extends Action {
 	@SuppressWarnings("unchecked")
 	@Override
 	public ParentCanvas getView() {
-		// TODO Auto-generated method stub
+		// NOTHING TO DO.
 		return null;
 	}
 
@@ -36,10 +36,12 @@ public class PriceLevelAction extends Action {
 	}
 
 	public ImageResource getBigImage() {
+		// NOTHING TO DO.
 		return null;
 	}
 
 	public ImageResource getSmallImage() {
+		// NOTHING TO DO.
 		return null;
 	}
 
