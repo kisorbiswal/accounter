d4fdec3993800fff70b2614811977ff523aadb58
Syamalamba P <syamalamba.p@vimukti.com>
2011-07-28 14:17:33 +0000

    Former-commit-id: a3a7670e2e8b7ebb5d45300fa62d1d81808c5435

--- src/com/vimukti/accounter/web/client/ui/PriceLevelListDialog.java
+++ src/com/vimukti/accounter/web/client/ui/PriceLevelListDialog.java
@@ -194,7 +194,7 @@ public class PriceLevelListDialog extends GroupDialog<ClientPriceLevel> {
 
 	@Override
 	public String[] setColumns() {
-		return new String[] { Accounter.constants().Name(),
+		return new String[] { Accounter.constants().name(),
 				Accounter.constants().percentage() };
 	}
 
