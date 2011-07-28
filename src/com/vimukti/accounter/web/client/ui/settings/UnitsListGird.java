ddb11ae10441020ce9817bd736dd75a5eb6205c1
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-07-28 04:26:29 +0000

    Former-commit-id: 674704525563b162695121535789d59feb0cc213

---
+++ src/com/vimukti/accounter/web/client/ui/settings/UnitsListGird.java
@@ -0,0 +1,85 @@
+package com.vimukti.accounter.web.client.ui.settings;
+
+import com.google.gwt.core.client.GWT;
+import com.vimukti.accounter.core.Measurement;
+import com.vimukti.accounter.web.client.ui.core.InvalidTransactionEntryException;
+import com.vimukti.accounter.web.client.ui.grids.ListGrid;
+
+public class UnitsListGird extends ListGrid<Measurement> {
+
+	MesurementListView view;
+	SettingsMessages messages = GWT.create(SettingsMessages.class);
+
+	public UnitsListGird(boolean isMultiSelectionEnable) {
+		super(isMultiSelectionEnable);
+	}
+
+	@Override
+	protected String[] getColumns() {
+		return new String[] { messages.getMeasurmentName(),
+				messages.getMeasurmentDescription(), messages.getUnitName() };
+	}
+
+	@Override
+	protected int getColumnType(int col) {
+		return ListGrid.COLUMN_TYPE_TEXT;
+	}
+
+	@Override
+	protected int getCellWidth(int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean validateGrid() throws InvalidTransactionEntryException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setView(MesurementListView preAddedListView) {
+		this.view = preAddedListView;
+	}
+
+	@Override
+	protected Object getColumnValue(Measurement obj, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getSelectValues(Measurement obj, int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void onValueChange(Measurement obj, int index, Object value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean isEditable(Measurement obj, int row, int index) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void onClick(Measurement obj, int row, int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDoubleClick(Measurement obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected int sort(Measurement obj1, Measurement obj2, int index) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
