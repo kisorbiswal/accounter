b07c52d5ca80d53a5c9144ae943a3db97b35b2af
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-01 10:38:10 +0000

    Former-commit-id: 498827d5b57d5461f3470f216776abfcde8de734

---
+++ src/com/vimukti/accounter/web/client/ui/combo/CurrencyCombo.java
@@ -0,0 +1,54 @@
+package com.vimukti.accounter.web.client.ui.combo;
+
+import com.vimukti.accounter.web.client.core.ClientCurrency;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.ActionFactory;
+
+public class CurrencyCombo extends CustomCombo<ClientCurrency> {
+
+	public CurrencyCombo(String title) {
+		super(title);
+
+	}
+
+	@Override
+	public SelectItemType getSelectItemType() {
+
+		return SelectItemType.CURRENCY;
+	}
+
+	@Override
+	protected String getDisplayName(ClientCurrency object) {
+
+		if (object != null)
+			return object.getName() != null ? object.getName() : "";
+		else
+			return "";
+	}
+
+	@Override
+	protected String getColumnData(ClientCurrency object, int row, int col) {
+		switch (col) {
+		case 0:
+			return object.getName();
+		}
+		return null;
+
+	}
+
+	@Override
+	public String getDefaultAddNewCaption() {
+
+		return comboConstants.newCurrency();
+	}
+
+	@Override
+	public void onAddNew() {
+		Action action = ActionFactory.getNewCurrencyAction();
+		action.setActionSource(this);
+
+		action.run(null, true);
+
+	}
+
+}
