6b2027fe909736756f9d423b4deeba1d9dad4a7f
Umasree V <umasree.v@vimukti.com>
2011-08-02 06:02:41 +0000

    Former-commit-id: 9f169837b102e0fc85c2672fa2eea2846eff641a

--- test/com/vimukti/accounter/developer/api/ReportsApiServletTest.java
+++ test/com/vimukti/accounter/developer/api/ReportsApiServletTest.java
@@ -75,6 +75,73 @@ public class ReportsApiServletTest extends TestCase {
 
 	}
 
+	private void sendEcSalesListDetails() {
+		String apikey = null, secretKey = null, companyId = null;
+		long currentTimeMillis = System.currentTimeMillis();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+				"yyy-MM-ddTHH:mm:ssZ");
+		String format = simpleDateFormat.format(currentTimeMillis);
+		String string = "/api/xmlreports/ecsaleslistdetails?"
+				+ "&CompanyId="
+				+ companyId
+				+ "&Expire="
+				+ format
+				+ "&Name=umasree&StartDate=2011-07-01T12:00:00Z&EndDate=2011-07-30T12:00:00Z";
+
+		HttpTester request = new HttpTester();
+		request.setMethod("GET");
+		request.setHeader("Host", "tester");
+		request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		String signature = getSignature(string, apikey, secretKey);
+		request.setURI("/api/xmlreports/ecsaleslistdetails?ApiKey="
+				+ apikey
+				+ "&Signature="
+				+ signature
+				+ "&CompanyId="
+				+ companyId
+				+ "&Expire="
+				+ format
+				+ "&Name=umasree&StartDate=2011-07-01T12:00:00Z&EndDate=2011-07-30T12:00:00Z");
+
+	}
+
+	private void sendVat100Report() {
+		String apikey = null, secretKey = null, companyId = null;
+		long taxAgency = 0;
+		long currentTimeMillis = System.currentTimeMillis();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+				"yyy-MM-ddTHH:mm:ssZ");
+		String format = simpleDateFormat.format(currentTimeMillis);
+		String string = "/api/xmlreports/vat100report?"
+				+ "&CompanyId="
+				+ companyId
+				+ "&Expire="
+				+ format
+				+ "&TaxAgency="
+				+ taxAgency
+				+ "&StartDate=2011-07-01T12:00:00Z&EndDate=2011-07-30T12:00:00Z";
+
+		HttpTester request = new HttpTester();
+		request.setMethod("GET");
+		request.setHeader("Host", "tester");
+		request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		String signature = getSignature(string, apikey, secretKey);
+		request.setURI("/api/xmlreports/vat100report?ApiKey="
+				+ apikey
+				+ "&Signature="
+				+ signature
+				+ "&CompanyId="
+				+ companyId
+				+ "&Expire="
+				+ format
+				+ "&TaxAgency="
+				+ taxAgency
+				+ "&StartDate=2011-07-01T12:00:00Z&EndDate=2011-07-30T12:00:00Z");
+
+	}
+
 	private String getSignature(String string, String apikey, String secretKey) {
 		// TODO Auto-generated method stub
 		return null;
