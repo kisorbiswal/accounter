9404222359471e64aa3ce0d63632ae022e891a3f
Umasree V <umasree.v@vimukti.com>
2011-08-02 08:43:37 +0000

    Former-commit-id: 6a56a33fb16d6a7c3cd00388471f564be533cd56

--- src/com/vimukti/accounter/developer/api/ApiSerializationFactory.java
+++ src/com/vimukti/accounter/developer/api/ApiSerializationFactory.java
@@ -8,10 +8,34 @@ import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 import com.vimukti.accounter.api.core.ApiResult;
 import com.vimukti.accounter.web.client.core.ClientCustomer;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.client.core.ClientItem;
 import com.vimukti.accounter.web.client.core.ClientVendor;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.core.Lists.DummyDebitor;
+import com.vimukti.accounter.web.client.core.Lists.OpenAndClosedOrders;
+import com.vimukti.accounter.web.client.core.Lists.PayeeStatementsList;
+import com.vimukti.accounter.web.client.core.reports.AgedDebtors;
+import com.vimukti.accounter.web.client.core.reports.AmountsDueToVendor;
 import com.vimukti.accounter.web.client.core.reports.BaseReport;
+import com.vimukti.accounter.web.client.core.reports.DepositDetail;
+import com.vimukti.accounter.web.client.core.reports.ECSalesList;
+import com.vimukti.accounter.web.client.core.reports.ECSalesListDetail;
+import com.vimukti.accounter.web.client.core.reports.ExpenseList;
+import com.vimukti.accounter.web.client.core.reports.MostProfitableCustomers;
+import com.vimukti.accounter.web.client.core.reports.ReverseChargeListDetail;
+import com.vimukti.accounter.web.client.core.reports.SalesByCustomerDetail;
+import com.vimukti.accounter.web.client.core.reports.SalesTaxLiability;
+import com.vimukti.accounter.web.client.core.reports.TransactionDetailByAccount;
+import com.vimukti.accounter.web.client.core.reports.TransactionDetailByTaxItem;
+import com.vimukti.accounter.web.client.core.reports.TransactionHistory;
+import com.vimukti.accounter.web.client.core.reports.TrialBalance;
+import com.vimukti.accounter.web.client.core.reports.UncategorisedAmountsReport;
+import com.vimukti.accounter.web.client.core.reports.VATDetail;
+import com.vimukti.accounter.web.client.core.reports.VATItemDetail;
+import com.vimukti.accounter.web.client.core.reports.VATItemSummary;
+import com.vimukti.accounter.web.client.core.reports.VATSummary;
 import com.vimukti.accounter.web.client.exception.AccounterException;
+import com.vimukti.accounter.web.client.ui.reports.CheckDetailReport;
 
 public class ApiSerializationFactory {
 	private static XStream stream;
@@ -27,8 +51,36 @@ public class ApiSerializationFactory {
 	}
 
 	private void initializeStream() {
-		// TODO Auto-generated method stub
-
+		stream.alias("SalesByCustomerDetail", SalesByCustomerDetail.class);
+		stream.alias("PayeeStatementsList", PayeeStatementsList.class);
+		stream.alias("AgedDebtors", AgedDebtors.class);
+		stream.alias("CheckDetailReport", CheckDetailReport.class);
+		stream.alias("DepositDetail", DepositDetail.class);
+		stream.alias("ExpenseList", ExpenseList.class);
+		stream.alias("DummyDebitor", DummyDebitor.class);
+		stream.alias("AmountsDueToVendor", AmountsDueToVendor.class);
+		stream.alias("TransactionHistory", TransactionHistory.class);
+		stream.alias("MostProfitableCustomers", MostProfitableCustomers.class);
+		stream.alias("ReverseChargeListDetail", ReverseChargeListDetail.class);
+		stream.alias("ECSalesListDetail", ECSalesListDetail.class);
+		stream.alias("ECSalesList", ECSalesList.class);
+		stream.alias("TransactionDetailByTaxItem",
+				TransactionDetailByTaxItem.class);
+		stream.alias("TrialBalance", TrialBalance.class);
+		stream.alias("Item", ClientItem.class);
+		stream.alias("Customer", ClientCustomer.class);
+		stream.alias("VATItemDetail", VATItemDetail.class);
+		stream.alias("VATItemSummary", VATItemSummary.class);
+		stream.alias("Vendor", ClientVendor.class);
+		stream.alias("UncategorisedAmountsReport",
+				UncategorisedAmountsReport.class);
+		stream.alias("SalesTaxLiability", SalesTaxLiability.class);
+		stream.alias("VATSummary", VATSummary.class);
+		stream.alias("TransactionDetailByAccount",
+				TransactionDetailByAccount.class);
+		stream.alias("VATDetail", VATDetail.class);
+		stream.alias("OpenAndClosedOrders", OpenAndClosedOrders.class);
+		stream.alias("FinanceDate", ClientFinanceDate.class);
 	}
 
 	IAccounterCore deserialize(InputStream inputStream) throws Exception {
