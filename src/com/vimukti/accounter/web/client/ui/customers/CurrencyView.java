f15b59f127940615078b346aed3d00d9e5728e5b
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-03 08:31:12 +0000

    Former-commit-id: 0a3508fb51cdeeb8fbe48cd238306ed416b82a0c

---
+++ src/com/vimukti/accounter/web/client/ui/customers/CurrencyView.java
@@ -0,0 +1,184 @@
+package com.vimukti.accounter.web.client.ui.customers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.ClientCurrency;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.core.Utility;
+import com.vimukti.accounter.web.client.externalization.AccounterConstants;
+import com.vimukti.accounter.web.client.ui.UIUtils;
+import com.vimukti.accounter.web.client.ui.core.BaseView;
+import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
+import com.vimukti.accounter.web.client.ui.forms.TextItem;
+
+public class CurrencyView extends BaseView<ClientCurrency> {
+	TextItem currencyNameText, formalNameText;
+	private ClientCurrency currency;
+	private ClientCurrency existCurrency;
+	private AccounterConstants currencyConstants;
+	private DynamicForm currencyForm;
+	private boolean wait;
+	private ClientCompany company = getCompany();
+	private DecoratedTabPanel tabSet;
+	private ArrayList<DynamicForm> listforms;
+
+	@Override
+	public void init() {
+		super.init();
+		createControls();
+		setSize("100%", "100%");
+
+	}
+
+	@Override
+	public void saveSuccess(IAccounterCore result) {
+		if (result != null) {
+
+			super.saveSuccess(result);
+
+		} else {
+			saveFailed(new Exception());
+		}
+
+	}
+
+	/*
+	 * private VerticalPanel getGeneralTab() {
+	 * 
+	 * currencyNameText = new TextItem(currencyConstants.currencyName());
+	 * currencyNameText.setHelpInformation(true);
+	 * currencyNameText.setWidth(100); currencyNameText.setRequired(true);
+	 * 
+	 * formalNameText = new TextItem(currencyConstants.currencyFormalName());
+	 * formalNameText.setHelpInformation(true); formalNameText.setWidth(100);
+	 * formalNameText.setRequired(true);
+	 * 
+	 * currencyForm = UIUtils.form(currencyConstants.currency());
+	 * currencyForm.setFields(currencyNameText, formalNameText);
+	 * currencyForm.setWidth("100%");
+	 * currencyForm.getCellFormatter().setWidth(0, 0, "205");
+	 * 
+	 * if (existCurrency != null) {
+	 * currencyNameText.setValue(existCurrency.getName());
+	 * formalNameText.setValue(existCurrency.getFormalName());
+	 * 
+	 * } mainPanel.add(currencyForm); return mainPanel;
+	 * 
+	 * }
+	 */
+
+	private ClientCurrency getCurrencyObject() {
+
+		if (existCurrency != null) {
+			currency = existCurrency;
+
+		} else {
+			currency = new ClientCurrency();
+			currency.setName(currencyNameText.getValue().toString());
+			currency.setFormalName(formalNameText.getValue().toString());
+
+		}
+		return currency;
+
+	}
+
+	public void saveAndUpdateView() throws Exception {
+
+		if (!wait) {
+			try {
+				ClientCurrency currency = getCurrencyObject();
+				if (existCurrency == null) {
+					if (!isObjectExist(company.getCurrencies(), currency))
+						createObject(currency);
+				} else
+					alterObject(currency);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw e;
+			}
+		}
+
+	}
+
+	private void createControls() {
+		currencyNameText = new TextItem(currencyConstants.currencyName());
+		currencyNameText.setHelpInformation(true);
+		currencyNameText.setWidth(100);
+		currencyNameText.setRequired(true);
+
+		formalNameText = new TextItem(currencyConstants.currencyFormalName());
+		formalNameText.setHelpInformation(true);
+		formalNameText.setWidth(100);
+		formalNameText.setRequired(true);
+
+		currencyForm = UIUtils.form(currencyConstants.currency());
+		currencyForm.setFields(currencyNameText, formalNameText);
+		currencyForm.setWidth("100%");
+		currencyForm.getCellFormatter().setWidth(0, 0, "205");
+
+		if (existCurrency != null) {
+			currencyNameText.setValue(existCurrency.getName());
+			formalNameText.setValue(existCurrency.getFormalName());
+
+		}
+
+		VerticalPanel panel = new VerticalPanel();
+		panel.add(currencyForm);
+		mainPanel.add(panel);
+
+	}
+
+	@Override
+	public void deleteFailed(Throwable caught) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteSuccess(Boolean result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void processupdateView(IAccounterCore core, int command) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getViewTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DynamicForm> getForms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onEdit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printPreview() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
