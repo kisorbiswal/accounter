72fed3ae24f71235936c86a5cfe06b6e04456a79
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-03 06:32:50 +0000

    Former-commit-id: 7b256b1ea1c0254c43d40e7007e728bfcbc0496b

---
+++ src/com/vimukti/accounter/web/client/ui/customers/NewCurrencyAction.java
@@ -0,0 +1,64 @@
+package com.vimukti.accounter.web.client.ui.customers;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallback;
+
+public class NewCurrencyAction extends Action {
+	private CurrencyView view;
+
+	public NewCurrencyAction(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run(Object data, Boolean isDependent) {
+		runAsync(data, isDependent);
+
+	}
+
+	private void runAsync(final Object data, final Boolean isDependent) {
+		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+
+			@Override
+			public void onCreated() {
+				// TODO Auto-generated method stub
+				view = new CurrencyView();
+				try {
+					MainFinanceWindow.getViewManager().showView(view, data,
+							isDependent, NewCurrencyAction.this);
+				} catch (Throwable t) {
+					onCreateFailed(t);
+				}
+
+			}
+
+			@Override
+			public void onCreateFailed(Throwable t) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+	}
+
+	@Override
+	public ImageResource getBigImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageResource getSmallImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryToken() {
+		return "newCurrency";
+	}
+
+}
