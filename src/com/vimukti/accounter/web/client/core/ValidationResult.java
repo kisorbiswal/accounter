2b2c9acdd6279ec96ae16cdb88c56c79b9ca925e
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-08-05 05:15:53 +0000

    Former-commit-id: e171f3f8fa65c9930ee5fa97c6f682009bc067e9

--- src/com/vimukti/accounter/web/client/core/ValidationResult.java
+++ src/com/vimukti/accounter/web/client/core/ValidationResult.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class ValidationResult {
 	private List<Error> errors = new ArrayList<ValidationResult.Error>();
+	private List<Warning> warnings = new ArrayList<ValidationResult.Warning>();
 
 	public void addError(Object obj, String msg) {
 		this.errors.add(new Error(obj, msg));
@@ -12,6 +13,10 @@ public class ValidationResult {
 
 	public void add(ValidationResult result) {
 		errors.addAll(result.errors);
+	}
+
+	public void addWarning(Object obj, String msg) {
+		warnings.add(new Warning(obj, msg));
 	}
 
 	public static class Error {
@@ -38,6 +43,34 @@ public class ValidationResult {
 		public void setMessage(String message) {
 			this.message = message;
 		}
+	}
+
+	public static class Warning {
+		private Object source;
+		private String message;
+
+		/**
+		 * Creates new Instance
+		 */
+		public Warning(Object source, String msg) {
+			this.source = source;
+			this.message = msg;
+		}
+
+		/**
+		 * @return the source
+		 */
+		public Object getSource() {
+			return source;
+		}
+
+		/**
+		 * @return the message
+		 */
+		public String getMessage() {
+			return message;
+		}
+
 	}
 
 	public boolean haveErrors() {
