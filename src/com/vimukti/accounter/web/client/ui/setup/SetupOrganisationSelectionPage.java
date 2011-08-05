84d0eb189a1765677ae7cd39f1a7e850ad497ad3
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-05 10:52:41 +0000

    Former-commit-id: f7517853bbba4065e7923db9ba96b3edc5a33284

--- src/com/vimukti/accounter/web/client/ui/setup/SetupOrganisationSelectionPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupOrganisationSelectionPage.java
@@ -1,18 +1,70 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.ui.CustomLabel;
 
 public class SetupOrganisationSelectionPage extends AbstractSetupPage {
+	private static final String ORG_TYPE = "OrganizationType";
+	private RadioButton soleProprietorshipRadio;
+	private RadioButton partnershipRadio;
+	private RadioButton llcRadio;
+	private RadioButton corporationRadio;
+	private RadioButton sCorporationRadio;
+	private RadioButton nonProfitRadio;
+	private RadioButton otherNoneRadio;
 
 	@Override
 	public String getHeader() {
-		return this.accounterConstants.howisYourCompanyOrganized();
+		return accounterConstants.howIsYourCompanyOrganized();
 	}
 
 	@Override
 	public VerticalPanel getPageBody() {
-		// TODO Auto-generated method stub
-		return null;
+		VerticalPanel viewContainer = new VerticalPanel();
+		CustomLabel headerDesc = new CustomLabel(
+				accounterConstants.howIsYourCompanyOrganizedDesc());
+
+		viewContainer.add(headerDesc);
+		soleProprietorshipRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.soleProprietorship());
+		viewContainer.add(soleProprietorshipRadio);
+		viewContainer.add(new CustomLabel(accounterConstants
+				.soleProprietorshipDesc()));
+
+		partnershipRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.partnershipOrLLP());
+		viewContainer.add(partnershipRadio);
+		viewContainer.add(new CustomLabel(accounterConstants
+				.partnershipOrLLPDesc()));
+
+		llcRadio = new RadioButton(ORG_TYPE, accounterConstants.LLC());
+		viewContainer.add(llcRadio);
+		viewContainer.add(new CustomLabel(accounterConstants.LLCDesc()));
+
+		corporationRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.corporation());
+		viewContainer.add(corporationRadio);
+		viewContainer
+				.add(new CustomLabel(accounterConstants.corporationDesc()));
+
+		sCorporationRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.sCorporation());
+		viewContainer.add(sCorporationRadio);
+		viewContainer
+				.add(new CustomLabel(accounterConstants.sCorporationDesc()));
+
+		nonProfitRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.nonProfit());
+		viewContainer.add(nonProfitRadio);
+		viewContainer.add(new CustomLabel(accounterConstants.nonProfitDesc()));
+
+		otherNoneRadio = new RadioButton(ORG_TYPE,
+				accounterConstants.otherNone());
+		viewContainer.add(otherNoneRadio);
+		viewContainer.add(new CustomLabel(accounterConstants.otherNoneDesc()));
+
+		return viewContainer;
 	}
 
 	@Override
@@ -26,6 +78,5 @@ public class SetupOrganisationSelectionPage extends AbstractSetupPage {
 		// TODO Auto-generated method stub
 
 	}
-
 
 }
