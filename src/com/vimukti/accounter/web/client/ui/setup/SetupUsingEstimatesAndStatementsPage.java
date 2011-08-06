e140697b4450b352f079cc83526951fe8e1718cd
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-06 12:12:37 +0000

    Former-commit-id: a85d787736bf146958c0ff18c13d07215db1eb78

--- src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsPage.java
@@ -1,7 +1,6 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.vimukti.accounter.web.client.core.IAccounterCore;
 
 public class SetupUsingEstimatesAndStatementsPage extends AbstractSetupPage {
 
@@ -28,64 +27,9 @@ public class SetupUsingEstimatesAndStatementsPage extends AbstractSetupPage {
 	}
 
 	@Override
-	protected void onBack() {
+	public boolean doShow() {
 		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	protected void onNext() {
-		SetupTrackBillsAndTimeAction action = new SetupTrackBillsAndTimeAction(
-				"Bills and Time");
-		action.run(null, false);
-	}
-
-	@Override
-	protected String getViewTitle() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void fitToSize(int height, int width) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void onEdit() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void print() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void printPreview() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteFailed(Throwable caught) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteSuccess(Boolean result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void processupdateView(IAccounterCore core, int command) {
-		// TODO Auto-generated method stub
-
+		return false;
 	}
 
 }
