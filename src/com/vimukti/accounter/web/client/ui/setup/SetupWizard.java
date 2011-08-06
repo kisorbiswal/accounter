e98ff75f032e3bc81c28c24dae7988b51a14ae4d
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-06 14:18:58 +0000

    Former-commit-id: 727557c92b475487c9d6a7118663a73df3df9d4d

--- src/com/vimukti/accounter/web/client/ui/setup/SetupWizard.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupWizard.java
@@ -4,21 +4,25 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.CustomLabel;
 
 public class SetupWizard extends VerticalPanel {
+	private static final int START_PAGE = 0;
 	private VerticalPanel viewPanel;
 	private HorizontalPanel buttonPanel;
 	private VerticalPanel progressPanel;
 	private Button skipButton, backButton, nextButton;
+	private Button gotoAccounterButton;
 	private ClientCompanyPreferences preferences;
 
-	private int currentViewIndex = 0;
+	private int currentViewIndex = START_PAGE;
 
 	private AbstractSetupPage viewList[] = new AbstractSetupPage[] {
-			new SetupStartPage(), new SetupCompanyInfoPage(),
+			new SetupStartPage(this), new SetupCompanyInfoPage(),
 			new SetupIndustrySelectionPage(),
 			new SetupOrganisationSelectionPage(), new SetupReferPage(),
 			new SetupTrackEmployeesPage(), new SetupSellTypeAndSalesTaxPage(),
@@ -26,8 +30,22 @@ public class SetupWizard extends VerticalPanel {
 			new SetupTrackBillsAndTimePage(),
 			new SetupSelectFiscalYrDatePage(), new SetupSelectAccountsPage(),
 			new SetupComplitionPage() };
+
+	private Image progressImages[] = new Image[viewList.length - 2];
+	private String progressLabels[] = new String[] {
+			Accounter.constants().setCompanyInfo(),
+			Accounter.constants().selectIndustryType(),
+			Accounter.constants().companyOrganization(),
+			Accounter.constants().selectReferringNames(),
+			Accounter.constants().trackEmployeeExpenses(),
+			Accounter.constants().whatDoYouSell(),
+			Accounter.constants().setEstimatesAndStatements(),
+			Accounter.constants().setBillTracking(),
+			Accounter.constants().setFiscalYear(),
+			Accounter.constants().selectRequiredAccounts() };
 	private AbstractSetupPage previousView;
 	private AbstractSetupPage viewToShow;
+	private int progressImagesIndex;
 
 	public SetupWizard() {
 		preferences = Accounter.getCompany().getPreferences();
@@ -39,7 +57,21 @@ public class SetupWizard extends VerticalPanel {
 		viewPanel = new VerticalPanel();
 		progressPanel = new VerticalPanel();
 
+		// add progress steps
+		// setting images
+		for (int iii = 0; iii < progressImages.length; iii++) {
+			HorizontalPanel progressPanel1 = new HorizontalPanel();
+			CustomLabel label = new CustomLabel(progressLabels[iii]);
+			progressImages[iii] = new Image(Accounter.getFinanceImages()
+					.tickMark());
+			progressImages[iii].setVisible(false);
+			progressPanel1.add(progressImages[iii]);
+			progressPanel1.add(label);
+
+			progressPanel.add(progressPanel1);
+		}
 		buttonPanel = new HorizontalPanel();
+		buttonPanel.setVisible(false);
 
 		topPanel.add(viewPanel);
 		topPanel.add(progressPanel);
@@ -51,13 +83,20 @@ public class SetupWizard extends VerticalPanel {
 		skipButton = new Button(Accounter.constants().skip());
 		backButton = new Button(Accounter.constants().back());
 		nextButton = new Button(Accounter.constants().next());
+		gotoAccounterButton = new Button(Accounter.constants().gotoAccounter());
+
+		// making them invisible at the beginning
+		skipButton.setVisible(false);
+		backButton.setVisible(false);
+		nextButton.setVisible(false);
+		gotoAccounterButton.setVisible(false);
 
 		buttonPanel.add(skipButton);
 		buttonPanel.add(backButton);
 		buttonPanel.add(nextButton);
+		buttonPanel.add(gotoAccounterButton);
 
 		// adding handlers
-
 		skipButton.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -85,12 +124,18 @@ public class SetupWizard extends VerticalPanel {
 			}
 		});
 		previousView = null;
-		showView();
+		showStartPage();
+	}
+
+	private void showStartPage() {
+		viewToShow = viewList[START_PAGE];
+		this.viewPanel.add(viewToShow);
 	}
 
 	protected void showView() {
+		currentViewIndex++;
 		previousView = viewToShow;
-		if(previousView != null)
+		if (previousView != null)
 			this.viewPanel.remove(previousView);
 		viewToShow = viewList[currentViewIndex];
 		viewToShow.setPreferences(preferences);
@@ -100,5 +145,27 @@ public class SetupWizard extends VerticalPanel {
 			viewToShow.setPreferences(preferences);
 		}
 		this.viewPanel.add(viewToShow);
+
+		// checking button display related conditions
+		if (currentViewIndex != START_PAGE) {
+			buttonPanel.setVisible(true);
+			if (currentViewIndex != viewList.length - 1) {
+				skipButton.setVisible(true);
+				nextButton.setVisible(true);
+
+				gotoAccounterButton.setVisible(false);
+			} else {
+				gotoAccounterButton.setVisible(true);
+				skipButton.setVisible(false);
+				nextButton.setVisible(false);
+			}
+			backButton.setVisible(true);
+		}
+
+		// setting the progress
+		if (currentViewIndex > 1) {
+			progressImagesIndex = currentViewIndex - 2;
+			progressImages[progressImagesIndex].setVisible(true);
+		}
 	}
 }
