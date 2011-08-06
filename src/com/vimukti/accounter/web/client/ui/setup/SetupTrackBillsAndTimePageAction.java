f8dc058adbe4a348312ab3d1bbb9c348940b9a3e
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-08-06 06:03:09 +0000

    Former-commit-id: 8b99a05bbd6ae3d65bb234c6b1ea8e044d8396cc

---
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupTrackBillsAndTimePageAction.java
@@ -0,0 +1,37 @@
+package com.vimukti.accounter.web.client.ui.setup;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.core.Action;
+
+public class SetupTrackBillsAndTimePageAction extends Action {
+
+	public SetupTrackBillsAndTimePageAction(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ImageResource getBigImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageResource getSmallImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run(Object data, Boolean isDependent) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
