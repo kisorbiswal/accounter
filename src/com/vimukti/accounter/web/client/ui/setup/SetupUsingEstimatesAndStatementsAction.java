6f99a6acb8f92d51a3daae53e56f1fd452b99421
Lingarao R <lingarao.r@vimukti.com>
2011-08-06 12:14:52 +0000

    Former-commit-id: 8e1e67a11423227c2c5a7745e66767fb4d466793

--- src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsAction.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsAction.java
@@ -1,7 +1,6 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
 import com.google.gwt.resources.client.ImageResource;
-import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
 import com.vimukti.accounter.web.client.ui.core.Action;
 
 public class SetupUsingEstimatesAndStatementsAction extends Action {
@@ -31,15 +30,9 @@ public class SetupUsingEstimatesAndStatementsAction extends Action {
 	}
 
 	@Override
-	public void run() {
-		statementsPage = new SetupUsingEstimatesAndStatementsPage();
-		try {
-			MainFinanceWindow.getViewManager().showView(statementsPage, data,
-					isDependent, this);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void run(Object data, Boolean isDependent) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
