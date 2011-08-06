b31b8ce9c2f6d9236fb96f689a56b5c439574253
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-08-06 05:44:07 +0000

    Former-commit-id: 7877fb89610ffb7e646fb1888a5f1da59212f2a8

--- src/com/vimukti/accounter/web/client/ui/setup/SetupSelectFiscalYrDatePage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupSelectFiscalYrDatePage.java
@@ -1,18 +1,93 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.ui.combo.SelectCombo;
+import com.vimukti.accounter.web.client.ui.core.DateField;
+import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
 
 public class SetupSelectFiscalYrDatePage extends AbstractSetupPage {
+	VerticalPanel mainPanel;
+	Label heading, fiscalYearsameLabel, selectFirstMonth, secondHeading,
+			theDateisStartdateLabel, inOrdertoComplete, enterTransaction;
+	HTML explainHtml, startDateHtml;
+	SelectCombo monthsCombo;
+	RadioButton beginingYear, todaysDate;
+	DateField datepicker;
 
 	@Override
 	public String getHeader() {
-		return this.accounterConstants.selectFirstMonthOfFiscalYear();
+		return accounterConstants.selectFirstMonthOfFiscalYear();
+
 	}
 
 	@Override
 	public VerticalPanel getPageBody() {
-		// TODO Auto-generated method stub
-		return null;
+		creatControls();
+		return mainPanel;
+	}
+
+	private void creatControls() {
+		mainPanel = new VerticalPanel();
+		selectFirstMonth = new Label(accounterConstants
+				.selectFirstMonthOfFiscalYear());
+		mainPanel.add(selectFirstMonth);
+		fiscalYearsameLabel = new Label(accounterConstants
+				.fiscalYearsaemasTaxyear());
+		mainPanel.add(fiscalYearsameLabel);
+		monthsCombo = new SelectCombo(accounterConstants
+				.myFiscalYearsStartsIn());
+		monthsCombo.initCombo(getMonthNames());
+		DynamicForm dynmicform = new DynamicForm();
+		dynmicform.setFields(monthsCombo);
+		mainPanel.add(dynmicform);
+
+		// Scond part
+
+		secondHeading = new Label(accounterConstants.selectdateToTrackFinance());
+		mainPanel.add(secondHeading);
+		theDateisStartdateLabel = new Label(accounterConstants
+				.yourSelecteddateisStartdate());
+		mainPanel.add(theDateisStartdateLabel);
+		beginingYear = new RadioButton(accounterConstants
+				.beginingOfthefiscalYear());
+		mainPanel.add(beginingYear);
+		inOrdertoComplete = new Label(accounterConstants
+				.enterTransactionsTocompleteTaxreturns());
+		mainPanel.add(inOrdertoComplete);
+		todaysDate = new RadioButton(accounterConstants
+				.useTodaysDateasStartdate());
+		mainPanel.toString();
+		enterTransaction = new Label(accounterConstants
+				.enterTransactionsTocompleteTaxreturns());
+		mainPanel.add(enterTransaction);
+		datepicker = new DateField("");
+		DynamicForm dynamicform = new DynamicForm();
+		dynamicform.setFields(datepicker);
+		mainPanel.add(dynamicform);
+
+	}
+
+	private List<String> getMonthNames() {
+		String[] monthNames = { accounterConstants.january(),
+				accounterConstants.february(), accounterConstants.march(),
+				accounterConstants.april(), accounterConstants.may(),
+				accounterConstants.june(), accounterConstants.july(),
+				accounterConstants.august(), accounterConstants.september(),
+				accounterConstants.october(), accounterConstants.november(),
+				accounterConstants.december() };
+		List<String> monthNam = new ArrayList<String>();
+		for (int i = 0; i < monthNames.length; i++) {
+			monthNam.add(monthNames[i]);
+
+		}
+
+		return monthNam;
 	}
 
 	@Override
