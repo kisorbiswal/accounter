963365be55a83fe288c5e246ac9c148df95c5d9c
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-06 05:43:29 +0000

    Former-commit-id: 949b8f36e4402e13f761b4a0c5be485a20cd1a69

--- src/com/vimukti/accounter/web/client/ui/setup/SetupCurrencyPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupCurrencyPage.java
@@ -4,11 +4,12 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.ui.CustomLabel;
+import com.vimukti.accounter.web.client.ui.grids.CurrenciesGrid;
 
-public class SetupCurrencyPage extends AbstractSetupPage{
-	public static final String []currencyList = new String[]{};
+public class SetupCurrencyPage extends AbstractSetupPage {
+	public static final String[] currencyList = new String[] {};
 	private ListBox baseCurrencyList;
-	
+
 	@Override
 	public String getHeader() {
 		return accounterConstants.setSupportedCurrencies();
@@ -19,42 +20,51 @@ public class SetupCurrencyPage extends AbstractSetupPage{
 		VerticalPanel container = new VerticalPanel();
 		HorizontalPanel baseCurrencyHorPanel = new HorizontalPanel();
 		container.add(baseCurrencyHorPanel);
-		
-		CustomLabel baseCurrencyLabel = new CustomLabel(accounterConstants.baseCurrency());
+
+		CustomLabel baseCurrencyLabel = new CustomLabel(
+				accounterConstants.primaryCurrency());
 		baseCurrencyHorPanel.add(baseCurrencyLabel);
-		
-		 baseCurrencyList = new ListBox();
-		for(int i = 0; i <currencyList.length; i++ )
+
+		baseCurrencyList = new ListBox();
+		for (int i = 0; i < currencyList.length; i++)
 			baseCurrencyList.addItem(currencyList[i]);
-		
+
 		baseCurrencyHorPanel.add(baseCurrencyList);
-		//add currencies grid
-		
-		return null;
+
+		CustomLabel supportingCurrenciesLabel = new CustomLabel(
+				accounterConstants.supportingCurrencies());
+		container.add(supportingCurrenciesLabel);
+
+		// add currencies grid
+		CurrenciesGrid currenciesGrid = new CurrenciesGrid();
+		// TODO:: create list of currencies and add them to the grid
+
+		container.add(currenciesGrid);
+
+		return container;
 	}
 
 	@Override
 	public void onLoad() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onSave() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onBack() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onNext() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
