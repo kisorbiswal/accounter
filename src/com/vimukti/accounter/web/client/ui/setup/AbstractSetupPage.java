4dfb962a6cf644de9113f57c2f278329e813070c
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-06 11:58:10 +0000

    Former-commit-id: bc8090b32b773831950069a813245b4281fe8b0b

--- src/com/vimukti/accounter/web/client/ui/setup/AbstractSetupPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/AbstractSetupPage.java
@@ -1,40 +1,31 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.externalization.AccounterConstants;
 import com.vimukti.accounter.web.client.externalization.AccounterMessages;
-import com.vimukti.accounter.web.client.ui.AbstractBaseView;
 import com.vimukti.accounter.web.client.ui.Accounter;
 
-public abstract class AbstractSetupPage extends AbstractBaseView {
+public abstract class AbstractSetupPage extends VerticalPanel {
 	protected boolean progress;
 	protected Label header;
 	protected AccounterConstants accounterConstants;
 	protected AccounterMessages accounterMessages;
 	protected ClientCompanyPreferences preferences;
-	// protected static AbstractSetupPage setupPage;
-	protected VerticalPanel mainPanel;
-	protected HorizontalPanel buttonPanel, setupMainPanel;
-	protected Button skipButton, backButton, nextButton;
 
-	public AbstractSetupPage() {
-		preferences = Accounter.getCompany().getPreferences();
+	public AbstractSetupPage(ClientCompanyPreferences preferences) {
+		this.preferences = preferences;
 		accounterConstants = Accounter.constants();
 		accounterMessages = Accounter.messages();
-		createControls();
+	}
+
+	public AbstractSetupPage() {
+		this(Accounter.getCompany().getPreferences());
 	}
 
 	public abstract String getHeader();
 
-	// public abstract String getFooter() {
-	// return null;
-	// }
 	public boolean getProgress() {
 		return progress;
 	}
@@ -45,78 +36,14 @@ public abstract class AbstractSetupPage extends AbstractBaseView {
 
 	public abstract VerticalPanel getPageBody();
 
-	private void createControls() {
-		mainPanel = new VerticalPanel();
-		setupMainPanel = new HorizontalPanel();
-		HorizontalPanel back_nextbuttonPanel = new HorizontalPanel();
-		// progressPanel = new SetupProgressPanel(setupPage);
-
-		buttonPanel = new HorizontalPanel();
-		skipButton = new Button(Accounter.constants().skip());
-		backButton = new Button(Accounter.constants().back());
-		nextButton = new Button(Accounter.constants().next());
-
-		buttonPanel.add(skipButton);
-		skipButton.getElement().setAttribute("float", "left");
-		back_nextbuttonPanel.add(backButton);
-		back_nextbuttonPanel.add(nextButton);
-		buttonPanel.add(back_nextbuttonPanel);
-		back_nextbuttonPanel.getElement().setAttribute("float", "right");
-		header = new Label(getHeader());
-		mainPanel.add(header);
-		header.addStyleName("setup_header_label");
-		mainPanel.add(getPageBody());
-		if ((this instanceof SetupStartPage)
-				|| (this instanceof SetupComplitionPage)) {
-
-		} else {
-			mainPanel.add(buttonPanel);
-		}
-
-		setupMainPanel.add(mainPanel);
-		// setupMainPanel.add(progressPanel);
-		setupMainPanel.setSize("100%", "100%");
-		add(setupMainPanel);
-		setSize("96%", "100%");
-		addStyleName("setup_panel");
-		skipButton.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent arg0) {
-				SetupSkipDialog skipDialog = new SetupSkipDialog();
-				skipDialog.show();
-				skipDialog.center();
-			}
-		});
-
-		nextButton.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent arg0) {
-				onSave();
-				onNext();
-			}
-		});
-
-		backButton.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(ClickEvent arg0) {
-				onBack();
-			}
-		});
-
-	}
-
-	protected abstract void onBack();
-
-	protected abstract void onNext();
-
 	public abstract void onLoad();
 
 	public abstract void onSave();
 
-	public AbstractSetupPage getView() {
-		return this;
+	public abstract boolean doShow();
+
+	public void setPreferences(ClientCompanyPreferences preferences2) {
+		this.preferences = preferences;
 	}
+
 }
