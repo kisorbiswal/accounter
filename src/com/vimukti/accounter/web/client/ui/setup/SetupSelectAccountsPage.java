b1dcd8c9cbcbcb363dbc3292f32e3311def82d23
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-08-08 05:17:31 +0000

    Former-commit-id: 466759518c0ca44f04a0cc01f9f8ca645d06dd25

--- src/com/vimukti/accounter/web/client/ui/setup/SetupSelectAccountsPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupSelectAccountsPage.java
@@ -12,7 +12,7 @@ public class SetupSelectAccountsPage extends AbstractSetupPage {
 	@Override
 	public VerticalPanel getPageBody() {
 		// TODO Auto-generated method stub
-		return null;
+		return new VerticalPanel();
 	}
 
 	@Override
