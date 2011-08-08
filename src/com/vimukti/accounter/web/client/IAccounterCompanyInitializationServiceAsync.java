2853d3ddfcdaa129279a17f1ff53ec03755dd43b
Nagaraju P <nagaraju.p@vimukti.com>
2011-08-08 13:25:42 +0000

    Former-commit-id: c7218a11bc22123e10a4c731aedaf9d067f3f22a

--- src/com/vimukti/accounter/web/client/IAccounterCompanyInitializationServiceAsync.java
+++ src/com/vimukti/accounter/web/client/IAccounterCompanyInitializationServiceAsync.java
@@ -4,7 +4,7 @@
 package com.vimukti.accounter.web.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 
 /**
  * @author Prasanna Kumar G
@@ -12,6 +12,6 @@ import com.vimukti.accounter.web.client.core.ClientCompany;
  */
 public interface IAccounterCompanyInitializationServiceAsync {
 
-	void createCompany(int accountType, AsyncCallback<ClientCompany> callback);
-
+	void initalizeCompany(ClientCompanyPreferences preferences,
+			AsyncCallback<Boolean> callback);
 }
