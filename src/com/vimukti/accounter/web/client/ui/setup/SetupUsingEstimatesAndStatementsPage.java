aaea7303bd1db6d395fc6356ad4aa386a786b268
Lingarao R <lingarao.r@vimukti.com>
2011-08-08 05:04:04 +0000

    Former-commit-id: ef30b877bd3dae0a69e8701a3f4d66eb2bfe685e

--- src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsPage.java
+++ src/com/vimukti/accounter/web/client/ui/setup/SetupUsingEstimatesAndStatementsPage.java
@@ -1,8 +1,14 @@
 package com.vimukti.accounter.web.client.ui.setup;
 
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class SetupUsingEstimatesAndStatementsPage extends AbstractSetupPage {
+	private static final String ESTIMATES = "Estimates";
+	private VerticalPanel mainPanel;
+	private HTML titleHtml, descrptionHtml, subTitleHtml, html, html2, html3;
+	private RadioButton yesRadioButton, noRadioButton;
 
 	@Override
 	public String getHeader() {
@@ -11,6 +17,26 @@ public class SetupUsingEstimatesAndStatementsPage extends AbstractSetupPage {
 
 	@Override
 	public VerticalPanel getPageBody() {
+
+		yesRadioButton = new RadioButton(ESTIMATES, accounterConstants.yes());
+		mainPanel.add(yesRadioButton);
+
+		noRadioButton = new RadioButton(ESTIMATES, accounterConstants.no());
+		mainPanel.add(yesRadioButton);
+
+		titleHtml = new HTML("Using Statements in Accounter");
+		titleHtml.setStyleName("BOLD");
+
+		descrptionHtml = new HTML(
+				"Billing statements are send to customers to list charges accumulated over a period of time .Statements may be sent at regular intervals,as in a monthly statement,or when a customer payment is past due.");
+		subTitleHtml = new HTML("  Some example:");
+		html = new HTML(
+				".An attorney invoices a client for multiple services provided .if the invoice is not paid,the attorney can then send the client a reminder statement");
+		html2 = new HTML(
+				". A gym sned each member a monthly statement that includes fees and any overdue payments or finance charges");
+
+		html3 = new HTML("Do you want to use billing statements in Acconter ? ");
+
 		return new VerticalPanel();
 	}
 
