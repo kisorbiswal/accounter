0334a4fb1cfe681fa847c7a0fa5b108e766c9a0c
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-12 04:49:32 +0000

    Former-commit-id: 4882be34303280d99df33aeb5feffb874f162673

--- src/com/vimukti/accounter/web/client/ui/reports/APAgingSummaryReportAction.java
+++ src/com/vimukti/accounter/web/client/ui/reports/APAgingSummaryReportAction.java
@@ -1,5 +1,7 @@
 package com.vimukti.accounter.web.client.ui.reports;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
@@ -40,22 +42,22 @@ public class APAgingSummaryReportAction extends Action {
 
 	public void runAsync(final Object data, final Boolean isDependent) {
 
-		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+		GWT.runAsync(new RunAsyncCallback() {
 
-			public void onCreated() {
+			@Override
+			public void onSuccess() {
 
-				try {
-					report = new APAgingSummaryReport();
-					MainFinanceWindow.getViewManager().showView(report, data,
-							isDependent, APAgingSummaryReportAction.this);
-				} catch (Throwable t) {
-					onCreateFailed(t);
-				}
+				report = new APAgingSummaryReport();
+				MainFinanceWindow.getViewManager().showView(report, data,
+						isDependent, APAgingSummaryReportAction.this);
 
 			}
 
-			public void onCreateFailed(Throwable t) {
-				// //UIUtils.logError("Failed to Load Report..", t);
+			@Override
+			public void onFailure(Throwable arg0) {
+				Accounter
+						.showError(Accounter.constants().unableToshowtheview());
+
 			}
 		});
 
