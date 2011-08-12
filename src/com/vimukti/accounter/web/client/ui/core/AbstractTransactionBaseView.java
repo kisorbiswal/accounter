7bbdf7feaadda7e50049e65fc3aafcd332a7e43b
Lakshmi B <lakshmi.b@vimukti.com>
2011-08-12 09:21:27 +0000

    Former-commit-id: 9ea26ce2774dec2b2d0fc30624a695267f6f7f55

--- src/com/vimukti/accounter/web/client/ui/core/AbstractTransactionBaseView.java
+++ src/com/vimukti/accounter/web/client/ui/core/AbstractTransactionBaseView.java
@@ -851,8 +851,13 @@ public abstract class AbstractTransactionBaseView<T extends ClientTransaction>
 		updateTransaction();
 
 		ValidationResult result = new ValidationResult();
-
-		if (this.transaction.getTotal() < 0) {
+		//Validations
+		//1. Transaction total <= 0 ERROR
+		//2. TransactionItem line total <= 0 ERROR
+		//3. If(accountingType == UK) 
+		//		if(taxCodeName == "New S" && transactionDate is before 4 Jan 2011)
+		//				ERROR			
+		if (this.transaction.getTotal() <= 0) {
 			result.addError(this, Accounter.constants()
 					.transactiontotalcannotbe0orlessthan0());
 		}
