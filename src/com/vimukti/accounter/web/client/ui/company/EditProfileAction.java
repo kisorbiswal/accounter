1ff4f84b9edd7e4c905af1bd061d248adb5d7984
Syamalamba P <syamalamba.p@vimukti.com>
2011-08-16 14:07:12 +0000

    Former-commit-id: 3275b55f5f9cd376c634fda280067ec9abcebb95

---
+++ src/com/vimukti/accounter/web/client/ui/company/EditProfileAction.java
@@ -0,0 +1,44 @@
+package com.vimukti.accounter.web.client.ui.company;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.core.Action;
+
+public class EditProfileAction extends Action {
+	private EditProfileDialog view;
+
+	public EditProfileAction(String text) {
+		super(text);
+	}
+
+	@Override
+	public void run() {
+		runAsync(data);
+
+	}
+
+	private void runAsync(Object data) {
+		view = new EditProfileDialog(Accounter.constants().editProfile(),
+				null);
+		view.show();
+		view.center();
+	}
+
+	@Override
+	public ImageResource getBigImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageResource getSmallImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryToken() {
+		return "editProfile";
+	}
+
+}
---
+++ src/com/vimukti/accounter/web/client/ui/company/EditProfileDialog.java
@@ -0,0 +1,102 @@
+package com.vimukti.accounter.web.client.ui.company;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.AccounterAsyncCallback;
+import com.vimukti.accounter.web.client.core.ClientUser;
+import com.vimukti.accounter.web.client.core.ClientUserInfo;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.exception.AccounterException;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.Header;
+import com.vimukti.accounter.web.client.ui.core.BaseDialog;
+import com.vimukti.accounter.web.client.ui.forms.TextBoxItem;
+
+public class EditProfileDialog extends BaseDialog {
+	private TextBoxItem firstNameTextItem;
+	private TextBoxItem lastNameTextItem;
+	private VerticalPanel mainPanel;
+	private ClientUser clientUser;
+
+	public EditProfileDialog(String editProfile, String desc) {
+		super(editProfile, desc);
+		clientUser = Accounter.getUser();
+		createControls();
+	}
+
+	private void createControls() {
+		firstNameTextItem = new TextBoxItem();
+		firstNameTextItem.setValue(clientUser.getFirstName());
+		lastNameTextItem = new TextBoxItem();
+		lastNameTextItem.setValue(clientUser.getLastName());
+		mainPanel = new VerticalPanel();
+		mainPanel.add(firstNameTextItem);
+		mainPanel.add(lastNameTextItem);
+		okbtn.setText(Accounter.constants().saveButton());
+		setBodyLayout(mainPanel);
+	}
+
+	protected boolean saveEditedProfile() {
+		// Send request to edit profile.
+		String firstName = firstNameTextItem.getValue();
+		String lastName = lastNameTextItem.getValue();
+		if (firstName.isEmpty() || firstName == null || lastName.isEmpty()
+				|| lastName == null) {
+			addError(this, Accounter.constants().nameShouldnotbeempty());
+			return false;
+		} else if (firstName.equals(clientUser.getFirstName())
+				&& lastName.equals(clientUser.getLastName())) {
+			addError(this, Accounter.constants().bothnamessameasprevious());
+			return false;
+		} else {
+			final ClientUserInfo userInfo = clientUser.toUserInfo();
+			userInfo.setFirstName(firstName);
+			userInfo.setLastName(lastName);
+			userInfo.setFullName(firstName + " " + lastName);
+			Accounter.createCRUDService().updateUser(userInfo,
+					new AccounterAsyncCallback<Long>() {
+						@Override
+						public void onException(AccounterException caught) {
+
+						}
+
+						@Override
+						public void onResultSuccess(Long result) {
+							if (result != null) {
+								removeFromParent();
+								Accounter.showInformation(Accounter.constants()
+										.updatedSuccessfully());
+								Header.userName.setText("Hello "
+										+ userInfo.getFullName());
+							} else {
+								addError(this, Accounter.constants()
+										.yourPresentPasswordisWrong());
+							}
+						}
+
+					});
+
+		}
+		return false;
+	}
+
+	@Override
+	public void deleteFailed(AccounterException caught) {
+
+	}
+
+	@Override
+	public void deleteSuccess(Boolean result) {
+
+	}
+
+	@Override
+	public void processupdateView(IAccounterCore core, int command) {
+
+	}
+
+	@Override
+	protected boolean onOK() {
+		return saveEditedProfile();
+	}
+
+}
