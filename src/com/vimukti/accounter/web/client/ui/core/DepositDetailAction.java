4e7a5fcba6bb9a9dd77d424ce84532083d365c24
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-17 05:15:29 +0000

    Former-commit-id: 4a3d44d517949bddce1ec1bd1a6ec0af3a1f80e6

--- src/com/vimukti/accounter/web/client/ui/core/DepositDetailAction.java
+++ src/com/vimukti/accounter/web/client/ui/core/DepositDetailAction.java
@@ -1,5 +1,7 @@
 package com.vimukti.accounter.web.client.ui.core;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.core.reports.DepositDetail;
 import com.vimukti.accounter.web.client.ui.Accounter;
@@ -16,18 +18,23 @@ public class DepositDetailAction extends Action {
 
 	public void runAsync(final Object data, final Boolean isDependent) {
 
-		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+		GWT.runAsync(new RunAsyncCallback() {
 
-			public void onCreated() {
-
+			@Override
+			public void onSuccess() {
 				AbstractReportView<DepositDetail> report = new DepositDetailReport();
 				MainFinanceWindow.getViewManager().showView(report, data,
 						isDependent, DepositDetailAction.this);
 
 			}
 
-		});
+			@Override
+			public void onFailure(Throwable arg0) {
+				Accounter
+						.showError(Accounter.constants().unableToshowtheview());
 
+			}
+		});
 	}
 
 	@Override
