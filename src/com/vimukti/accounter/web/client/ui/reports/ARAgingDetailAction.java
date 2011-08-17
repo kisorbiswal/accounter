a94290f7c9d9caea29afa6c4f67e1e4051711d8b
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-17 05:20:04 +0000

    Former-commit-id: f855069c478c4ed1d32ad98b06b5b838b28468a4

--- src/com/vimukti/accounter/web/client/ui/reports/ARAgingDetailAction.java
+++ src/com/vimukti/accounter/web/client/ui/reports/ARAgingDetailAction.java
@@ -1,5 +1,7 @@
 package com.vimukti.accounter.web.client.ui.reports;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
@@ -24,17 +26,22 @@ public class ARAgingDetailAction extends Action {
 
 	public void runAsync(final Object data, final Boolean isDependent) {
 
-		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+		GWT.runAsync(new RunAsyncCallback() {
 
-			public void onCreated() {
+			@Override
+			public void onSuccess() {
 				report = new ARAgingDetailReport();
 				MainFinanceWindow.getViewManager().showView(report, data,
 						isDependent, ARAgingDetailAction.this);
 
 			}
-			 
-		});
 
+			@Override
+			public void onFailure(Throwable arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 
 	// @Override
