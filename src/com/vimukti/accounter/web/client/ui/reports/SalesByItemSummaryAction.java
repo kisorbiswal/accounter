a2cb1d21b799219be9a952fdf60cecbb5fb01f67
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-17 05:23:56 +0000

    Former-commit-id: fa3a26855a49677411879e285ac4f84a8059b0e4

--- src/com/vimukti/accounter/web/client/ui/reports/SalesByItemSummaryAction.java
+++ src/com/vimukti/accounter/web/client/ui/reports/SalesByItemSummaryAction.java
@@ -1,5 +1,7 @@
 package com.vimukti.accounter.web.client.ui.reports;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.core.reports.SalesByCustomerDetail;
 import com.vimukti.accounter.web.client.ui.Accounter;
@@ -22,17 +24,21 @@ public class SalesByItemSummaryAction extends Action {
 
 	public void runAsync(final Object data, final Boolean isDependent) {
 
-		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+		GWT.runAsync(new RunAsyncCallback() {
 
-			public void onCreated() {
-
+			@Override
+			public void onSuccess() {
 				AbstractReportView<SalesByCustomerDetail> report = new SalesByItemSummaryReport();
 				MainFinanceWindow.getViewManager().showView(report, data,
 						isDependent, SalesByItemSummaryAction.this);
-
 			}
 
-			 
+			@Override
+			public void onFailure(Throwable arg0) {
+				Accounter
+						.showError(Accounter.constants().unableToshowtheview());
+
+			}
 		});
 
 	}
