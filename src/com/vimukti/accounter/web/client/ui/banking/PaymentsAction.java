22ce9525d07cb21ae20ad4a550ac52c9e4b14288
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-17 04:41:29 +0000

    Former-commit-id: 3eeef08ecba251626bdb4a2de01a584c0a176b92

--- src/com/vimukti/accounter/web/client/ui/banking/PaymentsAction.java
+++ src/com/vimukti/accounter/web/client/ui/banking/PaymentsAction.java
@@ -1,5 +1,7 @@
 package com.vimukti.accounter.web.client.ui.banking;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
@@ -23,10 +25,10 @@ public class PaymentsAction extends Action {
 	}
 
 	public void runAsync(final Object data, final Boolean isDependent) {
-		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+		GWT.runAsync(new RunAsyncCallback() {
 
-			public void onCreated() {
-
+			@Override
+			public void onSuccess() {
 				PaymentListView view = PaymentListView.getInstance();
 
 				MainFinanceWindow.getViewManager().showView(view, data,
@@ -34,6 +36,12 @@ public class PaymentsAction extends Action {
 
 			}
 
+			@Override
+			public void onFailure(Throwable arg0) {
+				Accounter
+						.showError(Accounter.constants().unableToshowtheview());
+
+			}
 		});
 	}
 
