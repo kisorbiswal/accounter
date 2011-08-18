651aba79dd0181194d0741f4d2aa1598521f9035
Devi G <devi.g@vimukti.com>
2011-08-18 13:23:10 +0000

    Former-commit-id: ee27f37323f9306b84c34388fe39e8997bb12aa4

--- src/com/vimukti/accounter/web/client/externalization/AccounterMessages.java
+++ src/com/vimukti/accounter/web/client/externalization/AccounterMessages.java
@@ -110,4 +110,8 @@ public interface AccounterMessages extends Messages {
 	public String journalEntryVendor(String name);
 	
 	public String invalidAccountNumber(String name);
+
+	public String startSetupInfo();
+
+	public String skipSetupInfo();
 }
