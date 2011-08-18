f48cb3e8db1573e8ff55934753b69088b706fe2a
Devi G <devi.g@vimukti.com>
2011-08-18 10:28:52 +0000

    Former-commit-id: 1b1c7d70cb45a69d9b32f5d6992bdd3047fe5d01

--- src/com/vimukti/accounter/web/client/uibinder/setup/SetupCompanyInfoPage.java
+++ src/com/vimukti/accounter/web/client/uibinder/setup/SetupCompanyInfoPage.java
@@ -16,7 +16,7 @@ import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author Administrator
- *
+ * 
  */
 public class SetupCompanyInfoPage extends Composite implements HasText {
 
@@ -28,45 +28,33 @@ public class SetupCompanyInfoPage extends Composite implements HasText {
 	}
 
 	/**
-	 * Because this class has a default constructor, it can
-	 * be used as a binder template. In other words, it can be used in other
-	 * *.ui.xml files as follows:
-	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	 *   xmlns:g="urn:import:**user's package**">
-	 *  <g:**UserClassName**>Hello!</g:**UserClassName>
-	 * </ui:UiBinder>
-	 * Note that depending on the widget that is used, it may be necessary to
-	 * implement HasHTML instead of HasText.
+	 * Because this class has a default constructor, it can be used as a binder
+	 * template. In other words, it can be used in other *.ui.xml files as
+	 * follows: <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
+	 * xmlns:g="urn:import:**user's package**">
+	 * <g:**UserClassName**>Hello!</g:**UserClassName> </ui:UiBinder> Note that
+	 * depending on the widget that is used, it may be necessary to implement
+	 * HasHTML instead of HasText.
 	 */
 	public SetupCompanyInfoPage() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
-	@UiField
-	Button button;
-
 	public SetupCompanyInfoPage(String firstName) {
 		initWidget(uiBinder.createAndBindUi(this));
 
 		// Can access @UiField after calling createAndBindUi
-		button.setText(firstName);
-	}
-
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
 	}
 
 	public void setText(String text) {
-		button.setText(text);
 	}
 
 	/**
-	 * Gets invoked when the default constructor is called
-	 * and a string is provided in the ui.xml file.
+	 * Gets invoked when the default constructor is called and a string is
+	 * provided in the ui.xml file.
 	 */
 	public String getText() {
-		return button.getText();
+		return null;
 	}
 
 }
