d1220e77eb0e0fcd83145ea3d2f95f91f845b61d
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-08-18 10:16:29 +0000

    Former-commit-id: 4e3eb06533749935c86e6bb894535de581138720

---
+++ src/com/vimukti/accounter/services/IS2SService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.vimukti.accounter.services;
+
+import com.vimukti.accounter.web.client.data.ClientUser;
+import com.vimukti.accounter.web.client.exception.AccounterException;
+
+/**
+ * @author Prasanna Kumar G
+ * 
+ */
+public interface IS2SService {
+
+	public void createComapny(long companyID, String companyName,
+			int companyType, ClientUser user) throws AccounterException;
+
+	public boolean isAdmin(long companyID, String emailID);
+
+	public void deleteUserFromCompany(String email);
+
+}
---
+++ src/com/vimukti/accounter/services/IS2SServiceAsync.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.vimukti.accounter.services;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.vimukti.accounter.web.client.data.ClientUser;
+import com.vimukti.accounter.web.client.exception.AccounterException;
+
+/**
+ * @author Prasanna Kumar G
+ * 
+ */
+public interface IS2SServiceAsync {
+	public void createComapny(long companyID, String companyName,
+			int companyType, ClientUser user, AsyncCallback<Void> callback)
+			throws AccounterException;
+
+	public boolean isAdmin(long companyID, String emailID,
+			AsyncCallback<Boolean> callback);
+
+	public void deleteUserFromCompany(String email, AsyncCallback<Void> callback);
+}
