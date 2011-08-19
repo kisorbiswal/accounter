2c5e454c186bf215aeea95e3358325265459d921
Praveena A <praveena.a@vimukti.com>
2011-08-19 04:52:51 +0000

    Former-commit-id: 4bcc70ed5b81d1724323eaf6918759e29a64a5f4

--- src/com/vimukti/accounter/web/client/uibinder/setup/SetupSelectFiscalYrDatePage.java
+++ src/com/vimukti/accounter/web/client/uibinder/setup/SetupSelectFiscalYrDatePage.java
@@ -5,46 +5,56 @@ package com.vimukti.accounter.web.client.uibinder.setup;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.datepicker.client.DateBox;
 
 /**
  * @author Administrator
- *
+ * 
  */
 public class SetupSelectFiscalYrDatePage extends AbstractSetupPage {
 
 	private static SetupSelectFiscalYrDatePageUiBinder uiBinder = GWT
 			.create(SetupSelectFiscalYrDatePageUiBinder.class);
-	@UiField VerticalPanel viewPanel;
-	@UiField Label FiscalStarts;
-	@UiField HTML FiscalHeader;
-	@UiField Label StartDate;
-	@UiField RadioButton BeginingFiscal;
-	@UiField HTML BeginingFiscalText;
-	@UiField RadioButton TodaysDate;
-	@UiField HTML TodaysDateText;
-	@UiField DateBox FiscalDate;
+	@UiField
+	VerticalPanel viewPanel;
+	@UiField
+	Label FiscalStarts;
+	@UiField
+	HTML FiscalHeader;
+	@UiField
+	Label StartDate;
+	@UiField
+	RadioButton BeginingFiscal;
+	@UiField
+	HTML BeginingFiscalText;
+	@UiField
+	RadioButton TodaysDate;
+	@UiField
+	HTML TodaysDateText;
+	@UiField
+	DateBox FiscalDate;
+	@UiField
+	ListBox FiscalStartsList;
 
 	interface SetupSelectFiscalYrDatePageUiBinder extends
 			UiBinder<Widget, SetupSelectFiscalYrDatePage> {
 	}
 
 	/**
-	 * Because this class has a default constructor, it can
-	 * be used as a binder template. In other words, it can be used in other
-	 * *.ui.xml files as follows:
-	 * <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
-	 *   xmlns:g="urn:import:**user's package**">
-	 *  <g:**UserClassName**>Hello!</g:**UserClassName>
-	 * </ui:UiBinder>
-	 * Note that depending on the widget that is used, it may be necessary to
-	 * implement HasHTML instead of HasText.
+	 * Because this class has a default constructor, it can be used as a binder
+	 * template. In other words, it can be used in other *.ui.xml files as
+	 * follows: <ui:UiBinder xmlns:ui="urn:ui:com.google.gwt.uibinder"
+	 * xmlns:g="urn:import:**user's package**">
+	 * <g:**UserClassName**>Hello!</g:**UserClassName> </ui:UiBinder> Note that
+	 * depending on the widget that is used, it may be necessary to implement
+	 * HasHTML instead of HasText.
 	 */
 	public SetupSelectFiscalYrDatePage() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -65,14 +75,13 @@ public class SetupSelectFiscalYrDatePage extends AbstractSetupPage {
 	@Override
 	protected void onLoad() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void onSave() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 }
