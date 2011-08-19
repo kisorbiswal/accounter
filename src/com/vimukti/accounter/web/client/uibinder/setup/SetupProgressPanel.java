19399553ba2e9e309678df473a08b815316d047e
Praveena A <praveena.a@vimukti.com>
2011-08-19 06:24:04 +0000

    Former-commit-id: 711398a5dcef1cba14de940ec64ddca761fcb3c0

--- src/com/vimukti/accounter/web/client/uibinder/setup/SetupProgressPanel.java
+++ src/com/vimukti/accounter/web/client/uibinder/setup/SetupProgressPanel.java
@@ -7,6 +7,9 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Image;
 
 /**
  * @author Administrator
@@ -16,6 +19,31 @@ public class SetupProgressPanel extends AbstractSetupPage {
 
 	private static SetupProgressPanelUiBinder uiBinder = GWT
 			.create(SetupProgressPanelUiBinder.class);
+	@UiField VerticalPanel viewPanel;
+	@UiField Label startUp;
+	@UiField Label companyInformation;
+	@UiField Label industryType;
+	@UiField Label organization;
+	@UiField Label refferingName;
+	@UiField Label trackEmployee;
+	@UiField Label sell;
+	@UiField Label estimates;
+	@UiField Label currency;
+	@UiField Label billTracking;
+	@UiField Label fiscalYear;
+	@UiField Label requiredAccounts;
+	@UiField Image setupImage;
+	@UiField Image companyInfoImage;
+	@UiField Image industryTypeImage;
+	@UiField Image organizationImage;
+	@UiField Image referringImage;
+	@UiField Image trackImage;
+	@UiField Image sellImage;
+	@UiField Image estimateImage;
+	@UiField Image currencyImage;
+	@UiField Image billTrackingImage;
+	@UiField Image fiscalYearImage;
+	@UiField Image requiredImage;
 
 	interface SetupProgressPanelUiBinder extends
 			UiBinder<Widget, SetupProgressPanel> {
