7a280571c0f383fc81c33386afe7992d1f8d1411
srinivas.g <srinivas.g@vimukti.com>
2011-08-20 09:11:39 +0000

    Former-commit-id: dba5f59dba550946ba9987fc137c5ddb7ed761a7

---
+++ src/com/vimukti/accounter/main/ServerLocal.java
@@ -0,0 +1,15 @@
+package com.vimukti.accounter.main;
+
+import java.util.Locale;
+
+public class ServerLocal {
+	private static ThreadLocal<Locale> local = new ThreadLocal<Locale>();
+	
+	public static Locale get(){
+		return local.get();
+	}
+	
+	public static void set(Locale locale){
+		local.set(locale);
+	}
+}
