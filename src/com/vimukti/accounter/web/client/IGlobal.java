ef8405853d2e0d4ee87857917bd799a135b69d32
srinivas.g <srinivas.g@vimukti.com>
2011-08-20 09:15:18 +0000

    Former-commit-id: b838eab23bb6e5e67510bb69f475d6a2bb625fa8

---
+++ src/com/vimukti/accounter/web/client/IGlobal.java
@@ -0,0 +1,14 @@
+package com.vimukti.accounter.web.client;
+
+import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
+import com.vimukti.accounter.web.client.externalization.AccounterConstants;
+
+public interface IGlobal {
+
+	public AccounterConstants constants();
+
+	public ClientCompanyPreferences preferences();
+	
+	
+
+}
