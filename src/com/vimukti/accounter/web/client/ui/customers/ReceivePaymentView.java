c321ac9613e539017e0661ad8ada85af6a33925c
Nagaraju P <nagaraju.p@vimukti.com>
2011-08-20 06:58:47 +0000

    Former-commit-id: 341a520eb4b6cea2298f1b22d7e011a89dac4cdd

--- src/com/vimukti/accounter/web/client/ui/customers/ReceivePaymentView.java
+++ src/com/vimukti/accounter/web/client/ui/customers/ReceivePaymentView.java
@@ -168,8 +168,7 @@ public class ReceivePaymentView extends
 
 								if (result.size() > 0) {
 									gridView.removeAllRecords();
-									gridView
-											.initCreditsAndPayments(selectedCustomer);
+									gridView.initCreditsAndPayments(selectedCustomer);
 									addTransactionRecievePayments(result);
 								} else {
 									gridView.addEmptyMessage(Accounter
@@ -221,10 +220,8 @@ public class ReceivePaymentView extends
 
 			ClientTransactionReceivePayment record = new ClientTransactionReceivePayment();
 
-			record
-					.setDueDate(receivePaymentTransaction.getDueDate() != null ? receivePaymentTransaction
-							.getDueDate().getDate()
-							: 0);
+			record.setDueDate(receivePaymentTransaction.getDueDate() != null ? receivePaymentTransaction
+					.getDueDate().getDate() : 0);
 			record.setNumber(receivePaymentTransaction.getNumber());
 
 			record.setInvoiceAmount(receivePaymentTransaction
@@ -233,11 +230,8 @@ public class ReceivePaymentView extends
 			record.setInvoice(receivePaymentTransaction.getTransactionId());
 			record.setAmountDue(receivePaymentTransaction.getAmountDue());
 			record.setDummyDue(receivePaymentTransaction.getAmountDue());
-			record
-					.setDiscountDate(receivePaymentTransaction
-							.getDiscountDate() != null ? receivePaymentTransaction
-							.getDiscountDate().getDate()
-							: 0);
+			record.setDiscountDate(receivePaymentTransaction.getDiscountDate() != null ? receivePaymentTransaction
+					.getDiscountDate().getDate() : 0);
 
 			record.setCashDiscount(receivePaymentTransaction.getCashDiscount());
 
@@ -371,8 +365,7 @@ public class ReceivePaymentView extends
 								.setTransactionReceivePayment(payment);
 					}
 
-				payment
-						.setTransactionCreditsAndPayments(tranCreditsandPayments);
+				payment.setTransactionCreditsAndPayments(tranCreditsandPayments);
 			}
 			paymentsList.add(payment);
 			payment.getTempCredits().clear();
@@ -439,8 +432,7 @@ public class ReceivePaymentView extends
 					return;
 				Double amount = 0.00D;
 				try {
-					amount = DataUtils
-							.getAmountStringAsDouble(value.toString());
+					amount = DataUtils.getAmountStringAsDouble(value.toString());
 					setAmount(DataUtils.isValidAmount(value.toString()) ? amount
 							: 0.00D);
 					paymentAmountChanged(amount);
@@ -640,9 +632,9 @@ public class ReceivePaymentView extends
 			// .getNearestTaxRate(((Date) transactionDateItem
 			// .getValue()).getTime())));
 		} // else
-		// fraction = 0.0;
-		// if (vatFraction != null)
-		// vatFraction.setAmount(fraction);
+			// fraction = 0.0;
+			// if (vatFraction != null)
+			// vatFraction.setAmount(fraction);
 	}
 
 	@Override
@@ -1023,23 +1015,21 @@ public class ReceivePaymentView extends
 		// 6. isValidReceivePaymentAmount?
 		// 7. unUsedPaymentsAmount > 0 add warning
 		if (!AccounterValidator.isValidTransactionDate(this.transactionDate)) {
-			result.addError(transactionDateItem, accounterConstants
-					.invalidateTransactionDate());
+			result.addError(transactionDateItem,
+					accounterConstants.invalidateTransactionDate());
 		}
 
 		if (AccounterValidator
 				.isInPreventPostingBeforeDate(this.transactionDate)) {
-			result.addError(transactionDateItem, accounterConstants
-					.invalidateDate());
+			result.addError(transactionDateItem,
+					accounterConstants.invalidateDate());
 		}
 
 		result.add(FormItem.validate(customerCombo, paymentMethodCombo,
 				depositInCombo));
 
 		if (AccounterValidator.isBlankTransaction(gridView)) {
-			result
-					.addError(gridView, Accounter.constants()
-							.selectTransaction());
+			result.addError(gridView, Accounter.constants().selectTransaction());
 		} else
 			result.add(gridView.validateGrid());
 
@@ -1091,7 +1081,7 @@ public class ReceivePaymentView extends
 	}
 
 	@Override
-	public void deleteSuccess(IAccounterCore result){
+	public void deleteSuccess(IAccounterCore result) {
 
 	}
 
@@ -1211,8 +1201,8 @@ public class ReceivePaymentView extends
 
 		getTransactionReceivePayments(this.getCustomer());
 		memoTextAreaItem.setDisabled(isEdit);
-		transaction = null;
-
+		transaction = new ClientReceivePayment();
+		data = transaction;
 		// this.rpcUtilService.getTransactionReceivePayments(customer
 		// .getID(),
 		// new
