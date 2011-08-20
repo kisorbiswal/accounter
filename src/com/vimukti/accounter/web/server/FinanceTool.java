6aac0460fe7e65d65ada02ec03ccd36700a5bab9
Saiprasad N <saiprasad.n@vimukti.com>
2011-08-20 13:47:16 +0000

    Former-commit-id: 9376d309d410aa05df669988efbea54d2d48368a

--- src/com/vimukti/accounter/web/server/FinanceTool.java
+++ src/com/vimukti/accounter/web/server/FinanceTool.java
@@ -11032,6 +11032,10 @@ public class FinanceTool implements IFinanceDAOService {
 							: (String) object[14]);
 					statementsList.setPaymentTerm(object[15] == null ? null
 							: (String) object[15]);
+
+					statementsList.setTransactionId(object[16] == null ? null
+							: ((Long) object[16]).longValue());
+
 					long ageing = getAgeing(
 							statementsList.getTransactionDate(),
 							statementsList.getDueDate(), toDate);
