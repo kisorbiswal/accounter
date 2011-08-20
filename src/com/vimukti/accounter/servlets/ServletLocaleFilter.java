1b110a971e8990b825693918df67f14e11f44390
srinivas.g <srinivas.g@vimukti.com>
2011-08-20 09:37:35 +0000

    Former-commit-id: c6a4d6b8f2334f57d886aa0d3f5a0df0eb7063e4

---
+++ src/com/vimukti/accounter/servlets/ServletLocaleFilter.java
@@ -0,0 +1,32 @@
+package com.vimukti.accounter.servlets;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import com.vimukti.accounter.main.ServerLocal;
+
+public class ServletLocaleFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1,
+			FilterChain arg2) throws IOException, ServletException {
+		arg2.doFilter(arg0, arg1);
+		ServerLocal.set(arg0.getLocale());		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
