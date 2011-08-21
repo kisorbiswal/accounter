bbd556eab92b4d9ef7371ffbf185805285fef3cb
srinivas.g <srinivas.g@vimukti.com>
2011-08-21 13:14:52 +0000

    Former-commit-id: 91e3d4ad28ebc18510e27fdf890c5d8badb8bdec

---
+++ src/com/vimukti/accounter/core/InvoiceExcelTemplete.java
@@ -0,0 +1,394 @@
+package com.vimukti.accounter.core;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import org.hibernate.Session;
+
+import com.vimukti.accounter.main.ServerConfiguration;
+import com.vimukti.accounter.utils.CSVWriter;
+import com.vimukti.accounter.utils.HibernateUtil;
+
+/**
+ * this class is used to generate Invoice report in Excel format.
+ * 
+ * @author vimukti28
+ * 
+ */
+public class InvoiceExcelTemplete {
+	private BrandingTheme brandingTheme;
+	private Invoice invoice;
+	private Company company;
+
+	public InvoiceExcelTemplete(Invoice invoice, BrandingTheme brandingTheme) {
+		this.invoice = invoice;
+		this.brandingTheme = brandingTheme;
+		this.company = getCompany();
+
+	}
+
+	public void generateToExcel() throws IOException {
+
+		Address billaddress = invoice.getBillingAddress();
+
+		Address shipaddress = invoice.getShippingAdress();
+
+		Address registeredAddress = company.getRegisteredAddress();
+
+		Vector<String> regisAddress = getAddress(registeredAddress, false);
+		Vector<String> billAddress = getAddress(billaddress, true);
+		Vector<String> shipAddress = getAddress(shipaddress, false);
+
+		Vector<String> invoiceLabels = new Vector<String>();
+		invoiceLabels.add("Invoice Number");
+		invoiceLabels.add("Invoice Date");
+		invoiceLabels.add("Order Number");
+		invoiceLabels.add("Customer Number");
+
+		Vector<String> invoiceValues = new Vector<String>();
+		invoiceValues.add(invoice.getNumber());
+		invoiceValues.add(invoice.getDate().toString());
+		invoiceValues.add(invoice.getOrderNum());
+		invoiceValues.add(invoice.getCustomer().getNumber());
+
+		// for comparing the invoice labels and registered address sizes
+		if (invoiceLabels.size() < regisAddress.size()) {
+			while (invoiceLabels.size() < regisAddress.size()) {
+				invoiceLabels.add("");
+				invoiceValues.add("");
+			}
+		} else {
+			while (regisAddress.size() < invoiceLabels.size()) {
+				regisAddress.add("");
+			}
+		}
+
+		// for comparing the bill to address and shipping to address
+		if (billAddress.size() < shipAddress.size()) {
+			while (billAddress.size() < shipAddress.size()) {
+				billAddress.add("");
+
+			}
+		} else {
+			while (shipAddress.size() < billAddress.size()) {
+				shipAddress.add("");
+			}
+		}
+
+		SalesPerson salesPerson = invoice.getSalesPerson();
+		String salesname = salesPerson != null ? ((salesPerson.getFirstName() == null ? ""
+				: salesPerson.getFirstName())
+				+ "" + (salesPerson.getLastName() == null ? "" : salesPerson
+				.getLastName()))
+				: "";
+
+		ShippingMethod shippingMethod = invoice.getShippingMethod();
+		String shipName = shippingMethod != null ? shippingMethod.getName()
+				: "";
+
+		PaymentTerms paymentTerms = invoice.getPaymentTerm();
+		String payment = paymentTerms != null ? paymentTerms.getName() : "";
+
+		// boolean sales = salesname.trim().length() > 0 ? true : false;
+		// boolean ship = shipName.trim().length() > 0 ? true : false;
+		// boolean paymentterms = payment.trim().length() > 0 ? true : false;
+
+		String dueDate = invoice.getDueDate().toString();
+
+		String termsnPayment = forNullValue(brandingTheme
+				.getTerms_And_Payment_Advice());
+
+		String paypalEmail = forNullValue(brandingTheme.getPayPalEmailID());
+
+		String vatStringLabel = getVendorString("VAT No: ", "Tax No: ");
+
+		String vatStringValue = forNullValue(company.getPreferences()
+				.getVATregistrationNumber());
+
+		String sortCodeLabel = " Sort Code: ";
+		String sortCodeValue = forNullValue(company.getSortCode());
+
+		String bankAccountNumLabel = "Bank Account No: ";
+		String bankAccountNumValue = forNullValue(company.getBankAccountNo());
+
+		String comanyRegNum = checkIsNull(company.getRegistrationNumber());
+
+		// InvoiceExcelTemplete excelTemplete = new
+		// InvoiceExcelTemplete(invoice, brandingTheme);
+		// excelTemplete.generateToExcel();
+
+		String excelFileName = "d:\\csv\\Invoice_" + invoice.getNumber()
+				+ ".csv";
+
+		try {
+			FileWriter f = new FileWriter(excelFileName);
+			PrintWriter pw = new PrintWriter(f);
+			CSVWriter csvWriter = new CSVWriter(pw, false, ',',
+					System.getProperty("line.separator"));
+
+			csvWriter.writeln(new String[] { "", "",
+					brandingTheme.getOverDueInvoiceTitle(), "", "", "" });
+			csvWriter.writeln();
+
+			// third row
+			for (int i = 0; i < regisAddress.size(); i++) {
+
+				csvWriter.writeln(new String[] { "", regisAddress.elementAt(i),
+						"", "", invoiceLabels.elementAt(i),
+						invoiceValues.elementAt(i) });
+				// csvWriter.writeln();
+			}
+
+			csvWriter.writeln();
+
+			csvWriter.writeln(new String[] { "", "Bill To", "", "", "Ship To",
+					"" });
+
+			csvWriter.writeln();
+
+			// fifth row
+			for (int i = 0; i < billAddress.size(); i++) {
+
+				csvWriter.writeln(new String[] { billAddress.elementAt(i), "",
+						"", "", shipAddress.elementAt(i), "" });
+
+			}
+
+			csvWriter.writeln();
+			// for shipping, sales person and payment term labels
+
+			csvWriter.writeln(new String[] { "Sales Person", "",
+					"Shipping Method", "", "Payment Terms", "" });
+
+			// for shipping, sales person and payment term values
+
+			csvWriter.writeln(new String[] { salesname, "", shipName, "",
+					payment, "" });
+
+			csvWriter.writeln();
+
+			csvWriter.writeln();
+			// for displaying invoice details
+
+			csvWriter.writeln(new String[] { "Description", "Qty",
+					"Unit Price", "Total Price",
+					getVendorString("VAT Rate", "Tax Rate"),
+					getVendorString("VAT Amount", "Tax Amount") });
+
+			List<TransactionItem> transactionItem = invoice
+					.getTransactionItems();
+			for (Iterator iterator = transactionItem.iterator(); iterator
+					.hasNext();) {
+				TransactionItem transItem = (TransactionItem) iterator.next();
+
+				f.append(transItem.getDescription() + ','
+						+ transItem.getQuantity() + ','
+						+ transItem.getUnitPrice() + ','
+						+ transItem.getLineTotal() + ','
+						+ transItem.getTaxCode() + ','
+						+ transItem.getVATfraction() + "\n");
+				csvWriter.writeln(new String[] {
+						transItem.getDescription(),
+						String.valueOf(transItem.getQuantity().getValue()),
+						String.valueOf(transItem.getUnitPrice()),
+						String.valueOf(transItem.getLineTotal()),
+						getDecimalsUsingMaxDecimals(transItem.getVATfraction(),
+								null, 2),
+						String.valueOf(transItem.getVATfraction()) });
+
+			}
+
+			csvWriter.writeln(new String[] { invoice.getMemo(), "", "", "",
+					"Sub Total",
+					largeAmountConversation(invoice.getNetAmount()) });
+
+			csvWriter.writeln(new String[] {
+					"",
+					"",
+					"",
+					"",
+					getVendorString("VAT Total", "Tax Total"),
+					largeAmountConversation((invoice.getTotal() - invoice
+							.getNetAmount())) });
+
+			csvWriter.writeln(new String[] { "", "", "", "", "Total",
+					largeAmountConversation(invoice.getTotal()) });
+
+			csvWriter.writeln();
+
+			csvWriter
+					.writeln(new String[] { "DueDate", dueDate, "", "", "", "" });
+
+			csvWriter.writeln();
+
+			csvWriter
+					.writeln(new String[] { termsnPayment, "", "", "", "", "" });
+
+			csvWriter.writeln();
+
+			csvWriter.writeln(new String[] { paypalEmail, "", "", "", "", "" });
+
+			csvWriter.writeln();
+
+			csvWriter.writeln(new String[] { vatStringLabel, vatStringValue,
+					sortCodeLabel, sortCodeValue, bankAccountNumLabel,
+					bankAccountNumValue });
+
+			csvWriter.writeln();
+
+			String regAddress = "";
+
+			for (Iterator iterator = regisAddress.iterator(); iterator
+					.hasNext();) {
+				String string = (String) iterator.next();
+				regAddress += string + ',';
+
+			}
+
+			csvWriter.writeln(new String[] { "company name", regAddress, "",
+					"", "", "" });
+
+			csvWriter.writeln();
+
+			csvWriter.writeln(new String[] { "", "Company Registration No:",
+					comanyRegNum, "", "", "" });
+
+			csvWriter.writeln();
+
+			csvWriter.close();
+
+			System.err.println("completed... writing in csv format");
+
+		} finally {
+
+		}
+
+	}
+
+	/**
+	 * this method is used to get billing address and shipping address
+	 */
+	public Vector<String> getAddress(Address address, boolean val) {
+		Vector<String> addressVector = new Vector<String>();
+
+		if (val) {
+			// this is used only for billing and shipping address
+			String customerName = checkIsNull(invoice.getCustomer().getName());
+			if (customerName.length() > 0) {
+				addressVector.addElement(customerName);
+			}
+		}
+		String add1 = checkIsNull(address.getAddress1());
+		if (add1.length() > 0) {
+			addressVector.addElement(add1);
+		}
+		String street = checkIsNull(address.getStreet());
+		if (street.length() > 0) {
+			addressVector.addElement(street);
+		}
+		String city = checkIsNull(address.getCity());
+		if (city.length() > 0) {
+			addressVector.addElement(city);
+		}
+		String state = checkIsNull(address.getStateOrProvinence());
+		if (state.length() > 0) {
+			addressVector.addElement(state);
+		}
+		String zip = checkIsNull(address.getZipOrPostalCode());
+		if (zip.length() > 0) {
+			addressVector.addElement(zip);
+		}
+		String country = checkIsNull(address.getCountryOrRegion());
+		if (country.length() > 0) {
+			addressVector.addElement(country);
+		}
+
+		return addressVector;
+	}
+
+	private String getVendorString(String forUk, String forUs) {
+		/*
+		 * return company.getAccountingType() == company.ACCOUNTING_TYPE_US ?
+		 * forUs : forUk;
+		 */;
+		return forUk;
+	}
+
+	public String checkIsNull(String value) {
+		return value == null ? "" : value;
+	}
+
+	private StringBuffer getImage() {
+		StringBuffer original = new StringBuffer();
+		// String imagesDomain = "/do/downloadFileFromFile?";
+		Session session = HibernateUtil.getCurrentSession();
+		Company Company = (Company) session.get(Company.class, 1L);
+		original.append("<img src='file:///");
+		original.append(ServerConfiguration.getAttachmentsDir() + "/"
+				+ Company.getFullName() + "/" + brandingTheme.getFileName());
+		original.append("'/>");
+		return original;
+	}
+
+	public Company getCompany() {
+		company = HibernateUtil.getCurrentSession() != null ? (Company) HibernateUtil
+				.getCurrentSession().get(Company.class, 1L) : (Company) Utility
+				.getCurrentSession().get(Company.class, 1L);
+
+		if (company == null)
+			return null;
+
+		company.toCompany(company);
+		return company;
+	}
+
+	public String forNullValue(String value) {
+		return value != null ? value : "";
+	}
+
+	private String getDecimalsUsingMaxDecimals(double quantity, String amount,
+			int maxDecimalPoint) {
+		String qty = "";
+		String max;
+		if (maxDecimalPoint != 0) {
+			if (amount == null)
+				qty = String.valueOf(quantity);
+			else
+				qty = amount;
+			max = qty.substring(qty.indexOf(".") + 1);
+			if (maxDecimalPoint > max.length()) {
+				for (int i = max.length(); maxDecimalPoint != i; i++) {
+					qty = qty + "0";
+				}
+			}
+		} else {
+			qty = String.valueOf((long) quantity);
+		}
+
+		String temp = qty.contains(".") ? qty.replace(".", "-").split("-")[0]
+				: qty;
+		return insertCommas(temp)
+				+ (qty.contains(".") ? "."
+						+ qty.replace(".", "-").split("-")[1] : "");
+	}
+
+	private static String insertCommas(String str) {
+
+		if (str.length() < 4) {
+			return str;
+		}
+		return insertCommas(str.substring(0, str.length() - 3)) + ","
+				+ str.substring(str.length() - 3, str.length());
+	}
+
+	private String largeAmountConversation(double amount) {
+		String amt = Utility.decimalConversation(amount);
+		amt = getDecimalsUsingMaxDecimals(0.0, amt, 2);
+		return (amt);
+	}
+
+}
---
+++ src/com/vimukti/accounter/core/InvoicePDFTemplete.java
@@ -0,0 +1,514 @@
+package com.vimukti.accounter.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import com.vimukti.accounter.main.ServerConfiguration;
+import com.vimukti.accounter.utils.MiniTemplator;
+import com.vimukti.accounter.utils.MiniTemplator.TemplateSyntaxException;
+
+/**
+ * this class is used to generate Invoice report in PDF format
+ * 
+ * @author vimukti28
+ * 
+ */
+public class InvoicePDFTemplete {
+	private Invoice invoice;
+	private BrandingTheme brandingTheme;
+	private int maxDecimalPoints;
+	private static final String templateFileName = "templetes" + File.separator
+			+ "InvoiceTemplete.html";
+	private Company company;
+
+	public InvoicePDFTemplete(Invoice invoice, BrandingTheme brandingTheme,
+			Company company) {
+		this.invoice = invoice;
+		this.brandingTheme = brandingTheme;
+		this.company = company;
+		this.maxDecimalPoints = getMaxDecimals(invoice);
+
+	}
+
+	public String getFileName() {
+		return "Invoice_" + this.invoice.getNumber();
+	}
+
+	public String generatePDF() throws TemplateSyntaxException, IOException {
+		String outPutString = "";
+		MiniTemplator t;
+
+		// TODO for displaying the company address
+
+		String cmpAdd = "";
+		Address cmpTrad = company.getTradingAddress();
+		if (cmpTrad.getType() == Address.TYPE_COMPANY_REGISTRATION) {
+			if (cmpTrad != null)
+				cmpAdd = forUnusedAddress(cmpTrad.getAddress1(), false)
+						+ forUnusedAddress(cmpTrad.getStreet(), false)
+						+ forUnusedAddress(cmpTrad.getCity(), false)
+						+ forUnusedAddress(cmpTrad.getStateOrProvinence(),
+								false)
+						+ forUnusedAddress(cmpTrad.getZipOrPostalCode(), false)
+						+ forUnusedAddress(cmpTrad.getCountryOrRegion(), false);
+		}
+
+		if (cmpAdd.equals("")) {
+			// String contactDetails = brandingTheme.getContactDetails() != null
+			// ? brandingTheme
+			// .getContactDetails() : this.company.getName();
+			cmpAdd = forNullValue(company.getTradingName());
+		} else {
+			cmpAdd = forNullValue(company.getTradingName()) + "<br/>" + cmpAdd;
+		}
+
+		try {
+			t = new MiniTemplator(templateFileName);
+			String image = getCompany();
+
+			// setting logo Image
+			if (brandingTheme.isShowLogo()) {
+				String logoAlligment = getLogoAlignment();
+				t.setVariable("getLogoAlignment", logoAlligment);
+
+				t.setVariable("logoImage", image);
+				t.addBlock("showlogo");
+			}
+
+			// TODO for company trading name and address
+			t.setVariable("comapany", cmpAdd);
+
+			// TODO For setting the Contact Details
+			String contactDetails = forNullValue(brandingTheme
+					.getContactDetails());
+			t.setVariable("companyDetails", contactDetails);
+
+			// setting invoice number
+			boolean hasInvNumber = false;
+			String invNumber = forNullValue(invoice.getNumber());
+			if (invNumber.trim().length() > 0) {
+				hasInvNumber = true;
+				t.setVariable("invoiceNumber", invNumber);
+				t.addBlock("invNumber");
+			}
+			if (hasInvNumber) {
+				t.addBlock("invNumberHead");
+			}
+
+			// setting invoice date
+			boolean hasInvDate = false;
+			String invDate = invoice.getDate().toString();
+			if (invDate.trim().length() > 0) {
+				hasInvDate = true;
+				t.setVariable("invoiceDate", invDate);
+				t.addBlock("invDate");
+			}
+			if (hasInvDate) {
+				t.addBlock("invDateHead");
+			}
+
+			// setting invoice order number
+			boolean hasInvorder = false;
+			String invOrderNum = forNullValue(invoice.getOrderNum());
+			if (invOrderNum.trim().length() > 0) {
+				hasInvorder = true;
+				t.setVariable("invoiceOrderNumber", invOrderNum);
+				t.addBlock("invOrderNum");
+			}
+			if (hasInvorder) {
+				t.addBlock("invOrderNumHead");
+			}
+
+			// setting invoice customer number
+			boolean hascustomernum = false;
+			String invCustomerNum = forNullValue(invoice.getCustomer()
+					.getNumber());
+			if (invCustomerNum.trim().length() > 0) {
+				hascustomernum = true;
+				t.setVariable("invoiceCustomerNumber", invCustomerNum);
+				t.addBlock("invCustNum");
+			}
+			if (hascustomernum) {
+				t.addBlock("invCustNumHead");
+			}
+
+			// setting billing address
+			boolean hasbillAddress = false;
+			Address bill = invoice.getBillingAddress();
+			if (bill != null) {
+				String billAddress = forUnusedAddress(invoice.getCustomer()
+						.getName(), false)
+						+ forUnusedAddress(bill.getAddress1(), false)
+						+ forUnusedAddress(bill.getStreet(), false)
+						+ forUnusedAddress(bill.getCity(), false)
+						+ forUnusedAddress(bill.getStateOrProvinence(), false)
+						+ forUnusedAddress(bill.getZipOrPostalCode(), false)
+						+ bill.getCountryOrRegion();
+
+				if (billAddress.trim().length() > 0) {
+					hasbillAddress = true;
+					t.setVariable("billingAddress", billAddress);
+					t.addBlock("billingAddressDetails");
+				}
+				if (hasbillAddress) {
+					t.addBlock("billhead");
+
+				}
+			}
+			// setting shipping address
+			boolean hasShippingAdd = false;
+			String shipAddress = "";
+			Address shpAdres = invoice.getShippingAdress();
+			if (shpAdres != null) {
+				shipAddress = forUnusedAddress(invoice.getCustomer().getName(),
+						false)
+						+ forUnusedAddress(shpAdres.getAddress1(), false)
+						+ forUnusedAddress(shpAdres.getStreet(), false)
+						+ forUnusedAddress(shpAdres.getCity(), false)
+						+ forUnusedAddress(shpAdres.getStateOrProvinence(),
+								false)
+						+ forUnusedAddress(shpAdres.getZipOrPostalCode(), false)
+						+ forUnusedAddress(shpAdres.getCountryOrRegion(), false);
+			}
+			if (shipAddress.trim().length() > 0) {
+				hasShippingAdd = true;
+				t.setVariable("shippingAddress", shipAddress);
+				t.addBlock("shippingAddressDetails");
+			}
+			if (hasShippingAdd) {
+				t.addBlock("shiphead");
+			}
+
+			// setting salesPerson name
+			boolean hasSalesPerson = false;
+			SalesPerson salesPerson = invoice.getSalesPerson();
+			String salesPersname = salesPerson != null ? ((salesPerson
+					.getFirstName() != null ? salesPerson.getFirstName() : "") + (salesPerson
+					.getLastName() != null ? salesPerson.getLastName() : ""))
+					: "";
+
+			if (salesPersname.trim().length() > 0) {
+				hasSalesPerson = true;
+				t.setVariable("salesPersonName", salesPersname);
+				t.addBlock("salesperson");
+			}
+			if (hasSalesPerson) {
+				t.addBlock("salespersonhead");
+			}
+			// setting shippingmethod name
+			boolean hasshippingmethod = false;
+			ShippingMethod shipMtd = invoice.getShippingMethod();
+			String shipMtdName = shipMtd != null ? shipMtd.getName() : "";
+
+			if (shipMtdName.trim().length() > 0) {
+				hasshippingmethod = true;
+				t.setVariable("shippingMethodName", shipMtdName);
+				t.addBlock("shippingmethod");
+			}
+			if (hasshippingmethod) {
+				t.addBlock("shippingmethodhead");
+			}
+
+			// setting payment terms
+			boolean hasPaymentTerms = false;
+			PaymentTerms paymentterm = invoice.getPaymentTerm();
+			String payterm = paymentterm != null ? paymentterm.getName() : "";
+
+			if (payterm.trim().length() > 0) {
+				hasPaymentTerms = true;
+				t.setVariable("paymentTerms", payterm);
+				t.addBlock("paymentterms");
+			}
+			if (hasPaymentTerms) {
+				t.addBlock("paymenttermshead");
+			}
+
+			String vatLabel = getVendorString("VAT Rate", "Tax Rate");
+			t.setVariable("VATRate", vatLabel);
+
+			String vatAmountLabel = getVendorString("VAT Amount", "Tax Amount");
+			t.setVariable("VATAmount", vatAmountLabel);
+
+			// for checking the show Column Headings
+			if (brandingTheme.isShowColumnHeadings()) {
+
+				if (brandingTheme.isShowTaxColumn()) {
+					t.addBlock("vatBlock");
+				}
+				t.addBlock("showLabels");
+			}
+
+			// setting item description quantity, unit price, total price and
+			// vat details
+			List<TransactionItem> transactionItems = invoice
+					.getTransactionItems();
+			for (Iterator iterator = transactionItems.iterator(); iterator
+					.hasNext();) {
+				TransactionItem item = (TransactionItem) iterator.next();
+
+				String description = forNullValue(item.getDescription());
+				String qty = forZeroAmounts(getDecimalsUsingMaxDecimals(item
+						.getQuantity().getValue(), null, maxDecimalPoints));
+				String unitPrice = forZeroAmounts(largeAmountConversation(item
+						.getUnitPrice()));
+				String totalPrice = largeAmountConversation(item.getLineTotal());
+				String vatRate = String.valueOf(Utility.getVATItemRate(
+						item.getTaxCode(), true));
+				String vatAmount = getDecimalsUsingMaxDecimals(
+						item.getVATfraction(), null, 2);
+
+				t.setVariable("description", description);
+				t.setVariable("quantity", qty);
+				t.setVariable("itemUnitPrice", unitPrice);
+				t.setVariable("itemTotalPrice", totalPrice);
+				if (brandingTheme.isShowTaxColumn()) {
+					t.setVariable("itemVatRate", vatRate);
+					t.setVariable("itemVatAmount", vatAmount);
+					t.addBlock("vatValueBlock");
+				}
+				t.addBlock("invoiceRecord");
+			}
+			// for displaying sub total, vat total, total
+			String subtotal = largeAmountConversation(invoice.getNetAmount());
+			t.setVariable("subTotal", subtotal);
+			if (brandingTheme.isShowTaxColumn()) {
+
+				String vatlabel = getVendorString("VAT Total", "Tax Total");
+				t.setVariable("vatlabel", vatlabel);
+				t.setVariable("vatTotal", largeAmountConversation((invoice
+						.getTotal() - invoice.getNetAmount())));
+				t.addBlock("VatTotal");
+			}
+			String total = largeAmountConversation(invoice.getTotal());
+			t.setVariable("total", total);
+			t.setVariable("blankText", invoice.getMemo());
+			t.addBlock("itemDetails");
+
+			t.setVariable("dueDate", invoice.getDueDate().toString());
+			t.addBlock("dueDateDetails");
+
+			t.setVariable("termsAndPaymentAdvice",
+					brandingTheme.getTerms_And_Payment_Advice());
+			t.addBlock("termsAndAdvice");
+
+			String email = forNullValue(brandingTheme.getPayPalEmailID());
+			if (email.trim().length() > 0) {
+				t.setVariable("email", email);
+				t.addBlock("paypalemail");
+			}
+
+			// for Vat String
+			String vatString = getVendorString("VAT No: ", "Tax No: ")
+					+ forNullValue(company.getPreferences()
+							.getVATregistrationNumber());
+			if (company.getPreferences().getVATregistrationNumber().length() > 0) {
+				if (brandingTheme.isShowTaxNumber()) {
+					t.setVariable("vatNum", vatString);
+					t.addBlock("vat");
+				}
+			}
+
+			// TODO for sortCode
+			String sortCode = " Sort Code: "
+					+ forNullValue(company.getSortCode());
+			if (company.getSortCode() != null) {
+				if (company.getSortCode().length() > 0) {
+					t.setVariable("sortCode", sortCode);
+					t.addBlock("sortcode");
+				}
+			}
+
+			// TODO for BankAccountNumber
+			String bankAccountNum = "Bank Account No: "
+					+ forNullValue(company.getBankAccountNo());
+			if (company.getBankAccountNo() != null) {
+				if (company.getBankAccountNo().length() > 0) {
+					t.setVariable("BankAccountNo", bankAccountNum);
+					t.addBlock("bankAccountNum");
+				}
+			}
+
+			// setting the theme styles
+			t.setVariable("fontStyle", brandingTheme.getFont());
+			t.setVariable("font", brandingTheme.getFontSize());
+			t.setVariable("bottomMargin",
+					String.valueOf(brandingTheme.getBottomMargin()));
+			t.setVariable("topMargin",
+					String.valueOf(brandingTheme.getTopMargin()));
+			t.setVariable("title", brandingTheme.getOverDueInvoiceTitle());
+			t.setVariable("addressPadding",
+					String.valueOf(brandingTheme.getAddressPadding()));
+
+			if (brandingTheme.isShowLogo()) {
+				t.addBlock("logo");
+			}
+
+			// TODO for displaying regestration address and Company Registration
+			// Number
+			String regestrationAddress = "";
+			Address reg = company.getRegisteredAddress();
+			if (reg.getType() == Address.TYPE_COMPANY) {
+				if (reg != null)
+					regestrationAddress = ("&nbsp;Registered Address: "
+							+ reg.getAddress1()
+							+ forUnusedAddress(reg.getStreet(), true)
+							+ forUnusedAddress(reg.getCity(), true)
+							+ forUnusedAddress(reg.getStateOrProvinence(), true)
+							+ forUnusedAddress(reg.getZipOrPostalCode(), true)
+							+ forUnusedAddress(reg.getCountryOrRegion(), true) + ".");
+			}
+
+			regestrationAddress = (company.getFullName() + regestrationAddress + ((company
+					.getRegistrationNumber() != null && !company
+					.getRegistrationNumber().equals("")) ? "<br/>Company Registration No: "
+					+ company.getRegistrationNumber()
+					: ""));
+
+			String trName = company.getTradingName();
+			if (regestrationAddress != null
+					&& regestrationAddress.trim().length() > 0) {
+				if (brandingTheme.isShowRegisteredAddress()) {
+					// t.setVariable("tradingName", trName);
+					t.setVariable("regestrationAddress", regestrationAddress);
+					t.addBlock("regestrationAddress");
+				}
+			}
+			t.addBlock("theme");
+
+			System.out.println("string......" + t.getFileString());
+			outPutString = t.getFileString();
+			// OutputStream outputstream = new ByteArrayOutputStream();
+			//
+			// java.io.InputStream inputStream = new ByteArrayInputStream(
+			// outPutString.getBytes());
+			// InputStreamReader reader = new InputStreamReader(inputStream);
+
+			// OutputStream templeteOutputStream = new
+			// create output stream
+			// then create input stream
+			// /pass that input stream to render method
+
+			// converter.generatePDF(template, sos, reader);
+
+		} catch (Exception e) {
+
+		}
+		return outPutString;
+	}
+
+	/*
+	 * For Max DecimalPoints
+	 */
+	private int getMaxDecimals(Invoice inv) {
+		String qty;
+		String max;
+		int temp = 0;
+		for (TransactionItem item : inv.getTransactionItems()) {
+			qty = String.valueOf(item.getQuantity());
+			max = qty.substring(qty.indexOf(".") + 1);
+			if (!max.equals("0")) {
+				if (temp < max.length()) {
+					temp = max.length();
+				}
+			}
+
+		}
+		return temp;
+	}
+
+	public String forUnusedAddress(String add, boolean isFooter) {
+		if (isFooter) {
+			if (add != null && !add.equals(""))
+				return ", " + add;
+		} else {
+			if (add != null && !add.equals(""))
+				return add + "<br/>";
+		}
+		return "";
+	}
+
+	public String forNullValue(String value) {
+		return value != null ? value : "";
+	}
+
+	private String getLogoAlignment() {
+		String logoAlignment = null;
+		if (brandingTheme.getPageSizeType() == 1) {
+			logoAlignment = "left";
+		} else {
+			logoAlignment = "right";
+		}
+		return logoAlignment;
+	}
+
+	private String getVendorString(String forUk, String forUs) {
+		return company.getAccountingType() == company.ACCOUNTING_TYPE_US ? forUs
+				: forUk;
+	}
+
+	private String getDecimalsUsingMaxDecimals(double quantity, String amount,
+			int maxDecimalPoint) {
+		String qty = "";
+		String max;
+		if (maxDecimalPoint != 0) {
+			if (amount == null)
+				qty = String.valueOf(quantity);
+			else
+				qty = amount;
+			max = qty.substring(qty.indexOf(".") + 1);
+			if (maxDecimalPoint > max.length()) {
+				for (int i = max.length(); maxDecimalPoint != i; i++) {
+					qty = qty + "0";
+				}
+			}
+		} else {
+			qty = String.valueOf((long) quantity);
+		}
+
+		String temp = qty.contains(".") ? qty.replace(".", "-").split("-")[0]
+				: qty;
+		return insertCommas(temp)
+				+ (qty.contains(".") ? "."
+						+ qty.replace(".", "-").split("-")[1] : "");
+	}
+
+	public String forZeroAmounts(String amount) {
+		String[] amt = amount.replace(".", "-").split("-");
+		if (amt[0].equals("0")) {
+			return "";
+		}
+		return amount;
+	}
+
+	private static String insertCommas(String str) {
+
+		if (str.length() < 4) {
+			return str;
+		}
+		return insertCommas(str.substring(0, str.length() - 3)) + ","
+				+ str.substring(str.length() - 3, str.length());
+	}
+
+	private String largeAmountConversation(double amount) {
+		String amt = Utility.decimalConversation(amount);
+		amt = getDecimalsUsingMaxDecimals(0.0, amt, 2);
+		return (amt);
+	}
+
+	public String getCompany() {
+
+		StringBuffer original = new StringBuffer();
+		// String imagesDomain = "/do/downloadFileFromFile?";
+
+		original.append("<img src='file:///");
+		original.append(ServerConfiguration.getAttachmentsDir() + "/"
+				+ company.getAccountingType() + "/"
+				+ brandingTheme.getFileName());
+		original.append("'/>");
+		System.err.println(original.toString());
+		return original.toString();
+
+	}
+}
