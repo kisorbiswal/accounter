97d95ed22e713c555fb32429528b8ba0b4568186
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-08-22 06:12:17 +0000

    Former-commit-id: dda1d30112ff36bac530443a7ab2453f27e55cfc

--- src/com/vimukti/accounter/servlets/MaintananceServlet.java
+++ src/com/vimukti/accounter/servlets/MaintananceServlet.java
@@ -19,6 +19,7 @@ public class MaintananceServlet extends BaseServlet {
 	private static final long serialVersionUID = -1163973484857078003L;
 	private static final String MAINTANANCE_VIEW = "/WEB-INF/serverMaintain.jsp";
 	private static final String UNDER_CONSTRUCTION_VIEW = "/WEB-INF/maintananceInfo.jsp";
+	private static final String LOGIN_VIEW = "/WEB-INF/login.jsp";
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -31,11 +32,21 @@ public class MaintananceServlet extends BaseServlet {
 			throws ServletException, IOException {
 
 		Session openSession = HibernateUtil.openSession(LOCAL_DATABASE);
-		ServerConfiguration.setUnderMaintainance(true);
-		// TODO updateTable(maintanance table query);
+		String paswdString = req.getParameter("adminPasswordBox");
+		if (paswdString != null
+				&& paswdString == ServerConfiguration.getAdminPassword()) {
+			if (req.getParameter("option1") != null
+					&& req.getParameter("option1").equals("on")) {
+				ServerConfiguration.setUnderMaintainance(true);
+
+				// TODO updateTable();
+			}
+		}
 
 		if (ServerConfiguration.isUnderMaintainance()) {
 			dispatch(req, resp, UNDER_CONSTRUCTION_VIEW);
+		} else {
+			dispatch(req, resp, LOGIN_VIEW);
 		}
 
 	}
