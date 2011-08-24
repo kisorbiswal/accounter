ee86d76169d53038b8cc2d9b27af4191b36d5910
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-24 06:10:50 +0000

    Former-commit-id: b210ed405268b3632879cf3821b59273f892d99a

--- src/com/vimukti/accounter/web/client/ui/company/ItemsAction.java
+++ src/com/vimukti/accounter/web/client/ui/company/ItemsAction.java
@@ -3,6 +3,7 @@ package com.vimukti.accounter.web.client.ui.company;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.ItemListView;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
@@ -17,7 +18,7 @@ public class ItemsAction extends Action {
 
 	public ItemsAction(String text) {
 		super(text);
-		this.catagory = Accounter.constants().customer();
+		this.catagory = Global.get().customer();
 	}
 
 	public ItemsAction(String text, String catageory) {
