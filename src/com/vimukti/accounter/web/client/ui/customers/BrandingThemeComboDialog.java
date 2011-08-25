fc74abcd99a51029efce6d55e070ea335e4ae28c
srinivas.g <srinivas.g@vimukti.com>
2011-08-25 05:38:55 +0000

    Former-commit-id: 66ee11bdfb96dc87901015bd52f71b0e3f7db202

--- src/com/vimukti/accounter/web/client/ui/customers/BrandingThemeComboDialog.java
+++ src/com/vimukti/accounter/web/client/ui/customers/BrandingThemeComboDialog.java
@@ -1,11 +1,14 @@
 package com.vimukti.accounter.web.client.ui.customers;
 
+import java.util.List;
+
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.core.ClientBrandingTheme;
 import com.vimukti.accounter.web.client.core.ClientInvoice;
 import com.vimukti.accounter.web.client.core.ClientTransaction;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.core.Lists.InvoicesList;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.UIUtils;
 import com.vimukti.accounter.web.client.ui.combo.BrandingThemeCombo;
@@ -17,11 +20,19 @@ public class BrandingThemeComboDialog extends BaseDialog {
 	private BrandingThemeCombo brandingThemeTypeCombo;
 	private ClientTransaction clientTransaction;
 	private ClientBrandingTheme brandingTheme;
+	private List<InvoicesList> list;
 
 	public BrandingThemeComboDialog(String title, String desc,
 			ClientTransaction clientTransaction) {
 		super(title, desc);
 		this.clientTransaction = clientTransaction;
+		createControls();
+	}
+
+	public BrandingThemeComboDialog(String title, String desc,
+			List<InvoicesList> list) {
+		super(title, desc);
+		this.list = list;
 		createControls();
 	}
 
@@ -61,16 +72,60 @@ public class BrandingThemeComboDialog extends BaseDialog {
 	}
 
 	private void print() {
+		if (list == null) {
+			// for printing individual invoice and customer credit memo pdf
+			// documents
+			if (ClientTransaction.TYPE_INVOICE == clientTransaction.getType()) {
+				UIUtils.downloadAttachment(
+						((ClientInvoice) clientTransaction).getID(),
+						ClientTransaction.TYPE_INVOICE, brandingTheme.getID());
+			} else if (ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO == clientTransaction
+					.getType()) {
+				UIUtils.downloadAttachment(clientTransaction.getID(),
+						ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO,
+						brandingTheme.getID());
+			}
+		} else {
+			// for printing multiple documents
+			StringBuffer ids = new StringBuffer();
 
-		if (ClientTransaction.TYPE_INVOICE == clientTransaction.getType()) {
-			UIUtils.downloadAttachment(
-					((ClientInvoice) clientTransaction).getID(),
-					ClientTransaction.TYPE_INVOICE, brandingTheme.getID());
-		} else if (ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO == clientTransaction
-				.getType()) {
-			UIUtils.downloadAttachment(clientTransaction.getID(),
-					ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO,
-					brandingTheme.getID());
+			for (int i = 0; i < list.size(); i++) {
+				InvoicesList invoice = list.get(i);
+
+				if (invoice.isPrint()) {
+
+					String id = String.valueOf(invoice.getTransactionId());
+
+					ids = ids.append(id + ",");
+
+				}
+			}
+
+			String[] arrayIds = ids.toString().split(",");
+			int type = 0;
+			for (int i = 0; i < list.size(); i++) {
+				InvoicesList invoicesList = list.get(i);
+				if (invoicesList.isPrint()) {
+					if (Integer.parseInt(arrayIds[0]) == invoicesList
+							.getTransactionId()) {
+
+						type = invoicesList.getType();
+					}
+				}
+
+			}
+
+			if (type == ClientTransaction.TYPE_INVOICE) {
+				UIUtils.downloadMultipleAttachment(ids.toString(),
+						ClientTransaction.TYPE_INVOICE, brandingTheme.getID());
+
+			} else if (type == ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO) {
+				UIUtils.downloadMultipleAttachment(ids.toString(),
+						ClientTransaction.TYPE_CUSTOMER_CREDIT_MEMO,
+						brandingTheme.getID());
+
+			}
+
 		}
 
 	}
