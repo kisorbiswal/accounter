de7a09fc1745bc385d25800ae33000526e221b68
srinivas.g <srinivas.g@vimukti.com>
2011-08-26 05:26:24 +0000

    Former-commit-id: ec37609e90917d8178102fb7d4d69ede6dd00e30

--- src/com/vimukti/accounter/web/client/ui/customers/InvoicePrintDialog.java
+++ src/com/vimukti/accounter/web/client/ui/customers/InvoicePrintDialog.java
@@ -3,16 +3,16 @@ package com.vimukti.accounter.web.client.ui.customers;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
 import com.vimukti.accounter.web.client.ui.core.BaseDialog;
 import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
 
 public class InvoicePrintDialog extends BaseDialog {
+	private String description;
 
-	public InvoicePrintDialog(String title, String desc) {
+	public InvoicePrintDialog(String title, String desc, String description) {
 		super(title, desc);
-
+		this.description = description;
 		createControls();
 	}
 
@@ -23,8 +23,7 @@ public class InvoicePrintDialog extends BaseDialog {
 
 	private void createControls() {
 
-		Label label = new Label(Global.get().constants()
-				.pleaseSelectReportsOfSameType());
+		Label label = new Label(description);
 
 		label.setWidth("70%");
 
