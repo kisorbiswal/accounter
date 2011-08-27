027949dae65943a634b371af4ec298a94ed6430d
srinivas.g <srinivas.g@vimukti.com>
2011-08-27 11:39:56 +0000

    Former-commit-id: 5f7fea0c72303de3e6397d3c2b31760e91efd598

--- src/com/vimukti/accounter/web/client/IAccounterHomeViewServiceAsync.java
+++ src/com/vimukti/accounter/web/client/IAccounterHomeViewServiceAsync.java
@@ -355,4 +355,10 @@ public interface IAccounterHomeViewServiceAsync {
 	void mergeAccount(ClientAccount fromClientAccount,
 			ClientAccount toClientAccount, AsyncCallback<Void> callback);
 
+	// this method is used to send Pdf as an attachment in email
+	public void sendPdfInMail(long objectID, int type, long brandingThemeId,
+			String mimeType, String subject, String content,
+			String senderEmail, String recipientEmail, String ccEmail,
+			AsyncCallback<Void> callback);
+
 }
