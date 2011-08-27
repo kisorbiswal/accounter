9ce10ca3905b99729b82ab15ff1b52939cfdd7f8
srinivas.g <srinivas.g@vimukti.com>
2011-08-27 11:43:11 +0000

    Former-commit-id: 22e620a620939ab4c14ba1dddef657867f97eed6

--- src/com/vimukti/accounter/web/client/ui/customers/EmailView.java
+++ src/com/vimukti/accounter/web/client/ui/customers/EmailView.java
@@ -2,23 +2,29 @@ package com.vimukti.accounter.web.client.ui.customers;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.core.ClientInvoice;
+import com.vimukti.accounter.web.client.core.ClientTransaction;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
 import com.vimukti.accounter.web.client.exception.AccounterException;
 import com.vimukti.accounter.web.client.externalization.AccounterConstants;
 import com.vimukti.accounter.web.client.ui.AbstractBaseView;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
 import com.vimukti.accounter.web.client.ui.UIUtils;
+import com.vimukti.accounter.web.client.ui.combo.EmailCombo;
+import com.vimukti.accounter.web.client.ui.combo.IAccounterComboSelectionChangeHandler;
 import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
 import com.vimukti.accounter.web.client.ui.forms.TextAreaItem;
 import com.vimukti.accounter.web.client.ui.forms.TextItem;
 
-public class EmailView extends AbstractBaseView {
-
+public class EmailView extends AbstractBaseView implements AsyncCallback<Void> {
+	private ClientInvoice invoice;
 	private TextItem fromAddress;
 	private TextItem toAddress;
 	private TextItem ccAddress;
@@ -26,11 +32,22 @@ public class EmailView extends AbstractBaseView {
 	private TextAreaItem emailBody;
 	private Label attachmentLabel;
 
+	private EmailCombo fromAddcombo;
+
+	String from;
 	private Button sendBtn;
 	private Button cancelBtn;
 	final DynamicForm form1 = UIUtils.form(Accounter.constants().type());
 	final DynamicForm form2 = UIUtils.form(Accounter.constants().type());
 	final DynamicForm form3 = UIUtils.form(Accounter.constants().type());
+
+	public EmailView(ClientInvoice inovoice) {
+		this.invoice = inovoice;
+	}
+
+	public EmailView() {
+
+	}
 
 	@Override
 	public void init() {
@@ -40,20 +57,22 @@ public class EmailView extends AbstractBaseView {
 	}
 
 	public void createControls() {
-		String fileName = "";
-		String date = "";
+
+		fromAddcombo = new EmailCombo("From");
+
 		String message = "Dear Customer,"
 				+ "\n\n"
-				+ " May we remind you that the"
-				+ fileName
-				+ "issued on"
-				+ date
-				+ "is due for payment."
+				+ " May we remind you that the Invoice_"
+				+ this.invoice.getNumber()
+				+ " issued on"
+				+ invoice.getDate()
+				+ " is due for payment."
 				+ "\n"
-				+ "If you have already paid for this invoice, accept our apologies and ignore this reminder."
+				+ " If you have already paid for this invoice, accept our apologies and ignore this reminder."
 				+ "\n"
-				+ "Feel free to get in touch with us for any clarifications."
-				+ "\n" + "Thanks in advance for the payment.";
+				+ " Feel free to get in touch with us for any clarifications."
+				+ "\n" + " Thanks in advance for the payment." + "\n\n\n"
+				+ "Regards";
 
 		fromAddress = new TextItem("From");
 		fromAddress.setWidth(80);
@@ -79,13 +98,15 @@ public class EmailView extends AbstractBaseView {
 		attachmentLabel = new Label(constants.attachments());
 
 		TextAreaItem attachmentItem = new TextAreaItem();
+		attachmentItem.setValue("Invoice_" + invoice.getNumber());
+		attachmentItem.setDisabled(true);
 		attachmentItem.setWidth(60);
 
 		form2.setFields(attachmentItem);
 		vPanel.add(attachmentLabel);
 		vPanel.add(form2);
 
-		form1.setFields(fromAddress, toAddress, ccAddress, subject);
+		form1.setFields(fromAddcombo, toAddress, ccAddress, subject);
 		HorizontalPanel horPanel = new HorizontalPanel();
 		horPanel.add(form1);
 		horPanel.add(vPanel);
@@ -105,9 +126,11 @@ public class EmailView extends AbstractBaseView {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				// TODO Auto-generated method stub
+
+				updateControls();
 
 			}
+
 		});
 
 		cancelBtn = new Button(constants.cancel());
@@ -115,6 +138,7 @@ public class EmailView extends AbstractBaseView {
 
 			@Override
 			public void onClick(ClickEvent event) {
+				MainFinanceWindow.getViewManager().closeCurrentView();
 
 			}
 		});
@@ -127,6 +151,41 @@ public class EmailView extends AbstractBaseView {
 		mainPanel.setWidth("100%");
 
 		this.add(mainPanel);
+
+		fromAddcombo
+				.addSelectionChangeHandler(new IAccounterComboSelectionChangeHandler<String>() {
+
+					@Override
+					public void selectedComboBoxItem(String selectItem) {
+						from = (String) selectItem;
+						System.err.println("............." + from);
+					}
+
+				});
+	}
+
+	private void updateControls() {
+
+		String fromAdd = fromAddress.getValue().toString() != null ? fromAddress
+				.getValue().toString() : "";
+		String ToAdd = toAddress.getValue().toString() != null ? toAddress
+				.getValue().toString() : "";
+		String ccAdd = ccAddress.getValue().toString() != null ? ccAddress
+				.getValue().toString() : "";
+		String sub = subject.getValue().toString() != null ? subject.getValue()
+				.toString() : "";
+		String body = emailBody.getValue().toString() != null ? fromAddress
+				.getValue().toString() : "";
+
+		long themeId = 0;
+		// UIUtils.sendPdfAttachment(((ClientInvoice) transaction).getID(),
+		// ClientTransaction.TYPE_INVOICE, themeId, "application/pdf",
+		// sub, body, from, ToAdd, ccAdd);
+		Accounter.createHomeService().sendPdfInMail(
+				((ClientInvoice) invoice).getID(),
+				ClientTransaction.TYPE_INVOICE, 1, "application/pdf", sub,
+				body, from, ToAdd, ccAdd, this);
+
 	}
 
 	@Override
@@ -144,7 +203,24 @@ public class EmailView extends AbstractBaseView {
 	@Override
 	protected String getViewTitle() {
 		// TODO Auto-generated method stub
-		return null;
+		return constants.email();
 	}
 
+	@Override
+	public void processupdateView(IAccounterCore core, int command) {
+		// TODO Auto-generated method stub
+		super.processupdateView(core, command);
+	}
+
+	@Override
+	public void onFailure(Throwable caught) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSuccess(Void result) {
+		// TODO Auto-generated method stub
+
+	}
 }
