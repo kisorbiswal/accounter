a34c901d8cc9b3e004ca48d916aec2cffab4a436
sravanthi.v <sravanthi.v@vimukti.com>
2011-08-27 12:38:32 +0000

    Former-commit-id: f85b9418df37b0470e443a62bfc39a1f7bb62c6d

--- src/com/vimukti/accounter/web/client/ui/companypreferences/panels/BankingAndOtherFinancialDetailsPanel.java
+++ src/com/vimukti/accounter/web/client/ui/companypreferences/panels/BankingAndOtherFinancialDetailsPanel.java
@@ -1,13 +1,77 @@
 package com.vimukti.accounter.web.client.ui.companypreferences.panels;
 
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.combo.CurrencyCombo;
+import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
+import com.vimukti.accounter.web.client.ui.forms.TextItem;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 
 public class BankingAndOtherFinancialDetailsPanel extends VerticalPanel {
+	private TextItem bankNameText, bankAccountNoText, sortCodeText,
+			companyRegistrationNoText, vatRegistartionNoText, federalTaxIDText;
+	private CurrencyCombo primaryCurrencyCombo;
+
+	private DynamicForm bankAndFinanceForm;
 
 	public BankingAndOtherFinancialDetailsPanel(
 			ClientCompanyPreferences companyPreferences) {
-		// TODO Auto-generated constructor stub
+		createControls();
+	}
+
+	private void createControls() {
+
+		bankNameText = new TextItem(Accounter.constants().bankName());
+		bankNameText.setHelpInformation(true);
+		bankNameText.setRequired(false);
+		bankNameText.setWidth(100);
+
+		bankAccountNoText = new TextItem(Accounter.messages().bankAccountNo(
+				Global.get().Account()));
+		bankAccountNoText.setHelpInformation(true);
+		bankAccountNoText.setRequired(false);
+		bankAccountNoText.setWidth(100);
+
+		sortCodeText = new TextItem(Accounter.constants().sortCode());
+		sortCodeText.setHelpInformation(true);
+		sortCodeText.setRequired(false);
+		sortCodeText.setWidth(100);
+
+		companyRegistrationNoText = new TextItem(Accounter.constants()
+				.companyRegistrationNumber());
+		companyRegistrationNoText.setHelpInformation(true);
+		companyRegistrationNoText.setRequired(false);
+		companyRegistrationNoText.setWidth(100);
+
+		vatRegistartionNoText = new TextItem(Accounter.constants()
+				.vatRegistrationNumber());
+		vatRegistartionNoText.setHelpInformation(true);
+		vatRegistartionNoText.setRequired(false);
+		vatRegistartionNoText.setWidth(100);
+
+		federalTaxIDText = new TextItem(Accounter.constants().federalTaxId());
+		federalTaxIDText.setHelpInformation(true);
+		federalTaxIDText.setRequired(false);
+		federalTaxIDText.setWidth(100);
+
+		primaryCurrencyCombo = new CurrencyCombo(Accounter.constants()
+				.primaryCurrency());
+		primaryCurrencyCombo.setHelpInformation(true);
+
+		bankAndFinanceForm = new DynamicForm();
+		bankAndFinanceForm.setWidth("80%");
+		bankAndFinanceForm.getCellFormatter().setWidth(0, 0, "225px");
+		bankAndFinanceForm.getCellFormatter().addStyleName(1, 0,
+				"memoFormAlign");
+		bankAndFinanceForm.getCellFormatter().addStyleName(2, 0,
+				"memoFormAlign");
+
+		bankAndFinanceForm.setFields(bankNameText, bankAccountNoText,
+				sortCodeText, companyRegistrationNoText, vatRegistartionNoText,
+				federalTaxIDText, primaryCurrencyCombo);
+		this.add(bankAndFinanceForm);
+
 	}
 
 }
--- src/com/vimukti/accounter/web/client/ui/companypreferences/panels/CompanyRegisteredeDetailsPanel.java
+++ src/com/vimukti/accounter/web/client/ui/companypreferences/panels/CompanyRegisteredeDetailsPanel.java
@@ -1,13 +1,97 @@
 package com.vimukti.accounter.web.client.ui.companypreferences.panels;
 
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
+import com.vimukti.accounter.web.client.ui.forms.TextItem;
 
 public class CompanyRegisteredeDetailsPanel extends VerticalPanel {
 
+	private TextItem registeredCompanyName, address1Text, address2Text,
+			cityText, stateText, postalcodeText, countryText;
+
 	public CompanyRegisteredeDetailsPanel(
 			ClientCompanyPreferences companyPreferences) {
-		// TODO Auto-generated constructor stub
+		createControls();
+	}
+
+	private void createControls() {
+
+		registeredCompanyName = new TextItem(Accounter.constants()
+				.registeredCompanyName());
+		registeredCompanyName.setHelpInformation(true);
+		registeredCompanyName.setRequired(false);
+		registeredCompanyName.setWidth(100);
+
+		address1Text = new TextItem(Accounter.constants().address1());
+		address1Text.setHelpInformation(true);
+		address1Text.setRequired(false);
+		address1Text.setWidth(100);
+
+		address2Text = new TextItem(Accounter.constants().address2());
+		address2Text.setHelpInformation(true);
+		address2Text.setRequired(false);
+		address2Text.setWidth(100);
+
+		cityText = new TextItem(Accounter.constants().city());
+		cityText.setHelpInformation(true);
+		cityText.setRequired(false);
+		cityText.setWidth(100);
+
+		stateText = new TextItem(Accounter.constants().state());
+		stateText.setHelpInformation(true);
+		stateText.setRequired(false);
+		stateText.setWidth(100);
+
+		postalcodeText = new TextItem(Accounter.constants().postalCode());
+		postalcodeText.setHelpInformation(true);
+		postalcodeText.setRequired(false);
+		postalcodeText.setWidth(100);
+
+		countryText = new TextItem(Accounter.constants().postalCode());
+		countryText.setHelpInformation(true);
+		countryText.setRequired(false);
+		countryText.setWidth(100);
+
+		DynamicForm companyForm = new DynamicForm();
+		companyForm.setWidth("80%");
+		companyForm.getCellFormatter().setWidth(0, 0, "225px");
+		companyForm.getCellFormatter().addStyleName(1, 0, "memoFormAlign");
+		companyForm.getCellFormatter().addStyleName(2, 0, "memoFormAlign");
+
+		Label addressLabel = new Label(Accounter.constants()
+				.registeredAddress());
+
+		companyForm.setFields(registeredCompanyName);
+
+		DynamicForm addressForm = new DynamicForm();
+
+		addressForm.setWidth("80%");
+		addressForm.getCellFormatter().setWidth(0, 0, "225px");
+		addressForm.getCellFormatter().addStyleName(1, 0, "memoFormAlign");
+		addressForm.getCellFormatter().addStyleName(2, 0, "memoFormAlign");
+
+		addressForm.setFields(address1Text, address2Text, cityText, stateText,
+				postalcodeText, countryText);
+
+		HorizontalPanel companyHorzPanel = new HorizontalPanel();
+		companyHorzPanel.setWidth("100%");
+		companyHorzPanel.add(companyForm);
+
+		HorizontalPanel addressHorzPanel = new HorizontalPanel();
+		addressHorzPanel.setWidth("100%");
+		addressHorzPanel.add(addressLabel);
+		addressHorzPanel.add(addressForm);
+
+		VerticalPanel mainPanel = new VerticalPanel();
+		mainPanel.add(companyHorzPanel);
+		mainPanel.add(addressHorzPanel);
+
+		this.add(mainPanel);
+
 	}
 
 }
