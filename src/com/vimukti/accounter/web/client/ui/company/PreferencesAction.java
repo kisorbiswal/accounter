cc683666631e260eb1380057ebaab8e6b266347a
Varakumar Pjd <varakumar.pjd@vimukti.com>
2011-08-29 13:13:27 +0000

    Former-commit-id: e6d06fe71ebdc9c0e1e48f9afb69e11b54e200f9

--- src/com/vimukti/accounter/web/client/ui/company/PreferencesAction.java
+++ src/com/vimukti/accounter/web/client/ui/company/PreferencesAction.java
@@ -3,11 +3,10 @@ package com.vimukti.accounter.web.client.ui.company;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
-import com.vimukti.accounter.web.client.ui.companypreferences.panels.CompanyInfoView;
 import com.vimukti.accounter.web.client.ui.core.Action;
 
 public class PreferencesAction extends Action {
-	private CompanyInfoView view;
+	private CompanyPreferencesView view;
 
 	public PreferencesAction(String text) {
 		super(text);
@@ -18,7 +17,7 @@ public class PreferencesAction extends Action {
 	public void run() {
 
 		try {
-			view = new CompanyInfoView();
+			view = new CompanyPreferencesView();
 			MainFinanceWindow.getViewManager().showView(view, null, true,
 					PreferencesAction.this);
 		} catch (Exception e) {
