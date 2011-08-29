3d75cc92a86c9418e9cf508ac3c9b140d1391658
Amrit Mishra <amrit.mishra@vimukti.com>
2011-08-29 13:12:13 +0000

    Former-commit-id: 864e8aac57f2ec3a4fa6d6246432913b54f926a3

--- src/com/vimukti/accounter/web/client/ui/company/NewBudgetAction.java
+++ src/com/vimukti/accounter/web/client/ui/company/NewBudgetAction.java
@@ -1,51 +1,84 @@
 package com.vimukti.accounter.web.client.ui.company;
 
-import java.util.List;
-
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.AccounterAsyncCallback;
+import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.ClientAccount;
+import com.vimukti.accounter.web.client.core.ClientCustomer;
 import com.vimukti.accounter.web.client.ui.Accounter;
-import com.vimukti.accounter.web.client.ui.NewAccountView;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
 import com.vimukti.accounter.web.client.ui.core.Action;
 
 public class NewBudgetAction extends Action<ClientAccount> {
 
-	private List<Integer> accountTypes;
-	protected NewAccountView view;
+	NewBudgetView view;
 
-	public NewBudgetAction(String newCustomer) {
-		super(newCustomer);
-		this.catagory = Accounter.constants().company();
+	public NewBudgetAction(String text) {
+		super(text);
+		this.catagory = Global.get().Budget();
+		super.setToolTip(Global.get().Budget());
 	}
 
-	@Override
+	public NewBudgetAction(String text, ClientCustomer customer,
+			AccounterAsyncCallback<Object> callback) {
+		super(text);
+		this.catagory = Global.get().Budget();
+	}
+
 	public void run() {
-		// TODO Auto-generated method stub
-
+		runAsync(data, isDependent);
 	}
 
-	@Override
+	public void runAsync(final Object data, final Boolean isDependent) {
+
+		GWT.runAsync(new RunAsyncCallback() {
+
+			@Override
+			public void onSuccess() {
+				view = new NewBudgetView();
+				MainFinanceWindow.getViewManager().showView(view, data,
+						isDependent, NewBudgetAction.this);
+
+			}
+
+			@Override
+			public void onFailure(Throwable arg0) {
+				Accounter
+						.showError(Accounter.constants().unableToshowtheview());
+
+			}
+		});
+	}
+
+	// @Override
+	// public ParentCanvas getView() {
+	// return this.view;
+	// }
+
 	public ImageResource getBigImage() {
-		// TODO Auto-generated method stub
+		// NOTHING TO DO.
 		return null;
 	}
 
-	@Override
 	public ImageResource getSmallImage() {
-		// TODO Auto-generated method stub
-		return null;
+		return Accounter.getFinanceMenuImages().newCustomer();
 	}
+
+	// @Override
+	// public String getImageUrl() {
+	// return "/images/new_customer.png";
+	// }
 
 	@Override
 	public String getHistoryToken() {
-		// TODO Auto-generated method stub
-		return null;
+		return "newBudget";
 	}
 
 	@Override
 	public String getHelpToken() {
-		// TODO Auto-generated method stub
-		return null;
+		return "add-Budget";
 	}
 
 }
