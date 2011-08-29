6da4b8b87f14be4122dc031038ad2d053d309005
Amrit Mishra <amrit.mishra@vimukti.com>
2011-08-29 07:03:14 +0000

    Former-commit-id: 0aaf7bf95c8124fba6cb606d7449c80c043bc5fb

--- src/com/vimukti/accounter/core/Budget.java
+++ src/com/vimukti/accounter/core/Budget.java
@@ -1,30 +1,33 @@
 package com.vimukti.accounter.core;
 
+import com.vimukti.accounter.web.client.exception.AccounterException;
 
-public class Budget {
+public class Budget implements IAccounterServerCore {
 
-	long id;
+	private static final long serialVersionUID = 1L;
 
-	/**
-	 * The month for which we are planning to make budget.
-	 */
-	int month;
+	private long id;
 
-	/**
-	 * The amount by which we create the budget for the specific period.
-	 */
-	double amount;
+	private String budgetName;
 
-	/**
-	 * The account in which we are making the budget amount to be deposited.
-	 */
-	Account account;
+	private Customer customer;
 
-	
-	String createdBy;
-	String lastModifier;
-	String createdDate;
-	String lastModifiedDate;
+	private Account account;
+
+	private double januaryAmount;
+	private double febrauaryAmount;
+	private double marchAmount;
+	private double aprilAmount;
+	private double mayAmount;
+	private double juneAmount;
+	private double julyAmount;
+	private double augustAmount;
+	private double septemberAmount;
+	private double octoberAmount;
+	private double novemberAmount;
+	private double decemberAmount;
+
+	private int version;
 
 	/**
 	 * @return the id
@@ -37,38 +40,24 @@ public class Budget {
 	 * @param id
 	 *            the id to set
 	 */
-	public void setID(long id){
+	public void setID(long id) {
 		this.id = id;
 	}
 
-	/**
-	 * @return the month
-	 */
-	public int getMonth() {
-		return month;
+	public String getBudgetName() {
+		return budgetName;
 	}
 
-	/**
-	 * @return the amount
-	 */
-	public double getAmount() {
-		return amount;
+	public void setBudgetName(String budgetname) {
+		this.budgetName = budgetname;
 	}
 
-	/**
-	 * @param month
-	 *            the month to set
-	 */
-	public void setMonth(int month) {
-		this.month = month;
+	public Customer getCustomer() {
+		return customer;
 	}
 
-	/**
-	 * @param amount
-	 *            the amount to set
-	 */
-	public void setAmount(double amount) {
-		this.amount = amount;
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 
 	/**
@@ -86,4 +75,213 @@ public class Budget {
 		this.account = account;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public double getJanuaryAmount() {
+		return januaryAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setJanuaryAmount(double amount) {
+		this.januaryAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getFebruaryAmount() {
+		return febrauaryAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setFebruaryAmount(double amount) {
+		this.febrauaryAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getMarchAmount() {
+		return marchAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setMarchAmount(double amount) {
+		this.marchAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getAprilAmount() {
+		return aprilAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setAprilAmount(double amount) {
+		this.aprilAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getMayAmount() {
+		return mayAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setMayAmount(double amount) {
+		this.mayAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getJuneAmount() {
+		return juneAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setJuneAmount(double amount) {
+		this.juneAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getJulyAmount() {
+		return julyAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setJulyAmount(double amount) {
+		this.julyAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getAugustAmount() {
+		return augustAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setAugustAmount(double amount) {
+		this.augustAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getSpetemberAmount() {
+		return septemberAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setSeptemberAmount(double amount) {
+		this.septemberAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getOctoberAmount() {
+		return octoberAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setOctoberAmount(double amount) {
+		this.octoberAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getNovemberAmount() {
+		return novemberAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setNovemberAmount(double amount) {
+		this.novemberAmount = amount;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public double getDecemberAmount() {
+		return decemberAmount;
+	}
+
+	/**
+	 * 
+	 * @param amount
+	 */
+	public void setDecemberAmount(double amount) {
+		this.decemberAmount = amount;
+	}
+
+	@Override
+	public int getVersion() {
+		return version;
+	}
+
+	@Override
+	public void setVersion(int version) {
+		this.version = version;
+
+	}
+
+	@Override
+	public boolean canEdit(IAccounterServerCore clientObject)
+			throws AccounterException {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
