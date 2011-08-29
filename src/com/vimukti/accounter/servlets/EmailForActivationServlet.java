77fe6df2f3036be4cf667e5b11794acb7b944fb3
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-08-29 10:32:02 +0000

    Former-commit-id: e84aeb1fe4dac1dc78f1fc328892abf267c41476

--- src/com/vimukti/accounter/servlets/EmailForActivationServlet.java
+++ src/com/vimukti/accounter/servlets/EmailForActivationServlet.java
@@ -26,49 +26,47 @@ public class EmailForActivationServlet extends BaseServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String email = req.getParameter("emailid");
-		
-		//Getting activation object using the mail id
+
+		// Getting activation object using the mail id
 		Activation activation = getActivationByEmailId(email);
-		
-		if(activation == null){
-			//send error message
-			req.setAttribute("errormessage", "Invalid email id, please enter the email id that you used during sign up process.");
+
+		if (activation == null) {
+			// send error message
+			req.setAttribute(
+					"errormessage",
+					"Invalid email id, please enter the email id that you used during sign up process.");
 			dispatch(req, resp, VIEW);
-		}else{
+		} else {
 			String token = activation.getToken();
 			Client client = getClient(email);
 			// Email to that user.
 			sendActivationEmail(token, client);
 			// Send to SignUp Success View
-			req.setAttribute(
-					"successmessage",
-					"Your activation code is sent to your mail id. Please check your email and enter your activation code below to activate your account.");
-			
-			//redirect to activation page
-			req.getRequestDispatcher(ACTIVATION_URL).include(req, resp);
-			
+
+			// redirect to activation page
+			redirectExternal(req, resp, ACTIVATION_URL + "?message=110");
+
 		}
-		
 
 	}
 
 	private Activation getActivationByEmailId(String email) {
 		Session session = HibernateUtil.getCurrentSession();
-		if(session == null){
+		if (session == null) {
 			session = HibernateUtil.openSession(LOCAL_DATABASE);
 		}
-		try{
-		if (session != null) {
-			if(!session.isOpen())
-				session = HibernateUtil.openSession(LOCAL_DATABASE);
-			Query query = session
-					.getNamedQuery("get.activation.by.emailid");
-			query.setParameter("emailId", email);
-			Activation val = (Activation)query.uniqueResult();
-			return val;
+		try {
+			if (session != null) {
+				if (!session.isOpen())
+					session = HibernateUtil.openSession(LOCAL_DATABASE);
+				Query query = session
+						.getNamedQuery("get.activation.by.emailid");
+				query.setParameter("emailId", email);
+				Activation val = (Activation) query.uniqueResult();
+				return val;
 
-		}
-		}catch(Exception e){
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
