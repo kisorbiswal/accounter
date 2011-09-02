330dd99c88b64fc32859728c2cbbc0058397b4cf
Devaraju K <devaraju.k@vimukti.com>
2011-09-02 10:52:08 +0000

    Former-commit-id: eb2026b0e943f849e665078d57cae41101e25467

---
+++ src/com/vimukti/accounter/web/client/ui/serverreports/SalesByLocationsummaryServerReport.java
@@ -0,0 +1,105 @@
+package com.vimukti.accounter.web.client.ui.serverreports;
+
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.client.core.reports.SalesByLocationSummary;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.reports.IFinanceReport;
+
+public class SalesByLocationsummaryServerReport extends
+		AbstractFinaneReport<SalesByLocationSummary> {
+	private String sectionName = "";
+	private String currentsectionName = "";
+	private double accountBalance = 0.0D;
+
+	public SalesByLocationsummaryServerReport(
+			IFinanceReport<SalesByLocationSummary> reportView) {
+		this.columnstoHide.add(3);
+		this.columnstoHide.add(5);
+		this.reportView = reportView;
+	}
+
+	public SalesByLocationsummaryServerReport(long startDate, long endDate,
+			int generationType) {
+		super(startDate, endDate, generationType);
+		this.columnstoHide.add(3);
+		this.columnstoHide.add(5);
+	}
+
+	@Override
+	public String[] getDynamicHeaders() {
+		return new String[] { "", Global.get().constants().total() };
+	}
+
+	@Override
+	public String getTitle() {
+		return Accounter.messages().salesByLocationSummary(
+				Global.get().Location());
+	}
+
+	@Override
+	public String[] getColunms() {
+		return new String[] { "", Global.get().constants().total() };
+	}
+
+	@Override
+	public int[] getColumnTypes() {
+		return new int[] { COLUMN_TYPE_TEXT, COLUMN_TYPE_AMOUNT };
+	}
+
+	@Override
+	public void processRecord(SalesByLocationSummary record) {
+		if (sectionDepth == 0) {
+			addSection(new String[] { "" },
+					new String[] {  getConstants().total() },
+					new int[] { 1 });
+		} else if (sectionDepth == 1) {
+			return;
+		}
+		// Go on recursive calling if we reached this place
+		processRecord(record);
+	}
+
+	@Override
+	public int getColumnWidth(int index) {
+		switch (index) {
+		case 0:
+			return 150;
+		case 1:
+			return 300;
+		}
+		return -1;
+	}
+
+	@Override
+	public Object getColumnData(SalesByLocationSummary record, int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			return getRecordSectionName(record);
+		case 1:
+			return record.getTotal();
+		}
+		return null;
+	}
+
+	private String getRecordSectionName(SalesByLocationSummary record) {
+		return record.getLocationName() == null ? "Not Specified" : record
+				.getLocationName();
+	}
+
+	@Override
+	public ClientFinanceDate getStartDate(SalesByLocationSummary obj) {
+		return obj.getStartDate();
+	}
+
+	@Override
+	public ClientFinanceDate getEndDate(SalesByLocationSummary obj) {
+		return obj.getEndDate();
+	}
+
+	@Override
+	public void makeReportRequest(long start, long end) {
+		// TODO Auto-generated method stub
+	}
+
+}
