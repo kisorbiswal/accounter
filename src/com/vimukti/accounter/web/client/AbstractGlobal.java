26fcdb0a8737613d2de0796796d58aae2a2f2688
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-02 05:57:55 +0000

    Former-commit-id: 4f3ab371237631ac51e3de31c2d5d2b4d2c4a53d

--- src/com/vimukti/accounter/web/client/AbstractGlobal.java
+++ src/com/vimukti/accounter/web/client/AbstractGlobal.java
@@ -6,7 +6,6 @@ package com.vimukti.accounter.web.client;
 import com.vimukti.accounter.web.client.core.ClientAccount;
 import com.vimukti.accounter.web.client.core.ClientCustomer;
 import com.vimukti.accounter.web.client.core.ClientVendor;
-import com.vimukti.accounter.web.client.ui.Accounter;
 
 /**
  * @author Prasanna Kumar G
@@ -29,7 +28,7 @@ public abstract class AbstractGlobal implements IGlobal {
 		case ClientCustomer.GUEST:
 			return constants().Guest().trim();
 		case ClientCustomer.PATITEINT:
-			return constants().patitent().trim();
+			return constants().Patitent().trim();
 		default:
 			return constants().Customer().trim();
 		}
@@ -44,7 +43,7 @@ public abstract class AbstractGlobal implements IGlobal {
 		case ClientCustomer.CLIENT:
 			return constants().client().trim();
 		case ClientCustomer.TENANT:
-			return constants().Tenant().trim();
+			return constants().tenant().trim();
 		case ClientCustomer.DONAR:
 			return constants().donar().trim();
 		case ClientCustomer.GUEST:
@@ -74,11 +73,11 @@ public abstract class AbstractGlobal implements IGlobal {
 		int referCustomers = preferences().getReferAccounts();
 		switch (referCustomers) {
 		case ClientAccount.ACCOUNT:
-			return Accounter.constants().account().trim();
+			return constants().account().trim();
 		case ClientAccount.LEGAND:
 			return constants().ledger().trim();
 		default:
-			return constants().Account().trim();
+			return constants().account().trim();
 		}
 	}
 
