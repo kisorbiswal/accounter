56ee7c3b4c76abe8ce420743ec5da83744527b86
Devaraju K <devaraju.k@vimukti.com>
2011-09-02 10:51:43 +0000

    Former-commit-id: ba15c278653ca7345dd506d57d41b75e6bd31557

---
+++ src/com/vimukti/accounter/web/client/ui/reports/SalesByLocationSummaryAction.java
@@ -0,0 +1,65 @@
+package com.vimukti.accounter.web.client.ui.reports;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.core.reports.SalesByLocationSummary;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallback;
+
+public class SalesByLocationSummaryAction extends Action {
+
+	public SalesByLocationSummaryAction(String text) {
+		super(text);
+	}
+
+	@Override
+	public void run() {
+		runAsync(data, isDependent);
+	}
+
+	private void runAsync(final Object data, final boolean isDependent) {
+
+		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+
+			public void onCreated() {
+
+				AbstractReportView<SalesByLocationSummary> report = new SalesByLocationsummaryReport();
+				MainFinanceWindow.getViewManager().showView(report, data,
+						isDependent, SalesByLocationSummaryAction.this);
+
+			}
+
+			public void onCreateFailed(Throwable t) {
+				// UIUtils.logError("Failed to Load Report...", t);
+			}
+		});
+
+	}
+
+	@Override
+	public ImageResource getBigImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageResource getSmallImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryToken() {
+		return Accounter.messages().getSalesByLocationDetails(
+				Global.get().Location());
+	}
+
+	@Override
+	public String getHelpToken() {
+		return "sales-by-location-summary";
+	}
+
+}
