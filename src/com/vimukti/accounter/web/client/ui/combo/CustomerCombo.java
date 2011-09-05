c5fdb9eb4f6856963dcf630259115cf6c12c0f88
srikanthj <srikanthj@vimukti.com>
2011-09-05 12:55:47 +0000

    Former-commit-id: b335c26db04c4e1868bd359a3641b274a4af31f3

--- src/com/vimukti/accounter/web/client/ui/combo/CustomerCombo.java
+++ src/com/vimukti/accounter/web/client/ui/combo/CustomerCombo.java
@@ -2,26 +2,31 @@ package com.vimukti.accounter.web.client.ui.combo;
 
 import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.ClientCustomer;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.exception.AccounterException;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.ISaveCallback;
 import com.vimukti.accounter.web.client.ui.core.ActionCallback;
 import com.vimukti.accounter.web.client.ui.core.ActionFactory;
+import com.vimukti.accounter.web.client.ui.core.QuickAddDialog;
+import com.vimukti.accounter.web.client.ui.core.QuickAddDialog.QuickAddListener;
 import com.vimukti.accounter.web.client.ui.customers.NewCustomerAction;
 
 public class CustomerCombo extends CustomCombo<ClientCustomer> {
 
 	public CustomerCombo(String title) {
 		super(title);
-		super.setToolTip(Accounter.messages().selectWhichWeHaveInOurCompanyOrAddNew(
-				Global.get().Customer()));
+		super.setToolTip(Accounter.messages()
+				.selectWhichWeHaveInOurCompanyOrAddNew(Global.get().Customer()));
 		initCombo(getCompany().getCustomers());
-
 	}
 
 	public CustomerCombo(String title, boolean isAddNewRequire) {
 		super(title, isAddNewRequire, 1);
 		if (isAddNewRequire)
-			super.setToolTip(Accounter.messages().selectWhichWeHaveInOurCompanyOrAddNew(
-					Global.get().Customer()));
+			super.setToolTip(Accounter.messages()
+					.selectWhichWeHaveInOurCompanyOrAddNew(
+							Global.get().Customer()));
 		initCombo(getCompany().getCustomers());
 	}
 
@@ -62,4 +67,50 @@ public class CustomerCombo extends CustomCombo<ClientCustomer> {
 		return null;
 	}
 
+
+	@Override
+	protected void selectionFaildOnClose() {
+		QuickAddDialog dialog = new QuickAddDialog("New Customer");
+		dialog.setDefaultText(textBox.getText());
+		dialog.setListener(new QuickAddListenerImpl());
+		dialog.show();
+	}
+
+	private class QuickAddListenerImpl implements
+			QuickAddListener<ClientCustomer> {
+
+		@Override
+		public void onAddAllInfo(String text) {
+			NewCustomerAction action = ActionFactory.getNewCustomerAction(text);
+			action.setCallback(new ActionCallback<ClientCustomer>() {
+
+				@Override
+				public void actionResult(ClientCustomer result) {
+					if (result.getDisplayName() != null)
+						addItemThenfireEvent(result);
+				}
+			});
+			action.run(null, true);
+		}
+
+		@Override
+		public void saveFailed(AccounterException exception) {
+			Accounter.showError(exception.getMessage());
+		}
+
+		@Override
+		public void saveSuccess(IAccounterCore object) {
+			if (object instanceof ClientCustomer) {
+				addItemThenfireEvent((ClientCustomer) object);
+			}
+		}
+
+		@Override
+		public ClientCustomer getData(String text) {
+			ClientCustomer clientCustomer = new ClientCustomer();
+			clientCustomer.setName(text);
+			return clientCustomer;
+		}
+
+	}
 }
