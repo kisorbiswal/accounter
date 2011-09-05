2693bc39e10fc17178e894aaed328da03b9532b2
Amrit Mishra <amrit.mishra@vimukti.com>
2011-09-05 10:48:08 +0000

    Former-commit-id: 5333f89c42d2a8083692f421ffb8a25269009eef

--- src/com/vimukti/accounter/web/client/ui/BudgetListView.java
+++ src/com/vimukti/accounter/web/client/ui/BudgetListView.java
@@ -1,10 +1,15 @@
 package com.vimukti.accounter.web.client.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.google.gwt.user.client.Window;
 import com.vimukti.accounter.web.client.Global;
-import com.vimukti.accounter.web.client.core.Lists.BudgetList;
+import com.vimukti.accounter.web.client.core.ClientBudget;
+import com.vimukti.accounter.web.client.core.ClientBudgetItem;
 import com.vimukti.accounter.web.client.externalization.AccounterConstants;
+import com.vimukti.accounter.web.client.ui.combo.IAccounterComboSelectionChangeHandler;
+import com.vimukti.accounter.web.client.ui.combo.SelectCombo;
 import com.vimukti.accounter.web.client.ui.core.Action;
 import com.vimukti.accounter.web.client.ui.core.ActionFactory;
 import com.vimukti.accounter.web.client.ui.core.BaseListView;
@@ -15,16 +20,24 @@ import com.vimukti.accounter.web.client.ui.grids.BudgetListGrid;
  * @author Amrit Mishra
  * 
  */
-public class BudgetListView extends BaseListView<BudgetList> {
+public class BudgetListView extends BaseListView<ClientBudget> {
 
 	AccounterConstants customerConstants;
-	private List<BudgetList> listOfBudgets;
+
+	SelectCombo currentView;
+
+	BudgetListGrid gridView = new BudgetListGrid();
+
+	List<ClientBudget> listOfBudgets = new ArrayList<ClientBudget>();
 
 	@Override
 	protected void initGrid() {
 		grid = new BudgetListGrid();
 		grid.init();
-
+		currentView.setSelectedItem(1);
+		initGridData(currentView.getSelectedValue(),
+				listOfBudgets.get(currentView.getSelectedIndex()));
+		Window.scrollTo(0, 0);
 	}
 
 	@Override
@@ -55,14 +68,68 @@ public class BudgetListView extends BaseListView<BudgetList> {
 
 	@Override
 	protected String getViewTitle() {
-		return Accounter.messages().customers(Global.get().Customer());
+		return Accounter.messages()
+				.customers(Global.get().constants().budget());
 
 	}
 
 	@Override
-	public void updateInGrid(BudgetList objectTobeModified) {
+	public void updateInGrid(ClientBudget objectTobeModified) {
 		// TODO Auto-generated method stub
 
 	}
 
+	@Override
+	protected SelectCombo getSelectItem() {
+		currentView = new SelectCombo(Accounter.constants().currentBudget());
+		currentView.setHelpInformation(true);
+
+		listOfBudgets = getCompany().getBudget();
+
+		listOfTypes = new ArrayList<String>();
+		for (ClientBudget budgets : listOfBudgets) {
+			listOfTypes.add(budgets.getBudgetName());
+		}
+
+		currentView.initCombo(listOfTypes);
+
+		if (UIUtils.isMSIEBrowser())
+			currentView.setWidth("150px");
+
+		currentView
+				.addSelectionChangeHandler(new IAccounterComboSelectionChangeHandler<String>() {
+
+					@Override
+					public void selectedComboBoxItem(String selectItem) {
+						if (currentView.getSelectedValue() != null) {
+							grid.setViewType(currentView.getSelectedValue());
+							filterList(currentView.getSelectedValue(),
+									listOfBudgets.get(currentView
+											.getSelectedIndex()));
+						}
+					}
+				});
+		return currentView;
+	}
+
+	private void initGridData(String selectedValue, ClientBudget clientBudget) {
+		grid.removeAllRecords();
+		List<ClientBudgetItem> allBudgetItems = clientBudget.getBudgetItem();
+		for (ClientBudgetItem item : allBudgetItems) {
+			item.setAccountsName(item.getAccount().getName());
+			grid.addData(item);
+		}
+
+	}
+
+	protected void filterList(String selectedValue, ClientBudget clientBudget) {
+
+		grid.removeAllRecords();
+		List<ClientBudgetItem> allBudgetItems = clientBudget.getBudgetItem();
+		for (ClientBudgetItem item : allBudgetItems) {
+			item.setAccountsName(item.getAccount().getName());
+			grid.addData(item);
+		}
+
+	}
 }
