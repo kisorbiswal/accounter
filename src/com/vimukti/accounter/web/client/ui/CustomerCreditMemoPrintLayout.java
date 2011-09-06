55f33f41a04edd20b351153dab2dc837559fa315
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-06 05:23:01 +0000

    Former-commit-id: f0416aa5e4be3c14c5499a04b653b40a085c7647

--- src/com/vimukti/accounter/web/client/ui/CustomerCreditMemoPrintLayout.java
+++ src/com/vimukti/accounter/web/client/ui/CustomerCreditMemoPrintLayout.java
@@ -53,209 +53,214 @@ public class CustomerCreditMemoPrintLayout extends VerticalPanel {
 	}
 
 	public void createTemplate() {
-		VerticalPanel mainVPanel = new VerticalPanel();
-		PrintTemplateUtils util = new PrintTemplateUtils();
-
-		List<String> removeHeaderBackground = new ArrayList<String>();
-
-		HTML lab1 = new HTML(Accounter.messages().creaditHTML());
-
-		HorizontalPanel labeldateNoLayout = new HorizontalPanel();
-		labeldateNoLayout.setSize("auto", "100%");
-		labeldateNoLayout.add(lab1);
-
-		Map<String, String> dateNumMap = getMap(new String[] {
-				Accounter.messages().customerName(Global.get().Customer()),
-				Accounter.getCompany().getCustomer(creditMemo.getCustomer())
-						.getName(),
-				Accounter.constants().creditDate(),
-				UIUtils.dateFormat(new ClientFinanceDate(creditMemo
-						.getTransactionDate())),
-				Accounter.constants().creditNo(), creditMemo.getNumber() + "" });
-
-		HorizontalPanel datepanel = new HorizontalPanel();
-		datepanel.setSize("300px", "150px");
-		FlexTable table = util.getWidget1(3, 2, dateNumMap, true,
-				removeHeaderBackground);
-		table.setSize("100%", "100%");
-		datepanel.add(table);
-
-		HorizontalPanel adressHPanel = new HorizontalPanel();
-		adressHPanel.setSize("100%", "100%");
-		adressHPanel.setSpacing(10);
-		adressHPanel.getElement().setAttribute("cellpaddding", "1");
-
-		ClientCustomer customer = Accounter.getCompany().getCustomer(
-				creditMemo.getCustomer());
-		String billAdrs = "<br/><br/><br/><br/><br/>";
-		Set<ClientAddress> bill = customer.getAddress();
-		for (ClientAddress add : bill) {
-			if (add.getType() == ClientAddress.TYPE_BILL_TO) {
-				if (add != null) {
-					billAdrs = "<p align=\"left\"><font size=\"3\">"
-							+ add.getAddress1() + "<br/>" + add.getStreet()
-							+ "<br/>" + add.getCity() + "<br/>"
-							+ add.getStateOrProvinence() + "<br/>"
-							+ add.getZipOrPostalCode() + "<br/>"
-							+ add.getCountryOrRegion() + "</font></p>";
-				}
-			}
-		}
-
-		Map<String, String> billAdrsMap = getMap(Accounter.messages()
-				.customerBillTo(Global.get().Customer()), billAdrs);
-		FlexTable billToTable = util.getWidget(2, 1, billAdrsMap, true);
-		billToTable.setSize("280px", "150px");
-		adressHPanel.add(billToTable);
-		adressHPanel.setCellHorizontalAlignment(billToTable,
-				HasHorizontalAlignment.ALIGN_LEFT);
-
-		HorizontalPanel reasonPanel = new HorizontalPanel();
-		reasonPanel.setSize("100%", "100%");
-
-		String memo = creditMemo.getMemo() + "<br/>";
-		Map<String, String> reasonMap = getMap(Accounter.constants()
-				.reasonForIssue(), memo);
-		FlexTable reasonTabel = util.getWidget(2, 1, reasonMap, true);
-		reasonTabel.setSize("100%", "75px");
-		reasonPanel.add(reasonTabel);
-		// HorizontalPanel customerNameHPanel = new HorizontalPanel();
-		// customerNameHPanel.setSize("100%", "100%");
-		// customerNameHPanel.setSpacing(10);
-		// customerNameHPanel.getElement().setAttribute("cellpaddding", "1");
-		// ClientCustomer customer =
-		// FinanceApplication.getCompany().getCustomer(
+		// VerticalPanel mainVPanel = new VerticalPanel();
+		// PrintTemplateUtils util = new PrintTemplateUtils();
+		//
+		// List<String> removeHeaderBackground = new ArrayList<String>();
+		//
+		// HTML lab1 = new HTML(Accounter.messages().creaditHTML());
+		//
+		// HorizontalPanel labeldateNoLayout = new HorizontalPanel();
+		// labeldateNoLayout.setSize("auto", "100%");
+		// labeldateNoLayout.add(lab1);
+		//
+		// Map<String, String> dateNumMap = getMap(new String[] {
+		// Accounter.messages().customerName(Global.get().Customer()),
+		// Accounter.getCompany().getCustomer(creditMemo.getCustomer())
+		// .getName(),
+		// Accounter.constants().creditDate(),
+		// UIUtils.dateFormat(new ClientFinanceDate(creditMemo
+		// .getTransactionDate())),
+		// Accounter.constants().creditNo(), creditMemo.getNumber() + "" });
+		//
+		// HorizontalPanel datepanel = new HorizontalPanel();
+		// datepanel.setSize("300px", "150px");
+		// FlexTable table = util.getWidget1(3, 2, dateNumMap, true,
+		// removeHeaderBackground);
+		// table.setSize("100%", "100%");
+		// datepanel.add(table);
+		//
+		// HorizontalPanel adressHPanel = new HorizontalPanel();
+		// adressHPanel.setSize("100%", "100%");
+		// adressHPanel.setSpacing(10);
+		// adressHPanel.getElement().setAttribute("cellpaddding", "1");
+		//
+		// ClientCustomer customer = Accounter.getCompany().getCustomer(
 		// creditMemo.getCustomer());
-		// String custName = customer != null ? customer.getName() : " ";
-		// Map<String, String> custNameMap = getMap("Customer", custName);
-		// FlexTable custNameTable = util.getWidget(2, 1, custNameMap, true);
-		// custNameTable.setSize("220px", "100px");
-		// customerNameHPanel.add(custNameTable);
+		// String billAdrs = "<br/><br/><br/><br/><br/>";
+		// Set<ClientAddress> bill = customer.getAddress();
+		// for (ClientAddress add : bill) {
+		// if (add.getType() == ClientAddress.TYPE_BILL_TO) {
+		// if (add != null) {
+		// billAdrs = "<p align=\"left\"><font size=\"3\">"
+		// + add.getAddress1() + "<br/>" + add.getStreet()
+		// + "<br/>" + add.getCity() + "<br/>"
+		// + add.getStateOrProvinence() + "<br/>"
+		// + add.getZipOrPostalCode() + "<br/>"
+		// + add.getCountryOrRegion() + "</font></p>";
+		// }
+		// }
+		// }
 		//
-		// Map<String, String> projectMap = getMap(new String[] { "P.O. NO.",
-		// "&nbsp;", "Project", "&nbsp;" });
+		// Map<String, String> billAdrsMap = getMap(Accounter.messages()
+		// .customerBillTo(Global.get().Customer()), billAdrs);
+		// FlexTable billToTable = util.getWidget(2, 1, billAdrsMap, true);
+		// billToTable.setSize("280px", "150px");
+		// adressHPanel.add(billToTable);
+		// adressHPanel.setCellHorizontalAlignment(billToTable,
+		// HasHorizontalAlignment.ALIGN_LEFT);
 		//
-		// HorizontalPanel projectPanel = new HorizontalPanel();
-		// projectPanel.setSize("100%", "100%");
-		// FlexTable projectTable = util.getWidget(2, 2, projectMap, true);
-		// projectTable.setSize("220px", "100px");
-		// projectTable.getElement().setAttribute("align", "right");
-		// customerNameHPanel.add(projectTable);
-
-		Map<List<String>, Map<Integer, List<String>>> gridData = getGridDataMap();
-		VerticalPanel gridPanel = new VerticalPanel();
-		FlexTable grid = util.getGridWidget(gridData, true);
-		grid.setSize("auto", "72px");
-		gridPanel.add(grid);
-		gridPanel.setSize("100%", "100%");
-
-		if (Accounter.getCompany().getAccountingType() == ClientCompany.ACCOUNTING_TYPE_UK) {
-			vatPanel = new HorizontalPanel();
-
-			vatPanel.setSize("100%", "100%");
-			List<String> footer1Headers = new ArrayList<String>();
-			footer1Headers.add("VAT%");
-			footer1Headers.add(Accounter.constants().vatAmount());
-			FlexTable vatTabl = util.getFooterWidget(view.getGridForPrinting()
-					.getVATDetailsMapForPrinting(), footer1Headers);
-			vatTabl.getElement().setAttribute("style",
-					"border-left:1px solid #000;border-bottom:1px solid #000;");
-			vatTabl.setSize("100%", "100%");
-			// vatPanel.add(vatTabl);
-		}
-
-		double lineTotal = view.getGridForPrinting().getTotal();
-
-		if (Accounter.getCompany().getAccountingType() == ClientCompany.ACCOUNTING_TYPE_UK) {
-			vatTotal = view.getGridForPrinting().getVatTotal();
-			totalAmount = lineTotal + vatTotal;
-
-			Map<String, String> footer2Map = new LinkedHashMap<String, String>();
-			footer2Map = getMap(
-					"Sub Total",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(lineTotal) + "</p>",
-					"VAT Total",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(vatTotal) + "</p>",
-					"Total",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(totalAmount) + "</p>");
-			// footer2Map.put("0",
-			// "<strong>Sub Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-			// + DataUtils.getAmountAsString(lineTotal));
-			// footer2Map.put("1",
-			// "<strong>VAT Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-			// + DataUtils.getAmountAsString(vatTotal));
-			// footer2Map
-			// .put(
-			// "2",
-			// "<strong>Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-			// + DataUtils.getAmountAsString(totalAmount));
-			FlexTable totalsTabel = util.getWidget1(3, 2, footer2Map, true,
-					removeHeaderBackground);
-			totalsTabel.setSize("50%", "50%");
-			vatPanel.add(totalsTabel);
-			vatPanel.setCellHorizontalAlignment(totalsTabel,
-					HasHorizontalAlignment.ALIGN_RIGHT);
-
-		} else {
-			taxTotal = view.getSalesTax();
-			totalAmount = lineTotal + taxTotal;
-
-			Map<String, String> footer2Map = new LinkedHashMap<String, String>();
-			footer2Map = getMap(
-					"Sub Total",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(lineTotal) + "</p>",
-					"Total Tax",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(taxTotal) + "</p>",
-					"Total",
-					"<p align=\"right\">"
-							+ DataUtils.getAmountAsString(totalAmount) + "</p>");
-
-			// footer2Map.put("0",
-			// "<strong>Sub Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
-			// + DataUtils.getAmountAsString(lineTotal));
-			// footer2Map.put("1",
-			// "<strong>Total Tax</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"
-			// + DataUtils.getAmountAsString(taxTotal));
-			// footer2Map
-			// .put(
-			// "2",
-			// "<strong>Total</strong>&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp"
-			// + DataUtils.getAmountAsString(totalAmount));
-			FlexTable totalsTable = util.getWidget1(3, 2, footer2Map, true,
-					removeHeaderBackground);
-			totalsTable.setCellPadding(3);
-			totalsTable.setSize("50%", "50%");
-			vatPanel.add(totalsTable);
-			vatPanel.setCellHorizontalAlignment(totalsTable,
-					HasHorizontalAlignment.ALIGN_RIGHT);
-		}
-		gridPanel.add(vatPanel);
-
-		mainVPanel.add(labeldateNoLayout);
-		labeldateNoLayout.getElement().getParentElement()
-				.setAttribute("align", "right");
-		mainVPanel.add(datepanel);
-		datepanel.getElement().getParentElement()
-				.setAttribute("align", "right");
-		adressHPanel.setStyleName(Accounter.constants().shiftBottom());
-		mainVPanel.add(adressHPanel);
-
-		reasonPanel.setStyleName(Accounter.constants().shiftBottom());
-		mainVPanel.add(reasonPanel);
-		// customerNameHPanel.setStyleName("ShiftBottom");
-		// mainVPanel.add(customerNameHPanel);
-		// projectPanel.setStyleName("ShiftBottom");
-		// mainVPanel.add(projectPanel);
-		gridPanel.setStyleName(Accounter.constants().shiftBottom());
-		mainVPanel.add(gridPanel);
-
-		add(mainVPanel);
-		setSize("100%", "100%");
+		// HorizontalPanel reasonPanel = new HorizontalPanel();
+		// reasonPanel.setSize("100%", "100%");
+		//
+		// String memo = creditMemo.getMemo() + "<br/>";
+		// Map<String, String> reasonMap = getMap(Accounter.constants()
+		// .reasonForIssue(), memo);
+		// FlexTable reasonTabel = util.getWidget(2, 1, reasonMap, true);
+		// reasonTabel.setSize("100%", "75px");
+		// reasonPanel.add(reasonTabel);
+		// // HorizontalPanel customerNameHPanel = new HorizontalPanel();
+		// // customerNameHPanel.setSize("100%", "100%");
+		// // customerNameHPanel.setSpacing(10);
+		// // customerNameHPanel.getElement().setAttribute("cellpaddding", "1");
+		// // ClientCustomer customer =
+		// // FinanceApplication.getCompany().getCustomer(
+		// // creditMemo.getCustomer());
+		// // String custName = customer != null ? customer.getName() : " ";
+		// // Map<String, String> custNameMap = getMap("Customer", custName);
+		// // FlexTable custNameTable = util.getWidget(2, 1, custNameMap, true);
+		// // custNameTable.setSize("220px", "100px");
+		// // customerNameHPanel.add(custNameTable);
+		// //
+		// // Map<String, String> projectMap = getMap(new String[] { "P.O. NO.",
+		// // "&nbsp;", "Project", "&nbsp;" });
+		// //
+		// // HorizontalPanel projectPanel = new HorizontalPanel();
+		// // projectPanel.setSize("100%", "100%");
+		// // FlexTable projectTable = util.getWidget(2, 2, projectMap, true);
+		// // projectTable.setSize("220px", "100px");
+		// // projectTable.getElement().setAttribute("align", "right");
+		// // customerNameHPanel.add(projectTable);
+		//
+		// Map<List<String>, Map<Integer, List<String>>> gridData =
+		// getGridDataMap();
+		// VerticalPanel gridPanel = new VerticalPanel();
+		// FlexTable grid = util.getGridWidget(gridData, true);
+		// grid.setSize("auto", "72px");
+		// gridPanel.add(grid);
+		// gridPanel.setSize("100%", "100%");
+		//
+		// if (Accounter.getCompany().getAccountingType() ==
+		// ClientCompany.ACCOUNTING_TYPE_UK) {
+		// vatPanel = new HorizontalPanel();
+		//
+		// vatPanel.setSize("100%", "100%");
+		// List<String> footer1Headers = new ArrayList<String>();
+		// footer1Headers.add("VAT%");
+		// footer1Headers.add(Accounter.constants().vatAmount());
+		// FlexTable vatTabl = util.getFooterWidget(view.getGridForPrinting()
+		// .getVATDetailsMapForPrinting(), footer1Headers);
+		// vatTabl.getElement().setAttribute("style",
+		// "border-left:1px solid #000;border-bottom:1px solid #000;");
+		// vatTabl.setSize("100%", "100%");
+		// // vatPanel.add(vatTabl);
+		// }
+		//
+		// double lineTotal = view.getGridForPrinting().getTotal();
+		//
+		// if (Accounter.getCompany().getAccountingType() ==
+		// ClientCompany.ACCOUNTING_TYPE_UK) {
+		// vatTotal = view.getGridForPrinting().getVatTotal();
+		// totalAmount = lineTotal + vatTotal;
+		//
+		// Map<String, String> footer2Map = new LinkedHashMap<String, String>();
+		// footer2Map = getMap(
+		// "Sub Total",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(lineTotal) + "</p>",
+		// "VAT Total",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(vatTotal) + "</p>",
+		// "Total",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(totalAmount) + "</p>");
+		// // footer2Map.put("0",
+		// // "<strong>Sub Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		// // + DataUtils.getAmountAsString(lineTotal));
+		// // footer2Map.put("1",
+		// // "<strong>VAT Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		// // + DataUtils.getAmountAsString(vatTotal));
+		// // footer2Map
+		// // .put(
+		// // "2",
+		// //
+		// "<strong>Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		// // + DataUtils.getAmountAsString(totalAmount));
+		// FlexTable totalsTabel = util.getWidget1(3, 2, footer2Map, true,
+		// removeHeaderBackground);
+		// totalsTabel.setSize("50%", "50%");
+		// vatPanel.add(totalsTabel);
+		// vatPanel.setCellHorizontalAlignment(totalsTabel,
+		// HasHorizontalAlignment.ALIGN_RIGHT);
+		//
+		// } else {
+		// taxTotal = view.getSalesTax();
+		// totalAmount = lineTotal + taxTotal;
+		//
+		// Map<String, String> footer2Map = new LinkedHashMap<String, String>();
+		// footer2Map = getMap(
+		// "Sub Total",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(lineTotal) + "</p>",
+		// "Total Tax",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(taxTotal) + "</p>",
+		// "Total",
+		// "<p align=\"right\">"
+		// + DataUtils.getAmountAsString(totalAmount) + "</p>");
+		//
+		// // footer2Map.put("0",
+		// // "<strong>Sub Total</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
+		// // + DataUtils.getAmountAsString(lineTotal));
+		// // footer2Map.put("1",
+		// // "<strong>Total Tax</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp"
+		// // + DataUtils.getAmountAsString(taxTotal));
+		// // footer2Map
+		// // .put(
+		// // "2",
+		// //
+		// "<strong>Total</strong>&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp"
+		// // + DataUtils.getAmountAsString(totalAmount));
+		// FlexTable totalsTable = util.getWidget1(3, 2, footer2Map, true,
+		// removeHeaderBackground);
+		// totalsTable.setCellPadding(3);
+		// totalsTable.setSize("50%", "50%");
+		// vatPanel.add(totalsTable);
+		// vatPanel.setCellHorizontalAlignment(totalsTable,
+		// HasHorizontalAlignment.ALIGN_RIGHT);
+		// }
+		// gridPanel.add(vatPanel);
+		//
+		// mainVPanel.add(labeldateNoLayout);
+		// labeldateNoLayout.getElement().getParentElement()
+		// .setAttribute("align", "right");
+		// mainVPanel.add(datepanel);
+		// datepanel.getElement().getParentElement()
+		// .setAttribute("align", "right");
+		// adressHPanel.setStyleName(Accounter.constants().shiftBottom());
+		// mainVPanel.add(adressHPanel);
+		//
+		// reasonPanel.setStyleName(Accounter.constants().shiftBottom());
+		// mainVPanel.add(reasonPanel);
+		// // customerNameHPanel.setStyleName("ShiftBottom");
+		// // mainVPanel.add(customerNameHPanel);
+		// // projectPanel.setStyleName("ShiftBottom");
+		// // mainVPanel.add(projectPanel);
+		// gridPanel.setStyleName(Accounter.constants().shiftBottom());
+		// mainVPanel.add(gridPanel);
+		//
+		// add(mainVPanel);
+		// setSize("100%", "100%");
 	}
 
 	private Map<String, String> getMap(String... args) {
@@ -273,38 +278,38 @@ public class CustomerCreditMemoPrintLayout extends VerticalPanel {
 
 	private Map<List<String>, Map<Integer, List<String>>> getGridDataMap() {
 
-		AbstractTransactionGrid<ClientTransactionItem> grid = view
-				.getGridForPrinting();
-		List<String> headerNames = Arrays.asList(grid
-				.getColumnNamesForPrinting());
-
+//		AbstractTransactionGrid<ClientTransactionItem> grid = view
+//				.getGridForPrinting();
+//		List<String> headerNames = Arrays.asList(grid
+//				.getColumnNamesForPrinting());
+//
 		Map<List<String>, Map<Integer, List<String>>> gridData = new LinkedHashMap<List<String>, Map<Integer, List<String>>>();
-		int i = 0;
-		List<String> row;
-		Map<Integer, List<String>> rows = new LinkedHashMap<Integer, List<String>>();
-		for (ClientTransactionItem item : grid.getRecords()) {
-			row = new ArrayList<String>();
-			for (int col = 0; col < headerNames.size(); col++) {
-				String val = grid.getColumnValueForPrinting(item, col);
-				if (val != null && val.length() > 9) {
-					String split[] = val.split(" ");
-					val = "";
-					for (int j = 0; j < split.length; j++) {
-						if (split[j].length() > 9)
-							val = val
-									+ "  "
-									+ splitStringBy9(split[j].substring(8),
-											split[j].substring(0, 8));
-						else
-							val = val + "  " + split[j];
-					}
-				}
-				row.add(val);
-			}
-			rows.put(new Integer(i), row);
-			i++;
-		}
-		gridData.put(headerNames, rows);
+//		int i = 0;
+//		List<String> row;
+//		Map<Integer, List<String>> rows = new LinkedHashMap<Integer, List<String>>();
+//		for (ClientTransactionItem item : grid.getRecords()) {
+//			row = new ArrayList<String>();
+//			for (int col = 0; col < headerNames.size(); col++) {
+//				String val = grid.getColumnValueForPrinting(item, col);
+//				if (val != null && val.length() > 9) {
+//					String split[] = val.split(" ");
+//					val = "";
+//					for (int j = 0; j < split.length; j++) {
+//						if (split[j].length() > 9)
+//							val = val
+//									+ "  "
+//									+ splitStringBy9(split[j].substring(8),
+//											split[j].substring(0, 8));
+//						else
+//							val = val + "  " + split[j];
+//					}
+//				}
+//				row.add(val);
+//			}
+//			rows.put(new Integer(i), row);
+//			i++;
+//		}
+//		gridData.put(headerNames, rows);
 		return gridData;
 	}
 
