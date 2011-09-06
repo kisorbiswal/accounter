d3429d2ead7728b227590451d60b104e1e2673ee
Devi G <devi.g@vimukti.com>
2011-09-06 06:51:05 +0000

    Former-commit-id: 5b1a460495032d2f3c980d79cc7386f42f3ae099

--- src/com/vimukti/accounter/web/client/uibinder/companypreferences/CustomerAndVendorsSettingsPage.java
+++ src/com/vimukti/accounter/web/client/uibinder/companypreferences/CustomerAndVendorsSettingsPage.java
@@ -1,19 +1,44 @@
 package com.vimukti.accounter.web.client.uibinder.companypreferences;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.Widget;
 
-public class CustomerAndVendorsSettingsPage extends Composite implements
-		HasText {
+public class CustomerAndVendorsSettingsPage extends AbstractCompanyInfoPanel {
 
+	@UiField
+	Label chargeTaxLabelItem;
+	@UiField
+	RadioButton chargeTaxYesRadioButton;
+	@UiField
+	RadioButton chargeTaxNoRadioButton;
+	@UiField
+	Label managingBillLabelItem;
+	@UiField
+	RadioButton managingBillYesRadioButton;
+	@UiField
+	RadioButton managingBillNoRadioButton;
+	@UiField
+	Label createEstimatesLabelItem;
+	@UiField
+	RadioButton createEstimatesYesRadioButton;
+	@UiField
+	RadioButton createEstimatesNoRadioButton;
+	@UiField
+	Label usingStatementsLabelItem;
+	@UiField
+	RadioButton usingStatementsYesRadioButton;
+	@UiField
+	RadioButton usingStatementsNoRadioButton;
+	@UiField
+	Label billsLabel;
+	@UiField
+	RadioButton billsYesRadioButton;
+	@UiField
+	RadioButton billsNoRadioButton;
 	private static CustomerAndVendorsSettingsPageUiBinder uiBinder = GWT
 			.create(CustomerAndVendorsSettingsPageUiBinder.class);
 
@@ -23,27 +48,91 @@ public class CustomerAndVendorsSettingsPage extends Composite implements
 
 	public CustomerAndVendorsSettingsPage() {
 		initWidget(uiBinder.createAndBindUi(this));
+		createControls();
 	}
 
-	@UiField
-	Button button;
+	private void createControls() {
+		chargeTaxLabelItem.setText(constants.doyouchargesalestax());
 
-	public CustomerAndVendorsSettingsPage(String firstName) {
-		initWidget(uiBinder.createAndBindUi(this));
-		button.setText(firstName);
+		chargeTaxYesRadioButton.setText(constants.yes());
+		chargeTaxNoRadioButton.setText(constants.no());
+
+		managingBillLabelItem.setText(constants.managingBills());
+
+		managingBillYesRadioButton.setText(constants.yes());
+		managingBillNoRadioButton.setText(constants.no());
+
+		createEstimatesLabelItem.setText(constants
+				.wanttoCreateEstimatesInAccounter());
+
+		createEstimatesYesRadioButton.setText(constants.yes());
+		createEstimatesNoRadioButton.setText(constants.no());
+
+		usingStatementsLabelItem.setText(constants.doyouWantToUseStatements());
+
+		usingStatementsYesRadioButton.setText(constants.yes());
+		usingStatementsNoRadioButton.setText(constants.no());
+
+		billsLabel.setText(constants.doyouwantTrackBills());
+
+		billsYesRadioButton.setText(constants.yes());
+		billsNoRadioButton.setText(constants.no());
+
 	}
 
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
+	@Override
+	public void onLoad() {
+		if (companyPreferences.isDoYouChargesalesTax())
+			chargeTaxYesRadioButton.setValue(true);
+		else
+			chargeTaxNoRadioButton.setValue(false);
+
+		if (companyPreferences.isDoyouKeepTrackofBills())
+			managingBillYesRadioButton.setValue(true);
+		else
+			managingBillNoRadioButton.setValue(false);
+		
+		if (companyPreferences.isDoyouwantEstimates())
+			createEstimatesYesRadioButton.setValue(true);
+		else
+			createEstimatesNoRadioButton.setValue(false);
+		if (companyPreferences.isDoyouwantstatements())
+			usingStatementsYesRadioButton.setValue(true);
+		else
+			usingStatementsNoRadioButton.setValue(false);
+
+		if (companyPreferences.isDoyouKeepTrackofBills())
+			billsYesRadioButton.setValue(true);
+		else
+			billsNoRadioButton.setValue(false);
 	}
 
-	public void setText(String text) {
-		button.setText(text);
-	}
+	@Override
+	public void onSave() {
+		if (chargeTaxYesRadioButton.getValue())
+			companyPreferences.setDoYouPaySalesTax(true);
+		else
+			companyPreferences.setDoYouChargesalesTax(false);
 
-	public String getText() {
-		return button.getText();
+		if (managingBillYesRadioButton.getValue())
+			companyPreferences.setDoyouKeepTrackofBills(true);
+		else
+			companyPreferences.setDoyouKeepTrackofBills(false);
+
+		if (createEstimatesYesRadioButton.getValue())
+			companyPreferences.setDoyouwantEstimates(true);
+		else
+			companyPreferences.setDoyouwantEstimates(false);
+
+		if (usingStatementsYesRadioButton.getValue())
+			companyPreferences.setDoyouwantstatements(true);
+		else
+			companyPreferences.setDoyouwantstatements(false);
+
+		if (billsYesRadioButton.getValue())
+			companyPreferences.setDoyouKeepTrackofBills(true);
+		else
+			companyPreferences.setDoyouKeepTrackofBills(false);
 	}
 
 }
