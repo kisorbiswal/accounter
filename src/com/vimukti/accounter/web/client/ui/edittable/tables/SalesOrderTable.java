e8d3e40a2c14c61ddde929957759de46c30c2b73
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-06 10:09:57 +0000

    Former-commit-id: be203d2e41882231b81d7fb2c5af6f5f6881d45c

---
+++ src/com/vimukti/accounter/web/client/ui/edittable/tables/SalesOrderTable.java
@@ -0,0 +1,100 @@
+package com.vimukti.accounter.web.client.ui.edittable.tables;
+
+import java.util.Arrays;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.core.ClientAccount;
+import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.ClientTransactionItem;
+import com.vimukti.accounter.web.client.core.ListFilter;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.edittable.DeleteColumn;
+import com.vimukti.accounter.web.client.ui.edittable.DescriptionEditColumn;
+import com.vimukti.accounter.web.client.ui.edittable.ImageEditColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionDiscountColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionItemNameColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionQuantityColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionTotalColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionUnitPriceColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionVatCodeColumn;
+import com.vimukti.accounter.web.client.ui.edittable.TransactionVatColumn;
+
+public abstract class SalesOrderTable extends CustomerTransactionTable {
+	@Override
+	protected void initColumns() {
+		this.addColumn(new ImageEditColumn<ClientTransactionItem>() {
+
+			@Override
+			public ImageResource getResource(ClientTransactionItem row) {
+				return SalesOrderTable.this.getImage(row);
+			}
+
+			@Override
+			public int getWidth() {
+				return 20;
+			}
+
+		});
+
+		this.addColumn(new TransactionItemNameColumn() {
+
+			@Override
+			public ListFilter<ClientAccount> getAccountsFilter() {
+				return new ListFilter<ClientAccount>() {
+
+					@Override
+					public boolean filter(ClientAccount account) {
+						if (Accounter.getCompany().getAccountingType() == ClientCompany.ACCOUNTING_TYPE_UK) {
+							if (Arrays.asList(ClientAccount.TYPE_BANK,
+									ClientAccount.TYPE_CREDIT_CARD,
+									ClientAccount.TYPE_OTHER_CURRENT_ASSET,
+									ClientAccount.TYPE_FIXED_ASSET).contains(
+									account.getType())) {
+								return true;
+							}
+						} else {
+							if (Arrays.asList(ClientAccount.TYPE_CREDIT_CARD,
+									ClientAccount.TYPE_OTHER_CURRENT_ASSET,
+									ClientAccount.TYPE_BANK,
+									ClientAccount.TYPE_FIXED_ASSET).contains(
+									account.getType())) {
+
+								return true;
+							}
+						}
+						return false;
+					}
+				};
+			}
+
+		});
+
+		this.addColumn(new DescriptionEditColumn());
+
+		this.addColumn(new TransactionQuantityColumn());
+
+		this.addColumn(new TransactionUnitPriceColumn());
+
+		this.addColumn(new TransactionDiscountColumn());
+
+		this.addColumn(new TransactionTotalColumn());
+
+		if (getCompany().getPreferences().getDoYouPaySalesTax()) {
+			if (this.getCompany().getAccountingType() == ClientCompany.ACCOUNTING_TYPE_UK) {
+				this.addColumn(new TransactionVatCodeColumn());
+			}
+
+			this.addColumn(new TransactionVatColumn());
+		}
+
+		
+		
+		this.addColumn(new DeleteColumn<ClientTransactionItem>() {
+
+			@Override
+			protected void onDelete(ClientTransactionItem row) {
+				delete(row);
+			}
+		});
+	}
+}
