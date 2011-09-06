ed7435b01a9418a19167750d84129e15f8fb4c4d
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-06 05:47:38 +0000

    Former-commit-id: 92f113787781f9c8c181a76e99b2b2c45ce16648

--- src/com/vimukti/accounter/core/Company.java
+++ src/com/vimukti/accounter/core/Company.java
@@ -247,8 +247,6 @@ public class Company extends CreatableObject implements IAccounterServerCore {
 
 	private ArrayList<TAXItem> taxItems = new ArrayList<TAXItem>();
 
-	private ArrayList<Budget> budget = new ArrayList<Budget>();
-
 	private ArrayList<TAXAgency> taxAgencies = new ArrayList<TAXAgency>();
 
 	Set<NominalCodeRange> nominalCodeRange = new HashSet<NominalCodeRange>();
@@ -4319,8 +4317,6 @@ public class Company extends CreatableObject implements IAccounterServerCore {
 
 		cmp.vendors = this.getVendors();
 
-		cmp.budget = this.getBudget();
-
 		cmp.taxAgencies = this.getTaxAgencies();
 
 		cmp.taxItemGroups = this.getTaxItemGroups();
@@ -4604,14 +4600,6 @@ public class Company extends CreatableObject implements IAccounterServerCore {
 
 	public void setLocations(ArrayList<Location> arrayList) {
 		this.locations = arrayList;
-	}
-
-	public ArrayList<Budget> getBudget() {
-		return budget;
-	}
-
-	public void setBudget(ArrayList<Budget> budget) {
-		this.budget = budget;
 	}
 
 }
