f146ab987ee29a3af57a6e7747bfeb19f012f695
Devaraju K <devaraju.k@vimukti.com>
2011-09-06 13:02:59 +0000

    Former-commit-id: 7a44687861d92a7768b9fe74cfbf406ecc2a577b

--- src/com/vimukti/accounter/web/client/uibinder/companypreferences/CompanyOtherDetailsPage.java
+++ src/com/vimukti/accounter/web/client/uibinder/companypreferences/CompanyOtherDetailsPage.java
@@ -3,41 +3,32 @@ package com.vimukti.accounter.web.client.uibinder.companypreferences;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 
 public class CompanyOtherDetailsPage extends AbstractCompanyInfoPanel {
 
 	private static CompanyOtherDetailsPageUiBinder uiBinder = GWT
 			.create(CompanyOtherDetailsPageUiBinder.class);
-
-	// // @UiField
-	// VerticalPanel viewPanel;
-	//
-	// // @UiField
-	// Label contactDetailLabel;
-	//
-	// // @UiField
-	// TextBox phoneTextBox;
-	//
-	// // @UiField
-	// Label phoneTextBoxLabel;
-	//
-	// // @UiField
-	// TextBox emailFieldBox;
-	//
-	// // @UiField
-	// Label emailFieldBoxLabel;
-	//
-	// // @UiField
-	// TextBox faxTextBox;
-	//
-	// // @UiField
-	// Label faxTextBoxLabel;
-	//
-	// // @UiField
-	// TextBox webTextBox;
-	//
-	// // @UiField
-	// Label webTextBoxLabel;
+	@UiField
+	Label contactDetailLabel;
+	@UiField
+	Label phoneTextBoxLabel;
+	@UiField
+	TextBox phoneTextBox;
+	@UiField
+	Label emailFieldBoxLabel;
+	@UiField
+	TextBox emailFieldBox;
+	@UiField
+	Label faxTextBoxLabel;
+	@UiField
+	TextBox faxTextBox;
+	@UiField
+	Label webTextBoxLabel;
+	@UiField
+	TextBox webTextBox;
 
 	interface CompanyOtherDetailsPageUiBinder extends
 			UiBinder<Widget, CompanyOtherDetailsPage> {
@@ -45,32 +36,32 @@ public class CompanyOtherDetailsPage extends AbstractCompanyInfoPanel {
 
 	public CompanyOtherDetailsPage() {
 		initWidget(uiBinder.createAndBindUi(this));
-		// createControls();
+		createControls();
 	}
 
-	// private void createControls() {
-	//
-	// contactDetailLabel.setText(constants.contactDetails());
-	// phoneTextBoxLabel.setText(constants.phone());
-	// emailFieldBoxLabel.setText(constants.email());
-	// faxTextBoxLabel.setText(constants.fax());
-	// webTextBoxLabel.setText(constants.webSite());
-	// }
+	private void createControls() {
+
+		contactDetailLabel.setText(constants.contactDetails());
+		phoneTextBoxLabel.setText(constants.phone());
+		emailFieldBoxLabel.setText(constants.email());
+		faxTextBoxLabel.setText(constants.fax());
+		webTextBoxLabel.setText(constants.webSite());
+	}
 
 	@Override
 	public void onLoad() {
-		// phoneTextBox.setValue(company.getPhone());
-		// emailFieldBox.setValue(company.getCompanyEmail());
-		// webTextBox.setValue(company.getWebSite());
-		// faxTextBox.setValue(company.getFax());
+		phoneTextBox.setValue(company.getPhone());
+		emailFieldBox.setValue(company.getCompanyEmail());
+		webTextBox.setValue(company.getWebSite());
+		faxTextBox.setValue(company.getFax());
 	}
 
 	@Override
 	public void onSave() {
-		// company.setPhone(phoneTextBox.getValue());
-		// company.setCompanyEmail(emailFieldBox.getValue());
-		// company.setWebSite(emailFieldBox.getValue());
-		// company.setFax(emailFieldBox.getValue());
+		company.setPhone(phoneTextBox.getValue());
+		company.setCompanyEmail(emailFieldBox.getValue());
+		company.setWebSite(emailFieldBox.getValue());
+		company.setFax(emailFieldBox.getValue());
 	}
 
 }
