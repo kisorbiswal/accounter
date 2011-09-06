1dfd43bc230686c17216acad531cdd188ec83715
Devaraju K <devaraju.k@vimukti.com>
2011-09-06 13:03:41 +0000

    Former-commit-id: a1c8dda4faefa111be46e64dd706ae263d04f596

--- src/com/vimukti/accounter/web/client/uibinder/companypreferences/OrganizationDetailsPage.java
+++ src/com/vimukti/accounter/web/client/uibinder/companypreferences/OrganizationDetailsPage.java
@@ -1,67 +1,40 @@
 package com.vimukti.accounter.web.client.uibinder.companypreferences;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.vimukti.accounter.web.client.ui.setup.OrganizationTypeConstants;
+import com.google.gwt.uibinder.client.UiField;
 
 public class OrganizationDetailsPage extends AbstractCompanyInfoPanel {
 
 	private static OrganizationDetailsPageUiBinder uiBinder = GWT
 			.create(OrganizationDetailsPageUiBinder.class);
+	@UiField Label soleProprietorshipRadioLabel;
+	@UiField RadioButton partnershipRadio;
+	@UiField Label partnershipRadioLabel;
+	@UiField RadioButton llcRadio;
+	@UiField Label llcRadioLabel;
+	@UiField RadioButton corporationRadio;
+	@UiField Label corporationRadioLabel;
+	@UiField RadioButton sCorporationRadio;
+	@UiField Label sCorporationRadioLabel;
+	@UiField RadioButton nonProfitRadio;
+	@UiField Label nonProfitRadioLabel;
+	@UiField RadioButton otherNoneRadio;
+	@UiField Label otherNoneRadioLabel;
+	@UiField RadioButton soleProprietorshipRadio;
+	@UiField ListBox llcFormCombo;
 
-	// private RadioButton soleProprietorshipRadio, partnershipRadio, llcRadio,
-	// corporationRadio, sCorporationRadio, nonProfitRadio,
-	// otherNoneRadio;
-	// private SelectCombo llcFormCombo;
-	// private String ORG_TYPE;
-	//
-	// // @UiField
-	// VerticalPanel viewPanel;
-	//
-	// // @UiField
-	// RadioButton soleProprietorshipRadio;
-	//
-	// // @UiField
-	// RadioButton partnershipRadio;
-	//
-	// // @UiField
-	// RadioButton llcRadio;
-	//
-	// // @UiField
-	// RadioButton corporationRadio;
-	//
-	// // @UiField
-	// RadioButton sCorporationRadio;
-	//
-	// // @UiField
-	// RadioButton nonProfitRadio;
-	//
-	// // @UiField
-	// RadioButton otherNoneRadio;
-	//
-	// // @UiField
-	// ListBox llcFormCombo;
-	//
-	// // @UiField
-	// Label soleProprietorshipRadioLabel;
-	//
-	// // @UiField
-	// Label partnershipRadioLabel;
-	//
-	// // @UiField
-	// Label llcRadioLabel;
-	//
-	// // @UiField
-	// Label corporationRadioLabel;
-	//
-	// // @UiField
-	// Label sCorporationRadioLabel;
-	//
-	// // @UiField
-	// Label nonProfitRadioLabel;
-	//
-	// // @UiField
-	// Label otherNoneRadioLabel;
+	 private String ORG_TYPE;
+	
+
 
 	interface OrganizationDetailsPageUiBinder extends
 			UiBinder<Widget, OrganizationDetailsPage> {
@@ -69,79 +42,136 @@ public class OrganizationDetailsPage extends AbstractCompanyInfoPanel {
 
 	public OrganizationDetailsPage() {
 		initWidget(uiBinder.createAndBindUi(this));
-		// createControls();
+		 createControls();
 	}
 
-	// private void createControls() {
-	// ORG_TYPE = constants.organisation();
-	// soleProprietorshipRadio.setName(constants.soleProprietorship());
-	// soleProprietorshipRadio.setHTML(ORG_TYPE);
-	// partnershipRadio.setName(constants.partnershipOrLLP());
-	// partnershipRadio.setHTML(ORG_TYPE);
-	// llcRadio.setName(constants.LLC());
-	// llcRadio.setHTML(ORG_TYPE);
-	// corporationRadio.setName(constants.corporation());
-	// corporationRadio.setHTML(ORG_TYPE);
-	// sCorporationRadio.setName(constants.sCorporation());
-	// sCorporationRadio.setHTML(ORG_TYPE);
-	// nonProfitRadio.setName(constants.nonProfit());
-	// nonProfitRadio.setHTML(ORG_TYPE);
-	// otherNoneRadio.setName(constants.otherNone());
-	// otherNoneRadio.setHTML(ORG_TYPE);
-	// llcFormCombo.addItem(constants.llcSingleMemberForm());
-	// llcFormCombo.addItem(constants.llcMultiMemberForm());
-	// }
+	private void createControls() {
+		ORG_TYPE = constants.organisation();
+		soleProprietorshipRadio.setName(ORG_TYPE);
+		soleProprietorshipRadio.setHTML(constants.soleProprietorship());
+		soleProprietorshipRadioLabel
+				.setText(constants.soleProprietorshipDesc());
+		partnershipRadio.setName(ORG_TYPE);
+		partnershipRadio.setHTML(constants.partnershipOrLLP());
+		partnershipRadioLabel.setText(constants.partnershipOrLLPDesc());
+		llcRadio.setName(ORG_TYPE);
+		llcRadio.setHTML(constants.LLC());
+		llcRadioLabel.setText(constants.LLCDesc());
+		corporationRadio.setName(ORG_TYPE);
+		corporationRadio.setHTML(constants.corporation());
+		corporationRadioLabel.setText(constants.corporationDesc());
+		sCorporationRadio.setName(ORG_TYPE);
+		sCorporationRadio.setHTML(constants.sCorporation());
+		sCorporationRadioLabel.setText(constants.sCorporationDesc());
+		nonProfitRadio.setName(ORG_TYPE);
+		nonProfitRadio.setHTML(constants.nonProfit());
+		nonProfitRadioLabel.setText(constants.nonProfitDesc());
+		otherNoneRadio.setName(ORG_TYPE);
+		otherNoneRadio.setHTML(constants.otherNone());
+		llcFormCombo.addItem(constants.llcSingleMemberForm());
+		llcFormCombo.addItem(constants.llcMultiMemberForm());
+		soleProprietorshipRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+		partnershipRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+		llcRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(true);
+			}
+		});
+		corporationRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+		sCorporationRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+		nonProfitRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+		otherNoneRadio.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				llcFormCombo.setEnabled(false);
+			}
+		});
+
+	}
 
 	@Override
 	public void onLoad() {
-		// switch (companyPreferences.getOrganizationType()) {
-		// case OrganizationTypeConstants.SOLE_PROPRIETORSHIP:
-		// soleProprietorshipRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.CORPORATION:
-		// corporationRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.S_CORPORATION:
-		// sCorporationRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.LLC:
-		// llcRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.PARTNERSHIP:
-		// partnershipRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.NON_PROFIT:
-		// nonProfitRadio.setValue(true);
-		// break;
-		// case OrganizationTypeConstants.OTHER:
-		// otherNoneRadio.setValue(true);
-		// }
+		 switch (companyPreferences.getOrganizationType()) {
+		 case OrganizationTypeConstants.SOLE_PROPRIETORSHIP:
+		 soleProprietorshipRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.CORPORATION:
+		 corporationRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.S_CORPORATION:
+		 sCorporationRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.LLC:
+		 llcRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.PARTNERSHIP:
+		 partnershipRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.NON_PROFIT:
+		 nonProfitRadio.setValue(true);
+		 break;
+		 case OrganizationTypeConstants.OTHER:
+		 otherNoneRadio.setValue(true);
+		 }
 	}
 
 	@Override
 	public void onSave() {
-		// if (soleProprietorshipRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.SOLE_PROPRIETORSHIP);
-		// } else if (corporationRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.CORPORATION);
-		// } else if (sCorporationRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.S_CORPORATION);
-		// } else if (llcRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.LLC);
-		// } else if (partnershipRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.PARTNERSHIP);
-		// } else if (nonProfitRadio.getValue()) {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.NON_PROFIT);
-		// } else {
-		// companyPreferences
-		// .setOrganizationType(OrganizationTypeConstants.OTHER);
-		// }
+		 if (soleProprietorshipRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.SOLE_PROPRIETORSHIP);
+		 } else if (corporationRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.CORPORATION);
+		 } else if (sCorporationRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.S_CORPORATION);
+		 } else if (llcRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.LLC);
+		 } else if (partnershipRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.PARTNERSHIP);
+		 } else if (nonProfitRadio.getValue()) {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.NON_PROFIT);
+		 } else {
+		 companyPreferences
+		 .setOrganizationType(OrganizationTypeConstants.OTHER);
+		 }
 	}
 
 }
