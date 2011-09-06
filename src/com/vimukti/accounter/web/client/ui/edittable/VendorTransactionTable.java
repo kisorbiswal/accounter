9aed336a1c65bdb7f4350026426a6eb363f2ae6d
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-06 06:04:22 +0000

    Former-commit-id: 6fc4b30432c1e64f37b177e12805591393d0c7cb

---
+++ src/com/vimukti/accounter/web/client/ui/edittable/VendorTransactionTable.java
@@ -0,0 +1,13 @@
+package com.vimukti.accounter.web.client.ui.edittable;
+
+import com.vimukti.accounter.web.client.core.ClientTransactionItem;
+
+public class VendorTransactionTable extends EditTable<ClientTransactionItem> {
+	public VendorTransactionTable() {
+		initTable();
+	}
+
+	private void initTable() {
+		
+	}
+}
