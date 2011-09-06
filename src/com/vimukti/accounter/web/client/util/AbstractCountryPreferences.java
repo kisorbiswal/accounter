2989677935427b8abcc7236546925c240b7f0000
sravanthi.v <sravanthi.v@vimukti.com>
2011-09-06 12:32:00 +0000

    Former-commit-id: a8de41eb407a3524038c54c28b3bf8e4b34967f0

---
+++ src/com/vimukti/accounter/web/client/util/AbstractCountryPreferences.java
@@ -0,0 +1,11 @@
+package com.vimukti.accounter.web.client.util;
+
+public abstract class AbstractCountryPreferences implements ICountryPreferences {
+
+	@Override
+	public OrganizationType[] getOrganizationTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
