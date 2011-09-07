4379ec5212d0db9d59554c81182cecaa708e33c5
Amrit Mishra <amrit.mishra@vimukti.com>
2011-09-07 13:02:40 +0000

    Former-commit-id: 7e64e7e51c0cdb3a7654d9ee30a199bea5dc536c

---
+++ src/com/vimukti/accounter/web/client/ui/reports/BudgetReportAction.java
@@ -0,0 +1,70 @@
+package com.vimukti.accounter.web.client.ui.reports;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallback;
+
+public class BudgetReportAction extends Action {
+
+	protected BudgetReportView report;
+
+	public BudgetReportAction(String text) {
+		super(text);
+		this.catagory = Accounter.constants().report();
+	}
+
+	public void runAsync(final Object data, final Boolean isDependent) {
+
+		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+
+			public void onCreated() {
+
+				report = new BudgetReportView();
+				MainFinanceWindow.getViewManager().showView(report, data,
+						isDependent, BudgetReportAction.this);
+				// Accounter.showError("Not yet Implemented");
+
+			}
+
+		});
+
+	}
+
+	// @Override
+	// public ParentCanvas getView() {
+	// // its not using any where return null;
+	// return this.report;
+	// }
+
+	@Override
+	public void run() {
+		runAsync(data, isDependent);
+	}
+
+	public ImageResource getBigImage() {
+		return null;
+	}
+
+	public ImageResource getSmallImage() {
+		return Accounter.getFinanceMenuImages().reports();
+	}
+
+	// @Override
+	// public String getImageUrl() {
+	// return "/images/reports.png";
+	// }
+
+	@Override
+	public String getHistoryToken() {
+		return "budgetReport";
+	}
+
+	@Override
+	public String getHelpToken() {
+		return "budget-report";
+	}
+
+}
---
+++ src/com/vimukti/accounter/web/client/ui/reports/BudgetReportView.java
@@ -0,0 +1,26 @@
+package com.vimukti.accounter.web.client.ui.reports;
+
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.client.core.reports.TrialBalance;
+
+public class BudgetReportView extends AbstractReportView<TrialBalance> {
+
+	@Override
+	public void makeReportRequest(ClientFinanceDate start, ClientFinanceDate end) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getToolbarType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void OnRecordClick(TrialBalance record) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
