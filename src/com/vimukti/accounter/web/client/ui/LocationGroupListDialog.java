b257179670e6f8046fe6ed722e856634078f7f66
Devaraju K <devaraju.k@vimukti.com>
2011-09-08 11:43:13 +0000

    Former-commit-id: 22e1a827a22805e3150ed2b101c3f5f696ad4c51

---
+++ src/com/vimukti/accounter/web/client/ui/LocationGroupListDialog.java
@@ -0,0 +1,162 @@
+package com.vimukti.accounter.web.client.ui;
+
+import java.util.List;
+
+import com.vimukti.accounter.web.client.core.AccounterCoreType;
+import com.vimukti.accounter.web.client.core.ClientLocation;
+import com.vimukti.accounter.web.client.core.ValidationResult;
+import com.vimukti.accounter.web.client.externalization.AccounterConstants;
+import com.vimukti.accounter.web.client.ui.core.GroupDialog;
+import com.vimukti.accounter.web.client.ui.core.GroupDialogButtonsHandler;
+import com.vimukti.accounter.web.client.ui.customers.AddnewLocationDialog;
+import com.vimukti.accounter.web.client.ui.grids.DialogGrid.GridRecordClickHandler;
+
+public class LocationGroupListDialog extends GroupDialog<ClientLocation> {
+
+	ClientLocation clientLocation;
+	private GroupDialogButtonsHandler dialogButtonsHandler;
+	AccounterConstants accounterConstants = Accounter.constants();
+	List<ClientLocation> clientLocationGroups;
+	private AddnewLocationDialog locationGroupDg;
+
+	public LocationGroupListDialog(String title, String descript) {
+		super(title, descript);
+		setWidth("400px");
+		initialise();
+		center();
+	}
+
+	private void initialise() {
+		getGrid().setType(AccounterCoreType.LOCATION);
+		getGrid().addRecordClickHandler(
+				new GridRecordClickHandler<ClientLocation>() {
+
+					@Override
+					public boolean onRecordClick(ClientLocation core, int column) {
+						ClientLocation clientLocation = core;
+						if (clientLocation != null)
+							enableEditRemoveButtons(true);
+						else
+							enableEditRemoveButtons(false);
+
+						return false;
+					}
+
+				});
+
+		dialogButtonsHandler = new GroupDialogButtonsHandler() {
+
+			public void onCloseButtonClick() {
+
+			}
+
+			public void onFirstButtonClick() {
+				showAddEditGroupDialog(null);
+			}
+
+			public void onSecondButtonClick() {
+				showAddEditGroupDialog((ClientLocation) listGridView
+						.getSelection());
+
+			}
+
+			public void onThirdButtonClick() {
+				deleteObject(getSelectedLocationGroup());
+				if (clientLocationGroups == null)
+					enableEditRemoveButtons(false);
+			}
+
+			public ClientLocation getSelectedLocationGroup() {
+				return (ClientLocation) listGridView.getSelection();
+			}
+		};
+		addGroupButtonsHandler(dialogButtonsHandler);
+		this.okbtn.setVisible(false);
+
+	}
+
+	public long getSelectedLocationId() {
+		return ((ClientLocation) listGridView.getSelection()).getID();
+	}
+
+	public void showAddEditGroupDialog(ClientLocation rec) {
+		clientLocation = rec;
+		locationGroupDg = new AddnewLocationDialog(this, Accounter.constants()
+				.locationGroup(), "", clientLocation);
+		locationGroupDg.show();
+	}
+
+	@Override
+	public String[] setColumns() {
+		return new String[] { Accounter.constants().locationName(), };
+	}
+
+	@Override
+	protected List<ClientLocation> getRecords() {
+		return (List<ClientLocation>) getCompany().getLocations();
+	}
+
+	@Override
+	public boolean onOK() {
+		if (locationGroupDg != null) {
+			if (clientLocation != null) {
+				editLocationGroups();
+				locationGroupDg = null;
+			} else {
+				createLocationSGroups();
+				locationGroupDg = null;
+			}
+
+		}
+		return true;
+	}
+
+	@Override
+	public Object getGridColumnValue(ClientLocation obj, int index) {
+		ClientLocation clientLocation = obj;
+		if (clientLocation != null) {
+			switch (index) {
+			case 0:
+				return clientLocation.getLocationName();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public ValidationResult validate() {
+		ValidationResult result = new ValidationResult();
+		String name = locationGroupDg.getLocationGroupName();
+		if (name != null)
+			if (!UIUtils.isCharactersOnly(name)) {
+				result.addError(this, "Please Enter Valid Location Name");
+			}
+		if (clientLocation != null) {
+			String locationGroupName = clientLocation.getName();
+			ClientLocation groupByName = company.getLocationByName(name);
+			if (!(locationGroupName.equalsIgnoreCase(name) ? true
+					: groupByName == null)) {
+				result.addError(this, accounterConstants.alreadyExist());
+			}
+
+		} else {
+			ClientLocation locationGroupByName = company
+					.getLocationByName(name);
+			if (locationGroupByName != null) {
+				result.addError(this, Accounter.constants()
+						.aLocationAlreadyExistswiththisname());
+			}
+		}
+
+		return null;
+	}
+
+	protected void createLocationSGroups() {
+		saveOrUpdate(locationGroupDg.createOrEditLocation());
+	}
+
+	protected void editLocationGroups() {
+		saveOrUpdate(locationGroupDg.createOrEditLocation());
+	}
+
+}
