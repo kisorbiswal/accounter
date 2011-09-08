f460ce8686d42cfbae9b525c9362402e4612d08d
Devaraju K <devaraju.k@vimukti.com>
2011-09-08 11:42:46 +0000

    Former-commit-id: e9bfdce35e5647a4caa2a5a14a1c8bc6e4472a9d

--- src/com/vimukti/accounter/web/client/ui/customers/AddnewLocationDialog.java
+++ src/com/vimukti/accounter/web/client/ui/customers/AddnewLocationDialog.java
@@ -4,6 +4,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.vimukti.accounter.web.client.ValueCallBack;
 import com.vimukti.accounter.web.client.core.ClientLocation;
 import com.vimukti.accounter.web.client.core.ValidationResult;
+import com.vimukti.accounter.web.client.ui.LocationGroupListDialog;
 import com.vimukti.accounter.web.client.ui.core.BaseDialog;
 import com.vimukti.accounter.web.client.ui.forms.DynamicForm;
 import com.vimukti.accounter.web.client.ui.forms.TextItem;
@@ -20,12 +21,36 @@ public class AddnewLocationDialog extends BaseDialog<ClientLocation> {
 	private ValueCallBack<ClientLocation> successCallback;
 	private DynamicForm form;
 	private TextItem locationName;
+	ClientLocation clientLocation;
+	LocationGroupListDialog locationGroupListDialog;
 
 	public AddnewLocationDialog(String title) {
 		super(title, "");
 		setWidth("400px");
 		createControls();
 		center();
+	}
+
+	public AddnewLocationDialog(
+			LocationGroupListDialog locationGroupListDialog, String title,
+			String string, ClientLocation clientLocation2) {
+		super(title, "");
+		this.clientLocation = clientLocation2;
+		this.locationGroupListDialog = locationGroupListDialog;
+		setWidth("400px");
+		createControls();
+		center();
+	}
+
+	private void initGridData() {
+		// List<ClientLocation> locationList = getCompany().getLocations();
+		// for (ClientLocation clientItem : locationList) {
+		// if (clientItem.getLocationName() != null) {
+		// locationGroupListDialog.add
+		// continue;
+		// }
+		// }
+		// }
 	}
 
 	private void createControls() {
@@ -36,36 +61,45 @@ public class AddnewLocationDialog extends BaseDialog<ClientLocation> {
 		locationName.setRequired(true);
 		VerticalPanel layout = new VerticalPanel();
 		form.setItems(locationName);
+		if (this.clientLocation != null) {
+			locationName.setValue(clientLocation.getName());
+		}
 		layout.add(form);
 		setBodyLayout(layout);
 	}
 
 	@Override
 	protected ValidationResult validate() {
-		return form.validate();
+		ValidationResult result = form.validate();
+		if (locationGroupListDialog != null)
+			result.add(locationGroupListDialog.validate());
+		return result;
 	}
 
 	@Override
 	protected boolean onOK() {
-		if (successCallback != null) {
-			successCallback.execute(createLocation());
-		}
-		return true;
-	}
-
-	/**
-	 * creating the ClientLocation
-	 * 
-	 * @return {@link ClientLocation}
-	 */
-	private ClientLocation createLocation() {
-		ClientLocation location = new ClientLocation();
-		location.setLocationName(locationName.getValue());
-		return location;
+		return locationGroupListDialog.onOK();
 	}
 
 	public void addSuccessCallback(
 			ValueCallBack<ClientLocation> newContactHandler) {
 		this.successCallback = newContactHandler;
+	}
+
+	public String getLocationGroupName() {
+		return this.locationName.getValue().toString();
+	}
+
+	public ClientLocation createOrEditLocation() {
+		if (clientLocation == null) {
+			clientLocation = new ClientLocation();
+			this.clientLocation.setLocationName(locationName.getValue()
+					.toString());
+		} else {
+			this.clientLocation.setLocationName(locationName.getValue()
+					.toString());
+		}
+
+		return this.clientLocation;
 	}
 }
