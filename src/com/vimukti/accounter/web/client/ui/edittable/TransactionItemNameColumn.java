a8dc2b94add1519cf090ee43b827806547a93f8b
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-09 11:49:34 +0000

    Former-commit-id: 8a22f71d543a46b712a0e30783a1261422e9032a

--- src/com/vimukti/accounter/web/client/ui/edittable/TransactionItemNameColumn.java
+++ src/com/vimukti/accounter/web/client/ui/edittable/TransactionItemNameColumn.java
@@ -1,12 +1,14 @@
 package com.vimukti.accounter.web.client.ui.edittable;
 
 import com.vimukti.accounter.web.client.core.ClientAccount;
+import com.vimukti.accounter.web.client.core.ClientCompany;
 import com.vimukti.accounter.web.client.core.ClientItem;
 import com.vimukti.accounter.web.client.core.ClientTransactionItem;
 import com.vimukti.accounter.web.client.core.IAccountable;
 import com.vimukti.accounter.web.client.core.ListFilter;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.ItemView;
+import com.vimukti.accounter.web.client.ui.core.DecimalUtil;
 
 public abstract class TransactionItemNameColumn extends
 		ComboColumn<ClientTransactionItem, IAccountable> {
@@ -30,7 +32,6 @@ public abstract class TransactionItemNameColumn extends
 				public boolean filter(ClientItem e) {
 					return true;
 				}
-
 			});
 
 	@Override
@@ -61,6 +62,25 @@ public abstract class TransactionItemNameColumn extends
 	@Override
 	public int getWidth() {
 		return 150;
+	}
+
+	@Override
+	protected void setValue(ClientTransactionItem row, IAccountable newValue) {
+		row.setAccountable(newValue);
+		if (newValue != null) {
+			ClientItem selectItem = (ClientItem) newValue;
+			row.setUnitPrice(selectItem.getSalesPrice());
+			row.setTaxable(selectItem.isTaxable());
+			double lt = row.getQuantity().getValue() * row.getUnitPrice();
+			double disc = row.getDiscount();
+			row.setLineTotal(DecimalUtil.isGreaterThan(disc, 0) ? (lt - (lt
+					* disc / 100)) : lt);
+
+			if (Accounter.getCompany().getAccountingType() == ClientCompany.ACCOUNTING_TYPE_UK) {
+				row.setTaxCode(selectItem.getTaxCode() != 0 ? selectItem
+						.getTaxCode() : 0);
+			}
+		}
 	}
 
 	@Override
