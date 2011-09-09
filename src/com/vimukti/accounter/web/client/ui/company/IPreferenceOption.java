ea2251b30a3d849fc71d991e8453258eaeaaf5ff
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-09 10:01:37 +0000

    Former-commit-id: 5e4b2329bd21d42da4ca6ad57350da277a88d93f

---
+++ src/com/vimukti/accounter/web/client/ui/company/IPreferenceOption.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.vimukti.accounter.web.client.ui.company;
+
+/**
+ * @author Prasanna Kumar G
+ * 
+ */
+public interface IPreferenceOption {
+
+	public String getTitle();
+
+	public void onSave();
+
+	public void onLoad();
+
+	public String getAnchor();
+
+}
