12f5cb198f813d7999b39a1eb9433fab4fc96a31
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-09 10:36:27 +0000

    Former-commit-id: cc08fb5a7fa6a841dd4f962aa94b05aaad7e1f19

---
+++ src/com/vimukti/accounter/web/client/ui/company/options/AbstractOptionPanel.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.vimukti.accounter.web.client.ui.company.options;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.vimukti.accounter.web.client.ui.company.IPreferenceOption;
+
+/**
+ * @author Prasanna Kumar G
+ * 
+ */
+public abstract class AbstractOptionPanel extends Composite implements
+		IPreferenceOption {
+
+	@Override
+	public String getAnchor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		super.onLoad();
+	}
+
+}
