caf609f64634a6f640057ef2041f74715c4b63b5
Devaraju K <devaraju.k@vimukti.com>
2011-09-12 06:40:41 +0000

    Former-commit-id: 99b8d5de87460decf742880e3b1fe80b5e330bb3

--- src/com/vimukti/accounter/web/client/ui/company/options/AbstractPreferenceOption.java
+++ src/com/vimukti/accounter/web/client/ui/company/options/AbstractPreferenceOption.java
@@ -4,15 +4,23 @@
 package com.vimukti.accounter.web.client.ui.company.options;
 
 import com.google.gwt.user.client.ui.Composite;
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.externalization.AccounterConstants;
 import com.vimukti.accounter.web.client.externalization.AccounterMessages;
 import com.vimukti.accounter.web.client.ui.Accounter;
+
 
 /**
  * @author Prasanna Kumar G
  * 
  */
 public abstract class AbstractPreferenceOption extends Composite {
+
+	protected static ClientCompanyPreferences companyPreferences = Global.get()
+			.preferences();
+	protected static ClientCompany company = Accounter.getCompany();
 
 	@Override
 	public void onLoad() {
