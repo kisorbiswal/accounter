e8527d72e71e33f82b0070aeebfe257d4e1d315c
Devaraju K <devaraju.k@vimukti.com>
2011-09-13 12:47:49 +0000

    Former-commit-id: e29f5d0931947f7dda25a0d1e9bd90941abf06b1

--- src/com/vimukti/accounter/web/client/ui/company/PreferencesAction.java
+++ src/com/vimukti/accounter/web/client/ui/company/PreferencesAction.java
@@ -3,11 +3,11 @@ package com.vimukti.accounter.web.client.ui.company;
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.company.options.PreferenceSettingsView;
 import com.vimukti.accounter.web.client.ui.core.Action;
-import com.vimukti.accounter.web.client.uibinder.companypreferences.CompanyInfoPage;
 
 public class PreferencesAction extends Action {
-	private CompanyInfoPage page;
+	private PreferenceSettingsView page;
 	public static int CATEGORY;
 	public static final int COMPANY = 1;
 	public static final int SETTINGS = 2;
@@ -25,7 +25,7 @@ public class PreferencesAction extends Action {
 	public void run() {
 
 		try {
-			page = new CompanyInfoPage();
+			page = new PreferenceSettingsView();
 			MainFinanceWindow.getViewManager().showView(page, null, true,
 					PreferencesAction.this);
 		} catch (Exception e) {
