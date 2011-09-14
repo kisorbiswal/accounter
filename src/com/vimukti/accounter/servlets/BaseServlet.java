87dd6ba3217ce54a7679cf16f138e04bbc687b44
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-14 09:08:05 +0000

    Former-commit-id: c2512ca626f98cecb491fbe6e173abbbfc9dafa4

--- src/com/vimukti/accounter/servlets/BaseServlet.java
+++ src/com/vimukti/accounter/servlets/BaseServlet.java
@@ -324,8 +324,8 @@ public class BaseServlet extends HttpServlet {
 	protected String buildMainServerURL(String url) {
 		StringBuilder mainServerURL = new StringBuilder("http://");
 		mainServerURL.append(ServerConfiguration.getMainServerDomain());
-		// mainServerURL.append(':');
-		// mainServerURL.append(ServerConfiguration.getMainServerPort());
+		mainServerURL.append(':');
+		mainServerURL.append(ServerConfiguration.getMainServerPort());
 		mainServerURL.append(url);
 		return mainServerURL.toString();
 	}
@@ -341,8 +341,8 @@ public class BaseServlet extends HttpServlet {
 
 		StringBuilder mainServerURL = new StringBuilder("http://");
 		mainServerURL.append(companyServerAddress);
-		// mainServerURL.append(':');
-		// mainServerURL.append(ServerConfiguration.getMainServerPort());
+		mainServerURL.append(':');
+		mainServerURL.append(ServerConfiguration.getMainServerPort());
 		mainServerURL.append(url);
 		return mainServerURL.toString();
 	}
