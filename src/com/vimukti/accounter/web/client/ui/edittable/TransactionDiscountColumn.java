58e1ac248035769653b61c92b090b9b3e408419a
Varakumar Pjd <varakumar.pjd@vimukti.com>
2011-09-17 14:03:54 +0000

    Former-commit-id: 3b906b10ad420e83e0d2bd22b690382971144c3f

--- src/com/vimukti/accounter/web/client/ui/edittable/TransactionDiscountColumn.java
+++ src/com/vimukti/accounter/web/client/ui/edittable/TransactionDiscountColumn.java
@@ -3,6 +3,7 @@ package com.vimukti.accounter.web.client.ui.edittable;
 import com.google.gwt.user.client.ui.TextBox;
 import com.vimukti.accounter.web.client.core.ClientTransactionItem;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.core.DecimalUtil;
 
 public class TransactionDiscountColumn extends
 		AmountColumn<ClientTransactionItem> {
@@ -15,6 +16,13 @@ public class TransactionDiscountColumn extends
 	@Override
 	protected void setAmount(ClientTransactionItem row, double value) {
 		row.setDiscount(value);
+		if (row.getType() != ClientTransactionItem.TYPE_SALESTAX) {
+			// TODO doubt, currencyConversion.
+			double lt = row.getQuantity().getValue() * row.getUnitPrice();
+			double disc = row.getDiscount();
+			row.setLineTotal(DecimalUtil.isGreaterThan(disc, 0) ? (lt - (lt
+					* disc / 100)) : lt);
+		}
 		getTable().update(row);
 	}
 
