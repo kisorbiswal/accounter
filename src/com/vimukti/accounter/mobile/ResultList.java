ad86268b187561608dfb5f569423bc1999cb834c
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-17 11:50:42 +0000

    Former-commit-id: f44df30dc026cb8e0fefd483080765d5bcdfd5a7

--- src/com/vimukti/accounter/mobile/ResultList.java
+++ src/com/vimukti/accounter/mobile/ResultList.java
@@ -12,6 +12,18 @@ public class ResultList extends ArrayList<Record> {
 
 	private boolean isMultiSelection;
 
+	/**
+	 * Creates new Instance
+	 */
+	public ResultList(String name) {
+		this(name, false);
+	}
+
+	public ResultList(String name, boolean isMultiSelection) {
+		this.name = name;
+		this.isMultiSelection = isMultiSelection;
+	}
+
 	public boolean isMultiSelection() {
 		return isMultiSelection;
 	}
