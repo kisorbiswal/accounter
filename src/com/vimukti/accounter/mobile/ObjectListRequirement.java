635d06249b6f816f3c7a01d4fc82778148dd39ff
Rajesh Akkineni <rajesh@vimukti.com>
2011-09-17 07:14:19 +0000

    Former-commit-id: 5326c5f8aad0c32561be8c3a62bcd86490e137e7

---
+++ src/com/vimukti/accounter/mobile/ObjectListRequirement.java
@@ -0,0 +1,7 @@
+package com.vimukti.accounter.mobile;
+
+import java.util.List;
+
+public abstract class ObjectListRequirement extends Requirement {
+	public abstract void addRequirements(List<Requirement> list);
+}
