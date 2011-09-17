a83b9da82b7276b3d77f325624ea83ced672c937
Nagaraju P <nagaraju.p@vimukti.com>
2011-09-17 11:23:15 +0000

    Former-commit-id: 80ca65e37949523739f59d9053f2033767cc31f3

---
+++ src/com/vimukti/accounter/mobile/commands/AbstractTransactionCommand.java
@@ -0,0 +1,135 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import com.vimukti.accounter.core.Customer;
+import com.vimukti.accounter.core.Item;
+import com.vimukti.accounter.core.TransactionItem;
+import com.vimukti.accounter.mobile.Command;
+import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.RequirementType;
+import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
+
+public abstract class AbstractTransactionCommand extends Command {
+	private static final int ITEMS_TO_SHOW = 5;
+	private static final int CUSTOMERS_TO_SHOW = 5;
+
+	protected Result itemsRequirement(Context context) {
+		Requirement itemsReq = get("items");
+		if (!itemsReq.isDone()) {
+			List<TransactionItem> transactionItems = context.getSelections();
+			if (transactionItems.size() > 0) {
+				itemsReq.setValue(transactionItems);
+			} else {
+				return itemsResult(itemsReq, context);
+			}
+		}
+		return null;
+	}
+
+	protected Result itemsResult(Requirement itemsReq, Context context) {
+		Result result = context.makeResult();
+		List<Item> items = getItems(context.getSession());
+		ResultList list = new ResultList();
+		Object last = context.getLast(RequirementType.ITEM);
+		int num = 0;
+		if (last != null) {
+			list.add(creatItemRecord((Item) last));
+			num++;
+		}
+		for (Item item : items) {
+			if (item != last) {
+				list.add(creatItemRecord(item));
+				num++;
+			}
+			if (num == ITEMS_TO_SHOW) {
+				break;
+			}
+		}
+		list.setMultiSelection(true);
+		if (list.size() > 0) {
+			result.add("Slect an Item(s).");
+		} else {
+			result.add("You don't have Items.");
+		}
+
+		result.add(list);
+		CommandList commands = new CommandList();
+		commands.add("Create New Item");
+		return result;
+	}
+
+	protected Result customerRequirement(Context context) {
+		Requirement customerReq = get("customer");
+		if (!customerReq.isDone()) {
+			Customer customer = context.getSelection();
+			if (customer != null) {
+				customerReq.setValue(customer);
+			} else {
+				return customerResult(context);
+			}
+		}
+		return null;
+	}
+
+	protected Result customerResult(Context context) {
+		Result result = context.makeResult();
+		ResultList customersList = new ResultList();
+
+		Object last = context.getLast(RequirementType.CUSTOMER);
+		int num = 0;
+		if (last != null) {
+			customersList.add(createCustomerRecord((Customer) last));
+			num++;
+		}
+		List<Customer> customers = getCustomers(context.getSession());
+		for (Customer customer : customers) {
+			if (customer != last) {
+				customersList.add(createCustomerRecord(customer));
+				num++;
+			}
+			if (num == CUSTOMERS_TO_SHOW) {
+				break;
+			}
+		}
+		int size = customersList.size();
+		StringBuilder message = new StringBuilder();
+		if (size > 0) {
+			message.append("Select a Customer");
+		}
+		CommandList commandList = new CommandList();
+		commandList.add("Create");
+
+		result.add(message.toString());
+		result.add(customersList);
+		result.add(commandList);
+		result.add("Type for Customer");
+		return result;
+	}
+
+	private Record creatItemRecord(Item item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List<Item> getItems(Session session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List<Customer> getCustomers(Session session) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	protected Record createCustomerRecord(Customer last) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- src/com/vimukti/accounter/mobile/commands/NewInvoiceCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewInvoiceCommand.java
@@ -3,22 +3,19 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.Session;
-
+import com.vimukti.accounter.core.Address;
+import com.vimukti.accounter.core.Contact;
 import com.vimukti.accounter.core.Customer;
-import com.vimukti.accounter.mobile.Command;
-import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.core.PaymentTerms;
+import com.vimukti.accounter.core.TransactionItem;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.ObjectListRequirement;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.RequirementType;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
 
-public class NewInvoiceCommand extends Command {
-
-	private static final int CUSTOMERS_TO_SHOW = 5;
+public class NewInvoiceCommand extends AbstractTransactionCommand {
 
 	@Override
 	public String getId() {
@@ -40,10 +37,11 @@ public class NewInvoiceCommand extends Command {
 				list.add(new Requirement("vatCode", true, true));
 			}
 		});
+
 		list.add(new Requirement("date", true, true));
 		list.add(new Requirement("number", true, false));
 		list.add(new Requirement("paymentTerms", true, true));
-		list.add(new Requirement("contac", true, true));
+		list.add(new Requirement("contact", true, true));
 		list.add(new Requirement("billTo", true, true));
 		list.add(new Requirement("shipTo", true, true));
 		list.add(new Requirement("due", true, true));
@@ -53,63 +51,113 @@ public class NewInvoiceCommand extends Command {
 
 	@Override
 	public Result run(Context context) {
-		Requirement customerReq = get("customer");
-		if (!customerReq.isDone()) {
-			return customerResult(context);
-		} else {
+		Result result = customerRequirement(context);
+		if (result == null) {
 			// TODO
 		}
 
-		Requirement itemsReq = get("items");
-		if (!itemsReq.isDone()) {
-			// return
+		result = itemsRequirement(context);
+		if (result == null) {
+			// TODO
 		}
 
+		result = createOptionalResult(context);
+		if (result == null) {
+			// TODO
+		}
 		return null;
 	}
 
-	private Record createDateRecord(Date date) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Result customerResult(Context context) {
+	private Result createOptionalResult(Context context) {
+		
 		Result result = context.makeResult();
-		ResultList customersList = new ResultList();
-		Object last = context.getLast(RequirementType.CUSTOMER);
-		if (last != null) {
-			customersList.add(createCustomerRecord((Customer) last));
-		}
-		List<Customer> customers = getCustomers(context.getSession());
-		for (int i = 0; i < CUSTOMERS_TO_SHOW || i < customers.size(); i++) {
-			Customer customer = customers.get(i);
-			if (customer != last) {
-				customersList.add(createCustomerRecord(customer));
-			}
-		}
-		int size = customersList.size();
-		StringBuilder message = new StringBuilder();
-		if (size > 0) {
-			message.append("Select a Customer");
-		}
-		CommandList commandList = new CommandList();
-		commandList.add("Create");
 
-		result.add(message.toString());
-		result.add(customersList);
-		result.add(commandList);
-		result.add("Type for Customer");
+		result.add("Invoice is ready to create with following values.");
+		ResultList list = new ResultList();
+
+		Customer customer = (Customer) get("customer").getValue();
+		Record custRecord = new Record(customer);
+		custRecord.add("Name", "Customer");
+		custRecord.add("Value", customer.getName());
+		list.add(custRecord);
+
+		Date transDate = (Date) get("date").getDefaultValue();
+		Record transDateRecord = new Record(transDate);
+		transDateRecord.add("Name", "Invoice Date");
+		transDateRecord.add("Value", transDate.toString());
+		list.add(transDateRecord);
+
+		Contact contact = (Contact) get("contact").getValue();
+		Record contactRecord = new Record(contact);
+		contactRecord.add("Name", "Customer Contact");
+		contactRecord.add("Value", contact.getName());
+		list.add(contactRecord);
+
+		String invoiceNo = (String) get("number").getValue();
+		Record invoiceNoRec = new Record(invoiceNo);
+		invoiceNoRec.add("Name", "Invoice Number");
+		invoiceNoRec.add("Value", invoiceNo);
+		list.add(invoiceNoRec);
+
+		PaymentTerms paymentTerm = (PaymentTerms) get("paymentTerms")
+				.getValue();
+		Record paymentTermRecord = new Record(paymentTerm);
+		paymentTermRecord.add("Name", "Payment Terms");
+		paymentTermRecord.add("Value", paymentTerm.getName());
+		list.add(paymentTermRecord);
+
+		Address billTo = (Address) get("billTo").getValue();
+		Record billToRecord = new Record(billTo);
+		billToRecord.add("Name", "Bill To");
+		billToRecord.add("Value", billTo.toString());
+		list.add(billToRecord);
+
+		Address shipTo = (Address) get("shipTo").getValue();
+		Record shipToRecord = new Record(shipTo);
+		shipToRecord.add("Name", "Ship To");
+		shipToRecord.add("Value", shipTo.toString());
+		list.add(shipToRecord);
+
+		Date dueDate = (Date) get("due").getDefaultValue();
+		Record dueDateRecord = new Record(dueDate);
+		dueDateRecord.add("Name", "Due Date");
+		dueDateRecord.add("Value", dueDate.toString());
+		list.add(dueDateRecord);
+
+		String orderNo = (String) get("orderNo").getDefaultValue();
+		Record orderNoRecord = new Record(orderNo);
+		orderNoRecord.add("Name", "Order No");
+		orderNoRecord.add("Value", orderNo);
+		list.add(orderNoRecord);
+
+		String memo = (String) get("memo").getDefaultValue();
+		Record memoRecord = new Record(orderNo);
+		memoRecord.add("Name", "Memo");
+		memoRecord.add("Value", memo);
+		list.add(memoRecord);
+
+		result.add(list);
+
+		result.add("Items:-");
+		ResultList items = new ResultList();
+		List<TransactionItem> transItems = get("items").getValue();
+		for (TransactionItem item : transItems) {
+			Record itemRec = new Record(item);
+			itemRec.add("Name", item.getItem().getName());
+			itemRec.add("Total", item.getLineTotal());
+			itemRec.add("VatCode", item.getVATfraction());
+		}
+		result.add(items);
+
+		ResultList actions = new ResultList();
+		Record moreItems = new Record(null);
+		moreItems.add("", "Add more items");
+		actions.add(moreItems);
+		Record finish = new Record(null);
+		finish.add("", "Finish to create Invoice.");
+		actions.add(finish);
+		result.add(actions);
+
 		return result;
 	}
-
-	private List<Customer> getCustomers(Session session) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private Record createCustomerRecord(Customer last) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
