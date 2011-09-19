b8e54c713a3b15b7c31ed00fbc99b00255d85662
Amrit Mishra <amrit.mishra@vimukti.com>
2011-09-19 11:02:35 +0000

    Former-commit-id: 9de044a348f60a66e2217cf03ac7081a8ec13aa3

---
+++ src/com/vimukti/accounter/mobile/commands/NewSalesOrderCommand.java
@@ -0,0 +1,55 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.ObjectListRequirement;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+
+public class NewSalesOrderCommand extends AbstractTransactionCommand {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+
+		list.add(new Requirement("customer", false, true));
+		list.add(new Requirement("status", false, true));
+		list.add(new ObjectListRequirement("items", false, true) {
+
+			@Override
+			public void addRequirements(List<Requirement> list) {
+				list.add(new Requirement("name", false, true));
+				list.add(new Requirement("desc", true, true));
+				list.add(new Requirement("quantity", true, true));
+				list.add(new Requirement("price", true, true));
+				list.add(new Requirement("discount", true, true));
+				list.add(new Requirement("total", true, true));
+			}
+		});
+
+		list.add(new Requirement("contact", true, false));
+		list.add(new Requirement("phone", true, true));
+		list.add(new Requirement("billto", true, false));
+		list.add(new Requirement("date", true, true));
+		list.add(new Requirement("orderno", true, true));
+		list.add(new Requirement("customerorderno", true, true));
+		list.add(new Requirement("paymentterms", true, true));
+		list.add(new Requirement("shippingterms", true, true));
+		list.add(new Requirement("shippingmethods", true, true));
+		list.add(new Requirement("duedate", true, true));
+
+	}
+
+	@Override
+	public Result run(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
