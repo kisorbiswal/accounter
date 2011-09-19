6cd3410b3b714323e4a3e56f8f213bd81b4f6940
Amrit Mishra <amrit.mishra@vimukti.com>
2011-09-19 11:26:47 +0000

    Former-commit-id: 5220e8c0c7d2531dd7942344e2f889cb28b28fc4

---
+++ src/com/vimukti/accounter/mobile/commands/PurchaseOrderListCommand.java
@@ -0,0 +1,30 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Command;
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+
+public class PurchaseOrderListCommand extends Command {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Result run(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
