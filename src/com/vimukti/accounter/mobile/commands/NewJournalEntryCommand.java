b59f78591e41566bba46c5beb77248078e798008
Saiprasad N <saiprasad.n@vimukti.com>
2011-09-20 09:11:38 +0000

    Former-commit-id: bd6f5bb06afc334bad0a97c932a161e0387c9b85

--- src/com/vimukti/accounter/mobile/commands/NewJournalEntryCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewJournalEntryCommand.java
@@ -2,18 +2,30 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.List;
 
-import com.vimukti.accounter.mobile.Command;
+import com.vimukti.accounter.core.Entry;
+import com.vimukti.accounter.core.TransactionItem;
+import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.ObjectListRequirement;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 
 /**
  * 
  * @author Sai Prasad N
- *
+ * 
  */
 public class NewJournalEntryCommand extends AbstractTransactionCommand {
+
+	private static final String TRANSACTION_DATE = "TransactionDate";
+	private static final String VOUCHER = "Voucher";
+	private static final String VOUCHER_NUMBER = "Voucher Number";
+	private static final String DATE = "Date";
+	private static final String ACCOUNT = "Account";
+	private static final String CREDIT = "Credit";
+	private static final String DEBIT = "Debit";
+	private static final String MEMO = "memo";
 
 	@Override
 	public String getId() {
@@ -23,39 +35,100 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement("TransactionDate", false, true));
-		list.add(new ObjectListRequirement("voucher", false, true) {
+		list.add(new Requirement(TRANSACTION_DATE, false, true));
+		list.add(new ObjectListRequirement(VOUCHER, false, true) {
 
 			@Override
 			public void addRequirements(List<Requirement> list) {
-				list.add(new Requirement("VoucherNumber", false, true));
-				list.add(new Requirement("Date", false, true));
-				list.add(new Requirement("Accont", false, true));
-				list.add(new Requirement("Credit", false, true));
-				list.add(new Requirement("Debit", false, true));
-
+				list.add(new Requirement(VOUCHER_NUMBER, false, true));
+				list.add(new Requirement(DATE, false, true));
+				list.add(new Requirement(ACCOUNT, false, true));
+				list.add(new Requirement(CREDIT, false, true));
+				list.add(new Requirement(DEBIT, false, true));
 			}
 		});
-		list.add(new ObjectListRequirement("voucher", false, true) {
-
-			@Override
-			public void addRequirements(List<Requirement> list) {
-				list.add(new Requirement("VoucherNumber", false, true));
-				list.add(new Requirement("Date", false, true));
-				list.add(new Requirement("Accont", false, true));
-				list.add(new Requirement("Credit", false, true));
-				list.add(new Requirement("Debit", false, true));
-
-			}
-		});
-
-		list.add(new Requirement("Memo", true, true));
+		
+		list.add(new Requirement(MEMO, true, true));
 
 	}
 
 	@Override
 	public Result run(Context context) {
+		Result result = null;
+		result = entryRequirement(context);
+		if (result != null) {
+			return result;
+		}
+		result = createOptionalResult(context);
+		if (result != null) {
+			return result;
+		}
+
+		return null;
+	}
+
+	private Result createOptionalResult(Context context) {
+		context.setAttribute(INPUT_ATTR, "optional");
+
+		Object selection = context.getSelection(ACTIONS);
+		if (selection != null) {
+			ActionNames actionName = (ActionNames) selection;
+			switch (actionName) {
+			case ADD_MORE_ITEMS:
+				return items(context);
+			case FINISH:
+				context.removeAttribute(INPUT_ATTR);
+				return null;
+			default:
+				break;
+			}
+		}
+
+		Result result = null;
+		Requirement entriesReq = get("entries");
+		List<Entry> entries = entriesReq.getValue();
+
+		selection = context.getSelection("entries");
+		if (selection != null) {
+			// TODO
+		}
+		
+		ResultList list = new ResultList("values");
+		result=transactionDateRequirement(context, list, selection);
+		if (result != null) {
+			return result;
+		}
+		
+
+		return null;
+	}
+
+	private Result transactionDateRequirement(Context context, ResultList list,
+			Object selection) {
 		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Result entryRequirement(Context context) {
+		Requirement entryReq = get("entries");
+		List<Entry> entries = context.getSelections("entries");
+		if (!entryReq.isDone()) {
+			if (entries.size() > 0) {
+				entryReq.setValue(entries);
+			} else {
+				return Entries(context);
+			}
+		}
+		if (entries != null && entries.size() > 0) {
+			List<Entry> entries1 = entryReq.getValue();
+			entries1.addAll(entries);
+		}
+		return null;
+	}
+
+	private Result Entries(Context context) {
+		// TODO Auto-generated method stub
+
 		return null;
 	}
 
