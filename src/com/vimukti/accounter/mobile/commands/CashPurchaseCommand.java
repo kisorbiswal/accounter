9a60dbb8110f6e4defb27a9fdce5aa779ecb424e
Devaraju K <devaraju.k@vimukti.com>
2011-09-20 05:53:29 +0000

    Former-commit-id: 3c54ec4a485a68afc848df14723a3d602911c383

---
+++ src/com/vimukti/accounter/mobile/commands/CashPurchaseCommand.java
@@ -0,0 +1,29 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+
+public class CashPurchaseCommand extends AbstractTransactionCommand {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+		// list.add(arg0)
+
+	}
+
+	@Override
+	public Result run(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
