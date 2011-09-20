457cff208738565210038471eed1d1bbe8886f59
Lingarao R <lingarao.r@vimukti.com>
2011-09-20 09:56:17 +0000

    Former-commit-id: d3f257443ef9c34f5159c7817668fdccc64f8c20

--- src/com/vimukti/accounter/mobile/commands/NewInvoiceCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewInvoiceCommand.java
@@ -384,32 +384,6 @@ public class NewInvoiceCommand extends AbstractTransactionCommand {
 		return result;
 	}
 
-	private Result memoRequirement(Context context, ResultList list,
-			Object selection) {
-		Requirement req = get("memo");
-		String memo = (String) req.getValue();
-		String attribute = (String) context.getAttribute(INPUT_ATTR);
-		if (attribute.equals("memo")) {
-			String input = context.getSelection(TEXT);
-			if (input == null) {
-				input = context.getString();
-			}
-			memo = input;
-			req.setValue(memo);
-		}
-
-		if (selection == memo) {
-			context.setAttribute(attribute, "memo");
-			return text(context, "Enter Invoice memo", memo);
-		}
-
-		Record memoRecord = new Record(memo);
-		memoRecord.add("Name", "Memo");
-		memoRecord.add("Value", memo);
-		list.add(memoRecord);
-		return null;
-	}
-
 	private Result orderNoRequirement(Context context, ResultList list,
 			Object selection) {
 
