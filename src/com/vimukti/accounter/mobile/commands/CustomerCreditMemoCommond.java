1f64ad0eb310d4324be024b67835744fb2918025
Devaraju K <devaraju.k@vimukti.com>
2011-09-20 12:46:10 +0000

    Former-commit-id: a56ec01285b857246a892c1f48d7a0f76be212d3

---
+++ src/com/vimukti/accounter/mobile/commands/CustomerCreditMemoCommond.java
@@ -0,0 +1,58 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.ObjectListRequirement;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+
+public class CustomerCreditMemoCommond extends AbstractTransactionCommand {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+
+		list.add(new Requirement("customer", false, true));
+		list.add(new ObjectListRequirement("items", false, true) {
+
+			@Override
+			public void addRequirements(List<Requirement> list) {
+				list.add(new Requirement("name", false, true));
+				list.add(new Requirement("desc", true, true));
+				list.add(new Requirement("quantity", true, true));
+				list.add(new Requirement("price", true, true));
+				list.add(new Requirement("vatCode", true, true));
+			}
+		});
+		list.add(new ObjectListRequirement("accounts", false, true) {
+
+			@Override
+			public void addRequirements(List<Requirement> list) {
+				list.add(new Requirement("name", false, true));
+				list.add(new Requirement("desc", true, true));
+				list.add(new Requirement("quantity", false, true));
+				list.add(new Requirement("price", false, true));
+			}
+		});
+		list.add(new Requirement("date", true, true));
+		list.add(new Requirement("number", true, false));
+		list.add(new Requirement("contact", true, true));
+		list.add(new Requirement("memo", true, true));
+		list.add(new Requirement("depositOrTransferTo", false, true));
+		list.add(new Requirement("deliveryDate", true, true));
+
+	}
+
+	@Override
+	public Result run(Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
