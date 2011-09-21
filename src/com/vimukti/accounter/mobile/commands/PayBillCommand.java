4c637a5f8f49c2f706c78ed1d6f302f0a5ffc048
Saiprasad N <saiprasad.n@vimukti.com>
2011-09-21 06:57:53 +0000

    Former-commit-id: ac45957620b7a130059203a323845b9bfdd7f5bf

--- src/com/vimukti/accounter/mobile/commands/PayBillCommand.java
+++ src/com/vimukti/accounter/mobile/commands/PayBillCommand.java
@@ -2,10 +2,19 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.List;
 
+import com.vimukti.accounter.core.Vendor;
+import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 
+/**
+ * 
+ * @author Sai Prasad N
+ * 
+ */
 public class PayBillCommand extends AbstractTransactionCommand {
 
 	@Override
@@ -19,6 +28,7 @@ public class PayBillCommand extends AbstractTransactionCommand {
 		list.add(new Requirement(VENDOR, false, true));
 		list.add(new Requirement(PAY_FROM, false, true));
 		list.add(new Requirement(PAYMENT_MENTHOD, false, true));
+		list.add(new Requirement(DATE, true, true));
 
 	}
 
@@ -43,7 +53,51 @@ public class PayBillCommand extends AbstractTransactionCommand {
 		if (result != null) {
 			return result;
 		}
-		
+		result = paymentMethodRequirement(context);
+		if (result != null) {
+			return result;
+		}
+		result = createOptionalResult(context);
+		if (result != null) {
+			return result;
+		}
+
+		return null;
+	}
+
+	private Result createOptionalResult(Context context) {
+		context.setAttribute(INPUT_ATTR, "optional");
+
+		Object selection = context.getSelection(ACTIONS);
+		if (selection != null) {
+			ActionNames actionName = (ActionNames) selection;
+			switch (actionName) {
+			case ADD_MORE_ITEMS:
+				return items(context);
+			case FINISH:
+				context.removeAttribute(INPUT_ATTR);
+				return null;
+			default:
+				break;
+			}
+			selection = context.getSelection("values");
+			ResultList list = new ResultList("values");
+
+			Requirement vendorReq = get(VENDOR);
+			Vendor vendor = (Vendor) vendorReq.getValue();
+			Record vendorRecord = new Record(vendor);
+			vendorRecord.add("Name", VENDOR);
+			vendorRecord.add("Value", vendor.getName());
+
+			list.add(vendorRecord);
+
+			Result result = payFromRequirement(context, list, selection);
+			if (result != null) {
+				return result;
+			}
+			result = paymentMethodRequirement(context);
+
+		}
 		return null;
 	}
 
