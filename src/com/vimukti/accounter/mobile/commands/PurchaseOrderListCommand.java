f66c35405e2f6e5d160b183c815e64c0df92417a
Amrit Mishra <amrit.mishra@vimukti.com>
2011-09-21 07:07:12 +0000

    Former-commit-id: bd87ebf4b1ed4f068889b8d5f509a59a27b01b5f

--- src/com/vimukti/accounter/mobile/commands/PurchaseOrderListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/PurchaseOrderListCommand.java
@@ -1,13 +1,20 @@
 package com.vimukti.accounter.mobile.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.vimukti.accounter.mobile.Command;
+import org.hibernate.Session;
+
+import com.vimukti.accounter.core.PurchaseOrder;
 import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 
-public class PurchaseOrderListCommand extends Command {
+public class PurchaseOrderListCommand extends AbstractTransactionCommand {
+
+	private static final String CURRENT_VIEW = "Open";
 
 	@Override
 	public String getId() {
@@ -17,14 +24,138 @@ public class PurchaseOrderListCommand extends Command {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		// TODO Auto-generated method stub
+		list.add(new Requirement(CURRENT_VIEW, true, true));
 
 	}
 
 	@Override
 	public Result run(Context context) {
+		Result result = null;
+
+		result = createOptionalResult(context);
+		if (result != null) {
+			return result;
+		}
+		return null;
+	}
+
+	private Result createOptionalResult(Context context) {
+		context.setAttribute(INPUT_ATTR, "optional");
+
+		Object selection = context.getSelection(CURRENT_VIEW);
+
+		ResultList list = new ResultList("viewlist");
+		Result result = viewTypeRequirement(context, list, selection);
+		if (result != null) {
+			return result;
+		}
+		String viewType = get(CURRENT_VIEW).getValue();
+		result = purchaseOrderList(context, viewType);
+		return result;
+	}
+
+	private Result viewTypeRequirement(Context context, ResultList list,
+			Object selection) {
+
+		Object viewType = context.getSelection(CURRENT_VIEW);
+		Requirement viewReq = get(CURRENT_VIEW);
+		String view = viewReq.getValue();
+
+		if (selection == view) {
+			return viewTypes(context, view);
+
+		}
+		if (viewType != null) {
+			view = (String) viewType;
+			viewReq.setValue(view);
+		}
+
+		Record viewtermRecord = new Record(view);
+		viewtermRecord.add("Name", "viewType");
+		viewtermRecord.add("Value", view);
+		list.add(viewtermRecord);
+		return null;
+	}
+
+	private Result purchaseOrderList(Context context, String viewType) {
+		Result result = context.makeResult();
+		result.add("Purchase Order List");
+		ResultList purchaseList = new ResultList("purchaseOrderList");
+		int num = 0;
+		List<PurchaseOrder> orders = getPurchaseOrder(
+				context.getHibernateSession(), viewType);
+		for (PurchaseOrder order : orders) {
+			purchaseList.add(createNewPurchaseOrderRecord(order));
+			num++;
+			if (num == EXPENSES_TO_SHOW) {
+				break;
+			}
+		}
+		result.add(purchaseList);
+		return result;
+	}
+
+	private Record createNewPurchaseOrderRecord(PurchaseOrder order) {
+
+		Record record = new Record(order);
+		record.add("customer", order.getVendor());
+		record.add("phone", order.getPhone());
+		record.add("status", order.getStatus());
+		record.add("orderNo", order.getNumber());
+		record.add("paymentTerms", order.getPaymentTerm());
+		record.add("shippingMethods", order.getShippingMethod());
+
+		return record;
+	}
+
+	private List<PurchaseOrder> getPurchaseOrder(Session hibernateSession,
+			String viewType) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private Result viewTypes(Context context, String view) {
+		ResultList list = new ResultList("viewslist");
+		Result result = null;
+		List<String> viewTypes = getViewTypes();
+		result = context.makeResult();
+		result.add("Select View Type");
+
+		int num = 0;
+		if (view != null) {
+			list.add(createViewTypeRecord(view));
+			num++;
+		}
+		for (String v : viewTypes) {
+			if (v != view) {
+				list.add(createViewTypeRecord(v));
+				num++;
+			}
+			if (num == 0) {
+				break;
+			}
+
+		}
+
+		result.add(list);
+
+		return result;
+
+	}
+
+	private Record createViewTypeRecord(String view) {
+		Record record = new Record(view);
+		record.add("Name", "ViewType");
+		record.add("Value", view);
+		return record;
+	}
+
+	private List<String> getViewTypes() {
+		List<String> list = new ArrayList<String>();
+		list.add("Open");
+		list.add("Completed");
+		list.add("Canceled");
+		return list;
 	}
 
 }
