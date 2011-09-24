4a4ff5429f2d9cb5ccfbbe48891f66ddae12e356
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-09-24 10:57:59 +0000

    Former-commit-id: 692d06989abc35a5f0692a7debcc46a26061491d

--- src/com/vimukti/accounter/web/client/util/CountryPreferenceFactory.java
+++ src/com/vimukti/accounter/web/client/util/CountryPreferenceFactory.java
@@ -3,6 +3,243 @@ package com.vimukti.accounter.web.client.util;
 import com.vimukti.accounter.web.client.countries.*;
 
 public class CountryPreferenceFactory {
+	private static final String UNITED_KINGDOM = "United Kingdom";
+	private static final String UNITED_STATES = "United States";
+	private static final String AFGHANISTAN = "Afghanistan";
+	private static final String ALBANIA = "Albania";
+	private static final String ALGERIA = "Algeria";
+	private static final String AMERICAN_SAMOA = "American Samoa";
+	private static final String ANDORRA = "Andorra";
+	private static final String ANGUILLA = "Anguilla";
+	private static final String ANTARCTICA = "Antarctica";
+	private static final String ANTIGUA_AND_BARBUDA = "Antigua and Barbuda";
+	private static final String ARGENTINA = "Argentina";
+	private static final String ARMENIA = "Armenia";
+	private static final String ARUBA = "Aruba";
+	private static final String AUSTRIA = "Austria";
+	private static final String AUSTRALIA = "Australia";
+	private static final String AZERBAIJAN = "Azerbaijan";
+	private static final String BAHAMAS_THE = "Bahamas, The";
+	private static final String BAHRAIN = "Bahrain";
+	private static final String BANGLADESH = "Bangladesh";
+	private static final String BARBADOS = "Barbados";
+	private static final String BELARUS = "Belarus";
+	private static final String BELGIUM = "Belgium";
+	private static final String BELIZE = "Belize";
+	private static final String BENIN = "Benin";
+	private static final String BERMUDA = "Bermuda";
+	private static final String BHUTAN = "Bhutan";
+	private static final String BOLIVIA = "Bolivia";
+	private static final String BOSNIA_AND_HERZEGOVINA = "Bosnia and Herzegovina";
+	private static final String BOTSWANA = "Botswana";
+	private static final String BOUVET_ISLAND = "Bouvet Island";
+	private static final String BRAZIL = "Brazil";
+	private static final String BRITISH_INDIAN_OCEAN_TERRITORY = "British Indian Ocean Territory";
+	private static final String BRUNEI = "Brunei";
+	private static final String BULGARIA = "Bulgaria";
+	private static final String BURKINA_FASO = "Burkina Faso";
+	private static final String BURUNDI = "Burundi";
+	private static final String CAMBODIA = "Cambodia";
+	private static final String CAMEROON = "Cameroon";
+	private static final String CANADA = "Canada";
+	private static final String CAPE_VERDE = "Cape Verde";
+	private static final String CAYMAN_ISLANDS = "Cayman Islands";
+	private static final String CENTRAL_AFRICAN_REPUBLIC = "Central African Republic";
+	private static final String CHAD = "Chad";
+	private static final String CHILE = "Chile";
+	private static final String CHINA = "China";
+	private static final String CHRISTMAS_ISLAND = "Christmas Island";
+	private static final String COCOS_ISLANDS = "Cocos (Keeling) Islands";
+	private static final String COLOMBIA = "Colombia";
+	private static final String COMOROS = "Comoros";
+	private static final String CONGO_DEMOCRATIC = "Congo, Democratic Republic of the";
+	private static final String CONGO_REPUBLIC = "Congo, Republic of the";
+	private static final String COOK_ISLANDS = "Cook Islands";
+	private static final String COSTA_RICA = "Costa Rica";
+	private static final String COTE_DIVOIRE = "Cote d'Ivoire";
+	private static final String CROATIA = "Croatia";
+	private static final String CUBA = "Cuba";
+	private static final String CYPRUS = "Cyprus";
+	private static final String CZECH_REPUBLIC = "Czech Republic";
+	private static final String DENMARK = "Denmark";
+	private static final String DJIBOUTI = "Djibouti";
+	private static final String DOMINICA = "Dominica";
+	private static final String DOMINICAN_REPUBLIC = "Dominican Republic";
+	private static final String ECUADOR = "Ecuador";
+	private static final String EGYPT = "Egypt";
+	private static final String EL_SALVADOR = "El Salvador";
+	private static final String EQUATORIAL_GUINEA = "Equatorial Guinea";
+	private static final String ERITREA = "Eritrea";
+	private static final String ESTONIA = "Estonia";
+	private static final String ETHIOPIA = "Ethiopia";
+	private static final String FALKLAND_ISLANDS = "Falkland Islands (Islas Malvinas)";
+	private static final String FAROE_ISLANDS = "Faroe Islands";
+	private static final String FIJI = "Fiji";
+	private static final String FINLAND = "Finland";
+	private static final String FRANCE = "France";
+	private static final String FRENCH_GUIANA = "French Guiana";
+	private static final String FRENCH_POLYNESIA = "French Polynesia";
+	private static final String FRENCH_SOUTHERN_AND_ANTARCTIC_LANDS = "French Southern and Antarctic Lands";
+	private static final String GABON = "Gabon";
+	private static final String GAMBIA_THE = "Gambia, The";
+	private static final String GEORHIA = "Georgia";
+	private static final String GERMANY = "Germany";
+	private static final String GHANA = "Ghana";
+	private static final String GIBRALTAR = "Gibraltar";
+	private static final String GREECE = "Greece";
+	private static final String GREENLAND = "Greenland";
+	private static final String GRENADA = "Grenada";
+	private static final String GUADELOUPE = "Guadeloupe";
+	private static final String GUAM = "Guam";
+	private static final String GUATEMALA = "Guatemala";
+	private static final String GUINEA = "Guinea";
+	private static final String GUINEA_BISSAU = "Guinea-Bissau";
+	private static final String GUYANA = "Guyana";
+	private static final String HAITI = "Haiti";
+	private static final String HEARD_ISLAND_AND_MCDONALD_ISLANDS = "Heard Island and McDonald Islands";
+	private static final String HOLY_SEE = "Holy See (Vatican City)";
+	private static final String HONDURAS = "Honduras";
+	private static final String HONG_KONG = "Hong Kong";
+	private static final String HUNGARY = "Hungary";
+	private static final String ICELAND = "Iceland";
+	private static final String INDIA = "India";
+	private static final String INDONESIA = "Indonesia";
+	private static final String IRAN = "Iran";
+	private static final String IRAQ = "Iraq";
+	private static final String IRELAND = "Ireland";
+	private static final String ISLE_OF_MAN = "Isle of Man";
+	private static final String ISRAEL = "Israel";
+	private static final String ITALY = "Italy";
+	private static final String JAMAICA = "Jamaica";
+	private static final String JAN_MAYEN = "Jan Mayen";
+	private static final String JAPAN = "Japan";
+	private static final String JORDAN = "Jordan";
+	private static final String KAZAKHSTAN = "Kazakhstan";
+	private static final String KENYA = "Kenya";
+	private static final String KIRIBATI = "Kiribati";
+	private static final String KOREA_NORTH = "Korea, North";
+	private static final String KOREA_SOUTH = "Korea, South";
+	private static final String KUWAIT = "Kuwait";
+	private static final String KYRGYZSTAN = "Kyrgyzstan";
+	private static final String LAOS = "Laos";
+	private static final String LATVIA = "Latvia";
+	private static final String LEBANON = "Lebanon";
+	private static final String LESOTHO = "Lesotho";
+	private static final String LIBERIA = "Liberia";
+	private static final String LIBYA = "Libya";
+	private static final String LIECHTENSTEIN = "Liechtenstein";
+	private static final String LITHUANIA = "Lithuania";
+	private static final String LUXEMBOURG = "Luxembourg";
+	private static final String MACAO = "Macao";
+	private static final String MACEDONIA = "Macedonia";
+	private static final String MADAGASCAR = "Madagascar";
+	private static final String MALAWI = "Malawi";
+	private static final String MALAYSIA = "Malaysia";
+	private static final String MALDIVES = "Maldives";
+	private static final String MALI = "Mali";
+	private static final String MALTA = "Malta";
+	private static final String MARSHALL_ISLANDS = "Marshall Islands";
+	private static final String MARTINIQUE = "Martinique";
+	private static final String MAURITANIA = "Mauritania";
+	private static final String MAURITIUS = "Mauritius";
+	private static final String MAYOTTE = "Mayotte";
+	private static final String MEXICO = "Mexico";
+	private static final String MICRONESIA_FEDERATED = "Micronesia, Federated States of";
+	private static final String MOLDOVA = "Moldova";
+	private static final String MONACO = "Monaco";
+	private static final String MONGOLIA = "Mongolia";
+	private static final String MONTSERRAT = "Montserrat";
+	private static final String MOROCCO = "Morocco";
+	private static final String MOZAMBIQUE = "Mozambique";
+	private static final String NAMIBIA = "Namibia";
+	private static final String NAURU = "Nauru";
+	private static final String NEPAL = "Nepal";
+	private static final String NETHERLANDS = "Netherlands";
+	private static final String NETHERLANDS_ANTILLES = "Netherlands Antilles";
+	private static final String NEW_CALEDONIA = "New Caledonia";
+	private static final String NEW_ZEALAND = "New Zealand";
+	private static final String NICARAGUA = "Nicaragua";
+	private static final String NIGER = "Niger";
+	private static final String NIGERIA = "Nigeria";
+	private static final String NIUE = "Niue";
+	private static final String NORFOLK_ISLAND = "Norfolk Island";
+	private static final String NORTHERN_MARIANA_ISLANDS = "Northern Mariana Islands";
+	private static final String NORWAY = "Norway";
+	private static final String OMAN = "Oman";
+	private static final String PAKISTAN = "Pakistan";
+	private static final String PALAU = "Palau";
+	private static final String PANAMA = "Panama";
+	private static final String PAPUA_NEW_GUINEA = "Papua New Guinea";
+	private static final String PARAGUAY = "Paraguay";
+	private static final String PERU = "Peru";
+	private static final String PHILIPPINES = "Philippines";
+	private static final String PITCAIRN_ISLANDS = "Pitcairn Islands";
+	private static final String POLAN = "Poland";
+	private static final String PORTUGAL = "Portugal";
+	private static final String PUERTO_RICO = "Puerto Rico";
+	private static final String QATAR = "Qatar";
+	private static final String REUNION = "Reunion";
+	private static final String ROMANIA = "Romania";
+	private static final String RUSSIA = "Russia";
+	private static final String RWANDA = "Rwanda";
+	private static final String SAINT_HELENA = "Saint Helena";
+	private static final String SAINT_KITTS_AND_NEVIS = "Saint Kitts and Nevis";
+	private static final String SAINT_LUCIA = "Saint Lucia";
+	private static final String SAINT_PIERRE_AND_MIQUELON = "Saint Pierre and Miquelon";
+	private static final String SAINT_VINCENT_AND_THE_GRENADINES = "Saint Vincent and the Grenadines";
+	private static final String SAMOA = "Samoa";
+	private static final String SAN_MARINO = "San Marino";
+	private static final String SAO_TOME_AND_PRINCIPE = "Sao Tome and Principe";
+	private static final String SAUDI_ARABIA = "Saudi Arabia";
+	private static final String SENEGAL = "Senegal";
+	private static final String SERBIA_AND_MONTENEGRO = "Serbia and Montenegro";
+	private static final String SEYCHELLES = "Seychelles";
+	private static final String SIERRA_LEONE = "Sierra Leone";
+	private static final String SINGAPORE = "Singapore";
+	private static final String SLOVAKIA = "Slovakia";
+	private static final String SLOVENIA = "Slovenia";
+	private static final String SOLOMON_ISLANDS = "Solomon Islands";
+	private static final String SOMALIA = "Somalia";
+	private static final String SOUTH_AFRICA = "South Africa";
+	private static final String SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS = "South Georgia and the South Sandwich Islands";
+	private static final String SPAIN = "Spain";
+	private static final String SRI_LANKA = "Sri Lanka";
+	private static final String SUDAN = "Sudan";
+	private static final String SURINAME = "Suriname";
+	private static final String SVALBARD = "Svalbard";
+	private static final String SWAZILAND = "Swaziland";
+	private static final String SWEDEN = "Sweden";
+	private static final String SWITZERLAND = "Switzerland";
+	private static final String SYRIA = "Syria";
+	private static final String TAIWAN = "Taiwan";
+	private static final String TAJIKISTAN = "Tajikistan";
+	private static final String TANZANIA = "Tanzania";
+	private static final String THAILAND = "Thailand";
+	private static final String TIMOR_LESTE = "Timor-Leste";
+	private static final String TOGO = "Togo";
+	private static final String TOKELAU = "Tokelau";
+	private static final String TONGA = "Tonga";
+	private static final String TRINIDAD_AND_TOBAGO = "Trinidad and Tobago";
+	private static final String TUNISIA = "Tunisia";
+	private static final String TURKEY = "Turkey";
+	private static final String TURKMENISTAN = "Turkmenistan";
+	private static final String TURKS_AND_CAICOS_ISLANDS = "Turks and Caicos Islands";
+	private static final String TUVALU = "Tuvalu";
+	private static final String UGANDA = "Uganda";
+	private static final String UKRAINE = "Ukraine";
+	private static final String UNITED_ARAB_EMIRATES = "United Arab Emirates";
+	private static final String URUGUAY = "Uruguay";
+	private static final String UZBEKISTAN = "Uzbekistan";
+	private static final String VANUATU = "Vanuatu";
+	private static final String VENEZUELA = "Venezuela";
+	private static final String VIETNAM = "Vietnam";
+	private static final String VIRGIN_ISLANDS = "Virgin Islands";
+	private static final String WALLIS_AND_FUTUNA = "Wallis and Futuna";
+	private static final String WESTERN_SAHARA = "Western Sahara";
+	private static final String YEMEN = "Yemen";
+	private static final String ZAMBIA = "Zambia";
+	private static final String ZIMBABWE = "Zimbabwe";
+
 	public static ICountryPreferences get(String countryCode) {
 		if (countryCode.equals("Algeria")) {
 			return new Algeria();
@@ -402,4 +639,56 @@ public class CountryPreferenceFactory {
 		}
 		return null;
 	}
+
+	public static String[] getCountries() {
+		return countries;
+	}
+
+	private static String[] countries = new String[] { UNITED_KINGDOM,
+			UNITED_STATES, AFGHANISTAN, ALBANIA, ALGERIA, AMERICAN_SAMOA,
+			ANDORRA, ANGUILLA, ANTARCTICA, ANTIGUA_AND_BARBUDA, ARGENTINA,
+			ARMENIA, ARUBA, AUSTRIA, AUSTRALIA, AZERBAIJAN, BAHAMAS_THE,
+			BAHRAIN, BANGLADESH, BARBADOS, BELARUS, BELGIUM, BELIZE, BENIN,
+			BERMUDA, BHUTAN, BOLIVIA, BOSNIA_AND_HERZEGOVINA, BOTSWANA,
+			BOUVET_ISLAND, BRAZIL, BRITISH_INDIAN_OCEAN_TERRITORY, BRUNEI,
+			BULGARIA, BURKINA_FASO, BURUNDI, CAMBODIA, CAMEROON, CANADA,
+			CAPE_VERDE, CAYMAN_ISLANDS, CENTRAL_AFRICAN_REPUBLIC, CHAD, CHILE,
+			CHINA, CHRISTMAS_ISLAND, COCOS_ISLANDS, COLOMBIA, COMOROS,
+			CONGO_DEMOCRATIC, CONGO_REPUBLIC, COOK_ISLANDS, COSTA_RICA,
+			COTE_DIVOIRE, CROATIA, CUBA, CYPRUS, CZECH_REPUBLIC, DENMARK,
+			DJIBOUTI, DOMINICA, DOMINICAN_REPUBLIC, ECUADOR, EGYPT,
+			EL_SALVADOR, EQUATORIAL_GUINEA, ERITREA, ESTONIA, ETHIOPIA,
+			FALKLAND_ISLANDS, FAROE_ISLANDS, FIJI, FINLAND, FRANCE,
+			FRENCH_GUIANA, FRENCH_POLYNESIA,
+			FRENCH_SOUTHERN_AND_ANTARCTIC_LANDS, GABON, GAMBIA_THE, GEORHIA,
+			GERMANY, GHANA, GIBRALTAR, GREECE, GREENLAND, GRENADA, GUADELOUPE,
+			GUAM, GUATEMALA, GUINEA, GUINEA_BISSAU, GUYANA, HAITI,
+			HEARD_ISLAND_AND_MCDONALD_ISLANDS, HOLY_SEE, HONDURAS, HONG_KONG,
+			HUNGARY, ICELAND, INDIA, INDONESIA, IRAN, IRAQ, IRELAND,
+			ISLE_OF_MAN, ISRAEL, ITALY, JAMAICA, JAN_MAYEN, JAPAN, JORDAN,
+			KAZAKHSTAN, KENYA, KIRIBATI, KOREA_NORTH, KOREA_SOUTH, KUWAIT,
+			KYRGYZSTAN, LAOS, LATVIA, LEBANON, LESOTHO, LIBERIA, LIBYA,
+			LIECHTENSTEIN, LITHUANIA, LUXEMBOURG, MACAO, MACEDONIA, MADAGASCAR,
+			MALAWI, MALAYSIA, MALDIVES, MALI, MALTA, MARSHALL_ISLANDS,
+			MARTINIQUE, MAURITANIA, MAURITIUS, MAYOTTE, MEXICO,
+			MICRONESIA_FEDERATED, MOLDOVA, MONACO, MONGOLIA, MONTSERRAT,
+			MOROCCO, MOZAMBIQUE, NAMIBIA, NAURU, NEPAL, NETHERLANDS,
+			NETHERLANDS_ANTILLES, NEW_CALEDONIA, NEW_ZEALAND, NICARAGUA, NIGER,
+			NIGERIA, NIUE, NORFOLK_ISLAND, NORTHERN_MARIANA_ISLANDS, NORWAY,
+			OMAN, PAKISTAN, PALAU, PANAMA, PAPUA_NEW_GUINEA, PARAGUAY, PERU,
+			PHILIPPINES, PITCAIRN_ISLANDS, POLAN, PORTUGAL, PUERTO_RICO, QATAR,
+			REUNION, ROMANIA, RUSSIA, RWANDA, SAINT_HELENA,
+			SAINT_KITTS_AND_NEVIS, SAINT_LUCIA, SAINT_PIERRE_AND_MIQUELON,
+			SAINT_VINCENT_AND_THE_GRENADINES, SAMOA, SAN_MARINO,
+			SAO_TOME_AND_PRINCIPE, SAUDI_ARABIA, SENEGAL,
+			SERBIA_AND_MONTENEGRO, SEYCHELLES, SIERRA_LEONE, SINGAPORE,
+			SLOVAKIA, SLOVENIA, SOLOMON_ISLANDS, SOMALIA, SOUTH_AFRICA,
+			SOUTH_GEORGIA_AND_THE_SOUTH_SANDWICH_ISLANDS, SPAIN, SRI_LANKA,
+			SUDAN, SURINAME, SVALBARD, SWAZILAND, SWEDEN, SWITZERLAND, SYRIA,
+			TAIWAN, TAJIKISTAN, TANZANIA, THAILAND, TIMOR_LESTE, TOGO, TOKELAU,
+			TONGA, TRINIDAD_AND_TOBAGO, TUNISIA, TURKEY, TURKMENISTAN,
+			TURKS_AND_CAICOS_ISLANDS, TUVALU, UGANDA, UKRAINE,
+			UNITED_ARAB_EMIRATES, URUGUAY, UZBEKISTAN, VANUATU, VENEZUELA,
+			VIETNAM, VIRGIN_ISLANDS, WALLIS_AND_FUTUNA, WESTERN_SAHARA, YEMEN,
+			ZAMBIA, ZIMBABWE };
 }
