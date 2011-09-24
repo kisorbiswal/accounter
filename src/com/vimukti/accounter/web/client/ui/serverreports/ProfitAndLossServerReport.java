569e4aaf725fbf656c0c97d58aefa6ba578015b8
Varakumar Pjd <varakumar.pjd@vimukti.com>
2011-09-24 11:17:59 +0000

    Former-commit-id: 9f846f6d77b624769294bdd6f4763e73b0e826be

--- src/com/vimukti/accounter/web/client/ui/serverreports/ProfitAndLossServerReport.java
+++ src/com/vimukti/accounter/web/client/ui/serverreports/ProfitAndLossServerReport.java
@@ -509,16 +509,16 @@ public class ProfitAndLossServerReport extends
 
 		int lastDay;
 		switch (month) {
-		case 0:
-		case 2:
-		case 4:
-		case 6:
+		case 1:
+		case 3:
+		case 5:
 		case 7:
-		case 9:
-		case 11:
+		case 8:
+		case 10:
+		case 12:
 			lastDay = 31;
 			break;
-		case 1:
+		case 2:
 			if (year % 4 == 0 && year % 100 == 0)
 				lastDay = 29;
 			else
