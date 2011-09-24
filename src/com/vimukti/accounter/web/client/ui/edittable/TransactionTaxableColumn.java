ba8e2e1736f1b7c0f4c4fec96ef49d8e8db85f42
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-24 05:21:18 +0000

    Former-commit-id: febc419eda79d8a943cfbfedfb50344182157830

--- src/com/vimukti/accounter/web/client/ui/edittable/TransactionTaxableColumn.java
+++ src/com/vimukti/accounter/web/client/ui/edittable/TransactionTaxableColumn.java
@@ -5,6 +5,7 @@ import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.vimukti.accounter.web.client.core.ClientTransactionItem;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.edittable.tables.AbstractTransactionTable;
 
 public class TransactionTaxableColumn extends
 		CheckboxEditColumn<ClientTransactionItem> {
@@ -12,6 +13,7 @@ public class TransactionTaxableColumn extends
 	@Override
 	protected void onChangeValue(boolean value, ClientTransactionItem row) {
 		row.setTaxable(value);
+		((AbstractTransactionTable) getTable()).update(row);
 	}
 
 	@Override
