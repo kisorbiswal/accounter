3c2c9d1554f7785da025faa349e5d4d2bacbdb76
Lingarao R <lingarao.r@vimukti.com>
2011-09-26 11:31:23 +0000

    Former-commit-id: 3b4b156f0456fd27704bb6d7ea50e810356e0923

--- src/com/vimukti/accounter/web/client/ui/company/options/CompanyDateFormateOption.java
+++ src/com/vimukti/accounter/web/client/ui/company/options/CompanyDateFormateOption.java
@@ -1,7 +1,5 @@
 package com.vimukti.accounter.web.client.ui.company.options;
 
-import java.util.ArrayList;
-import java.util.List;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
