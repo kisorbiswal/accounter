b5c9d9c5c05bc48e218b64f138b86a5930c4b9c7
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-09-28 04:42:14 +0000

    Former-commit-id: 719699f42846ab1e8742c6551a45fee8225a989b

---
+++ src/com/vimukti/accounter/mobile/commands/InactiveItemsListCommand.java
@@ -0,0 +1,17 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.core.Item;
+
+public class InactiveItemsListCommand extends ItemsCommand {
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	 
+}
