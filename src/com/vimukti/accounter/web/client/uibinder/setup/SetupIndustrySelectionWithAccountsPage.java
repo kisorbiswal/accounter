5df7671ec9672fb43c72e12473e0b882aeccd0c7
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-09-28 14:48:37 +0000

    Former-commit-id: bdd0bfb969d5c7072f68273e8fed43c8d179f777

--- src/com/vimukti/accounter/web/client/uibinder/setup/SetupIndustrySelectionWithAccountsPage.java
+++ src/com/vimukti/accounter/web/client/uibinder/setup/SetupIndustrySelectionWithAccountsPage.java
@@ -93,7 +93,7 @@ public class SetupIndustrySelectionWithAccountsPage extends AbstractSetupPage {
 	@Override
 	protected void onSave() {
 		setupWizard.setSelectedAccountsList(industriesWithAccounts
-				.get(industryList.getSelectedIndex()));
+				.get(industryList.getSelectedIndex() + 1));
 	}
 
 	@Override
