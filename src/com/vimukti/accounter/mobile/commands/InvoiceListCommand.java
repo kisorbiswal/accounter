fe7d9dbc71a4c2c7f245e153269049d1094ef12b
Saiprasad N <saiprasad.n@vimukti.com>
2011-09-29 07:18:35 +0000

    Former-commit-id: a698fb7d8a073302b4ef04cf808f46cc25959753

--- src/com/vimukti/accounter/mobile/commands/InvoiceListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/InvoiceListCommand.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.vimukti.accounter.core.Invoice;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
@@ -12,6 +11,7 @@ import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
 import com.vimukti.accounter.web.client.core.Lists.InvoicesList;
+import com.vimukti.accounter.web.client.ui.Accounter;
 
 /**
  * 
@@ -76,10 +76,10 @@ public class InvoiceListCommand extends AbstractTransactionCommand {
 	@Override
 	protected List<String> getViewTypes() {
 		List<String> list = new ArrayList<String>();
-		list.add("All");
-		list.add("Open");
-		list.add("Voided");
-		list.add("OverDue");
+		list.add(Accounter.constants().all());
+		list.add(Accounter.constants().open());
+		list.add(Accounter.constants().voided());
+		list.add(Accounter.constants().overDue());
 
 		return list;
 	}
