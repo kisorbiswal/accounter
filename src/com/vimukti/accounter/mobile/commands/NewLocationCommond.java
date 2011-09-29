6ecac9bf3a9bd9503d55e63c98592b98bcffd144
Devaraju K <devaraju.k@vimukti.com>
2011-09-29 05:42:29 +0000

    Former-commit-id: 6f29272b400a70d518558ed2390603c3a7baf17e

---
+++ src/com/vimukti/accounter/mobile/commands/NewLocationCommond.java
@@ -0,0 +1,53 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+
+public class NewLocationCommond extends AbstractCommand {
+
+	private static final String LOCATION_NAME = "LOCATION";
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+		list.add(new Requirement(LOCATION_NAME, false, true));
+
+	}
+
+	@Override
+	public Result run(Context context) {
+
+		Result result = createClassNameReq(context);
+		if (result != null) {
+			return result;
+		}
+		return null;
+	}
+
+	private Result createClassNameReq(Context context) {
+
+		Requirement requirement = get(LOCATION_NAME);
+		String className = context.getSelection(TEXT);
+		if (!requirement.isDone()) {
+			if (className != null) {
+				requirement.setValue(className);
+			} else {
+				return text(context, "Please enter the  Location", null);
+			}
+		}
+		String input = (String) context.getAttribute(INPUT_ATTR);
+		if (input.equals(LOCATION_NAME)) {
+			requirement.setValue(input);
+		}
+		return null;
+	}
+
+}
