557b85c00db86d5e6d545581649175cb4ab57237
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2011-10-12 05:07:30 +0000

    Former-commit-id: 9e84668c58cf1dc7d9e8e183edc2e6728fdfd5d3

--- src/com/vimukti/accounter/utils/HibernateUtil.java
+++ src/com/vimukti/accounter/utils/HibernateUtil.java
@@ -1,30 +1,15 @@
 package com.vimukti.accounter.utils;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class HibernateUtil {
 
-	private static final Logger LOG = Logger.getLogger(HibernateUtil.class);
-
-	// private static SessionFactory sessionFactory;
-
-	// SessionFactory = new
-	// Configuration().configure("/CollaberServer/hibernate.cfg.xml");
 	static ThreadLocal<Session> threadLocalSession = new ThreadLocal<Session>();
 
 	private static SessionFactory sessionFactory;
 
-	// public static SessionFactory getSessionFactory() {
-	// return sessionFactory;
-	// }
-	//
-	// public static void shutdown() {
-	// getSessionFactory().close();
-	// }
 	public static Session openSession() {
-		ConnectionProvider.setDBName("finance");
 		Session session = getSessionFactory().openSession();
 		threadLocalSession.set(session);
 		return session;
@@ -41,45 +26,11 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		Configuration config = new Configuration();
 		config.configure();
-		config.setProperty("hibernate.hbm2ddl.auto", "update");
-		config.setProperty("hibernate.connection.provider_class",
-				getConnectionProvider());
 		return config.buildSessionFactory();
-	}
-
-	private static String getConnectionProvider() {
-		return "com.vimukti.accounter.utils.LiveConnectionProvider";
 	}
 
 	public static Session getCurrentSession() {
 		return threadLocalSession.get();
-	}
-
-	public static void setSession(Session session) {
-		threadLocalSession.set(session);
-	}
-
-	public static void closeCurrentSession() {
-		// LOG.info("Close Session");
-
-		if (getCurrentSession() != null && getCurrentSession().isOpen()) {
-			getCurrentSession().close();
-			threadLocalSession.remove();
-		}
-	}
-
-	public static void rebuildSessionFactory() {
-		if (sessionFactory != null) {
-			sessionFactory.close();
-			sessionFactory = null;
-		}
-		Runtime.getRuntime().gc();
-		System.out.println("Memory--------------"
-				+ Runtime.getRuntime().totalMemory());
-	}
-
-	public static String getCurrentDatabse() {
-		return ConnectionProvider.getDBName();
 	}
 
 }
