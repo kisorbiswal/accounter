b75aec61bf777335a810788cf465cfe49f4a324b
Lingarao R <lingarao.r@vimukti.com>
2011-10-15 12:00:23 +0000

    Former-commit-id: 8f8b346a2effb7132f5a9b20d6917be3a6866bca

---
+++ src/com/vimukti/accounter/mobile/commands/NewItemGroupCommand.java
@@ -0,0 +1,69 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.vimukti.accounter.core.ItemGroup;
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
+
+public class NewItemGroupCommand extends AbstractTransactionCommand {
+	private static final String ITEMGROUP_NAME = "itemGroupName";
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+		list.add(new Requirement(ITEMGROUP_NAME, false, true));
+
+	}
+
+	@Override
+	public Result run(Context context) {
+		Object attribute = context.getAttribute(INPUT_ATTR);
+		if (attribute == null) {
+			context.setAttribute(INPUT_ATTR, "optional");
+		}
+		Result makeResult = context.makeResult();
+		makeResult.add(" ItemGroup is ready to create with following values.");
+		ResultList list = new ResultList("values");
+		makeResult.add(list);
+		ResultList actions = new ResultList(ACTIONS);
+		makeResult.add(actions);
+
+		Result result = nameRequirement(context, list, ITEMGROUP_NAME,
+				"Please enter the  ItemGroup Name");
+		if (result != null) {
+			return result;
+		}
+		markDone();
+		return createItemGroupObject(context);
+	}
+
+	private Result createItemGroupObject(Context context) {
+
+		ItemGroup group = new ItemGroup();
+		group.setCompany(context.getCompany());
+		group.setName(get(ITEMGROUP_NAME).getValue().toString());
+
+		Session session = context.getHibernateSession();
+		Transaction transaction = session.beginTransaction();
+		session.saveOrUpdate(group);
+		transaction.commit();
+		markDone();
+
+		Result result = new Result();
+		result.add(" ItemGroup was created successfully.");
+
+		return result;
+	}
+
+}
