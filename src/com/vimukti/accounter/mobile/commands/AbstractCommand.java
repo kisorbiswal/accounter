aa92fb4f0d8aa32e3805604c0ff26e252d06e188
Saiprasad N <saiprasad.n@vimukti.com>
2011-10-15 07:07:28 +0000

    Former-commit-id: c1fc811a8ca43d92ebdb50d2cce3889e462e436a

--- src/com/vimukti/accounter/mobile/commands/AbstractCommand.java
+++ src/com/vimukti/accounter/mobile/commands/AbstractCommand.java
@@ -2,8 +2,6 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
