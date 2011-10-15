927627726fd48a6ce0f8db8c342a0c7aae38e1c0
Devi G <devi.g@vimukti.com>
2011-10-15 06:43:11 +0000

    Former-commit-id: 931945f24afc5ef3f935fddd2d4633110c04fe9f

--- src/com/vimukti/accounter/web/client/ui/reports/ReportSectionView.java
+++ src/com/vimukti/accounter/web/client/ui/reports/ReportSectionView.java
@@ -90,8 +90,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getCashFlowStatementAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getCashFlowStatementAction());
 			}
 
 		});
@@ -116,8 +116,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getTransactionDetailByAccountAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getTransactionDetailByAccountAction());
 			}
 
 		});
@@ -143,8 +143,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getSalesByCustomerSummaryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getSalesByCustomerSummaryAction());
 			}
 
 		});
@@ -157,8 +157,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getSalesByCustomerDetailAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getSalesByCustomerDetailAction());
 			}
 
 		});
@@ -171,8 +171,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getSalesByItemSummaryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getSalesByItemSummaryAction());
 			}
 
 		});
@@ -185,8 +185,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getSalesByItemDetailAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getSalesByItemDetailAction());
 			}
 
 		});
@@ -213,8 +213,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getPurchaseByVendorSummaryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getPurchaseByVendorSummaryAction());
 			}
 
 		});
@@ -227,31 +227,31 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getPurchaseByVendorDetailAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getPurchaseByVendorDetailAction());
 			}
 
 		});
 
-		LinkItem purchaseByProductSummaryLink = new LinkItem();
-		purchaseByProductSummaryLink.setLinkTitle(Accounter.constants()
-				.purchaseByProductSummary());
-		purchaseByProductSummaryLink.setShowTitle(false);
-		purchaseByProductSummaryLink.addClickHandler(new ClickHandler() {
+		LinkItem purchaseByItemSummaryLink = new LinkItem();
+		purchaseByItemSummaryLink.setLinkTitle(Accounter.constants()
+				.purchaseByItemSummary());
+		purchaseByItemSummaryLink.setShowTitle(false);
+		purchaseByItemSummaryLink.addClickHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getPurchaseByItemSummaryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getPurchaseByItemSummaryAction());
 			}
 
 		});
 
-		LinkItem purchaseByProductDetailLink = new LinkItem();
-		purchaseByProductDetailLink.setLinkTitle(Accounter.constants()
-				.purchaseByProductDetail());
-		purchaseByProductDetailLink.setShowTitle(false);
-		purchaseByProductDetailLink.addClickHandler(new ClickHandler() {
+		LinkItem purchaseByItemDetailLink = new LinkItem();
+		purchaseByItemDetailLink.setLinkTitle(Accounter.constants()
+				.purchaseByItemDetail());
+		purchaseByItemDetailLink.setShowTitle(false);
+		purchaseByItemDetailLink.addClickHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
@@ -261,8 +261,8 @@ public class ReportSectionView extends BaseHomeView {
 		});
 
 		purchaseForm.setFields(purchaseBySupplierSummaryLink,
-				purchaseBySupplierDetailLink, purchaseByProductSummaryLink,
-				purchaseByProductDetailLink);
+				purchaseBySupplierDetailLink, purchaseByItemSummaryLink,
+				purchaseByItemDetailLink);
 
 		HorizontalPanel2.add(purchaseForm);
 
@@ -281,8 +281,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getCustomerTransactionHistoryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getCustomerTransactionHistoryAction());
 			}
 
 		});
@@ -306,7 +306,9 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc, ActionFactory.getAorpAgingDetailAction());
+				UIUtils
+						.runAction(acc, ActionFactory
+								.getAorpAgingDetailAction());
 			}
 
 		});
@@ -319,8 +321,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getVendorTransactionHistoryAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getVendorTransactionHistoryAction());
 			}
 
 		});
@@ -333,8 +335,8 @@ public class ReportSectionView extends BaseHomeView {
 
 			public void onClick(ClickEvent event) {
 				ClientAccount acc = null;
-				UIUtils.runAction(acc,
-						ActionFactory.getMostProfitableCustomersAction());
+				UIUtils.runAction(acc, ActionFactory
+						.getMostProfitableCustomersAction());
 			}
 
 		});
