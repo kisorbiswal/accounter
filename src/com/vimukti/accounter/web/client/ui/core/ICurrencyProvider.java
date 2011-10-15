72a208cc55e1bc27c80baca07a5a5085d75a0f7b
Rajesh Akkineni <rajesh@vimukti.com>
2011-10-15 05:00:01 +0000

    Former-commit-id: 4d6c15b8520eadac3184e7e5c8728d26e4660e9c

--- src/com/vimukti/accounter/web/client/ui/core/ICurrencyProvider.java
+++ src/com/vimukti/accounter/web/client/ui/core/ICurrencyProvider.java
@@ -2,7 +2,23 @@ package com.vimukti.accounter.web.client.ui.core;
 
 public interface ICurrencyProvider {
 
+	/**
+	 * This should return the given amount in the currency of the transaction.
+	 * If your base currency is USD and Transaction Currency is INR Then passing
+	 * 1.0 will return 50.0.
+	 * 
+	 * @param amount
+	 * @return
+	 */
 	public Double getAmountInTransactionCurrency(Double amount);
 
+	/**
+	 * This should return the given amount in the base currency. If your base
+	 * currency is INR and Transaction Currency is USD Then passing 1.0 will
+	 * return 50.0.
+	 * 
+	 * @param amount
+	 * @return
+	 */
 	public Double getAmountInBaseCurrency(Double amount);
 }
