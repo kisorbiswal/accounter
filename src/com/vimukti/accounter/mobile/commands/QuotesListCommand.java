7913630e985d7bb73a5c5efa42272c9961bb823d
srinivas.g <srinivas.g@vimukti.com>
2011-10-18 13:34:19 +0000

    Former-commit-id: 6d0fdf1c755e432408dec3309ba80d3f4847c9b8

--- src/com/vimukti/accounter/mobile/commands/QuotesListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/QuotesListCommand.java
@@ -3,14 +3,19 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.vimukti.accounter.core.ClientConvertUtil;
 import com.vimukti.accounter.core.Estimate;
+import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
-import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.core.ClientCustomer;
+import com.vimukti.accounter.web.client.core.ClientEstimate;
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.server.FinanceTool;
 
 /**
  * 
@@ -19,97 +24,182 @@ import com.vimukti.accounter.web.client.ui.Accounter;
  */
 public class QuotesListCommand extends AbstractTransactionCommand {
 
-	private static final String VIEW_BY = "ViewBy";
+	private static final String CURRENT_VIEW = "Current View";
+
+	private static final String OPEN = "Open";
+	private static final String REJECTED = "Rejected";
+	private static final String ACCEPTED = "Accepted";
+	private static final String EXPIRED = "Expired";
+	private static final String ALL = "All";
+
+	private static final int STATUS_OPEN = 0;
+	private static final int STATUS_REJECTED = 1;
+	private static final int STATUS_ACCECPTED = 2;
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
 
-		list.add(new Requirement(VIEW_BY, true, true));
+		list.add(new Requirement(CURRENT_VIEW, true, true));
 	}
 
 	@Override
 	public Result run(Context context) {
-		Result result = null;
 
+		Object attribute = context.getAttribute(INPUT_ATTR);
+		if (attribute == null) {
+			context.setAttribute(INPUT_ATTR, "optional");
+		}
+		Result result = null;
+		setDefaultValues();
 		result = createOptionalResult(context);
 		if (result != null) {
 			return result;
 		}
 		return result;
+
+	}
+
+	private void setDefaultValues() {
+		get(CURRENT_VIEW).setDefaultValue(OPEN);
+
 	}
 
 	private Result createOptionalResult(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-		Object selection = context.getSelection(VIEW_BY);
-		ResultList list = new ResultList("viewlist");
-		Result result = viewTypeRequirement(context, list, selection);
-		if (result != null) {
-			return result;
-		}
-		String viewType = get(VIEW_BY).getValue();
-		result = qoutesList(context, viewType);
-		return result;
-	}
 
-	private Result qoutesList(Context context, String viewType) {
-		Result result = context.makeResult();
-		result.add("Qoutes List");
-		ResultList billsListData = new ResultList("QoutesList");
-		int num = 0;
-		List<Estimate> estimates = getEstimates(viewType);
-		for (Estimate est : estimates) {
-			billsListData.add(createEstimateRecord(est));
-			num++;
-			if (num == ESTIMATES_TO_SHOW) {
+		List<String> viewType = new ArrayList<String>();
+		viewType.add(OPEN);
+		viewType.add(REJECTED);
+		viewType.add(ACCEPTED);
+		viewType.add(EXPIRED);
+		viewType.add(ALL);
+
+		ResultList resultList = new ResultList("values");
+		Object selection = context.getSelection(ACTIONS);
+		ActionNames actionNames;
+		if (selection != null) {
+			actionNames = (ActionNames) selection;
+			switch (actionNames) {
+			case FINISH:
+				markDone();
+				return null;
+			default:
 				break;
 			}
 		}
 
-		int size = billsListData.size();
-		StringBuilder message = new StringBuilder();
-		if (size > 0) {
-			message.append("Select a Estimate");
+		selection = context.getSelection("values");
+		Result result = stringListOptionalRequirement(context, resultList,
+				selection, CURRENT_VIEW, "Current View", viewType,
+				"Select View type", ITEMS_TO_SHOW);
+		if (result != null) {
+			return result;
 		}
-		CommandList commandList = new CommandList();
-		commandList.add("Create");
 
-		result.add(message.toString());
-		result.add(billsListData);
-		result.add(commandList);
-		result.add("Type for Estimate");
-
+		String view = get(CURRENT_VIEW).getValue();
+		result = getEstimatesList(context, view);
+		result.add(resultList);
 		return result;
 	}
 
-	private Record createEstimateRecord(Estimate est) {
+	private Result getEstimatesList(Context context, String view) {
+		ArrayList<ClientEstimate> estimates = getEstimates(context);
+
+		Result result = context.makeResult();
+		ResultList resultList = new ResultList("quotesList");
+
+		ArrayList<ClientEstimate> list = filterList(estimates, view);
+		for (ClientEstimate estimate : list) {
+
+			resultList.add(createEstimateRecord(estimate));
+
+		}
+
+		StringBuilder message = new StringBuilder();
+		if (resultList.size() > 0) {
+			message.append("Select a Quote");
+		}
+
+		result.add(message.toString());
+		result.add(resultList);
+
+		CommandList commandList = new CommandList();
+		commandList.add("Add a New Quote");
+		result.add(commandList);
+		return result;
+	}
+
+	public ArrayList<ClientEstimate> getEstimates(Context context) {
+		List<ClientEstimate> clientEstimate = new ArrayList<ClientEstimate>();
+		List<Estimate> serverEstimates = null;
+
+		try {
+
+			serverEstimates = new FinanceTool().getCustomerManager()
+					.getEstimates(context.getCompany().getID());
+			for (Estimate estimate : serverEstimates) {
+				clientEstimate.add(new ClientConvertUtil().toClientObject(
+						estimate, ClientEstimate.class));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new ArrayList<ClientEstimate>(clientEstimate);
+	}
+
+	private ArrayList<ClientEstimate> filterList(
+			ArrayList<ClientEstimate> list, String text) {
+
+		ArrayList<ClientEstimate> estimatesList = new ArrayList<ClientEstimate>();
+
+		for (ClientEstimate estimate : list) {
+			if (text.equals(OPEN)) {
+				if (estimate.getStatus() == STATUS_OPEN)
+					estimatesList.add(estimate);
+				continue;
+			}
+			if (text.equals(REJECTED)) {
+				if (estimate.getStatus() == STATUS_REJECTED)
+					estimatesList.add(estimate);
+			}
+			if (text.equals(ACCEPTED)) {
+				if (estimate.getStatus() == STATUS_ACCECPTED)
+					estimatesList.add(estimate);
+			}
+			if (text.equals(EXPIRED)) {
+				ClientFinanceDate expiryDate = new ClientFinanceDate(
+						estimate.getExpirationDate());
+				if (expiryDate.before(new ClientFinanceDate()))
+					estimatesList.add(estimate);
+			}
+			if (text.equals(ALL)) {
+				estimatesList.add(estimate);
+			}
+		}
+
+		return estimatesList;
+
+	}
+
+	private Record createEstimateRecord(ClientEstimate est) {
 		Record estrecord = new Record(est);
 		estrecord.add("Date", est.getDate());
 		estrecord.add("Number", est.getNumber());
-		estrecord.add("VendorName", est.getPayee().getName());
+		ClientCustomer customer = getClientCompany().getCustomer(
+				est.getCustomer());
+		estrecord.add("VendorName", customer != null ? customer.getName() : "");
 		estrecord.add("Phone", est.getPhone());
 		estrecord.add("ExpirationDate", est.getExpirationDate());
 		estrecord.add("DeliveryDate", est.getDeliveryDate());
 		estrecord.add("Total", est.getTotal());
 		return estrecord;
 
-	}
-
-	@Override
-	protected List<String> getViewTypes() {
-		List<String> list = new ArrayList<String>();
-		list.add(Accounter.constants().all());
-		list.add(Accounter.constants().open());
-		list.add(Accounter.constants().rejected());
-		list.add(Accounter.constants().accepted());
-		list.add(Accounter.constants().expired());
-
-		return list;
 	}
 
 }
