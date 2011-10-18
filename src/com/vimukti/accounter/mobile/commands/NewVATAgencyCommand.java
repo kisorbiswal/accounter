1b91831ca8de6aca5a9e751e3be508eba3a5dc4f
Lingarao R <lingarao.r@vimukti.com>
2011-10-18 04:56:50 +0000

    Former-commit-id: b3a38b31b119f776a531723be7e0a835f209bc75

--- src/com/vimukti/accounter/mobile/commands/NewVATAgencyCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewVATAgencyCommand.java
@@ -6,8 +6,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import org.hibernate.Session;
-
 import com.vimukti.accounter.core.Account;
 import com.vimukti.accounter.core.Address;
 import com.vimukti.accounter.core.Contact;
@@ -23,14 +21,11 @@ import com.vimukti.accounter.mobile.RequirementType;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
 import com.vimukti.accounter.web.client.core.ClientAccount;
-import com.vimukti.accounter.web.client.core.ClientAddress;
-import com.vimukti.accounter.web.client.core.ClientContact;
 import com.vimukti.accounter.web.client.core.ClientTAXAgency;
 
 public class NewVATAgencyCommand extends AbstractVATCommand {
 
 	private static final String PAYMENT_TERM = "paymentTerm";
-	private static final String VAT_RETURN = "vatReturn";
 	private static final String SALES_ACCOUNT = "salesLiabilityAccount";
 	private static final String PURCHASE_ACCOUNT = "purchaseLiabilityAccount";
 	private static final String ADDRESS = "address";
@@ -45,7 +40,7 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 	protected static final String CONTACT_EMAIL = "contactEmail";
 	private static final String IS_ACTIVE = "isActive";
 	private static final String SALES_ACCOUNTS = "salesAccounts";
-	private static final String VAT_RETURNS = "vatReturns";
+
 	private static final String VAT_AGENCY_ADDRESS = "vatAgencyAddress";
 
 	@Override
@@ -130,13 +125,13 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 			if (result != null) {
 				return result;
 			}
-			result = vatReturnRequirement(context);
+			result = vatReturnRequirement(context, list, VAT_RETURN);
 			if (result != null) {
 				return result;
 			}
 		}
 
-		result = createOptionalResult(context);
+		result = createOptionalResult(context, list, actions, makeResult);
 		if (result != null) {
 			return result;
 		}
@@ -228,19 +223,14 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 		return result;
 	}
 
-	private Result createOptionalResult(Context context) {
+	private Result createOptionalResult(Context context, ResultList list,
+			ResultList actions, Result makeResult) {
 		// context.setAttribute(INPUT_ATTR, "optional");
 
 		Object selection = context.getSelection(ACTIONS);
 		if (selection != null) {
 			ActionNames actionName = (ActionNames) selection;
 			switch (actionName) {
-			case ADD_MORE_CONTACTS:
-				return contact(
-						context,
-						getMessages().pleaseEnter(
-								getConstants().contactDetails()),
-						VAT_AGENCY_CONTACT, null);
 			case FINISH:
 				context.removeAttribute(INPUT_ATTR);
 				return null;
@@ -250,86 +240,6 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 		}
 
 		selection = context.getSelection("values");
-
-		// Requirement nameReq = get(NAME);
-		// String name = (String) nameReq.getValue();
-		// if (NAME == selection) {
-		// context.setAttribute(INPUT_ATTR, NAME);
-		// return text(context,
-		// getMessages().pleaseEnter(getConstants().taxAgency()), name);
-		// }
-
-		// Requirement paymentTermReq = get(PAYMENT_TERM);
-		// PaymentTerms paymentTerm = (PaymentTerms) paymentTermReq.getValue();
-		// if (PAYMENT_TERM == selection) {
-		// context.setAttribute(INPUT_ATTR, PAYMENT_TERM);
-		// return getPaymentTermsResult(context);
-		// }
-
-		Requirement salesAccountReq = get(SALES_ACCOUNT);
-		Account salesAccount = (Account) salesAccountReq.getValue();
-		if (SALES_ACCOUNT == selection) {
-			context.setAttribute(INPUT_ATTR, SALES_ACCOUNT);
-			return getSalesAccountResult(context);
-		}
-		Requirement addressReq = get(VAT_AGENCY_ADDRESS);
-		if (selection == "Address") {
-			ClientAddress oldAddress = addressReq.getValue();
-			Result result = address(context, "Address", VAT_AGENCY_ADDRESS,
-					oldAddress);
-			if (result != null) {
-				return result;
-			}
-		}
-
-		ResultList list = new ResultList("values");
-
-		// Record nameRecord = new Record(NAME);
-		// nameRecord.add(INPUT_ATTR, "Name");
-		// nameRecord.add("Value", name);
-		// list.add(nameRecord);
-		//
-		// Record paymentTermRecord = new Record(PAYMENT_TERM);
-		// paymentTermRecord.add(INPUT_ATTR, "Payment Term");
-		// paymentTermRecord.add("Value", paymentTerm.getName());
-		// list.add(paymentTermRecord);
-
-		Record salesAccountRecord = new Record(SALES_ACCOUNT);
-		salesAccountRecord.add(INPUT_ATTR, "Sales Liability Account");
-		salesAccountRecord.add("Value", salesAccount.getName());
-		list.add(salesAccountRecord);
-
-		if (getCompanyType(context) != ACCOUNTING_TYPE_US) {
-			Requirement purchseAccountReq = get(PURCHASE_ACCOUNT);
-			Account purchaseAccount = (Account) purchseAccountReq.getValue();
-			if (PURCHASE_ACCOUNT == selection) {
-				context.setAttribute(INPUT_ATTR, PURCHASE_ACCOUNT);
-				return getPurchaseAccountResult(context);
-			}
-
-			Requirement vatReturnReq = get(VAT_RETURN);
-			String vatReturn = (String) vatReturnReq.getValue();
-			if (VAT_RETURN == selection) {
-				context.setAttribute(INPUT_ATTR, SALES_ACCOUNT);
-				return getVatReturnResult(context);
-			}
-
-			Record purchaseAccountRecord = new Record(purchaseAccount);
-			purchaseAccountRecord.add(INPUT_ATTR, "Purchase Liability Account");
-			purchaseAccountRecord.add("Value", purchaseAccount.getName());
-			list.add(purchaseAccountRecord);
-
-			Record vatReturnRecord = new Record(vatReturn);
-			vatReturnRecord.add(INPUT_ATTR, "Vat Return");
-			vatReturnRecord.add("Value", vatReturn);
-			list.add(vatReturnRecord);
-		}
-
-		Address address = addressReq.getValue();
-		Record addressRecord = new Record("Address");
-		addressRecord.add(INPUT_ATTR, "Address");
-		addressRecord.add("Value", address);
-		list.add(addressRecord);
 
 		Result result = numberOptionalRequirement(context, list, selection,
 				PHONE, "Enter Phone Number");
@@ -355,17 +265,6 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 			return result;
 		}
 
-		Requirement contactReq = get(VAT_AGENCY_CONTACT);
-		Set<Contact> contacts = contactReq.getValue();
-		selection = context.getSelection(VAT_AGENCY_CONTACT);
-		if (selection != null) {
-			Result contact = contact(context, "vat agency contact",
-					VAT_AGENCY_CONTACT, (ClientContact) selection);
-			if (contact != null) {
-				return contact;
-			}
-		}
-
 		Requirement isActiveReq = get(IS_ACTIVE);
 		Boolean isActive = (Boolean) isActiveReq.getValue();
 
@@ -385,31 +284,10 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 		isActiveRecord.add("Value", activeString);
 		list.add(isActiveRecord);
 
-		result = context.makeResult();
-		result.add("Tax Agency is ready to create with following values.");
-		result.add(list);
-		ResultList contactsList = new ResultList(VAT_AGENCY_CONTACT);
-		for (Contact contact : contacts) {
-			Record contactRec = new Record(contact);
-			contactRec.add("primary", contact.getVersion());
-			contactRec.add("contactName", contact.getName());
-			contactRec.add("title", contact.getTitle());
-			contactRec.add("businessPhone", contact.getBusinessPhone());
-			contactRec.add("email", contact.getEmail());
-			contactsList.add(contactRec);
-		}
-
-		result.add(contactsList);
-		ResultList actions = new ResultList("actions");
-		Record moreContacts = new Record(ActionNames.ADD_MORE_CONTACTS);
-		moreContacts.add("", "Add more contacts");
-		actions.add(moreContacts);
 		Record finish = new Record(ActionNames.FINISH);
-		finish.add("", "Finish to create Tax Agency.");
+		finish.add("", "Finish to create Vat Agency.");
 		actions.add(finish);
-		result.add(actions);
-
-		return result;
+		return makeResult;
 	}
 
 	private Result purchaseAccountRequirement(Context context) {
@@ -556,63 +434,6 @@ public class NewVATAgencyCommand extends AbstractVATCommand {
 				accounts.add(account);
 		}
 		return accounts;
-	}
-
-	private Result vatReturnRequirement(Context context) {
-		Requirement vatReturnReq = get(VAT_RETURN);
-		String vatReturn = context.getSelection(VAT_RETURNS);
-		if (vatReturn != null) {
-			vatReturnReq.setValue(vatReturn);
-		}
-		if (!vatReturnReq.isDone()) {
-			return getVatReturnResult(context);
-		}
-		return null;
-	}
-
-	private Result getVatReturnResult(Context context) {
-		Result result = context.makeResult();
-		ResultList vatReturnsList = new ResultList(VAT_RETURNS);
-
-		Object last = context.getLast(RequirementType.VAT_RETURN);
-		if (last != null) {
-			vatReturnsList.add(createVatReturnRecord((String) last));
-		}
-
-		List<String> vatReturns = getVatReturns(context.getHibernateSession());
-		for (int i = 0; i < VALUES_TO_SHOW && i < vatReturns.size(); i++) {
-			String vatReturn = vatReturns.get(i);
-			if (vatReturn != last) {
-				vatReturnsList.add(createVatReturnRecord((String) vatReturn));
-			}
-		}
-
-		int size = vatReturnsList.size();
-		StringBuilder message = new StringBuilder();
-		if (size > 0) {
-			message.append("Please Select the Vat Return");
-		}
-
-		result.add(message.toString());
-		result.add(vatReturnsList);
-		result.add("Select the Vat Return");
-
-		return result;
-	}
-
-	private Record createVatReturnRecord(String vatReturn) {
-		Record record = new Record(vatReturn);
-		record.add("Name", vatReturn);
-		return record;
-	}
-
-	private List<String> getVatReturns(Session session) {
-
-		ArrayList<String> vatReturnList = new ArrayList<String>();
-		vatReturnList.add("UK VAT");
-		vatReturnList.add("VAT 3(Ireland)");
-
-		return vatReturnList;
 	}
 
 }
