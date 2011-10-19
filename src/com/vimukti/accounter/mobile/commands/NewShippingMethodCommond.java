974cea3a889e74d8b52edb4921d5cf64364d193a
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-10-19 12:38:57 +0000

    Former-commit-id: e0da2bb6b5e587d02808a75b794475bd56cbb34e

--- src/com/vimukti/accounter/mobile/commands/NewShippingMethodCommond.java
+++ src/com/vimukti/accounter/mobile/commands/NewShippingMethodCommond.java
@@ -2,13 +2,13 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.List;
 
-import com.vimukti.accounter.core.ShippingMethod;
 import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.web.client.core.ClientShippingMethod;
 
 public class NewShippingMethodCommond extends AbstractCommand {
 	private static final String SHIPPING_METHOD = "Shipping Mehtod";
@@ -16,7 +16,6 @@ public class NewShippingMethodCommond extends AbstractCommand {
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -30,95 +29,74 @@ public class NewShippingMethodCommond extends AbstractCommand {
 	public Result run(Context context) {
 		Result result = null;
 
-		Object selection = context.getSelection(ACTIONS);
-		selection = context.getSelection("values");
+		if (context.getAttribute(INPUT_ATTR) == null) {
+			context.setAttribute(INPUT_ATTR, "optional");
+		}
 
+		Result makeResult = context.makeResult();
+		makeResult
+				.add(" Shipping Method is ready to create with following values.");
 		ResultList list = new ResultList("values");
+		makeResult.add(list);
+		ResultList actions = new ResultList(ACTIONS);
+		makeResult.add(actions);
 
-		result = shppingMethodRequirment(context, list, (String) selection);
+		result = nameRequirement(context, list, SHIPPING_METHOD,
+				"Please Enter the shipping method name ");
 		if (result != null) {
 			return result;
 		}
-		result = createOptionalReq(context);
+
+		setDefaultValues();
+		result = createOptionalResult(context, list, actions, makeResult);
 		if (result != null) {
 			return result;
 		}
-		completeProcess(context);
-		markDone();
-		return null;
+		createShippingMethodObject(context);
+		return result;
 	}
 
-	private Result createOptionalReq(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
+	private Result createOptionalResult(Context context, ResultList list,
+			ResultList actions, Result makeResult) {
 		Object selection = context.getSelection(ACTIONS);
 		if (selection != null) {
 			ActionNames actionName = (ActionNames) selection;
 			switch (actionName) {
 			case FINISH:
+				markDone();
 				return null;
 			default:
 				break;
 			}
 		}
+
 		selection = context.getSelection("values");
 
-		ResultList list = new ResultList("values");
-
 		Result result = stringOptionalRequirement(context, list, selection,
-				DESCRIPTION, "Enter Description");
+				DESCRIPTION, "Enter description");
 		if (result != null) {
 			return result;
 		}
-		result = context.makeResult();
-		result.add(" Item is ready to create with following values.");
-		ResultList actions = new ResultList("actions");
+
 		Record finish = new Record(ActionNames.FINISH);
-		finish.add("", "Finish to create Item.");
+		finish.add("", "Finish to create Shipping Method");
 		actions.add(finish);
-		result.add(actions);
 
-		return result;
+		return makeResult;
+	}
+
+	private void setDefaultValues() {
+		get(DESCRIPTION).setDefaultValue("");
 
 	}
 
-	private void completeProcess(Context context) {
-		ShippingMethod shippingMethod = new ShippingMethod();
-		Requirement shippingRequirement = get(SHIPPING_METHOD);
-		String shippingMethodType = shippingRequirement.getValue();
-		shippingMethod.setName(shippingMethodType);
-		// TODO need to set Description is not required??
-		create(shippingMethod, context);
-	}
-
-	private Result shppingMethodRequirment(Context context, ResultList list,
-			String selection) {
-		Requirement req = get(SHIPPING_METHOD);
-		String shippingMethod = (String) req.getValue();
-
-		String attribute = (String) context.getAttribute(INPUT_ATTR);
-		if (attribute != null) {
-			if (attribute.equals(SHIPPING_METHOD)) {
-				String order = context.getSelection(NUMBER);
-				if (order == null) {
-					order = context.getString();
-				}
-				shippingMethod = order;
-				req.setValue(shippingMethod);
-			}
-		}
-
-		if (selection == shippingMethod) {
-			context.setAttribute(INPUT_ATTR, SHIPPING_METHOD);
-			return number(context, "Enter Shipping Method", shippingMethod);
-		}
-
-		Record invoiceNoRec = new Record(shippingMethod);
-		invoiceNoRec.add("Name", "Invoice Number");
-		invoiceNoRec.add("Value", shippingMethod);
-		list.add(invoiceNoRec);
-		return null;
-
+	private void createShippingMethodObject(Context context) {
+		ClientShippingMethod method = new ClientShippingMethod();
+		String name = get(SHIPPING_METHOD).getValue();
+		method.setName(name);
+		String desc = get(DESCRIPTION).getValue();
+		method.setDescription(desc);
+		create(method, context);
 	}
 
 }
