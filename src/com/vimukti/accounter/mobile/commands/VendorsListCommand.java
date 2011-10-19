eded6922ea76d9d0fd169354f22372a1bcd2c4c1
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-10-19 05:50:36 +0000

    Former-commit-id: 068d8ee49adf34f8ef27ab607c2e24bd37039058

--- src/com/vimukti/accounter/mobile/commands/VendorsListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/VendorsListCommand.java
@@ -1,10 +1,12 @@
 package com.vimukti.accounter.mobile.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
@@ -16,8 +18,7 @@ import com.vimukti.accounter.web.client.core.ClientVendor;
  * 
  */
 public class VendorsListCommand extends AbstractTransactionCommand {
-
-	private static final String ACTIVE = "active";
+	private static final String VENDOR_TYPE = "vendorType";
 
 	@Override
 	public String getId() {
@@ -26,71 +27,88 @@ public class VendorsListCommand extends AbstractTransactionCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement(ACTIVE, true, true));
+		// list.add(new Requirement(ACTIVE, true, true));
 
 	}
 
 	@Override
 	public Result run(Context context) {
-		Result result = null;
-
-		result = createVendorsList(context);
-		return result;
-	}
-
-	private Result createVendorsList(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		Object selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			ActionNames actionName = (ActionNames) selection;
-			switch (actionName) {
-			case FINISH:
-				return null;
-			default:
-				break;
-			}
-		}
-		selection = context.getSelection("values");
-
-		ResultList list = new ResultList("values");
-
-		Result result = isActiveRequirement(context, selection);
-		Boolean isActive = (Boolean) get(ACTIVE).getValue();
-		result = vendorsList(context, isActive);
+		Result result = createVendorsList(context);
 		if (result != null) {
 			return result;
 		}
 		return result;
 	}
 
-	private Result vendorsList(Context context, Boolean isActive) {
-		Result result = context.makeResult();
-		ResultList vendorsResult = new ResultList("vendors");
-		result.add("Vendors List");
-		int num = 0;
-		List<ClientVendor> vendors = null;// getVendors(isActive,
-		// context.getCompany());
-		for (ClientVendor vendor : vendors) {
-			vendorsResult.add(createPayeeRecord(vendor));
-			num++;
-			if (num == VENDORS_TO_SHOW) {
+	private Result createVendorsList(Context context) {
+		context.setAttribute(INPUT_ATTR, "optional");
+
+		ActionNames selection = context.getSelection(ACTIONS);
+		if (selection != null) {
+			switch (selection) {
+			case FINISH:
+				markDone();
+				return new Result();
+			case ACTIVE:
+				context.setAttribute(VENDOR_TYPE, true);
+				break;
+			case IN_ACTIVE:
+				context.setAttribute(VENDOR_TYPE, false);
+				break;
+			case ALL:
+				context.setAttribute(VENDOR_TYPE, null);
+				break;
+			default:
 				break;
 			}
 		}
-		int size = vendorsResult.size();
-		StringBuilder message = new StringBuilder();
-		if (size > 0) {
-			message.append("Select a Vendor");
+		Result result = vendorsList(context, selection);
+		return result;
+	}
+
+	private Result vendorsList(Context context, ActionNames selection) {
+		Result result = context.makeResult();
+		ResultList vendorsList = new ResultList("vendorssList");
+		result.add("Vendors List");
+
+		Boolean vendorType = (Boolean) context.getAttribute(VENDOR_TYPE);
+		List<ClientVendor> vendors = getVendors(vendorType);
+
+		ResultList actions = new ResultList("actions");
+
+		List<ClientVendor> pagination = pagination(context, selection, actions,
+				vendors, new ArrayList<ClientVendor>(), VALUES_TO_SHOW);
+
+		for (ClientVendor vendor : pagination) {
+			vendorsList.add(createVendorRecord(vendor));
 		}
-		CommandList commandList = new CommandList();
-		commandList.add("Create");
+
+		StringBuilder message = new StringBuilder();
+		if (vendorsList.size() > 0) {
+			message.append("Select an Vendor");
+		}
 
 		result.add(message.toString());
-		result.add(vendorsResult);
-		result.add(commandList);
-		result.add("Type for Vendor");
+		result.add(vendorsList);
 
+		Record inActiveRec = new Record(ActionNames.ACTIVE);
+		inActiveRec.add("", "Active Vendors");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.IN_ACTIVE);
+		inActiveRec.add("", "InActive Vendors");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.ALL);
+		inActiveRec.add("", "All Vendors");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.FINISH);
+		inActiveRec.add("", "Close");
+		actions.add(inActiveRec);
+
+		result.add(actions);
+
+		CommandList commandList = new CommandList();
+		commandList.add("Add Vendor");
+		result.add(commandList);
 		return result;
 
 	}
