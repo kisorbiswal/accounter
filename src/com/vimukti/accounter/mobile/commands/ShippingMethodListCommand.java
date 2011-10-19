6805b5f0e836d357340ad8fa593df7f8bdc89e94
srinivas.g <srinivas.g@vimukti.com>
2011-10-19 10:54:12 +0000

    Former-commit-id: 831f10e11cfe0c07c8ff38083d3d86040f71f37a

--- src/com/vimukti/accounter/mobile/commands/ShippingMethodListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/ShippingMethodListCommand.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.vimukti.accounter.core.ShippingMethod;
+import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
@@ -13,30 +14,39 @@ import com.vimukti.accounter.mobile.ResultList;
 
 public class ShippingMethodListCommand extends AbstractTransactionCommand {
 
-	private static final int SHIPPINGMETHODS_TO_SHOW = 5;
-
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public Result run(Context context) {
 		Result result = createShippingMethodsListReq(context);
-		if (result == null) {
-			// TODO
+		if (result != null) {
+			return result;
 		}
 		return null;
 	}
 
 	private Result createShippingMethodsListReq(Context context) {
+
+		Object selection = context.getSelection(ACTIONS);
+		ActionNames actionNames;
+		if (selection != null) {
+			actionNames = (ActionNames) selection;
+			switch (actionNames) {
+			case FINISH:
+				markDone();
+				return null;
+			default:
+				break;
+			}
+		}
 
 		Result result = shippingMethods(context);
 		if (result != null) {
@@ -46,22 +56,19 @@ public class ShippingMethodListCommand extends AbstractTransactionCommand {
 	}
 
 	private Result shippingMethods(Context context) {
-		ResultList shipingResultList = new ResultList("locationList");
+		ResultList shipingResultList = new ResultList("shippingmethodsList");
 
 		Result result = context.makeResult();
+
 		List<ShippingMethod> shippingList = getShippingMethodList(context);
-		int record = 0;
 		for (ShippingMethod shippingMethod : shippingList) {
 			shipingResultList.add(createShippingRecord(shippingMethod));
-			record++;
-			if (record == SHIPPINGMETHODS_TO_SHOW) {
-				break;
-			}
 		}
 		result.add(shipingResultList);
 		CommandList commandList = new CommandList();
-		commandList.add("Create");
-		// commandList.add("Remove");
+		commandList.add("Add a New Shipping Method");
+		commandList.add("Edit a New Shipping Method");
+		commandList.add("Delete a New Shipping Method");
 		result.add(commandList);
 
 		return result;
@@ -69,8 +76,8 @@ public class ShippingMethodListCommand extends AbstractTransactionCommand {
 
 	private Record createShippingRecord(ShippingMethod shippingMethod) {
 		Record record = new Record(shippingMethod);
-		record.add("Name", "shippingMethod");
-		record.add("value", shippingMethod.getName());
+		record.add("Name", shippingMethod.getName());
+		record.add("Description", shippingMethod.getDescription());
 		return record;
 	}
 
