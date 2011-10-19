eb661e72f3f63da4f2d46b96870454d5623fc7b9
Umasree V <umasree.v@vimukti.com>
2011-10-19 04:11:45 +0000

    Former-commit-id: 5cc2df0210640000841b591fc398a770e7dab062

--- src/com/vimukti/accounter/mobile/commands/NewJournalEntryCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewJournalEntryCommand.java
@@ -1,14 +1,23 @@
 package com.vimukti.accounter.mobile.commands;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import com.vimukti.accounter.core.Entry;
 import com.vimukti.accounter.mobile.ActionNames;
+import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.ObjectListRequirement;
+import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.RequirementType;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.web.client.core.ClientAccount;
+import com.vimukti.accounter.web.client.core.ClientEntry;
+import com.vimukti.accounter.web.client.core.ClientJournalEntry;
+import com.vimukti.accounter.web.client.core.ClientTransactionItem;
+import com.vimukti.accounter.web.client.core.ListFilter;
 
 /**
  * 
@@ -19,12 +28,14 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 
 	private static final String TRANSACTION_DATE = "TransactionDate";
 	private static final String VOUCHER = "Voucher";
-	private static final String VOUCHER_NUMBER = "Voucher Number";
-	private static final String DATE = "Date";
 	private static final String ACCOUNT = "Account";
 	private static final String CREDIT = "Credit";
 	private static final String DEBIT = "Debit";
-	private static final String MEMO = "memo";
+	private static final Object ENTRIES_PROCESS = "entriesProcess";
+	private static final String OLD_ENTRY_ATTR = "oldEntryAttribute";
+	private static final String ENTRY_PROPERTY_ATTR = "entryPropertyAttr";
+	private static final String ENTRY_DETAILS = "entryDetails";
+	private static final String ENTRIES = "entries";
 
 	@Override
 	public String getId() {
@@ -40,11 +51,11 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 
 			@Override
 			public void addRequirements(List<Requirement> list) {
-				list.add(new Requirement(VOUCHER_NUMBER, false, true));
-				list.add(new Requirement(DATE, false, true));
+				list.add(new Requirement(DATE, true, true));
 				list.add(new Requirement(ACCOUNT, false, true));
 				list.add(new Requirement(CREDIT, false, true));
 				list.add(new Requirement(DEBIT, false, true));
+				list.add(new Requirement(MEMO, true, true));
 			}
 		});
 
@@ -54,50 +65,194 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 
 	@Override
 	public Result run(Context context) {
+		Object attribute = context.getAttribute(INPUT_ATTR);
+		if (attribute == null) {
+			context.setAttribute(INPUT_ATTR, "optional");
+		}
+		setDefaultValues();
 
+		Result makeResult = context.makeResult();
+		makeResult
+				.add(" Journal Entry is ready to create with following values.");
+		ResultList list = new ResultList("values");
+		makeResult.add(list);
+		ResultList actions = new ResultList(ACTIONS);
+		makeResult.add(actions);
+
+		String process = (String) context.getAttribute(PROCESS_ATTR);
 		Result result = null;
-		result = journelentryNumReq(context);
-		if (result != null) {
-			return result;
-		}
-		result = entryRequirement(context);
-		if (result != null) {
-			return result;
-		}
-		result = createOptionalResult(context);
-		if (result != null) {
-			return result;
-		}
-
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param context
-	 * @return
-	 */
-	private Result journelentryNumReq(Context context) {
-
-		Requirement requirement = get(NUMBER);
-		if (!requirement.isDone()) {
-			String journelentryNum = context.getSelection(TEXT);
-			if (journelentryNum != null) {
-				requirement.setValue(journelentryNum);
-			} else {
-				return text(context, "Please enter the  Journel Entry Number",
-						null);
+		if (process != null) {
+			if (process.equals(ENTRIES_PROCESS)) {
+				result = entriesProcess(context);
+				if (result != null) {
+					return result;
+				}
 			}
 		}
-		String input = (String) context.getAttribute(INPUT_ATTR);
-		if (input.equals(NUMBER)) {
-			requirement.setValue(input);
-		}
-		return null;
 
+		result = numberRequirement(context, list, NUMBER,
+				"Please enter the  Journel Entry Number");
+		if (result != null) {
+			return result;
+		}
+		result = entryRequirement(context, makeResult, actions);
+		if (result != null) {
+			return result;
+		}
+		result = createOptionalResult(context, list, actions, makeResult);
+		if (result != null) {
+			return result;
+		}
+
+		return null;
 	}
 
-	private Result createOptionalResult(Context context) {
+	private Result entriesProcess(Context context) {
+		ClientEntry entry = (ClientEntry) context.getAttribute(OLD_ENTRY_ATTR);
+		Result result = entry(context, entry);
+		if (result == null) {
+			ActionNames actionName = context.getSelection(ACTIONS);
+			if (actionName == ActionNames.DELETE_ITEM) {
+				Requirement itemsReq = get("entries");
+				List<ClientJournalEntry> transItems = itemsReq.getValue();
+				transItems.remove(entry);
+				context.removeAttribute(OLD_ENTRY_ATTR);
+			}
+		}
+		return result;
+	}
+
+	private Result entry(Context context, ClientEntry entry) {
+		context.setAttribute(PROCESS_ATTR, ENTRIES_PROCESS);
+		context.setAttribute(OLD_ENTRY_ATTR, entry);
+
+		String lineAttr = (String) context.getAttribute(ENTRY_PROPERTY_ATTR);
+		if (lineAttr != null) {
+			context.removeAttribute(ENTRY_PROPERTY_ATTR);
+			if (lineAttr.equals(DATE)) {
+				entry.setEntryDate(context.getDate().getTime());
+			} else if (lineAttr.equals(ACCOUNT)) {
+				ClientAccount account = context.getSelection(ACCOUNT);
+				entry.setAccount(account.getID());
+			} else if (lineAttr.equals(CREDIT)) {
+				if (context.getDouble() != null) {
+					entry.setCredit(context.getDouble());
+				} else {
+					entry.setCredit(context.getInteger().doubleValue());
+				}
+			} else if (lineAttr.equals(DEBIT)) {
+				if (context.getDouble() != null) {
+					entry.setDebit(context.getDouble());
+				} else {
+					entry.setDebit(context.getInteger().doubleValue());
+				}
+			} else if (lineAttr.equals(MEMO)) {
+				entry.setMemo(context.getString());
+			}
+		} else {
+			Object selection = context.getSelection(ENTRY_DETAILS);
+			if (selection != null) {
+				if (selection.equals(DATE)) {
+					context.setAttribute(ENTRY_PROPERTY_ATTR, DATE);
+					return date(context, "Please enter the date", new Date(
+							entry.getEntryDate()));
+				} else if (selection.equals(ACCOUNT)) {
+					context.setAttribute(ENTRY_PROPERTY_ATTR, ACCOUNT);
+					return accounts(context, ACCOUNT,
+							new ListFilter<ClientAccount>() {
+
+								@Override
+								public boolean filter(ClientAccount e) {
+									// TODO Auto-generated method stub
+									return false;
+								}
+							});
+				} else if (selection.equals(CREDIT)) {
+					context.setAttribute(ENTRY_PROPERTY_ATTR, CREDIT);
+					return amount(context, "Please enter the credit amount",
+							entry.getCredit());
+				} else if (selection.equals(DEBIT)) {
+					context.setAttribute(ENTRY_PROPERTY_ATTR, "taxCode");
+					return amount(context, "Please enter the debit amount",
+							entry.getDebit());
+				} else if (selection.equals(MEMO)) {
+					context.setAttribute(ENTRY_PROPERTY_ATTR, MEMO);
+					return text(context, "Enter Memo", entry.getMemo());
+				}
+			} else {
+				selection = context.getSelection(ACTIONS);
+				if (selection == ActionNames.FINISH_ITEM) {
+					if (entry.getAccount() == 0) {
+						context.setAttribute(ENTRY_PROPERTY_ATTR, DATE);
+						return accounts(context, ACCOUNT,
+								new ListFilter<ClientAccount>() {
+
+									@Override
+									public boolean filter(ClientAccount e) {
+										// TODO Auto-generated method stub
+										return false;
+									}
+								});
+					}
+					context.removeAttribute(PROCESS_ATTR);
+					context.removeAttribute(OLD_ENTRY_ATTR);
+					return null;
+				} else if (selection == ActionNames.DELETE_ITEM) {
+					context.removeAttribute(PROCESS_ATTR);
+					return null;
+				}
+			}
+		}
+		ResultList list = new ResultList(ENTRY_DETAILS);
+		Record record = new Record(DATE);
+		record.add("", "Date");
+		record.add("", entry.getEntryDate());
+		list.add(record);
+
+		record = new Record(ACCOUNT);
+		record.add("", "Account");
+		record.add("", entry.getAccount());
+		list.add(record);
+
+		record = new Record(DEBIT);
+		record.add("", "Debit");
+		record.add("", entry.getDebit());
+		list.add(record);
+
+		record = new Record(CREDIT);
+		record.add("", "Credit");
+		record.add("", entry.getCredit());
+		list.add(record);
+
+		record = new Record(MEMO);
+		record.add("", "Memo");
+		record.add("", entry.getMemo());
+		list.add(record);
+
+		Result result = context.makeResult();
+		result.add("Item details");
+		result.add("Item Name :"
+				+ getClientCompany().getAccount(entry.getAccount()).getName());
+		result.add(list);
+
+		ResultList actions = new ResultList(ACTIONS);
+		record = new Record(ActionNames.DELETE_ITEM);
+		record.add("", "Delete");
+		actions.add(record);
+		record = new Record(ActionNames.FINISH_ITEM);
+		record.add("", "Finish");
+		actions.add(record);
+		result.add(actions);
+		return result;
+	}
+
+	private void setDefaultValues() {
+		get(TRANSACTION_DATE).setDefaultValue(new Date());
+		get(MEMO).setDefaultValue("");
+	}
+
+	private Result createOptionalResult(Context context, ResultList list,
+			ResultList actions, Result makeResult) {
 		context.setAttribute(INPUT_ATTR, "optional");
 
 		Object selection = context.getSelection(ACTIONS);
@@ -105,7 +260,7 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 			ActionNames actionName = (ActionNames) selection;
 			switch (actionName) {
 			case ADD_MORE_ITEMS:
-				return items(context);
+				return entries(context);
 			case FINISH:
 				context.removeAttribute(INPUT_ATTR);
 				return null;
@@ -116,17 +271,15 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 
 		Result result = null;
 		Requirement entriesReq = get("entries");
-		List<Entry> entries = entriesReq.getValue();
+		List<ClientEntry> entries = entriesReq.getValue();
 
 		selection = context.getSelection("entries");
 		if (selection != null) {
 			// TODO
 		}
 
-		ResultList list = new ResultList("values");
-
 		result = dateOptionalRequirement(context, list, DATE,
-				"Enter journelEntry Date", selection);
+				"Enter journalEntry Date", selection);
 		if (result != null) {
 			return result;
 		}
@@ -139,26 +292,99 @@ public class NewJournalEntryCommand extends AbstractTransactionCommand {
 		return null;
 	}
 
-	private Result entryRequirement(Context context) {
-		Requirement entryReq = get("entries");
-		List<Entry> entries = context.getSelections("entries");
-		if (!entryReq.isDone()) {
-			if (entries.size() > 0) {
-				entryReq.setValue(entries);
-			} else {
-				return Entries(context);
+	private Result entryRequirement(Context context, Result result,
+			ResultList actions) {
+		Requirement entriesReq = get(VOUCHER);
+		List<ClientEntry> items = context.getSelections(ENTRIES);
+		if (items != null && items.size() > 0) {
+			entriesReq.setValue(items);
+		}
+		if (!entriesReq.isDone()) {
+			return entries(context);
+		}
+
+		Object selection = context.getSelection("transactionItems");
+		if (selection != null) {
+			result = transactionItem(context, (ClientTransactionItem) selection);
+			if (result != null) {
+				return result;
 			}
 		}
-		if (entries != null && entries.size() > 0) {
-			List<Entry> entries1 = entryReq.getValue();
-			entries1.addAll(entries);
+
+		selection = context.getSelection(ACTIONS);
+		ActionNames actionName = (ActionNames) selection;
+		if (actionName != null && actionName == ActionNames.ADD_MORE_ITEMS) {
+			return items(context);
 		}
+
+		result.add("Items:-");
+		ResultList itemsList = new ResultList("transactionItems");
+		List<ClientTransactionItem> transItems = entriesReq.getValue();
+		for (ClientTransactionItem item : transItems) {
+			Record itemRec = new Record(item);
+			itemRec.add("Name", getClientCompany().getItem(item.getItem())
+					.getName());
+			itemRec.add(", Total", item.getLineTotal());
+			itemRec.add(", VatCode", item.getVATfraction());
+			itemsList.add(itemRec);
+		}
+		result.add(itemsList);
+
+		Record moreItems = new Record(ActionNames.ADD_MORE_ITEMS);
+		moreItems.add("", "Add more items");
+		actions.add(moreItems);
 		return null;
 	}
 
-	private Result Entries(Context context) {
-		// TODO Auto-generated method stub
+	private Result entries(Context context) {
+		Result result = context.makeResult();
+		List<ClientEntry> items = new ArrayList<ClientEntry>();// = getItems();
+		ResultList list = new ResultList(ENTRIES);
+		ClientEntry last = (ClientEntry) context.getLast(RequirementType.ENTRY);
+		int num = 0;
+		if (last != null) {
+			list.add(createEntryRecord(last));
+			num++;
+		}
+		Requirement entryReq = get(VOUCHER);
+		List<ClientEntry> entries = entryReq.getValue();
+		if (entries == null) {
+			entries = new ArrayList<ClientEntry>();
+		}
+		List<Long> availableItems = new ArrayList<Long>();
+		for (ClientEntry entry : entries) {
+			// availableItems.add(entry.getItem());
+		}
+		for (ClientEntry item : items) {
+			if (item != last && !availableItems.contains(item.getID())) {
+				list.add(createEntryRecord(item));
+				num++;
+			}
+			if (num == ITEMS_TO_SHOW) {
+				break;
+			}
+		}
+		list.setMultiSelection(true);
+		if (list.size() > 0) {
+			result.add("Slect an Item(s).");
+		} else {
+			result.add("You don't have Items.");
+			Record record = new Record("escape");
+			record.add("", "Skip");
+			ResultList resultList = new ResultList("escape");
+			resultList.add(record);
+			result.add(resultList);
+		}
 
+		result.add(list);
+		CommandList commands = new CommandList();
+		commands.add("Create New Item");
+		result.add(commands);
+		return result;
+	}
+
+	private Record createEntryRecord(ClientEntry last) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
