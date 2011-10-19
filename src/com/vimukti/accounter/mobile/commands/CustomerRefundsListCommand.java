ac29380c1cbd34d8821336cdfa8c084c05a24257
srinivas.g <srinivas.g@vimukti.com>
2011-10-19 05:12:27 +0000

    Former-commit-id: 87b53137cde28c105abd328781b8a6ef20589f16

--- src/com/vimukti/accounter/mobile/commands/CustomerRefundsListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/CustomerRefundsListCommand.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
@@ -15,11 +16,18 @@ import com.vimukti.accounter.web.client.core.ClientTransaction;
 import com.vimukti.accounter.web.client.core.Utility;
 import com.vimukti.accounter.web.client.core.Lists.CustomerRefundsList;
 import com.vimukti.accounter.web.client.ui.Accounter;
-import com.vimukti.accounter.web.client.ui.DataUtils;
-import com.vimukti.accounter.web.client.ui.UIUtils;
 import com.vimukti.accounter.web.server.FinanceTool;
 
 public class CustomerRefundsListCommand extends AbstractCommand {
+
+	private static final String CURRENT_VIEW = "Current View";
+
+	private static final String ISSUED = "Issued";
+	private static final String NOT_ISSUED = "Not issued";
+	private static final String VOIDED = "Voided";
+	private static final String ALL = "All";
+
+	private static final int ITEMS_TO_SHOW = 4;
 
 	@Override
 	public String getId() {
@@ -29,13 +37,13 @@ public class CustomerRefundsListCommand extends AbstractCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement(VIEW_BY, true, true));
+		list.add(new Requirement(CURRENT_VIEW, true, true));
 	}
 
 	@Override
 	public Result run(Context context) {
 		Result result = null;
-
+		setDefaultValues();
 		result = createOptionalResult(context);
 		if (result != null) {
 			return result;
@@ -43,19 +51,113 @@ public class CustomerRefundsListCommand extends AbstractCommand {
 		return result;
 	}
 
+	private void setDefaultValues() {
+		get(CURRENT_VIEW).setDefaultValue(ISSUED);
+
+	}
+
 	private Result createOptionalResult(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
 
-		Object selection = context.getSelection(VIEW_BY);
+		List<String> viewType = new ArrayList<String>();
+		viewType.add(NOT_ISSUED);
+		viewType.add(ISSUED);
+		viewType.add(VOIDED);
+		viewType.add(ALL);
 
-		ResultList list = new ResultList("viewlist");
-		Result result = viewTypeRequirement(context, list, selection);
+		ResultList resultList = new ResultList("values");
+		Object selection = context.getSelection(ACTIONS);
+		ActionNames actionNames;
+		if (selection != null) {
+			actionNames = (ActionNames) selection;
+			switch (actionNames) {
+			case FINISH:
+				markDone();
+				return null;
+			default:
+				break;
+			}
+		}
+
+		selection = context.getSelection("values");
+		Result result = stringListOptionalRequirement(context, resultList,
+				selection, CURRENT_VIEW, "Current View", viewType,
+				"Select View type", ITEMS_TO_SHOW);
 		if (result != null) {
 			return result;
 		}
-		String viewType = get(VIEW_BY).getValue();
-		result = customerRefunds(context, viewType);
+
+		String view = get(CURRENT_VIEW).getValue();
+		result = getCustomerRefundList(context, view);
+		result.add(resultList);
 		return result;
+	}
+
+	private Result getCustomerRefundList(Context context, String view) {
+		ArrayList<CustomerRefundsList> list = getCustomerRefundsList(context
+				.getCompany().getID());
+		ArrayList<CustomerRefundsList> filterList = filterList(view, list);
+
+		Result result = context.makeResult();
+		ResultList resultList = new ResultList("customerrefundsList");
+
+		for (CustomerRefundsList customer : filterList) {
+
+			resultList.add(createCustomerRefundRecord(customer));
+
+		}
+
+		StringBuilder message = new StringBuilder();
+		if (resultList.size() > 0) {
+			message.append("Select a Customer Refund record");
+		}
+
+		result.add(message.toString());
+		result.add(resultList);
+
+		CommandList commandList = new CommandList();
+		commandList.add("Add a New Customer Refund record");
+		result.add(commandList);
+		return result;
+	}
+
+	private ArrayList<CustomerRefundsList> filterList(String text,
+			ArrayList<CustomerRefundsList> list) {
+
+		ArrayList<CustomerRefundsList> newList = new ArrayList<CustomerRefundsList>();
+		for (CustomerRefundsList customerRefund : list) {
+			if (text.equals(NOT_ISSUED)) {
+				if ((customerRefund.getStatus() == STATUS_NOT_ISSUED || customerRefund
+						.getStatus() == STATUS_PARTIALLY_PAID)
+						&& (!customerRefund.isVoided()))
+					newList.add(customerRefund);
+			}
+			if (text.equals(ISSUED)) {
+				if (customerRefund.getStatus() == STATUS_ISSUED
+						&& (!customerRefund.isVoided()))
+					newList.add(customerRefund);
+			}
+			if (text.equals(VOIDED)) {
+				if (customerRefund.isVoided() && !customerRefund.isDeleted())
+					newList.add(customerRefund);
+			}
+			if (text.equals(ALL)) {
+
+				newList.add(customerRefund);
+			}
+		}
+		return newList;
+	}
+
+	public ArrayList<CustomerRefundsList> getCustomerRefundsList(long companyId) {
+		List<CustomerRefundsList> customerRefundsList = null;
+		try {
+
+			customerRefundsList = new FinanceTool().getCustomerManager()
+					.getCustomerRefundsList(companyId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<CustomerRefundsList>(customerRefundsList);
 	}
 
 	private Result customerRefunds(Context context, String viewType) {
@@ -68,7 +170,7 @@ public class CustomerRefundsListCommand extends AbstractCommand {
 				viewType, context.getCompany());
 
 		for (CustomerRefundsList cusRef : customerRefunds) {
-			billsListData.add(createCustoemrRefundRecord(cusRef));
+			billsListData.add(createCustomerRefundRecord(cusRef));
 			num++;
 			if (num == VALUES_TO_SHOW) {
 				break;
@@ -126,22 +228,19 @@ public class CustomerRefundsListCommand extends AbstractCommand {
 
 	}
 
-	private Record createCustoemrRefundRecord(CustomerRefundsList customerRefund) {
+	private Record createCustomerRefundRecord(CustomerRefundsList customerRefund) {
 		Record record = new Record(customerRefund);
-		record.add("Payment Date",
-				UIUtils.getDateByCompanyType(customerRefund.getPaymentDate()));
+		record.add("Payment Date", customerRefund.getPaymentDate());
 		record.add("Payment No.", customerRefund.getPaymentNumber());
 		record.add("Status", Utility.getStatus(
 				ClientTransaction.TYPE_CUSTOMER_REFUNDS,
 				customerRefund.getStatus()));
-		record.add("Issue Date",
-				UIUtils.getDateByCompanyType(customerRefund.getIssueDate()));
+		record.add("Issue Date", customerRefund.getIssueDate());
 		record.add("Name", customerRefund.getName());
 		record.add("Type",
 				Utility.getTransactionName((customerRefund.getType())));
 		record.add("Payment Method", customerRefund.getPaymentMethod());
-		record.add("Amount Paid",
-				DataUtils.getAmountAsString(customerRefund.getAmountPaid()));
+		record.add("Amount Paid", customerRefund.getAmountPaid());
 		record.add("Voided", customerRefund.isVoided() ? "Voided"
 				: "Not Voided");
 		return record;
