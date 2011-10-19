7cebc92edb03174f624a5a6d538c223137e4c31c
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-10-19 10:50:54 +0000

    Former-commit-id: 961653b2d5321a6475e1836245d7a50d0d7055d7

--- src/com/vimukti/accounter/mobile/commands/UsersCommand.java
+++ src/com/vimukti/accounter/mobile/commands/UsersCommand.java
@@ -1,8 +1,8 @@
 package com.vimukti.accounter.mobile.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.vimukti.accounter.core.User;
 import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
@@ -10,11 +10,10 @@ import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.web.client.core.ClientUserInfo;
 
 public class UsersCommand extends AbstractTransactionCommand {
-
-	private static final int USERS_TO_SHOW = 5;
-
+	private static final String USER_TYPE = "userType";
 	@Override
 	public String getId() {
 		return null;
@@ -27,72 +26,113 @@ public class UsersCommand extends AbstractTransactionCommand {
 
 	@Override
 	public Result run(Context context) {
-
-		Result result = optionalRequirements(context);
-		return result;
-	}
-
-	private Result optionalRequirements(Context context) {
-
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		Object selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			ActionNames actionName = (ActionNames) selection;
-			switch (actionName) {
-			case FINISH:
-				return null;
-			default:
-				break;
-			}
-		}
-		selection = context.getSelection("values");
-
-		ResultList list = new ResultList("values");
-
-		Result result = usersList(context);
+		Result result = createUsersList(context);
 		if (result != null) {
 			return result;
 		}
 		return result;
 	}
 
-	private Result usersList(Context context) {
-		Result result = context.makeResult();
-		ResultList userList = new ResultList("users");
-		int num = 0;
-		List<User> users = getUsersList();
-		for (User user : users) {
-			userList.add(createUserRecord(user));
-			num++;
-			if (num == USERS_TO_SHOW) {
+	private Result createUsersList(Context context) {
+		context.setAttribute(INPUT_ATTR, "optional");
+
+		ActionNames selection = context.getSelection(ACTIONS);
+		if (selection != null) {
+			switch (selection) {
+			case FINISH:
+				markDone();
+				return new Result();
+			case ACTIVE:
+				context.setAttribute(USER_TYPE, true);
+				break;
+			case IN_ACTIVE:
+				context.setAttribute(USER_TYPE, false);
+				break;
+			case ALL:
+				context.setAttribute(USER_TYPE, null);
+				break;
+			default:
 				break;
 			}
 		}
-
-		result.add(userList);
-
-		CommandList commandList = new CommandList();
-		commandList.add("Create");
-
-		result.add(commandList);
-
+		Result result = usersList(context,selection);
 		return result;
 	}
 
-	private List<User> getUsersList() {
-		// TODO need to get list of Users
-		return null;
+	private Result usersList(Context context, ActionNames selection) {
+		Result result = context.makeResult();
+		ResultList usersList = new ResultList("usersList");
+		result.add("Users List");
+
+		Boolean userType = (Boolean) context.getAttribute(USER_TYPE);
+		List<ClientUserInfo> users = getUsers(userType); 
+
+		ResultList actions = new ResultList("actions");
+
+		List<ClientUserInfo> pagination = pagination(context, selection, actions,
+				users, new ArrayList<ClientUserInfo>(), VALUES_TO_SHOW);
+
+		for (ClientUserInfo user : pagination) {
+			usersList.add(createUserRecord(user));
+		}
+
+		StringBuilder message = new StringBuilder();
+		if (usersList.size() > 0) {
+			message.append("Select an User");
+		}
+
+		result.add(message.toString());
+		result.add(usersList);
+
+		Record inActiveRec = new Record(ActionNames.ACTIVE);
+		inActiveRec.add("", "Active Users");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.IN_ACTIVE);
+		inActiveRec.add("", "InActive Users");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.ALL);
+		inActiveRec.add("", "All Users");
+		actions.add(inActiveRec);
+		inActiveRec = new Record(ActionNames.FINISH);
+		inActiveRec.add("", "Close");
+		actions.add(inActiveRec);
+
+		result.add(actions);
+
+		CommandList commandList = new CommandList();
+		commandList.add("Invite User");
+		result.add(commandList);
+		return result;
+
 	}
 
-	private Record createUserRecord(User user) {
+	 
+	private Record createUserRecord(ClientUserInfo user) {
 		Record record = new Record(user);
 		// record.add("First Name", user.getFirstName());
 		// record.add("Last Name", user.getLastName());
 		record.add("User Role", user.getUserRole());
-		record.add("Email Id", user.getClient().getEmailId());
-		// record.add("Status", user.isActive());
+		record.add("Email Id", user.getEmail());
+		record.add("Status", user.isActive()?"active" :"inactive");
 		return record;
 	}
+
+	private List<ClientUserInfo> getUsers(Boolean isActive) {
+
+		ArrayList<ClientUserInfo> users = getClientCompany().getUsersList(); 
+		if (isActive == null) {
+			return users;
+		}
+		ArrayList<ClientUserInfo> result = new ArrayList<ClientUserInfo>();
+		for (ClientUserInfo user : users) {
+			if (user.isActive() == isActive) {
+				result.add(user);
+			}
+		}
+		return result;
+	
+	}
+
+	 
 
 }
