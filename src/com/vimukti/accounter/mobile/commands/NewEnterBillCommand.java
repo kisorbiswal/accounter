5b8b98f070fc48b7511dd2aa2944b82bf8a4af7d
Saiprasad N <saiprasad.n@vimukti.com>
2011-10-20 11:09:25 +0000

    Former-commit-id: c3d1eaeeb5f3af57063c7adff96303e0f0d3f380

--- src/com/vimukti/accounter/mobile/commands/NewEnterBillCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewEnterBillCommand.java
@@ -12,6 +12,7 @@ import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.ClientAccount;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.core.ClientContact;
@@ -107,7 +108,8 @@ public class NewEnterBillCommand extends AbstractTransactionCommand {
 
 		setTransactionType(CUSTOMER_TRANSACTION);
 
-		result = createSupplierRequirement(context, list, VENDOR);
+		result = createSupplierRequirement(context, list, VENDOR, Global.get()
+				.Vendor());
 		if (result != null) {
 			return result;
 		}
