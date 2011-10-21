f38e362d245a104a6107ccd4bbe136be273a007c
Nagaraju P <nagaraju.p@vimukti.com>
2011-10-21 08:55:24 +0000

    Former-commit-id: 59737cbadf3f62a9c319553b5c579e394f6fbb0d

--- src/com/vimukti/accounter/mobile/commands/AuthenticationCommand.java
+++ src/com/vimukti/accounter/mobile/commands/AuthenticationCommand.java
@@ -56,12 +56,7 @@ public class AuthenticationCommand extends AbstractCommand {
 							.getNetworkId());
 					if (activationList == null || activationList.size() == 0) {
 						client = getClient(context.getString());
-						if (client != null) {
-							sendActivationMail(context.getNetworkId(),
-									client.getEmailId());
-							makeResult
-									.add("Activation code has sent to your email Id. Enter Activation code");
-						} else {
+						if (client == null) {
 							CommandList commandList = new CommandList();
 							commandList.add("Signup");
 							makeResult.add("Enter valid Accounter emailId");
@@ -77,10 +72,11 @@ public class AuthenticationCommand extends AbstractCommand {
 							getClient(activation.getEmailId()));
 					makeResult.add("Activation Success");
 				}
-			} else {
-				imUser = createIMUser(context.getNetworkType(),
-						context.getNetworkId(), client);
-				makeResult.add("Activation Success.");
+			}
+			if (client != null) {
+				sendActivationMail(context.getNetworkId(), client.getEmailId());
+				makeResult
+						.add("Activation code has sent to your email Id. Enter Activation code");
 			}
 		}
 		if (imUser != null) {
