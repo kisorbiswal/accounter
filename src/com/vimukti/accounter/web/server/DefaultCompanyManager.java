cf7b394393ef694c2fce4340f3ac06bad2fbc598
Amrit Mishra <amrit.mishra@vimukti.com>
2011-10-23 04:01:50 +0000

    Former-commit-id: 382a9635d8bf64ef6aeed3cd4e46079677214006

--- src/com/vimukti/accounter/web/server/DefaultCompanyManager.java
+++ src/com/vimukti/accounter/web/server/DefaultCompanyManager.java
@@ -3,11 +3,20 @@ package com.vimukti.accounter.web.server;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import com.vimukti.accounter.main.ServerConfiguration;
-import com.vimukti.accounter.web.client.core.ClientCompany;
+import com.vimukti.accounter.web.client.core.ClientAddress;
+import com.vimukti.accounter.web.client.core.ClientBankAccount;
+import com.vimukti.accounter.web.client.core.ClientContact;
+import com.vimukti.accounter.web.client.core.ClientCustomer;
+import com.vimukti.accounter.web.client.core.ClientEstimate;
+import com.vimukti.accounter.web.client.core.ClientItem;
+import com.vimukti.accounter.web.client.core.ClientTransactionItem;
+import com.vimukti.accounter.web.client.core.ClientVendor;
+import com.vimukti.accounter.web.client.core.DemoCompany;
 
 public class DefaultCompanyManager {
 
@@ -17,14 +26,42 @@ public class DefaultCompanyManager {
 	 */
 	public static void main(String[] args) throws IOException {
 
-		System.out.println("Default company manager");
 		XStream xStream = new XStream(new DomDriver());
 
-		xStream.alias("DefaultCompany", ClientCompany.class);
+		xStream = createAlias(xStream);
 
 		File file = getFile();
 
 		Object object = xStream.fromXML(new FileInputStream(file));
+
+	}
+
+	private static XStream createAlias(XStream xStream) {
+
+		xStream.alias("ClientCompany", DemoCompany.class);
+
+		xStream.alias("customers", List.class);
+		xStream.alias("Customer", ClientCustomer.class);
+
+		xStream.alias("ClientAddress", ClientAddress.class);
+		xStream.alias("ClientContact", ClientContact.class);
+
+		xStream.alias("vendors", List.class);
+		xStream.alias("Vendor", ClientVendor.class);
+
+		xStream.alias("accounts", List.class);
+		xStream.alias("BankAccount", ClientBankAccount.class);
+
+		xStream.alias("items", List.class);
+		xStream.alias("ClientItem", ClientItem.class);
+
+		xStream.alias("quotes", List.class);
+		xStream.alias("Quote", ClientEstimate.class);
+
+		xStream.alias("transactionItems", List.class);
+		xStream.alias("ClientTransactionItem", ClientTransactionItem.class);
+
+		return xStream;
 
 	}
 
