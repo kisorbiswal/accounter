9a66d2554fcf195e39aecae2bb027b69abc19eac
Nagaraju P <nagaraju.p@vimukti.com>
2011-10-24 13:02:01 +0000

    Former-commit-id: 7dc92137850d2e4d40940e35e3a0dc958ebe50e8

--- src/com/vimukti/accounter/mobile/commands/CompanyTypeCommand.java
+++ src/com/vimukti/accounter/mobile/commands/CompanyTypeCommand.java
@@ -3,13 +3,14 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.List;
 
 import com.vimukti.accounter.mobile.ActionNames;
+import com.vimukti.accounter.mobile.Command;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
 
-public class CompanyTypeCommand extends AbstractCommand {
+public class CompanyTypeCommand extends Command {
 
 	@Override
 	public String getId() {
@@ -28,7 +29,7 @@ public class CompanyTypeCommand extends AbstractCommand {
 		Object selection = context.getSelection("actions");
 		if (selection == null) {
 			makeResult
-					.add("Select “Start Setup” to enter the Setup wizard to add all the necessary information for company or else skip this wizard and add later.");
+					.add("Select 'Full Setup' to enter the Setup wizard to add all the necessary information for company or else select 'Partial Setup' to skip this wizard and add later.");
 			ResultList list = new ResultList("actions");
 			Record start = new Record(ActionNames.FULL_SETUP);
 			start.add("", "Full Setup");
@@ -37,6 +38,7 @@ public class CompanyTypeCommand extends AbstractCommand {
 			Record skip = new Record(ActionNames.PARTIAL_SETUP);
 			skip.add("", "Partial Setup");
 			list.add(skip);
+			makeResult.add(list);
 			return makeResult;
 		}
 		markDone();
