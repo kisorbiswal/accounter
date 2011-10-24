e6e29eb77f75d88a22a5ad8e5f39d0f5c934a630
Umasree V <umasree.v@vimukti.com>
2011-10-24 13:51:15 +0000

    Former-commit-id: 9d0f02eed9c57e99c8120c97bab9737e97c14ec6

---
+++ src/com/vimukti/accounter/mobile/requirements/CurrencyRequirement.java
@@ -0,0 +1,38 @@
+package com.vimukti.accounter.mobile.requirements;
+
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.web.client.core.ClientCurrency;
+
+public abstract class CurrencyRequirement extends
+		ListRequirement<ClientCurrency> {
+
+	public CurrencyRequirement(String requirementName, String displayString,
+			String recordName, boolean isOptional, boolean isAllowFromContext,
+			ChangeListner<ClientCurrency> listner) {
+		super(requirementName, displayString, recordName, isOptional,
+				isAllowFromContext, listner);
+	}
+
+	@Override
+	protected Record createRecord(ClientCurrency value) {
+		Record record = new Record(value);
+		record.add("", value.getName());
+		return record;
+	}
+
+	@Override
+	protected String getDisplayValue(ClientCurrency value) {
+		return value != null ? value.getName() : "";
+	}
+
+	@Override
+	protected String getCreateCommandString() {
+		return null;
+	}
+
+	@Override
+	protected String getSelectString() {
+		return getMessages().pleaseSelect(getConstants().currency());
+	}
+
+}
