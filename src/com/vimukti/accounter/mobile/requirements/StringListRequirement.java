39382a88b9c4df6953dc19444d4a68afd8056497
Nagaraju P <nagaraju.p@vimukti.com>
2011-10-25 11:42:45 +0000

    Former-commit-id: 600b49a1139f4a25aae038f7b2a4420cd745e512

---
+++ src/com/vimukti/accounter/mobile/requirements/StringListRequirement.java
@@ -0,0 +1,41 @@
+package com.vimukti.accounter.mobile.requirements;
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.requirements.ChangeListner;
+import com.vimukti.accounter.mobile.requirements.ListRequirement;
+
+public abstract class StringListRequirement extends ListRequirement<String> {
+
+	public StringListRequirement(String requirementName, String enterString,
+			String recordName, boolean isOptional, boolean isAllowFromContext,
+			ChangeListner<String> listner) {
+		super(requirementName, enterString, recordName, isOptional,
+				isAllowFromContext, listner);
+	}
+
+	@Override
+	protected String getEmptyString() {
+		return null;
+	}
+
+	@Override
+	protected Record createRecord(String value) {
+		Record record = new Record(value);
+		record.add("", value);
+		return record;
+	}
+
+	@Override
+	protected String getDisplayValue(String value) {
+		return value;
+	}
+
+	@Override
+	protected String getCreateCommandString() {
+		return null;
+	}
+
+	@Override
+	protected boolean filter(String e, String name) {
+		return e.startsWith(name);
+	}
+}
