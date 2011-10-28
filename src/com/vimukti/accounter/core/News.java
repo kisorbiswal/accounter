33f681095e195759b4fd457c5cb16f6720663445
Lingarao R <lingarao.r@vimukti.com>
2011-10-28 05:55:49 +0000

    Former-commit-id: ffa6ecfead05240b0c34b8a9dd8b89ca7b6776fc

---
+++ src/com/vimukti/accounter/core/News.java
@@ -0,0 +1,62 @@
+package com.vimukti.accounter.core;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+/**
+ * 
+ * @author vimukti2
+ * 
+ */
+public class News {
+
+	private long id;
+
+	private Date sqlDate;
+
+	private boolean sticky;
+
+	private String url;
+
+	private String newsText;
+
+	public boolean isSticky() {
+		return sticky;
+	}
+
+	public void setSticky(boolean sticky) {
+		this.sticky = sticky;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getNewsText() {
+		return newsText;
+	}
+
+	public void setNewsText(String newsText) {
+		this.newsText = newsText;
+	}
+
+	public Date getSqlDate() {
+		return sqlDate;
+	}
+
+	public void setSqlDate(Timestamp sqlDate) {
+		this.sqlDate = sqlDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
