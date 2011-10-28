365548021143187258d6bc1eab3497da5f85c817
Devaraju K <devaraju.k@vimukti.com>
2011-10-28 08:46:04 +0000

    Former-commit-id: d40c5af81739e99668a61fa3f4d85b5e94e8d2f0

---
+++ src/com/vimukti/accounter/web/client/ui/edittable/WareHouseDropDownTable.java
@@ -0,0 +1,71 @@
+package com.vimukti.accounter.web.client.ui.edittable;
+
+import java.util.List;
+
+import com.google.gwt.user.cellview.client.TextColumn;
+import com.vimukti.accounter.web.client.core.ClientWarehouse;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.ActionCallback;
+import com.vimukti.accounter.web.client.ui.core.ActionFactory;
+
+public class WareHouseDropDownTable extends
+		AbstractDropDownTable<ClientWarehouse> {
+
+	public WareHouseDropDownTable() {
+		super(Accounter.getCompany().getWarehouses());
+	}
+
+	@Override
+	public List<ClientWarehouse> getTotalRowsData() {
+		return Accounter.getCompany().getWarehouses();
+	}
+
+	@Override
+	protected ClientWarehouse getAddNewRow() {
+		ClientWarehouse wareHouse = new ClientWarehouse();
+		wareHouse.setName(Accounter.constants().addNewWareHouse());
+		return wareHouse;
+	}
+
+	@Override
+	public void initColumns() {
+		TextColumn<ClientWarehouse> textColumn = new TextColumn<ClientWarehouse>() {
+
+			@Override
+			public String getValue(ClientWarehouse object) {
+				return object.getDisplayName();
+			}
+		};
+		this.addColumn(textColumn);
+	}
+
+	@Override
+	protected boolean filter(ClientWarehouse t, String string) {
+		return t.getDisplayName().toLowerCase().startsWith(string);
+	}
+
+	@Override
+	protected String getDisplayValue(ClientWarehouse value) {
+		return value.getDisplayName();
+	}
+
+	@Override
+	protected void addNewItem(String text) {
+		Action<ClientWarehouse> action = ActionFactory.getWareHouseViewAction();
+		action.setCallback(new ActionCallback<ClientWarehouse>() {
+
+			@Override
+			public void actionResult(ClientWarehouse result) {
+				selectRow(result);
+			}
+		});
+		action.run(null, true);
+	}
+
+	@Override
+	protected void addNewItem() {
+		addNewItem("");
+	}
+
+}
