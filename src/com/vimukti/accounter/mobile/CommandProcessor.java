6e479135bf126f7f4619841588da5b9541af8d93
Umasree V <umasree.v@vimukti.com>
2011-10-29 11:19:36 +0000

    Former-commit-id: b63d85aef614d1b44a50224320beae03933fa367

--- src/com/vimukti/accounter/mobile/CommandProcessor.java
+++ src/com/vimukti/accounter/mobile/CommandProcessor.java
@@ -124,7 +124,7 @@ public class CommandProcessor {
 		if (!isSelected) {
 			context.setString(message.getOriginalMsg());
 		} else {
-			context.setString("");
+			context.setString(null);
 		}
 		Result result = null;
 		try {
@@ -197,7 +197,7 @@ public class CommandProcessor {
 		Context context = new Context(mSession);
 		context.setNetworkId(message.getNetworkId());
 		context.setNetworkType(message.getNetworkType());
-		context.setCommandString(message.getCommandString());
+		// context.setCommandString(message.getCommandString());
 		return context;
 	}
 }
