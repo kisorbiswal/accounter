b020030f4dfa20c1a5c7f35601216d3dea244e15
Umasree V <umasree.v@vimukti.com>
2011-10-31 13:34:58 +0000

    Former-commit-id: c48f3fd174f11aa3e1f4aacb82695340a689e44f

---
+++ src/com/vimukti/accounter/mobile/requirements/IssuePaymentTableRequirement.java
@@ -0,0 +1,107 @@
+package com.vimukti.accounter.mobile.requirements;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.web.client.core.ClientTransactionIssuePayment;
+
+public abstract class IssuePaymentTableRequirement extends
+		AbstractTableRequirement<ClientTransactionIssuePayment> {
+
+	private static final String DATE = "date";
+	private static final String NUMBER = "number";
+	private static final String NAME = "name";
+	private static final String MEMO = "memo";
+	private static final String AMOUNT = "amount";
+
+	public IssuePaymentTableRequirement(String requirementName,
+			String enterString, String recordName, boolean isCreatable,
+			boolean isOptional, boolean isAllowFromContext) {
+		super(requirementName, enterString, recordName, isCreatable,
+				isOptional, isAllowFromContext);
+	}
+
+	@Override
+	protected void addRequirement(List<Requirement> list) {
+		DateRequirement billDueDate = new DateRequirement(DATE, getMessages()
+				.pleaseEnter(getConstants().date()), getConstants().date(),
+				true, true);
+		billDueDate.setEditable(false);
+		list.add(billDueDate);
+
+		NumberRequirement billNo = new NumberRequirement(NUMBER, "",
+				getConstants().billNo(), true, true);
+		billNo.setEditable(false);
+		list.add(billNo);
+
+		NameRequirement name = new NameRequirement(NAME, "", getConstants()
+				.name(), true, true);
+		name.setEditable(false);
+		list.add(name);
+
+		NameRequirement memo = new NameRequirement(MEMO, "", getConstants()
+				.memo(), true, true);
+		name.setEditable(false);
+		list.add(memo);
+
+		AmountRequirement amount = new AmountRequirement(AMOUNT, getMessages()
+				.pleaseEnter(getConstants().amount()), getConstants().amount(),
+				true, true);
+		amount.setEditable(false);
+		list.add(amount);
+	}
+
+	@Override
+	protected String getEmptyString() {
+		return getMessages().youDontHaveAny(getConstants().paymentsToIssue());
+	}
+
+	@Override
+	protected void getRequirementsValues(ClientTransactionIssuePayment obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setRequirementsDefaultValues(
+			ClientTransactionIssuePayment obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected ClientTransactionIssuePayment getNewObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Record createFullRecord(ClientTransactionIssuePayment t) {
+		Record record = new Record(t);
+		record.add("", getConstants().date());
+		record.add("", t.getDate());
+		record.add("", getConstants().number());
+		record.add("", t.getNumber());
+		record.add("", getConstants().name());
+		record.add("", t.getName());
+		record.add("", getConstants().memo());
+		record.add("", t.getMemo());
+		record.add("", getConstants().amount());
+		record.add("", t.getAmount());
+		record.add("", getConstants().paymentMethod());
+		record.add("", t.getPaymentMethod());
+		return record;
+	}
+
+	@Override
+	protected Record createRecord(ClientTransactionIssuePayment t) {
+		return createFullRecord(t);
+	}
+
+	@Override
+	protected String getAddMoreString() {
+		return getMessages().addMore(getConstants().payments());
+	}
+
+}
