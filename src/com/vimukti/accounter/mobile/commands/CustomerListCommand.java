cb1fe2cdc4f234eba1d706ff48914daac65c25f6
Saiprasad N <saiprasad.n@vimukti.com>
2011-11-01 11:01:30 +0000

    Former-commit-id: 08ae22bf727a132625a7adb54304a6a13b78b96e

--- src/com/vimukti/accounter/mobile/commands/CustomerListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/CustomerListCommand.java
@@ -3,13 +3,12 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.ActionRequirement;
+import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
 import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.ClientCustomer;
 
@@ -18,9 +17,37 @@ import com.vimukti.accounter.web.client.core.ClientCustomer;
  * @author Lingarao.R
  * 
  */
-public class CustomerListCommand extends AbstractTransactionCommand {
+public class CustomerListCommand extends NewAbstractCommand {
 
-	private static final String CUSTOMER_TYPE = "customerType";
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getWelcomeMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+		get(VIEW_BY).setDefaultValue(getConstants().active());
+
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public String getId() {
@@ -30,90 +57,89 @@ public class CustomerListCommand extends AbstractTransactionCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
+		list.add(new ActionRequirement(VIEW_BY, null) {
+
+			@Override
+			protected List<String> getList() {
+				List<String> list = new ArrayList<String>();
+				list.add(getConstants().active());
+				list.add(getConstants().inActive());
+				return list;
+			}
+		});
+
+		list.add(new ShowListRequirement<ClientCustomer>("Customers", "", 10) {
+
+			@Override
+			protected String onSelection(ClientCustomer value) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			protected String getShowMessage() {
+
+				return getMessages().customerList(Global.get().Customer());
+			}
+
+			@Override
+			protected String getEmptyString() {
+
+				return "No customers are available";
+
+			}
+
+			@Override
+			protected Record createRecord(ClientCustomer value) {
+				Record customerRec = new Record(value);
+				customerRec.add(" ", value.getName());
+				customerRec.add("", value.getBalance());
+				return customerRec;
+
+			}
+
+			@Override
+			protected void setCreateCommand(CommandList list) {
+				list.add("Create Customer");
+
+			}
+
+			@Override
+			protected boolean filter(ClientCustomer e, String name) {
+				return e.getName().startsWith(name)
+						|| e.getNumber().startsWith(
+								"" + getNumberFromString(name));
+			}
+
+			@Override
+			protected List<ClientCustomer> getLists(Context context) {
+
+				return getCustomers(context);
+			}
+
+		});
 
 	}
 
-	@Override
-	public Result run(Context context) {
+	private List<ClientCustomer> getCustomers(Context context) {
+		String isActive = get(VIEW_BY).getValue();
+		ArrayList<ClientCustomer> customers = context.getClientCompany()
+				.getCustomers();
+		List<ClientCustomer> result = new ArrayList<ClientCustomer>();
+		for (ClientCustomer customer : customers) {
+			if (isActive.equals(getConstants().active())) {
+				if (customer.isActive()) {
+					result.add(customer);
+				}
 
-		Result result = createCustomerListReq(context);
-		if (result != null) {
-			return result;
-		}
-		return result;
-	}
+			} else {
+				if (!customer.isActive()) {
+					result.add(customer);
+				}
 
-	private Result createCustomerListReq(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		ActionNames selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			switch (selection) {
-			case FINISH:
-				markDone();
-				return new Result();
-			case ACTIVE:
-				context.setAttribute(CUSTOMER_TYPE, true);
-				break;
-			case IN_ACTIVE:
-				context.setAttribute(CUSTOMER_TYPE, false);
-				break;
-			case ALL:
-				context.setAttribute(CUSTOMER_TYPE, null);
-				break;
-			default:
-				break;
 			}
 		}
-		Result result = customersList(context, selection);
+
 		return result;
-	}
-
-	private Result customersList(Context context, ActionNames selection) {
-		Result result = context.makeResult();
-		ResultList customerslist = new ResultList("customersList");
-		result.add(Global.get().Customer() + "List");
-
-		Boolean accountType = (Boolean) context.getAttribute(CUSTOMER_TYPE);
-		List<ClientCustomer> customers = getCustomers(accountType);
-
-		ResultList actions = new ResultList("actions");
-
-		List<ClientCustomer> pagination = pagination(context, selection,
-				actions, customers, new ArrayList<ClientCustomer>(),
-				VALUES_TO_SHOW);
-
-		for (ClientCustomer customer : pagination) {
-			customerslist.add(createCustomerRecord(customer));
-		}
-
-		StringBuilder message = new StringBuilder();
-		if (customerslist.size() > 0) {
-			message.append(getMessages().pleaseSelect(Global.get().Account()));
-		}
-
-		result.add(message.toString());
-		result.add(customerslist);
-
-		Record inActiveRec = new Record(ActionNames.ACTIVE);
-		inActiveRec.add("", getConstants().active());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.IN_ACTIVE);
-		inActiveRec.add("", getConstants().inActive());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.ALL);
-		inActiveRec.add("", getConstants().all());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.FINISH);
-		inActiveRec.add("", getConstants().close());
-		actions.add(inActiveRec);
-
-		result.add(actions);
-
-		CommandList commandList = new CommandList();
-		commandList.add(getMessages().addNew(Global.get().Customer()));
-		result.add(commandList);
-		return result;
-
 	}
 }
