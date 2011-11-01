d3775fa7aeacce28b9cc7348a8dacd25e5a77108
Umasree V <umasree.v@vimukti.com>
2011-11-01 09:11:16 +0000

    Former-commit-id: a0cb0843367ed12807fe1f10b6513d0098b8555e

--- src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
+++ src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
@@ -7,23 +7,26 @@ import com.vimukti.accounter.core.ClientConvertUtil;
 import com.vimukti.accounter.core.FinanceDate;
 import com.vimukti.accounter.core.ServerConvertUtil;
 import com.vimukti.accounter.core.TAXAgency;
-import com.vimukti.accounter.mobile.ActionNames;
+import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.DateRequirement;
+import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
+import com.vimukti.accounter.mobile.requirements.TaxAgencyRequirement;
 import com.vimukti.accounter.web.client.core.ClientBox;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.ClientTAXAgency;
 import com.vimukti.accounter.web.client.core.ClientVATReturn;
 import com.vimukti.accounter.web.server.FinanceTool;
 
-public class FileVATCommand extends AbstractVATCommand {
+public class FileVATCommand extends NewAbstractTransactionCommand {
 
 	private static final String FROM_DATE = "fromDate";
 	private static final String TO_DATE = "toDate";
 	private static final String BOXES = "boxes";
+	private static final String TAX_AGENCY = "taxAgency";
 
 	@Override
 	public String getId() {
@@ -33,44 +36,108 @@ public class FileVATCommand extends AbstractVATCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement(TAX_AGENCY, false, true));
-		list.add(new Requirement(FROM_DATE, true, true));
-		list.add(new Requirement(TO_DATE, true, true));
+		list.add(new TaxAgencyRequirement(TAX_AGENCY, getMessages()
+				.pleaseEnterName(getConstants().taxAgency()), getConstants()
+				.taxAgency(), false, true, null) {
+
+			@Override
+			protected String getSetMessage() {
+				return getMessages().hasSelected(getConstants().taxAgency());
+			}
+
+			@Override
+			protected List<ClientTAXAgency> getLists(Context context) {
+				return context.getClientCompany().gettaxAgencies();
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return getMessages().youDontHaveAny(getConstants().taxAgency());
+			}
+
+			@Override
+			protected boolean filter(ClientTAXAgency e, String name) {
+				return e.getName().toLowerCase().startsWith(name);
+			}
+		});
+
+		list.add(new DateRequirement(FROM_DATE, getMessages().pleaseEnter(
+				getConstants().fromDate()), getConstants().fromDate(), true,
+				true));
+
+		list.add(new DateRequirement(TO_DATE, getMessages().pleaseEnter(
+				getConstants().toDate()), getConstants().toDate(), true, true));
+
 		list.add(new Requirement(BOXES, false, true));
+		list.add(new ShowListRequirement<ClientBox>(BOXES, "", 15) {
+
+			@Override
+			protected String onSelection(ClientBox value) {
+				return null;
+			}
+
+			@Override
+			protected String getShowMessage() {
+				return null;
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return getConstants()
+						.norecordstoshowinbetweentheselecteddates();
+			}
+
+			@Override
+			protected Record createRecord(ClientBox value) {
+				Record record = new Record(value);
+				record.add("", getConstants().vatLine());
+				record.add("", value.getName());
+				record.add("", getConstants().amount());
+				record.add("", value.getAmount());
+				return record;
+			}
+
+			@Override
+			protected void setCreateCommand(CommandList list) {
+
+			}
+
+			@Override
+			protected boolean filter(ClientBox e, String name) {
+				return false;
+			}
+
+			@Override
+			protected List<ClientBox> getLists(Context context) {
+				return getBoxes(context);
+			}
+		});
+	}
+
+	private List<ClientBox> getBoxes(Context context) {
+		ClientTAXAgency taxAgency = get(TAX_AGENCY).getValue();
+		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
+		ClientFinanceDate toDate = get(TO_DATE).getValue();
+		TAXAgency serverVatAgency;
+		try {
+			serverVatAgency = new ServerConvertUtil().toServerObject(
+					new TAXAgency(), taxAgency, context.getHibernateSession());
+			ClientVATReturn vatReturn = new ClientConvertUtil().toClientObject(
+					new FinanceTool().getTaxManager().getVATReturnDetails(
+							serverVatAgency, new FinanceDate(fromDate),
+							new FinanceDate(toDate),
+							context.getClientCompany().getID()),
+					ClientVATReturn.class);
+			return vatReturn.getBoxes();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ArrayList<ClientBox>();
+		}
+
 	}
 
 	@Override
-	public Result run(Context context) {
-		Object attribute = context.getAttribute(INPUT_ATTR);
-		if (attribute == null) {
-			context.setAttribute(INPUT_ATTR, "optional");
-		}
-		Result result = context.makeResult();
-
-		setDefaultValues();
-
-		// Preparing Result
-		Result makeResult = context.makeResult();
-		makeResult.add(getMessages().readyToCreate(getConstants().fileVAT()));
-		ResultList list = new ResultList("values");
-		makeResult.add(list);
-		ResultList actions = new ResultList(ACTIONS);
-		makeResult.add(actions);
-
-		result = taxAgencyRequirement(context, list, TAX_AGENCY);
-		if (result != null) {
-			return result;
-		}
-
-		result = createOptionalRequirement(context, list, actions, makeResult);
-		if (result != null) {
-			return result;
-		}
-
-		return createFileVat(context);
-	}
-
-	private Result createFileVat(Context context) {
+	protected Result onCompleteProcess(Context context) {
 		ClientVATReturn clientVATReturn = new ClientVATReturn();
 		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
 		clientVATReturn.setPeriodStartDate(fromDate.getDate());
@@ -86,98 +153,36 @@ public class FileVATCommand extends AbstractVATCommand {
 
 		create(clientVATReturn, context);
 
-		markDone();
-		Result result = new Result();
-		result.add(getMessages().createSuccessfully(
-				getConstants().fileVATReturn()));
-
-		return result;
+		return null;
 	}
 
-	private void setDefaultValues() {
-		ClientFinanceDate date = getClientCompany()
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getWelcomeMessage() {
+		return getMessages().creating(getConstants().fileVAT());
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		return getMessages().readyToCreate(getConstants().fileVAT());
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+		ClientFinanceDate date = context.getClientCompany()
 				.getCurrentFiscalYearStartDate();
 		get(FROM_DATE).setDefaultValue(date);
 		get(TO_DATE).setDefaultValue(new ClientFinanceDate());
 	}
 
-	private Result createOptionalRequirement(Context context, ResultList list,
-			ResultList actions, Result makeResult) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		Object selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			ActionNames actionName = (ActionNames) selection;
-			switch (actionName) {
-			case FINISH:
-				context.removeAttribute(INPUT_ATTR);
-				return null;
-			default:
-				break;
-			}
-		}
-
-		selection = context.getSelection("values");
-
-		Result result = dateOptionalRequirement(context, list, FROM_DATE,
-				getConstants().fromDate(),
-				getMessages().pleaseEnter(getConstants().fromDate()), selection);
-		if (result != null) {
-			return result;
-		}
-
-		result = dateOptionalRequirement(context, list, TO_DATE, getConstants()
-				.endDate(),
-				getMessages().pleaseEnter(getConstants().endDate()), selection);
-		if (result != null) {
-			return result;
-		}
-
-		ClientTAXAgency taxAgency = get(TAX_AGENCY).getValue();
-		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
-		ClientFinanceDate toDate = get(TO_DATE).getValue();
-
-		makeResult.add(getConstants().vatLine());
-		List<ClientBox> boxes2 = new ArrayList<ClientBox>();
-		try {
-			boxes2 = getBoxes(taxAgency, fromDate, toDate, context);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		for (ClientBox box : boxes2) {
-			makeResult.add(createBoxRecord(box));
-		}
-
-		Requirement boxesReq = get(BOXES);
-		boxesReq.setValue(boxes2);
-
-		Record finish = new Record(ActionNames.FINISH);
-		finish.add("", getMessages().finishToCreate(getConstants().fileVAT()));
-		actions.add(finish);
-
-		return makeResult;
-	}
-
-	private String createBoxRecord(ClientBox box) {
-		StringBuffer record = new StringBuffer();
-		record.append(box.getName() + "  ");
-		record.append(box.getAmount());
-		return record.toString();
-	}
-
-	private List<ClientBox> getBoxes(ClientTAXAgency taxAgency,
-			ClientFinanceDate fromDate, ClientFinanceDate toDate,
-			Context context) throws Exception {
-		TAXAgency serverVatAgency = new ServerConvertUtil().toServerObject(
-				new TAXAgency(), taxAgency, context.getHibernateSession());
-
-		ClientVATReturn vatReturn = new ClientConvertUtil().toClientObject(
-				new FinanceTool().getTaxManager().getVATReturnDetails(
-						serverVatAgency, new FinanceDate(fromDate),
-						new FinanceDate(toDate), getClientCompany().getID()),
-				ClientVATReturn.class);
-		return vatReturn.getBoxes();
+	@Override
+	public String getSuccessMessage() {
+		return getMessages().createSuccessfully(getConstants().fileVAT());
 	}
 
 }
