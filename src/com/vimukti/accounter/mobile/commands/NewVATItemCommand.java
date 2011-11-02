9c390cf4f4e92c6e7d7ce1f9833eedc73469ee95
Devaraju K <devaraju.k@vimukti.com>
2011-11-02 04:02:51 +0000

    Former-commit-id: 9967306ca3311cf4f519172d18eb4e0364304827

--- src/com/vimukti/accounter/mobile/commands/NewVATItemCommand.java
+++ src/com/vimukti/accounter/mobile/commands/NewVATItemCommand.java
@@ -3,245 +3,139 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
-
-import com.vimukti.accounter.mobile.ActionNames;
-import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
-import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.RequirementType;
 import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.AmountRequirement;
+import com.vimukti.accounter.mobile.requirements.BooleanRequirement;
+import com.vimukti.accounter.mobile.requirements.NameRequirement;
+import com.vimukti.accounter.mobile.requirements.TaxAgencyRequirement;
+import com.vimukti.accounter.mobile.requirements.VatReturnBoxRequirement;
+import com.vimukti.accounter.web.client.core.ClientCompany;
 import com.vimukti.accounter.web.client.core.ClientTAXAgency;
 import com.vimukti.accounter.web.client.core.ClientTAXItem;
 import com.vimukti.accounter.web.client.core.ClientVATReturnBox;
 
-public class NewVATItemCommand extends AbstractVATCommand {
+public class NewVATItemCommand extends NewAbstractCommand {
 
 	private static final String DESCRIPTION = "description";
-	private static final String IS_PERCENTAGE = "isPercentage";
 	private static final String IS_ACTIVE = "isActive";
 	private static final String VAT_RETURN_BOX = "vatReturnBox";
-	private static final String VAT_RETURN_BOXES = "vatReturnBoxes";
+	private static final String TAX_ITEM_NAME = "Tax Item Name";
+	private static final String TAX_RATE = "Tax Rate";
+	private static final String TAX_AGENCY = "Tax Agency";
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement(NAME, false, true));
-		list.add(new Requirement(DESCRIPTION, true, true));
-		list.add(new Requirement(AMOUNT, false, true));
-		list.add(new Requirement(IS_ACTIVE, true, true));
-		list.add(new Requirement(TAX_AGENCY, false, true));
-		// if (isUkCompany()) {
-		list.add(new Requirement(IS_PERCENTAGE, true, true));
-		list.add(new Requirement(VAT_RETURN_BOX, false, true));
-		// }
-	}
+		list.add(new NameRequirement(TAX_ITEM_NAME, "Enter Tax/Vat Item Name",
+				"Tax/Vat Item Name", false, true));
 
-	@Override
-	public Result run(Context context) {
-		Object attribute = context.getAttribute(INPUT_ATTR);
-		if (attribute == null) {
-			context.setAttribute(INPUT_ATTR, "optional");
-		}
-		Result result = context.makeResult();
+		list.add(new NameRequirement(DESCRIPTION, "Enter Description",
+				"Description", true, true));
 
-		Result makeResult = context.makeResult();
-		makeResult.add(getMessages().readyToCreate(getConstants().vatItem()));
-		ResultList list = new ResultList("values");
-		makeResult.add(list);
-		ResultList actions = new ResultList(ACTIONS);
-		makeResult.add(actions);
+		list.add(new AmountRequirement(TAX_RATE, "Enter Rate", "Tax Rate",
+				false, true));
 
-		setOptionalFields();
+		list.add(new TaxAgencyRequirement(TAX_AGENCY, "Create New Tax Agency",
+				"Tax Agency:", false, true, null) {
 
-		result = nameRequirement(context, list, NAME, getConstants()
-				.vatItemName(),
-				getMessages().pleaseEnter(getConstants().vatItemName()));
-		if (result != null) {
-			return result;
-		}
-
-		result = amountRequirement(context, list, AMOUNT, getMessages()
-				.pleaseEnter(getConstants().taxRate()), getConstants()
-				.taxRate());
-		if (result != null) {
-			return result;
-		}
-
-		result = taxAgencyRequirement(context, list, TAX_AGENCY);
-		if (result != null) {
-			return result;
-		}
-
-		if (getClientCompany().getPreferences().isTrackTax()) {
-			result = vatReturnBoxRequirement(context, list, VAT_RETURN,
-					(ClientTAXAgency) get(TAX_AGENCY).getValue());
-			if (result != null) {
-				return result;
+			@Override
+			protected String getSetMessage() {
+				return "Tax/Vat Agency has been selected";
 			}
-		}
 
-		result = createOptionalResult(context, list, actions, makeResult);
-		if (result != null) {
-			return result;
-		}
+			@Override
+			protected List<ClientTAXAgency> getLists(Context context) {
+				return context.getClientCompany().gettaxAgencies();
+			}
 
-		return createVATItem(context);
-	}
-
-	private Result vatReturnBoxRequirement(Context context, ResultList list,
-			String name, ClientTAXAgency taxAgency) {
-
-		Requirement vatReturnReq = get(VAT_RETURN_BOX);
-		ClientVATReturnBox vatReturn = context.getSelection(VAT_RETURN_BOXES);
-
-		if (vatReturn != null) {
-			vatReturnReq.setValue(vatReturn);
-		}
-
-		ClientVATReturnBox value = vatReturnReq.getValue();
-		Object selection = context.getSelection("values");
-		if (!vatReturnReq.isDone() || (VAT_RETURN_BOX == selection)) {
-			return getVatReturnBoxResult(context, taxAgency);
-		}
-
-		Record record = new Record(VAT_RETURN_BOX);
-		record.add("", getConstants().vatReturnBox());
-		record.add("", value.getName());
-		list.add(record);
-
-		return null;
-
-	}
-
-	private Result getVatReturnBoxResult(Context context,
-			ClientTAXAgency taxAgency) {
-		Result result = context.makeResult();
-		ResultList vatReturnsList = new ResultList(VAT_RETURN_BOXES);
-
-		Object last = context.getLast(RequirementType.VAT_RETURN_BOX);
-		List<ClientVATReturnBox> skipVatReturns = new ArrayList<ClientVATReturnBox>();
-		if (last != null) {
-			vatReturnsList
-					.add(createVatReturnBoxRecord((ClientVATReturnBox) last));
-			skipVatReturns.add((ClientVATReturnBox) last);
-		}
-
-		List<ClientVATReturnBox> vatReturns = getVatReturnBoxes(
-				context.getHibernateSession(), taxAgency);
-
-		ResultList actions = new ResultList("actions");
-		ActionNames selection = context.getSelection("actions");
-
-		List<ClientVATReturnBox> pagination = pagination(context, selection,
-				actions, vatReturns, skipVatReturns, VALUES_TO_SHOW);
-
-		for (ClientVATReturnBox taxagencies : pagination) {
-			vatReturnsList.add(createVatReturnBoxRecord(taxagencies));
-		}
-
-		int size = vatReturnsList.size();
-		StringBuilder message = new StringBuilder();
-		if (size > 0) {
-			message.append(getMessages().pleaseSelect(
-					getConstants().vatReturnBox()));
-		}
-
-		CommandList commandList = new CommandList();
-		commandList.add(getMessages().create(getConstants().taxAgency()));
-
-		result.add(message.toString());
-		result.add(commandList);
-		result.add(vatReturnsList);
-
-		return result;
-	}
-
-	private List<ClientVATReturnBox> getVatReturnBoxes(
-			Session hibernateSession, ClientTAXAgency vatAgency) {
-		List<ClientVATReturnBox> vatBoxes = getClientCompany()
-				.getVatReturnBoxes();
-		List<ClientVATReturnBox> vatBoxes2 = new ArrayList<ClientVATReturnBox>();
-		for (ClientVATReturnBox vatBox : vatBoxes) {
-			if (vatAgency.getVATReturn() == vatBox.getVatReturnType())
-				vatBoxes2.add(vatBox);
-		}
-		return vatBoxes2;
-	}
-
-	private Record createVatReturnBoxRecord(ClientVATReturnBox vatReturn) {
-		Record record = new Record(vatReturn);
-		record.add("Name", vatReturn.getName());
-		return record;
-	}
-
-	private void setOptionalFields() {
-		Requirement descReq = get(DESCRIPTION);
-		if (descReq.getDefaultValue() == null) {
-			descReq.setDefaultValue(new String());
-		}
-
-		Requirement isActiveReq = get(IS_ACTIVE);
-		if (isActiveReq.getDefaultValue() == null) {
-			isActiveReq.setDefaultValue(true);
-		}
-
-		Requirement isPercentageReq = get(IS_PERCENTAGE);
-		if (isPercentageReq.getDefaultValue() == null) {
-			isPercentageReq.setDefaultValue(true);
-		}
-	}
-
-	private Result createOptionalResult(Context context, ResultList list,
-			ResultList actions, Result makeResult) {
-		// context.setAttribute(INPUT_ATTR, "optional");
-
-		Object selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			ActionNames actionName = (ActionNames) selection;
-			switch (actionName) {
-			case FINISH:
-				context.removeAttribute(INPUT_ATTR);
+			@Override
+			protected String getEmptyString() {
 				return null;
-			default:
-				break;
 			}
+
+			@Override
+			protected boolean filter(ClientTAXAgency e, String name) {
+				return e.getName().startsWith(name);
+			}
+		});
+
+		list.add(new VatReturnBoxRequirement(VAT_RETURN_BOX, null,
+				"vat return box", false, true, null) {
+			@Override
+			protected String getEmptyString() {
+				return null;
+			}
+
+			@Override
+			protected String getSetMessage() {
+				return "vat return box has been Selected.";
+			}
+
+			@Override
+			protected List<ClientVATReturnBox> getLists(Context context) {
+				return context.getClientCompany().getVatReturnBoxes();
+			}
+
+			@Override
+			protected String getSelectString() {
+				return "Slect a Vat return box";
+			}
+		});
+
+		list.add(new BooleanRequirement(IS_ACTIVE, true) {
+
+			@Override
+			protected String getTrueString() {
+				return "item is active";
+			}
+
+			@Override
+			protected String getFalseString() {
+				return "item is in active";
+			}
+		});
+	}
+
+	protected List<String> getVatReturnBoxes(ClientCompany clientCompany) {
+		List<ClientVATReturnBox> vatReturnBoxList = clientCompany
+				.getVatReturnBoxes();
+		List<String> vatReturnBoxListNames = new ArrayList<String>();
+		if (!vatReturnBoxListNames.isEmpty()) {
+			for (ClientVATReturnBox vatReturnBox : vatReturnBoxList) {
+				vatReturnBoxListNames.add(vatReturnBox.getName());
+			}
+			return vatReturnBoxListNames;
 		}
-		selection = context.getSelection("values");
+		return null;
+	}
 
-		Result result = stringOptionalRequirement(context, list, selection,
-				DESCRIPTION, getConstants().description(), getMessages()
-						.pleaseEnter(getConstants().description()));
-		if (result != null) {
-			return result;
+	protected List<String> getTaxagencyList(ClientCompany clientCompany) {
+		List<ClientTAXAgency> taxAgencyList = clientCompany
+				.getActiveTaxAgencies();
+		List<String> taxAgencyListNames = new ArrayList<String>();
+		if (!taxAgencyListNames.isEmpty()) {
+			for (ClientTAXAgency taxAgency : taxAgencyList) {
+				taxAgencyListNames.add(taxAgency.getName());
+			}
+			return taxAgencyListNames;
 		}
-
-		booleanOptionalRequirement(context, selection, list, IS_ACTIVE,
-				getConstants().itemIsActive(), getConstants().itemIsInactive());
-
-		Record finish = new Record(ActionNames.FINISH);
-		finish.add("", getMessages().finishToCreate(getConstants().taxItem()));
-		actions.add(finish);
-
-		return makeResult;
+		return null;
 	}
 
 	private Result createVATItem(Context context) {
 		ClientTAXItem taxItem = new ClientTAXItem();
-		String name = (String) get(NAME).getValue();
+		String name = (String) get(TAX_ITEM_NAME).getValue();
 		String description = (String) get(DESCRIPTION).getValue();
-		double taxRate = Double.parseDouble((String) get(AMOUNT).getValue());
+		double taxRate = Double.parseDouble((String) get(TAX_RATE).getValue());
 		boolean isActive = (Boolean) get(IS_ACTIVE).getValue();
 		ClientTAXAgency taxAgency = (ClientTAXAgency) get(TAX_AGENCY)
 				.getValue();
-
 		taxItem.setPercentage(true);
 		if (getClientCompany().getPreferences().isTrackTax()) {
 			ClientVATReturnBox vatReturnBox = (ClientVATReturnBox) get(
@@ -253,15 +147,35 @@ public class NewVATItemCommand extends AbstractVATCommand {
 		taxItem.setTaxRate(taxRate);
 		taxItem.setActive(isActive);
 		taxItem.setTaxAgency(taxAgency.getID());
-
 		create(taxItem, context);
-
 		markDone();
+		return null;
+	}
 
-		Result result = new Result();
-		result.add(getMessages().createSuccessfully(getConstants().taxItem()));
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-		return result;
+	@Override
+	protected String getWelcomeMessage() {
+		return "New Vat Item commond is activated";
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		return "New vat item is ready to create with the following values";
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+		get(IS_ACTIVE).setDefaultValue(Boolean.TRUE);
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		return "New vat Item created Successfully";
 	}
 
 }
