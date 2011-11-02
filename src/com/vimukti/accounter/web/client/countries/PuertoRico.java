7049b809389af1ccead9794789798d0855fe5d77
Lingarao R <lingarao.r@vimukti.com>
2011-11-02 12:13:10 +0000

    Former-commit-id: a538c4607749e21b372819f1a71a86c4cd5da017

---
+++ src/com/vimukti/accounter/web/client/countries/PuertoRico.java
@@ -0,0 +1,18 @@
+package com.vimukti.accounter.web.client.countries;
+
+import com.vimukti.accounter.web.client.util.AbstractCountryPreferences;
+
+public class PuertoRico extends AbstractCountryPreferences {
+
+	@Override
+	public String[] getStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPreferredCurrency() {
+		return "USD";
+	}
+
+}
