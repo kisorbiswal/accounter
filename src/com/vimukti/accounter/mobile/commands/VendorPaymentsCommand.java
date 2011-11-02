bd86de0973da18f14f8e2e0a16e6eb2644b68e80
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-11-02 07:05:07 +0000

    Former-commit-id: 5130861da4cd0c25552823054126cda0d072731b

--- src/com/vimukti/accounter/mobile/commands/VendorPaymentsCommand.java
+++ src/com/vimukti/accounter/mobile/commands/VendorPaymentsCommand.java
@@ -3,24 +3,46 @@ package com.vimukti.accounter.mobile.commands;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
-import com.vimukti.accounter.services.DAOException;
+import com.vimukti.accounter.mobile.requirements.ActionRequirement;
+import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
 import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.core.ClientTransaction;
 import com.vimukti.accounter.web.client.core.Utility;
 import com.vimukti.accounter.web.client.core.Lists.PaymentsList;
-import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.server.FinanceTool;
 
-public class VendorPaymentsCommand extends AbstractTransactionCommand {
+public class VendorPaymentsCommand extends NewAbstractCommand {
 
-	private static final String VIEW_BY = "ViewBy";
+
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getWelcomeMessage() {
+		return getMessages().vendorPaymentList(Global.get().Vendor());
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		return getMessages().vendorPaymentList(Global.get().Vendor());
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+		get(VIEW_BY).setDefaultValue(getConstants().all());
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		return "Success";
+	}
 
 	@Override
 	public String getId() {
@@ -31,176 +53,109 @@ public class VendorPaymentsCommand extends AbstractTransactionCommand {
 	@Override
 	protected void addRequirements(List<Requirement> list) {
 
-	}
+		list.add(new ActionRequirement(VIEW_BY, null) {
 
-	@Override
-	public Result run(Context context) {
-		Result result = null;
-
-		result = getVendorPaymentsList(context);
-		if (result != null) {
-			return result;
-		}
-		return result;
-	}
-
-	private Result getVendorPaymentsList(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		ActionNames selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			switch (selection) {
-			case FINISH:
-				markDone();
-				return new Result(getConstants().closed());
-			case ISSUED:
-				context.setAttribute(VIEW_BY, 1);
-				break;
-			case NOT_ISSUED:
-				context.setAttribute(VIEW_BY, 2);
-				break;
-			case VOIDED:
-				context.setAttribute(VIEW_BY, 3);
-				break;
-			case ALL:
-				context.setAttribute(VIEW_BY, null);
-				break;
-			default:
-				break;
+			@Override
+			protected List<String> getList() {
+				List<String> list = new ArrayList<String>();
+				list.add(getConstants().notIssued());
+				list.add(getConstants().issued());
+				list.add(getConstants().voided());
+				list.add(getConstants().all());
+				return list;
 			}
-		}
-		Result result = vendorPaymentsList(context, selection);
-		return result;
-	}
+		});
 
-	private Result vendorPaymentsList(Context context, ActionNames selection) {
-		Result result = context.makeResult();
-		ResultList vendorPaymentsList = new ResultList("vendorPaymentsList");
-		result.add(getMessages().vendorPaymentsList(Global.get().Vendor()));
+		list.add(new ShowListRequirement<PaymentsList>(getMessages()
+				.vendorPaymentList(Global.get().Vendor()), "", 10) {
 
-		Integer vendorPaymentType = (Integer) context.getAttribute(VIEW_BY);
-		List<PaymentsList> vendorPayments = getVendorPayments(context
-				.getCompany().getID(), vendorPaymentType);
-
-		ResultList actions = new ResultList("actions");
-
-		List<PaymentsList> pagination = pagination(context, selection, actions,
-				vendorPayments, new ArrayList<PaymentsList>(), VALUES_TO_SHOW);
-		for (PaymentsList payment : pagination) {
-			vendorPaymentsList.add(createPaymentRecord(payment));
-		}
-
-		StringBuilder message = new StringBuilder();
-		if (vendorPaymentsList.size() > 0) {
-			message.append(getMessages().selectedit(
-					getMessages().vendorPayment(Global.get().Vendor())));
-		} else if (vendorPaymentsList.size() == 0) {
-			if (vendorPaymentType == 1) {
-				message.append(getMessages().youDontHaveAny(
-						getConstants().issuePayments()));
-			} else if (vendorPaymentType == 2) {
-				message.append(getMessages().youDontHaveAny(
-						getConstants().notIssuedPayments()));
-			} else if (vendorPaymentType == 3) {
-				message.append(getMessages().youDontHaveAny(
-						getConstants().voidedPayments()));
-			} else {
-				message.append(getMessages().youDontHaveAny(
-						getConstants().payments()));
+			@Override
+			protected String onSelection(PaymentsList value) {
+				return null;
 			}
-		}
 
-		result.add(message.toString());
-		result.add(vendorPaymentsList);
+			@Override
+			protected String getShowMessage() {
+				return getMessages().vendorPaymentList(Global.get().Vendor());
+			}
 
-		Record inActiveRec = new Record(ActionNames.ISSUED);
-		inActiveRec.add("", getConstants().issuePayments());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.NOT_ISSUED);
-		inActiveRec.add("", getConstants().notIssuedPayments());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.VOIDED);
-		inActiveRec.add("", getConstants().voidedPayments());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.ALL);
-		inActiveRec.add("", getConstants().payments());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.FINISH);
-		inActiveRec.add("", getConstants().close());
-		actions.add(inActiveRec);
+			@Override
+			protected String getEmptyString() {
+				return "No Payments are available";
+			}
 
-		result.add(actions);
-		CommandList commandList = new CommandList();
-		commandList.add("Add Vendor payment");
-		result.add(commandList);
-		return result;
+			@Override
+			protected Record createRecord(PaymentsList p) {
+				Record payment = new Record(p);
+				payment.add("", p.getPaymentDate());
+				payment.add("", p.getPaymentNumber());
+				payment.add("", p.getStatus());
+				payment.add("", p.getIssuedDate());
+				payment.add("", p.getName());
+				payment.add("", p.getPaymentMethodName());
+				payment.add("", p.getAmountPaid());
+				return payment;
+			}
 
+			@Override
+			protected void setCreateCommand(CommandList list) {
+				list.add("Add Vendor payment");
+			}
+
+			@Override
+			protected boolean filter(PaymentsList e, String name) {
+				return e.getName().startsWith(name)
+						|| e.getPaymentNumber().startsWith(
+								"" + getNumberFromString(name));
+			}
+
+			@Override
+			protected List<PaymentsList> getLists(Context context) {
+				return getListData(context);
+			}
+		});
 	}
 
-	private Record createPaymentRecord(PaymentsList p) {
-		Record payment = new Record(p);
-		payment.add("PaymentDate", p.getPaymentDate());
-		payment.add("PaymentNumber", p.getPaymentNumber());
-		payment.add("Status", p.getStatus());
-		payment.add("IssueDate", p.getIssuedDate());
-		payment.add("Name", p.getName());
-		payment.add("paymentMethod", p.getPaymentMethodName());
-		payment.add("AmountPaid", p.getAmountPaid());
-		payment.add("Voided", p.isVoided());
-		return payment;
-	}
+	protected List<PaymentsList> getListData(Context context) {
 
-	private List<PaymentsList> getVendorPayments(long companyId,
-			Integer vendorPaymentType) {
-		ArrayList<PaymentsList> vendorPaymentsList = null;
+		String currentView = get(VIEW_BY).getValue();
+		FinanceTool tool = new FinanceTool();
+		List<PaymentsList> result = new ArrayList<PaymentsList>();
+		List<PaymentsList> paymentsLists = null;
 		try {
-			vendorPaymentsList = new FinanceTool().getVendorManager()
-					.getVendorPaymentsList(companyId);
+			paymentsLists = tool.getVendorManager().getVendorPaymentsList(
+					context.getCompany().getId());
 
-		} catch (DAOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if (vendorPaymentType == null) {
-			return vendorPaymentsList;
+		if (paymentsLists != null) {
+			for (PaymentsList list : paymentsLists) {
+				if (currentView.equals(getConstants().notIssued())) {
+					if (Utility.getStatus(list.getType(), list.getStatus())
+							.equals(getConstants().notIssued())
+							&& !list.isVoided()) {
+						result.add(list);
+					}
+				} else if (currentView.equals(getConstants().issued())) {
+					if (Utility.getStatus(list.getType(), list.getStatus())
+							.equalsIgnoreCase(getConstants().issued())
+							&& !list.isVoided()) {
+						result.add(list);
+					}
+				} else if (currentView.equals(getConstants().voided())) {
+					if (list.isVoided()
+							&& list.getStatus() != ClientTransaction.STATUS_DELETED) {
+						result.add(list);
+					}
+				} else if (currentView.equalsIgnoreCase(getConstants().all())) {
+					result.addAll(paymentsLists);
+				}
+			}
+			return result;
 		}
-		ArrayList<PaymentsList> result = new ArrayList<PaymentsList>();
-		for (PaymentsList paymentsList : vendorPaymentsList) {
-			if (vendorPaymentType == 1) {
-				if (Utility.getStatus(paymentsList.getType(),
-						paymentsList.getStatus()).equalsIgnoreCase("Issued")
-						&& !paymentsList.isVoided()) {
-					result.add(paymentsList);
-				}
-			}
-			if (vendorPaymentType == 2) {
-				if (Utility.getStatus(paymentsList.getType(),
-						paymentsList.getStatus())
-						.equalsIgnoreCase("Not Issued")
-						&& !paymentsList.isVoided()) {
-					result.add(paymentsList);
-				}
-			}
-			if (vendorPaymentType == 3) {
-				if (paymentsList.isVoided()
-						&& paymentsList.getStatus() != ClientTransaction.STATUS_DELETED) {
-					result.add(paymentsList);
-				}
-			}
+		return null;
 
-		}
-		return result;
-
-	}
-
-	@Override
-	protected List<String> getViewTypes() {
-		List<String> list = new ArrayList<String>();
-		list.add(Accounter.constants().issued());
-		list.add(Accounter.constants().notIssued());
-		list.add(Accounter.constants().Voided());
-		list.add(Accounter.constants().all());
-
-		return list;
 	}
 
 }
