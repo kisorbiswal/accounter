79a882d63339baad6c68e5caba8e8dad74661c9f
Udaykumar Ch <udaykumar.ch@vimukti.com>
2011-11-02 05:54:45 +0000

    Former-commit-id: 9f1bd656ccabd128f763b3a9813037878748e16d

--- src/com/vimukti/accounter/mobile/commands/SalesPersonsListCommand.java
+++ src/com/vimukti/accounter/mobile/commands/SalesPersonsListCommand.java
@@ -2,19 +2,48 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import com.vimukti.accounter.mobile.ActionNames;
+import com.vimukti.accounter.core.SalesPerson;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
-import com.vimukti.accounter.web.client.core.ClientAddress;
+import com.vimukti.accounter.mobile.requirements.ActionRequirement;
+import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
 import com.vimukti.accounter.web.client.core.ClientSalesPerson;
 
-public class SalesPersonsListCommand extends AbstractCommand {
-	private static final String SALESPERSON_TYPE = "salespersonType";
+public class SalesPersonsListCommand extends NewAbstractCommand {
+
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getWelcomeMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+		get(VIEW_BY).setDefaultValue(getConstants().active());
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
 	public String getId() {
 		// TODO Auto-generated method stub
@@ -23,118 +52,88 @@ public class SalesPersonsListCommand extends AbstractCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
+
+		list.add(new ActionRequirement(VIEW_BY, null) {
+
+			@Override
+			protected List<String> getList() {
+				List<String> list = new ArrayList<String>();
+				list.add(getConstants().active());
+				list.add(getConstants().inActive());
+				return list;
+			}
+		});
+
+		list.add(new ShowListRequirement<ClientSalesPerson>(getConstants()
+				.salesPersonList(), "", 10) {
+
+			@Override
+			protected String onSelection(ClientSalesPerson value) {
+				return null;
+			}
+
+			@Override
+			protected String getShowMessage() {
+				return getConstants().salesPersonList();
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return "No Sales Persons are available";
+			}
+
+			@Override
+			protected Record createRecord(ClientSalesPerson salesPrson) {
+				Record record = new Record(salesPrson);
+				record.add("", salesPrson.getName());
+				record.add("", salesPrson.getJobTitle());
+				record.add("", salesPrson.getDateOfBirth());
+				record.add("", salesPrson.getEmail());
+				record.add("", salesPrson.getPhoneNo());
+				record.add("", salesPrson.getFaxNo());
+				record.add("", salesPrson.getMemo());
+				return record;
+			}
+
+			@Override
+			protected void setCreateCommand(CommandList list) {
+				list.add("Create Sales Person");
+			}
+
+			@Override
+			protected boolean filter(ClientSalesPerson e, String name) {
+				return e.getName().startsWith(name);
+			}
+
+			@Override
+			protected List<ClientSalesPerson> getLists(Context context) {
+
+				return getSalesPersons(context);
+			}
+		});
+
 	}
 
-	@Override
-	public Result run(Context context) {
-		Result result = null;
+	protected List<ClientSalesPerson> getSalesPersons(Context context) {
+		String isActive = get(VIEW_BY).getValue();
+		List<ClientSalesPerson> list = new ArrayList<ClientSalesPerson>();
+		List<ClientSalesPerson> salesPersons = context.getClientCompany()
+				.getSalesPersons();
+		if (salesPersons != null) {
+			for (ClientSalesPerson person : salesPersons) {
+				if (isActive.equals(getConstants().active())) {
+					if (person.isActive()) {
+						list.add(person);
+					}
+				} else {
+					if (!person.isActive()) {
+						list.add(person);
+					}
 
-		result = createSalesPersonsList(context);
-		return result;
-	}
-
-	private Result createSalesPersonsList(Context context) {
-		context.setAttribute(INPUT_ATTR, "optional");
-
-		ActionNames selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			switch (selection) {
-			case FINISH:
-				markDone();
-				return new Result();
-			case ACTIVE:
-				context.setAttribute(SALESPERSON_TYPE, true);
-				break;
-			case IN_ACTIVE:
-				context.setAttribute(SALESPERSON_TYPE, false);
-				break;
-			case ALL:
-				context.setAttribute(SALESPERSON_TYPE, null);
-				break;
-			default:
-				break;
+				}
 			}
 		}
-		Result result = salesPersonsList(context, selection);
-		return result;
-	}
-
-	private Result salesPersonsList(Context context,  ActionNames selection) {
-		Result result = context.makeResult();
-		ResultList salesPersonsList = new ResultList("salespersonsList");
-		result.add(getConstants().salesPersonList());
-
-		Boolean salesPersonsType = (Boolean) context.getAttribute(SALESPERSON_TYPE);
-		List<ClientSalesPerson> salesPersons = getSalesPersons(salesPersonsType);
-
-		ResultList actions = new ResultList("actions");
-
-		List<ClientSalesPerson> pagination = pagination(context, selection, actions,
-				salesPersons, new ArrayList<ClientSalesPerson>(), VALUES_TO_SHOW);
-
-		for (ClientSalesPerson salesPrson : pagination) {
-			salesPersonsList.add(createSalesPersonRecord(salesPrson));
-		}
-
-		StringBuilder message = new StringBuilder();
-		if (salesPersonsList.size() > 0) {
-			message.append(getMessages().pleaseSelect(getConstants().salesPerson()));
-		}
-
-		result.add(message.toString());
-		result.add(salesPersonsList);
-
-		Record inActiveRec = new Record(ActionNames.ACTIVE);
-		inActiveRec.add("", getConstants().active());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.IN_ACTIVE);
-		inActiveRec.add("",  getConstants().inActive());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.ALL);
-		inActiveRec.add("",  getConstants().all());
-		actions.add(inActiveRec);
-		inActiveRec = new Record(ActionNames.FINISH);
-		inActiveRec.add("", getConstants().close());
-		actions.add(inActiveRec);
-
-		result.add(actions);
-
-		CommandList commandList = new CommandList();
-		commandList.add(getConstants().add()+" " +getConstants().salesPerson());
-		result.add(commandList);
-		return result;
-
-	}
-
-	private List<ClientSalesPerson> getSalesPersons(
-			Boolean isActive) {
-		ArrayList<ClientSalesPerson> salesPersons = getClientCompany().getSalesPersons();
-		if (isActive == null) {
-			return salesPersons;
-		}
-		ArrayList<ClientSalesPerson> result = new ArrayList<ClientSalesPerson>();
-		for (ClientSalesPerson salesPerson : salesPersons) {
-			if (salesPerson.isActive() == isActive) {
-				result.add(salesPerson);
-			}
-		}
-		return result;
-	}
-
-	private Record createSalesPersonRecord(ClientSalesPerson salesPrson) {
-		Record record = new Record(salesPrson);
-		record.add("Active", salesPrson.isActive() ?getConstants().active():getConstants().inActive());
-		record.add("Sales Person", salesPrson.getName());
-		ClientAddress address = salesPrson.getAddress();
-		record.add("Address", address != null ? address.getAddress1() : "");
-		record.add("City", address != null ? address.getCity() : "");
-		record.add("State", address != null ? address.getStateOrProvinence()
-				: "");
-		record.add("Zip Code", address != null ? address.getZipOrPostalCode()
-				: "");
-		record.add("Phone", salesPrson.getPhoneNo());
-		record.add("Fax", salesPrson.getFaxNo());
-		return record;
+		return list;
 	}
 
 }
