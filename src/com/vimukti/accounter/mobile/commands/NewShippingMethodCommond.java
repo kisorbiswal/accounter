0c6f43c1aacb98d3f877a3deb3582448ca818e2c
Devaraju K <devaraju.k@vimukti.com>
2011-11-02 04:02:22 +0000

    Former-commit-id: 5199a901048e432f619a530147707bde746a383f

--- src/com/vimukti/accounter/mobile/commands/NewShippingMethodCommond.java
+++ src/com/vimukti/accounter/mobile/commands/NewShippingMethodCommond.java
@@ -2,18 +2,14 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.List;
 
-import org.hibernate.Query;
-import org.hibernate.Session;
-
-import com.vimukti.accounter.mobile.ActionNames;
 import com.vimukti.accounter.mobile.Context;
-import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.NameRequirement;
 import com.vimukti.accounter.web.client.core.ClientShippingMethod;
 
-public class NewShippingMethodCommond extends AbstractCommand {
+public class NewShippingMethodCommond extends NewAbstractCommand {
+
 	private static final String SHIPPING_METHOD_NAME = "Shipping Mehtod";
 	private static final String DESCRIPTION = "description";
 
@@ -24,111 +20,44 @@ public class NewShippingMethodCommond extends AbstractCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new Requirement(SHIPPING_METHOD_NAME, false, true));
-		list.add(new Requirement(DESCRIPTION, true, true));
+		list.add(new NameRequirement(SHIPPING_METHOD_NAME,
+				"Enter Shipping Method Name", "Shipping Method", false, true));
+		list.add(new NameRequirement(DESCRIPTION, "Enter Description",
+				"Description", true, true));
 	}
 
 	@Override
-	public Result run(Context context) {
-		Result result = null;
-
-		if (context.getAttribute(INPUT_ATTR) == null) {
-			context.setAttribute(INPUT_ATTR, "optional");
-		}
-
-		Result makeResult = context.makeResult();
-		makeResult.add(getMessages().readyToCreate(
-				getConstants().shippingMethod()));
-		ResultList list = new ResultList("values");
-		makeResult.add(list);
-		ResultList actions = new ResultList(ACTIONS);
-		makeResult.add(actions);
-
-		result = nameRequirement(
-				context,
-				list,
-				SHIPPING_METHOD_NAME,
-				getConstants().shippingMethod(),
-				getMessages().pleaseEnter(
-						getConstants().shippingMethod() + " "
-								+ getConstants().name()));
-		if (result != null) {
-			return result;
-		}
-
-		setDefaultValues();
-		result = createOptionalResult(context, list, actions, makeResult);
-		if (result != null) {
-			return result;
-		}
-		return createShippingMethodObject(context);
+	protected String initObject(Context context, boolean isUpdate) {
+		return null;
 	}
 
-	private Result createOptionalResult(Context context, ResultList list,
-			ResultList actions, Result makeResult) {
-		Object selection = context.getSelection(ACTIONS);
-		if (selection != null) {
-			ActionNames actionName = (ActionNames) selection;
-			switch (actionName) {
-			case FINISH:
-				if (validate(context)) {
-					makeResult
-							.add(getConstants().shippingMethodAlreadyExists());
-				} else {
-					return null;
-				}
-			default:
-				break;
-			}
-		}
-
-		selection = context.getSelection("values");
-
-		Result result = stringOptionalRequirement(context, list, selection,
-				DESCRIPTION, getConstants().description(), getMessages()
-						.pleaseEnter(getConstants().description()));
-		if (result != null) {
-			return result;
-		}
-
-		Record finish = new Record(ActionNames.FINISH);
-		finish.add("",
-				getMessages().finishToCreate(getConstants().shippingMethod()));
-		actions.add(finish);
-
-		return makeResult;
+	@Override
+	protected String getWelcomeMessage() {
+		return "Shipping Method Commond is activated";
 	}
 
-	private boolean validate(Context context) {
-		Session session = context.getHibernateSession();
-		String name = get(SHIPPING_METHOD_NAME).getValue();
-		Query query = session.getNamedQuery("getShippingmethod.by.Name")
-				.setString("name", name)
-				.setEntity("company", context.getCompany());
-		List list = query.list();
-		if (list != null && list.size() > 0) {
-			return true;
-		}
-		return false;
+	@Override
+	protected String getDetailsMessage() {
+		return " Shppping Method is ready to created with following details.";
 	}
 
-	private void setDefaultValues() {
-		get(DESCRIPTION).setDefaultValue("");
-
+	@Override
+	protected void setDefaultValues(Context context) {
 	}
 
-	private Result createShippingMethodObject(Context context) {
-		ClientShippingMethod method = new ClientShippingMethod();
-		String name = get(SHIPPING_METHOD_NAME).getValue();
-		method.setName(name);
-		String desc = get(DESCRIPTION).getValue();
-		method.setDescription(desc);
-		Result result;
+	@Override
+	public String getSuccessMessage() {
+		return "New Shipping Method is Created Successfully";
+	}
 
-		create(method, context);
-		result = new Result(getMessages().createSuccessfully(
-				getConstants().shippingMethod()));
-		return result;
+	@Override
+	protected Result onCompleteProcess(Context context) {
+		ClientShippingMethod newShippingTerm = new ClientShippingMethod();
+		newShippingTerm.setName((String) get(SHIPPING_METHOD_NAME).getValue());
+		newShippingTerm.setDescription((String) get(DESCRIPTION).getValue());
+		create(newShippingTerm, context);
+		markDone();
+		return null;
 	}
 
 }
