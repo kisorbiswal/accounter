fe05e19616b0972a042797f82bc4412caa1a720d
srinivas.g <srinivas.g@vimukti.com>
2011-11-03 11:50:51 +0000

    Former-commit-id: bc8959c25a412022a1e31f64c89fe1355d709860

--- src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
+++ src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
@@ -12,10 +12,14 @@ import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.AmountRequirement;
+import com.vimukti.accounter.mobile.requirements.CurrencyRequirement;
 import com.vimukti.accounter.mobile.requirements.DateRequirement;
 import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
 import com.vimukti.accounter.mobile.requirements.TaxAgencyRequirement;
 import com.vimukti.accounter.web.client.core.ClientBox;
+import com.vimukti.accounter.web.client.core.ClientCurrency;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.ClientTAXAgency;
 import com.vimukti.accounter.web.client.core.ClientTAXReturn;
@@ -58,6 +62,54 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 			@Override
 			protected boolean filter(ClientTAXAgency e, String name) {
 				return e.getName().toLowerCase().startsWith(name);
+			}
+		});
+
+		list.add(new CurrencyRequirement(CURRENCY, getMessages().pleaseSelect(
+				getConstants().currency()), getConstants().currency(), true,
+				true, null) {
+			@Override
+			public Result run(Context context, Result makeResult,
+					ResultList list, ResultList actions) {
+				if (getClientCompany().getPreferences().isEnableMultiCurrency()) {
+					return super.run(context, makeResult, list, actions);
+				} else {
+					return null;
+				}
+			}
+
+			@Override
+			protected List<ClientCurrency> getLists(Context context) {
+				return context.getClientCompany().getCurrencies();
+			}
+		});
+
+		list.add(new AmountRequirement(CURRENCY_FACTOR, getMessages()
+				.pleaseSelect(getConstants().currency()), getConstants()
+				.currency(), false, true) {
+			@Override
+			protected String getDisplayValue(Double value) {
+				String primaryCurrency = getClientCompany().getPreferences()
+						.getPrimaryCurrency();
+				ClientCurrency selc = get(CURRENCY).getValue();
+				return "1 " + selc.getFormalName() + " = " + value + " " + primaryCurrency;
+			}
+
+			@Override
+			public Result run(Context context, Result makeResult,
+					ResultList list, ResultList actions) {
+				if (get(CURRENCY).getValue() != null) {
+					if (getClientCompany().getPreferences()
+							.isEnableMultiCurrency()
+							&& !((ClientCurrency)get(CURRENCY).getValue()).equals(
+									getClientCompany().getPreferences()
+											.getPrimaryCurrency())) {
+						return super.run(context, makeResult, list, actions);
+					}
+				} 
+					return null;
+				
+				
 			}
 		});
 
@@ -151,6 +203,20 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 		List<ClientBox> boxes = get(BOXES).getValue();
 		clientVATReturn.setBoxes(boxes);
 
+		
+		
+		if (context.getClientCompany().getPreferences().isEnableMultiCurrency()) {
+			ClientCurrency currency = get(CURRENCY).getValue();
+			if (currency != null) {
+				clientVATReturn.setCurrency(currency.getID());
+			}
+
+			double factor = get(CURRENCY_FACTOR).getValue();
+			clientVATReturn.setCurrencyFactor(factor);
+		}
+		
+		
+		
 		create(clientVATReturn, context);
 
 		return null;
@@ -178,6 +244,8 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 				.getCurrentFiscalYearStartDate();
 		get(FROM_DATE).setDefaultValue(date);
 		get(TO_DATE).setDefaultValue(new ClientFinanceDate());
+		get(CURRENCY).setDefaultValue(null);
+		get(CURRENCY_FACTOR).setDefaultValue(1.0);
 	}
 
 	@Override
