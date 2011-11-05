84dd2fdc09178e53ee765bdade756f4870a34356
Devi G <devi.g@vimukti.com>
2011-11-05 13:02:25 +0000

    Former-commit-id: 939421350b5028b54430500ea6afd9162dc077aa

--- src/com/vimukti/accounter/web/client/ui/edittable/TextAreaEditColumn.java
+++ src/com/vimukti/accounter/web/client/ui/edittable/TextAreaEditColumn.java
@@ -1,5 +1,8 @@
 package com.vimukti.accounter.web.client.ui.edittable;
 
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBoxBase;
 
@@ -7,7 +10,21 @@ public abstract class TextAreaEditColumn<T> extends TextEditColumn<T> {
 
 	@Override
 	protected TextBoxBase createWidget() {
-		return new TextArea();
+		final TextArea textArea = new TextArea();
+		textArea.addKeyUpHandler(new KeyUpHandler() {
+
+			@Override
+			public void onKeyUp(KeyUpEvent event) {
+				updateHeight(textArea.getElement());
+			}
+		});
+
+		return textArea;
 	}
+
+	private native void updateHeight(Element text)/*-{
+		if (text.scrollHeight > text.clientHeight)
+			text.style.height = text.scrollHeight + "px";
+	}-*/;
 
 }
