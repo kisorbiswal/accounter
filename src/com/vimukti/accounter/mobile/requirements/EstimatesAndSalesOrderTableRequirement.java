790b84073c622badf28ce5b3d7880d06e440ad27
Nagaraju P <nagaraju.p@vimukti.com>
2011-11-08 10:54:16 +0000

    Former-commit-id: a1f8f771dfaa37be151a0330623c6e8d9fb15c98

---
+++ src/com/vimukti/accounter/mobile/requirements/EstimatesAndSalesOrderTableRequirement.java
@@ -0,0 +1,86 @@
+package com.vimukti.accounter.mobile.requirements;
+
+import java.util.List;
+
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.services.DAOException;
+import com.vimukti.accounter.web.client.core.ClientCustomer;
+import com.vimukti.accounter.web.client.core.ClientTransaction;
+import com.vimukti.accounter.web.client.core.Lists.EstimatesAndSalesOrdersList;
+import com.vimukti.accounter.web.server.FinanceTool;
+
+public abstract class EstimatesAndSalesOrderTableRequirement extends
+		AbstractTableRequirement<EstimatesAndSalesOrdersList> {
+
+	public EstimatesAndSalesOrderTableRequirement(String requirementName,
+			String enterString, String recordName, boolean isOptional,
+			boolean isAllowFromContext) {
+		super(requirementName, enterString, recordName, false, isOptional,
+				isAllowFromContext);
+	}
+
+	@Override
+	protected void addRequirement(List<Requirement> list) {
+
+	}
+
+	@Override
+	protected String getEmptyString() {
+		return getMessages().thereAreNo(getConstants().estimate());
+	}
+
+	@Override
+	protected void getRequirementsValues(EstimatesAndSalesOrdersList obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void setRequirementsDefaultValues(EstimatesAndSalesOrdersList obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected EstimatesAndSalesOrdersList getNewObject() {
+		return null;
+	}
+
+	@Override
+	protected Record createFullRecord(EstimatesAndSalesOrdersList value) {
+		Record rec = new Record(value);
+		if (value.getType() == ClientTransaction.TYPE_ESTIMATE)
+			rec.add("", getConstants().quote());
+		else
+			rec.add("", getConstants().salesOrder());
+
+		rec.add("", value.getTotal());
+
+		return rec;
+	}
+
+	@Override
+	protected List<EstimatesAndSalesOrdersList> getList() {
+		try {
+			return new FinanceTool().getCustomerManager()
+					.getEstimatesAndSalesOrdersList(getCustomer().getID(),
+							getClientCompany().getID());
+		} catch (DAOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected abstract ClientCustomer getCustomer();
+
+	@Override
+	protected Record createRecord(EstimatesAndSalesOrdersList t) {
+		return createFullRecord(t);
+	}
+
+	@Override
+	protected String getAddMoreString() {
+		return "Add More Estimates";
+	}
+}
