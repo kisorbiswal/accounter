07c5ed5127839adfa5485d694e995e98d6968e5f
Lingarao R <lingarao.r@vimukti.com>
2011-11-09 09:53:49 +0000

    Former-commit-id: 1b30eb765d61d89bc7427e668171363bbb94fdda

--- src/com/vimukti/accounter/mobile/utils/CommandUtils.java
+++ src/com/vimukti/accounter/mobile/utils/CommandUtils.java
@@ -1,7 +1,11 @@
 package com.vimukti.accounter.mobile.utils;
 
+import java.util.ArrayList;
+
+import com.vimukti.accounter.core.PaymentTerms;
 import com.vimukti.accounter.core.Vendor;
 import com.vimukti.accounter.web.client.core.AccounterCoreType;
+import com.vimukti.accounter.web.client.core.ClientItemGroup;
 import com.vimukti.accounter.web.client.core.IAccounterCore;
 
 public class CommandUtils {
@@ -13,6 +17,11 @@ public class CommandUtils {
 
 	public static IAccounterCore getClientObjectById(long id,
 			AccounterCoreType taxitem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static ClientItemGroup getItemGroupByName(String string) {
 		// TODO Auto-generated method stub
 		return null;
 	}
