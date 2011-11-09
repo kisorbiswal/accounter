3c11615e5036a93d0cbefccfefa4b4a0723181c9
Syamalamba P <syamalamba.p@vimukti.com>
2011-11-09 05:33:05 +0000

    Former-commit-id: d7aa9e0e28f94e9ac31c36b3f38e26dfe00dc604

---
+++ src/com/vimukti/accounter/mobile/utils/CommandUtils.java
@@ -0,0 +1,20 @@
+package com.vimukti.accounter.mobile.utils;
+
+import com.vimukti.accounter.core.Vendor;
+import com.vimukti.accounter.web.client.core.AccounterCoreType;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+
+public class CommandUtils {
+
+	public static Vendor getVendorByName(String vendorName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static IAccounterCore getClientObjectById(long id,
+			AccounterCoreType taxitem) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
