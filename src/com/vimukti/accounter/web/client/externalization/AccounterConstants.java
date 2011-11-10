b7f005f6ff39ecac7462321d17f5d8dc15b5bc1d
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-11-10 06:54:23 +0000

    Former-commit-id: 444d89337abf8a9b3e4ecf4d30eb06ddf39dd9be

--- src/com/vimukti/accounter/web/client/externalization/AccounterConstants.java
+++ src/com/vimukti/accounter/web/client/externalization/AccounterConstants.java
@@ -3741,4 +3741,6 @@ public interface AccounterConstants extends Constants {
 	public String oneUserCanEnterOneValueOnlyForEachMessage();
 
 	public String selectProperBankAccount();
+	
+	public String pleaseEntervalidCurrencyFactor();
 }
