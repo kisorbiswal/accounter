cd3c7d7d0e508784749446eaf273d3d0f855fc27
Nagaraju P <nagaraju.p@vimukti.com>
2011-11-11 06:59:00 +0000

    Former-commit-id: a73e50f73ec0d187ff4e9105bbf20e5168fad9aa

--- src/com/vimukti/accounter/web/client/externalization/AccounterMessages.java
+++ src/com/vimukti/accounter/web/client/externalization/AccounterMessages.java
@@ -199,8 +199,6 @@ public interface AccounterMessages extends Messages {
 
 	public String payeeGroup(String payee);
 
-	public String customerGroup(String customer);
-
 	public String payeeGroupAlreadyExists(String payee);
 
 	public String payeeGroupList(String payee);
