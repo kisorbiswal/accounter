f341c237f9561221d26577c60e75e6c1aa3315a6
Devi G <devi.g@vimukti.com>
2011-11-18 09:07:53 +0000

    Former-commit-id: 38cbfa5bb5aaf941868e31c7b562751a5614c914

--- src/com/vimukti/accounter/web/server/translate/TranslateServiceImpl.java
+++ src/com/vimukti/accounter/web/server/translate/TranslateServiceImpl.java
@@ -1,9 +1,14 @@
 package com.vimukti.accounter.web.server.translate;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.vimukti.accounter.main.ServerLocal;
 import com.vimukti.accounter.web.client.translate.ClientLanguage;
 import com.vimukti.accounter.web.client.translate.ClientMessage;
 import com.vimukti.accounter.web.client.translate.TranslateService;
@@ -17,6 +22,7 @@ public class TranslateServiceImpl extends RemoteServiceServlet implements
 	 */
 	private static final long serialVersionUID = 1L;
 	private static final String EMAIL_ID = "emailId";
+	private List<String> serverMatchList;
 
 	// @Override
 	// public ArrayList<Status> getStatus() {
@@ -86,6 +92,49 @@ public class TranslateServiceImpl extends RemoteServiceServlet implements
 
 	public List<ClientLanguage> getLanguages() {
 		return new FinanceTool().getLanguages();
+	}
+
+	public ClientLanguage getLocalLanguage() {
+		List<ClientLanguage> languages = new FinanceTool().getLanguages();
+		for (ClientLanguage clientLanguage : languages) {
+			if (clientLanguage.getLanguageCode().equals(
+					ServerLocal.get().getISO3Language())) {
+				return clientLanguage;
+			}
+
+		}
+		return languages.get(0);
+	}
+
+	public boolean validateUserValue(ClientMessage clientMessage, String data) {
+		serverMatchList = new ArrayList<String>();
+		Pattern regex = Pattern.compile("\\{([^}]*)\\}");
+		Matcher serverMatcher = regex.matcher(clientMessage.getValue());
+		while (serverMatcher.find()) {
+			serverMatchList.add(serverMatcher.group());
+		}
+
+		List<String> userMatchList = new ArrayList<String>();
+		Matcher userMatcher = regex.matcher(data);
+		while (userMatcher.find()) {
+			userMatchList.add(userMatcher.group());
+		}
+		if (serverMatchList.size() == userMatchList.size()) {
+			for (int i = 0; i < serverMatchList.size(); i++) {
+				if (!serverMatchList.get(i).equals(userMatchList.get(i))) {
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	public Set<String> getServerMatchList() {
+		Set<String> matchList = new HashSet<String>();
+		matchList.addAll(serverMatchList);
+		return matchList;
 	}
 
 	@Override
