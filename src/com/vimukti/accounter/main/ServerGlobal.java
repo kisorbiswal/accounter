f426400b094292f95bfce1083dcacd435f330a03
Rajesh Akkineni <rajesh@vimukti.com>
2011-11-19 13:00:24 +0000

    Former-commit-id: 52f189a14b4dd358b14d286329792cbb845557ec

--- src/com/vimukti/accounter/main/ServerGlobal.java
+++ src/com/vimukti/accounter/main/ServerGlobal.java
@@ -2,9 +2,6 @@ package com.vimukti.accounter.main;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 
 import com.vimukti.accounter.web.client.AbstractGlobal;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
@@ -13,17 +10,12 @@ import com.vimukti.accounter.web.server.i18n.ServerSideMessages;
 
 public class ServerGlobal extends AbstractGlobal {
 
-	private Map<Locale, AccounterMessages> messages = new HashMap<Locale, AccounterMessages>();
 
+	AccounterMessages messages = ServerSideMessages.get(AccounterMessages.class);
+	
 	public ServerGlobal() throws IOException {
 	}
 
-	private AccounterMessages createAccounterMessages(Locale locale)
-			throws IOException {
-		AccounterMessages messages = ServerSideMessages.get(AccounterMessages.class,
-				locale.getLanguage());
-		return messages;
-	}
 
 	@Override
 	public ClientCompanyPreferences preferences() {
@@ -32,17 +24,7 @@ public class ServerGlobal extends AbstractGlobal {
 
 	@Override
 	public AccounterMessages messages() {
-		Locale locale = ServerLocal.get();
-		AccounterMessages accounterMessages = this.messages.get(locale);
-		if (accounterMessages == null) {
-			try {
-				accounterMessages = createAccounterMessages(locale);
-				this.messages.put(locale, accounterMessages);
-			} catch (IOException e) {
-				throw new RuntimeException();
-			}
-		}
-		return accounterMessages;
+		return messages;
 	}
 
 	@Override
