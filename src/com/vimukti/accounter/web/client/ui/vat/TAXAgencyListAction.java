5d8d90410be1e7f35b433150b3531b6e89860cb9
Varakumar Pjd <varakumar.pjd@vimukti.com>
2011-11-21 10:52:01 +0000

    Former-commit-id: 0e20c4aa88874a58f1be43a6f351d31498df315e

---
+++ src/com/vimukti/accounter/web/client/ui/vat/TAXAgencyListAction.java
@@ -0,0 +1,51 @@
+package com.vimukti.accounter.web.client.ui.vat;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallback;
+
+public class TAXAgencyListAction extends Action {
+
+	public TAXAgencyListAction(String text) {
+		super(text);
+		this.catagory = messages.tax();
+	}
+
+	@Override
+	public void run() {
+		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+
+			public void onCreated() {
+
+				MainFinanceWindow.getViewManager().showView(
+						new TAXAgencyListView(), null, false,
+						TAXAgencyListAction.this);
+
+			}
+		});
+
+	}
+
+	public ImageResource getBigImage() {
+		return null;
+	}
+
+	public ImageResource getSmallImage() {
+		return Accounter.getFinanceMenuImages().vendors();
+	}
+
+	@Override
+	public String getHistoryToken() {
+
+		return "TAXAgencyList";
+	}
+
+	@Override
+	public String getHelpToken() {
+		return "taxAgency-list";
+	}
+
+}
