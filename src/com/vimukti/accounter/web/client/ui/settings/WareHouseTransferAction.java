4be82d029fed8ff23946a42fa18f7de842715fca
Devaraju K <devaraju.k@vimukti.com>
2011-11-21 10:16:31 +0000

    Former-commit-id: 414794d7df488d947c0a43db81b5e911806f3b40

--- src/com/vimukti/accounter/web/client/ui/settings/WareHouseTransferAction.java
+++ src/com/vimukti/accounter/web/client/ui/settings/WareHouseTransferAction.java
@@ -2,13 +2,14 @@ package com.vimukti.accounter.web.client.ui.settings;
 
 import com.google.gwt.resources.client.ImageResource;
 import com.vimukti.accounter.web.client.core.ClientStockTransfer;
+import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
 import com.vimukti.accounter.web.client.ui.core.Action;
 
 public class WareHouseTransferAction extends Action<ClientStockTransfer> {
 	public WareHouseTransferAction(String text) {
 		super(text);
-		// TODO Auto-generated constructor stub
+		this.catagory = Accounter.messages().inventory();
 	}
 
 	private WareHouseTransferView view;
