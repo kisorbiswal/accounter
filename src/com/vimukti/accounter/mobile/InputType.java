95c597545ca681ccbe918a603493c2f5fd9a57e4
Nagaraju P <nagaraju.p@vimukti.com>
2011-11-23 07:31:20 +0000

    Former-commit-id: 7d0e4d52874379b689e72c83d1e59dfac48a0211

--- src/com/vimukti/accounter/mobile/InputType.java
+++ src/com/vimukti/accounter/mobile/InputType.java
@@ -4,14 +4,20 @@ public class InputType {
 	int type;
 	private int inputType;
 	private String name;
+	private String value;
 
 	public InputType(int inputType) {
-		this(inputType, "");
+		this(inputType, "", "");
 	}
 
 	public InputType(int inputType, String name) {
+		this(inputType, name, "");
+	}
+
+	public InputType(int inputType, String name, String value) {
 		this.setInputType(inputType);
 		this.setName(name);
+		this.setValue(value);
 		this.type = 3;
 	}
 
@@ -29,5 +35,13 @@ public class InputType {
 
 	public void setInputType(int inputType) {
 		this.inputType = inputType;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 }
