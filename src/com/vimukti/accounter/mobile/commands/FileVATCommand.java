3ae045db4839e07c855df6b8243051e84db36a9f
Syamalamba P <syamalamba.p@vimukti.com>
2011-11-24 12:56:48 +0000

    Former-commit-id: 515ad726e2b99c057ac4a60bd04c15214de04b08

--- src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
+++ src/com/vimukti/accounter/mobile/commands/FileVATCommand.java
@@ -4,29 +4,35 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.vimukti.accounter.core.FinanceDate;
 import com.vimukti.accounter.core.TAXAgency;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 import com.vimukti.accounter.mobile.requirements.ChangeListner;
 import com.vimukti.accounter.mobile.requirements.DateRequirement;
 import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
 import com.vimukti.accounter.mobile.requirements.TaxAgencyRequirement;
+import com.vimukti.accounter.web.client.core.ClientBox;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.ClientTAXAgency;
 import com.vimukti.accounter.web.client.core.ClientTAXReturn;
 import com.vimukti.accounter.web.client.core.ClientTAXReturnEntry;
+import com.vimukti.accounter.web.client.countries.UnitedKingdom;
 import com.vimukti.accounter.web.client.ui.widgets.DateUtil;
+import com.vimukti.accounter.web.client.util.ICountryPreferences;
 import com.vimukti.accounter.web.server.FinanceTool;
 
 public class FileVATCommand extends NewAbstractTransactionCommand {
 
 	private static final String FROM_DATE = "fromDate";
 	private static final String TO_DATE = "toDate";
-	private static final String BOXES = "boxes";
+	private static final String TAXRETURNENTRIES = "taxreturnentries";
 	private static final String TAX_AGENCY = "taxAgency";
+	private static final String BOXES = "boxes";
 
 	@Override
 	public String getId() {
@@ -114,7 +120,74 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 		list.add(new DateRequirement(TO_DATE, getMessages().pleaseEnter(
 				getMessages().toDate()), getMessages().toDate(), true, true));
 
-		list.add(new ShowListRequirement<ClientTAXReturnEntry>(BOXES, "", 15) {
+		list.add(new ShowListRequirement<ClientBox>(BOXES, "", 15) {
+
+			@Override
+			public Result run(Context context, Result makeResult,
+					ResultList list, ResultList actions) {
+				ICountryPreferences countryPreferences = context.getCompany()
+						.getCountryPreferences();
+				if (countryPreferences instanceof UnitedKingdom
+						&& countryPreferences.isVatAvailable()) {
+					return super.run(context, makeResult, list, actions);
+				}
+				return null;
+			}
+
+			@Override
+			protected String onSelection(ClientBox value) {
+				return null;
+			}
+
+			@Override
+			protected String getShowMessage() {
+				return null;
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return getMessages().norecordstoshowinbetweentheselecteddates();
+			}
+
+			@Override
+			protected Record createRecord(ClientBox value) {
+				Record record = new Record(value);
+				record.add("", getMessages().vatLine());
+				record.add("", value.getName());
+				record.add("", getMessages().amount());
+				record.add("", value.getAmount());
+				return record;
+			}
+
+			@Override
+			protected void setCreateCommand(CommandList list) {
+
+			}
+
+			@Override
+			protected boolean filter(ClientBox e, String name) {
+				return false;
+			}
+
+			@Override
+			protected List<ClientBox> getLists(Context context) {
+				return getClientBoxes(context);
+			}
+		});
+
+		list.add(new ShowListRequirement<ClientTAXReturnEntry>(
+				TAXRETURNENTRIES, "", 15) {
+			@Override
+			public Result run(Context context, Result makeResult,
+					ResultList list, ResultList actions) {
+				ICountryPreferences countryPreferences = context.getCompany()
+						.getCountryPreferences();
+				if (!(countryPreferences instanceof UnitedKingdom && countryPreferences
+						.isVatAvailable())) {
+					return super.run(context, makeResult, list, actions);
+				}
+				return null;
+			}
 
 			@Override
 			protected String onSelection(ClientTAXReturnEntry value) {
@@ -158,8 +231,57 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 		});
 	}
 
+	protected List<ClientBox> getClientBoxes(Context context) {
+		TAXAgency taxAgency = get(TAX_AGENCY).getValue();
+		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
+		ClientFinanceDate toDate = get(TO_DATE).getValue();
+		try {
+			ClientTAXReturn vatReturn = new FinanceTool().getTaxManager()
+					.getVATReturnDetails(taxAgency, new FinanceDate(fromDate),
+							new FinanceDate(toDate), getCompanyId());
+			List<ClientBox> boxes = new ArrayList<ClientBox>();
+			double box3Amt = 0.0;
+			double box5Amt = 0.0;
+			double box4Amt = 0.0;
+			for (ClientBox box : vatReturn.getBoxes()) {
+
+				if (box.getBoxNumber() == 1 || box.getBoxNumber() == 2) {
+					box3Amt = box3Amt + box.getAmount();
+				}
+				if (box.getBoxNumber() == 3) {
+					box.setAmount(box3Amt);
+				}
+				if (box.getBoxNumber() == 4) {
+					box4Amt = box4Amt + box.getAmount();
+				}
+				if (box.getBoxNumber() == 4) {
+					box.setAmount(box4Amt);
+				}
+				if (box.getBoxNumber() == 4) {
+					box5Amt = box3Amt - box.getAmount();
+				}
+
+				if (box.getBoxNumber() == 5) {
+					box.setAmount(box5Amt);
+				}
+				boxes.add(box);
+			}
+			if (boxes.isEmpty()) {
+				context.makeResult().add(
+						getMessages()
+								.norecordstoshowinbetweentheselecteddates());
+			}
+			get(BOXES).setValue(boxes);
+			return boxes;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ArrayList<ClientBox>();
+		}
+	}
+
 	protected void taxAgencySelected(TAXAgency taxAgency) {
-		get(BOXES).setValue(new ArrayList<ClientTAXReturnEntry>());
+		get(TAXRETURNENTRIES).setValue(new ArrayList<ClientTAXReturnEntry>());
+		get(BOXES).setValue(new ArrayList<ClientBox>());
 		long lastTaxReturnEndDate = new FinanceTool().getTaxManager()
 				.getLastTaxReturnEndDate(taxAgency.getID(), getCompanyId());
 		ClientFinanceDate clientFinanceDate;
@@ -207,7 +329,10 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 					.getTaxManager().getTAXReturnEntries(getCompanyId(),
 							taxAgency.getID(), fromDate.getDate(),
 							toDate.getDate());
-			get(BOXES).setValue(taxReturnEntries);
+			if (taxReturnEntries.isEmpty()) {
+				context.makeResult().add(getMessages().selectTAXAgency());
+			}
+			get(TAXRETURNENTRIES).setValue(taxReturnEntries);
 			return taxReturnEntries;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -218,18 +343,49 @@ public class FileVATCommand extends NewAbstractTransactionCommand {
 	@Override
 	protected Result onCompleteProcess(Context context) {
 		ClientTAXReturn clientVATReturn = new ClientTAXReturn();
-		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
-		clientVATReturn.setPeriodStartDate(fromDate.getDate());
-
-		ClientFinanceDate toDate = get(TO_DATE).getValue();
-		clientVATReturn.setPeriodEndDate(toDate.getDate());
-		clientVATReturn.setTransactionDate(new ClientFinanceDate().getDate());
 		TAXAgency taxAgency = get(TAX_AGENCY).getValue();
+		ClientFinanceDate fromDate = get(FROM_DATE).getValue();
+		ClientFinanceDate toDate = get(TO_DATE).getValue();
+		List<ClientTAXReturnEntry> taxReturnEntries = get(TAXRETURNENTRIES)
+				.getValue();
+		if (!taxReturnEntries.isEmpty()) {
+			clientVATReturn.setPeriodStartDate(fromDate.getDate());
+			clientVATReturn.setPeriodEndDate(toDate.getDate());
+			clientVATReturn.setTaxReturnEntries(taxReturnEntries);
+		}
+		clientVATReturn.setTransactionDate(new ClientFinanceDate().getDate());
 		clientVATReturn.setTAXAgency(taxAgency.getID());
 
-		List<ClientTAXReturnEntry> taxReturnEntries = get(BOXES).getValue();
-		clientVATReturn.setTaxReturnEntries(taxReturnEntries);
+		List<ClientBox> boxes = get(BOXES).getValue();
+		clientVATReturn.setBoxes(boxes);
+		if (!boxes.isEmpty()) {
+			if (taxAgency != null) {
+				clientVATReturn.setTransactionDate(new ClientFinanceDate()
+						.getDate());
+			}
 
+			double salesTaxamount = 0, purchaseTaxAmount = 0;
+			for (ClientBox b : boxes) {
+				if (b.getBoxNumber() == 1 || b.getBoxNumber() == 2) {
+					salesTaxamount = salesTaxamount + b.getAmount();
+				} else if (b.getBoxNumber() == 4) {
+					purchaseTaxAmount = purchaseTaxAmount + b.getAmount();
+				} else if (b.getBoxNumber() == 10) {
+					purchaseTaxAmount = purchaseTaxAmount - b.getAmount();
+				}
+			}
+
+			double amount = clientVATReturn.getBoxes().get(4).getAmount()
+					+ clientVATReturn.getBoxes()
+							.get(clientVATReturn.getBoxes().size() - 1)
+							.getAmount();
+
+			clientVATReturn.setSalesTaxTotal(clientVATReturn.getBoxes().get(2)
+					.getAmount());
+			clientVATReturn.setPurchaseTaxTotal(clientVATReturn.getBoxes()
+					.get(3).getAmount());
+			clientVATReturn.setTotalTAXAmount(amount);
+		}
 		/*
 		 * if (context.getPreferences().isEnableMultiCurrency()) { Currency
 		 * currency = get(CURRENCY).getValue(); if (currency != null) {
