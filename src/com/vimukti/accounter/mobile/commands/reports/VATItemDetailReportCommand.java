b79bc5c028f8fae52d513a34998d900cb85061c9
Syamalamba P <syamalamba.p@vimukti.com>
2011-11-24 14:07:38 +0000

    Former-commit-id: 9798eea5f71bcaf6aa93d5f90b35aca74dc0bcba

--- src/com/vimukti/accounter/mobile/commands/reports/VATItemDetailReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/VATItemDetailReportCommand.java
@@ -1,28 +1,33 @@
 package com.vimukti.accounter.mobile.commands.reports;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-import org.hibernate.Session;
-
-import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.core.TAXItem;
+import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.web.client.core.Utility;
 import com.vimukti.accounter.web.client.core.reports.VATItemDetail;
+import com.vimukti.accounter.web.server.FinanceTool;
 
 public class VATItemDetailReportCommand extends
-		AbstractReportCommand<VATItemDetail> {
+		NewAbstractReportCommand<VATItemDetail> {
+	TAXItem taxItem;
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		add3ReportRequirements(list);
+		addDateRangeFromToDateRequirements(list);
 	}
 
 	@Override
 	protected Record createReportRecord(VATItemDetail record) {
 		Record ecRecord = new Record(record);
-		ecRecord.add("Type", Utility.getTransactionName(record
-				.getTransactionType()));
+		ecRecord.add("Type",
+				Utility.getTransactionName(record.getTransactionType()));
 		ecRecord.add("Date", record.getDate());
 		ecRecord.add("No", record.getTransactionNumber());
 		ecRecord.add("Name", record.getName());
@@ -33,22 +38,87 @@ public class VATItemDetailReportCommand extends
 	}
 
 	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
+	protected List<VATItemDetail> getRecords() {
+		if (taxItem == null) {
+			try {
+				return new FinanceTool().getReportManager()
+						.getVATItemDetailReport(getStartDate(), getEndDate(),
+								getCompanyId());
+			} catch (DAOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				return new FinanceTool().getReportManager()
+						.getVATItemDetailReport(taxItem.getName(),
+								getStartDate(), getEndDate(), getCompanyId());
+			} catch (DAOException e) {
+				e.printStackTrace();
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return new ArrayList<VATItemDetail>();
+	}
+
+	@Override
+	protected String addCommandOnRecordClick(VATItemDetail selection) {
+		return "update transaction " + selection.getTransactionId();
+	}
+
+	@Override
+	protected String getEmptyString() {
+		return "You don't have any VAT Item detail reports";
+	}
+
+	@Override
+	protected String getShowMessage() {
 		return null;
 	}
 
 	@Override
-	protected List<VATItemDetail> getRecords(Session session) {
-		// TODO Auto-generated method stub
+	protected String getSelectRecordString() {
+		return "Select a report to see the Transaction details";
+	}
+
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		String accountName = null;
+		String string = context.getString();
+		if (string != null) {
+			String[] split = string.split(",");
+			if (split.length > 1) {
+				context.setString(split[0]);
+				accountName = split[1];
+			}
+		}
+		if (accountName != null) {
+			Set<TAXItem> taxItems = context.getCompany().getTaxItems();
+			for (TAXItem taxItem : taxItems) {
+				if (taxItem.getName().equalsIgnoreCase(accountName)) {
+					this.taxItem = taxItem;
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
-	protected void addCommandOnRecordClick(VATItemDetail selection,
-			CommandList commandList) {
-		commandList.add(Utility.getTransactionName(selection
-				.getTransactionType()));
+	protected String getWelcomeMessage() {
+		return "VAT Item detail report command is activated";
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		return "VAT Item report details";
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		return "VAT Item detail report command closed successfully";
 	}
 
 }
--- src/com/vimukti/accounter/mobile/commands/reports/VATItemSummaryReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/VATItemSummaryReportCommand.java
@@ -1,20 +1,22 @@
 package com.vimukti.accounter.mobile.commands.reports;
 
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
-
-import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.web.client.core.reports.VATItemSummary;
+import com.vimukti.accounter.web.server.FinanceTool;
 
 public class VATItemSummaryReportCommand extends
-		AbstractReportCommand<VATItemSummary> {
+		NewAbstractReportCommand<VATItemSummary> {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		add3ReportRequirements(list);
+		addDateRangeFromToDateRequirements(list);
 	}
 
 	@Override
@@ -26,21 +28,58 @@ public class VATItemSummaryReportCommand extends
 	}
 
 	@Override
-	public String getId() {
+	protected List<VATItemSummary> getRecords() {
+		try {
+			return new FinanceTool().getReportManager()
+					.getVATItemSummaryReport(getStartDate(), getEndDate(),
+							getCompanyId());
+		} catch (DAOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return new ArrayList<VATItemSummary>();
+	}
+
+	@Override
+	protected String addCommandOnRecordClick(VATItemSummary selection) {
+		return "VAT Item Detail ," + selection.getName();
+	}
+
+	@Override
+	protected String getEmptyString() {
+		return "You don't have any VAT Item summary reports";
+	}
+
+	@Override
+	protected String getShowMessage() {
+		return null;
+	}
+
+	@Override
+	protected String getSelectRecordString() {
+		return "Select a report to see details";
+	}
+
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected List<VATItemSummary> getRecords(Session session) {
-		// TODO Auto-generated method stub
-		return null;
+	protected String getWelcomeMessage() {
+		return "VAT Item summary reprot command activated";
 	}
 
 	@Override
-	protected void addCommandOnRecordClick(VATItemSummary selection,
-			CommandList commandList) {
-		commandList.add("VAT Item Detail");
+	protected String getDetailsMessage() {
+		return "VAT item summary report details";
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		return "VAT Item summary report command closed succesfully";
 	}
 
 }
