52b6fb642a6bff3849db09f6db8048825ac9c4a0
Lingarao R <lingarao.r@vimukti.com>
2011-11-25 06:20:39 +0000

    Former-commit-id: f1fcacb1d35f75f5061d1d50e65b031fa1e4c924

--- src/com/vimukti/accounter/mobile/requirements/AccountRequirement.java
+++ src/com/vimukti/accounter/mobile/requirements/AccountRequirement.java
@@ -3,6 +3,7 @@ package com.vimukti.accounter.mobile.requirements;
 import com.vimukti.accounter.core.Account;
 import com.vimukti.accounter.mobile.CommandList;
 import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.utils.CommandUtils;
 
 public abstract class AccountRequirement extends ListRequirement<Account> {
 
@@ -24,7 +25,8 @@ public abstract class AccountRequirement extends ListRequirement<Account> {
 
 	@Override
 	protected String getDisplayValue(Account value) {
-		return value != null ? value.getName() : "";
+		return value != null ? value.getNumber() + "-" + value.getName() + "-"
+				+ CommandUtils.getAccountTypeString(value.getType()) : "";
 	}
 
 	@Override
