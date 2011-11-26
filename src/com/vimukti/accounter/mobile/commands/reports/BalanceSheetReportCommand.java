13fc4dbf6fa7dbc3135b90d40938a66d2d15f3d2
Devaraju K <devaraju.k@vimukti.com>
2011-11-26 09:17:26 +0000

    Former-commit-id: 46a74ccfe315b53cc9b09f86490b5db66e5e7076

--- src/com/vimukti/accounter/mobile/commands/reports/BalanceSheetReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/BalanceSheetReportCommand.java
@@ -25,10 +25,11 @@ public class BalanceSheetReportCommand extends
 	protected Record createReportRecord(TrialBalance record) {
 		Record trialRecord = new Record(record);
 		if (getCompany().getPreferences().getUseAccountNumbers() == true)
-			trialRecord.add("Category Number", record.getAccountNumber());
+			trialRecord.add(getMessages().categoryNumber(),
+					record.getAccountNumber());
 		else
 			return null;
-		trialRecord.add("", record.getAccountName());
+		trialRecord.add(getMessages().accountName(), record.getAccountName());
 		trialRecord
 				.add(getStartDate() + "-" + getEndDate(), record.getAmount());
 		trialRecord.add("", record.getAmount());
