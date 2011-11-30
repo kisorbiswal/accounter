f67b0885e16a76094f1ed2c69e5b404df6691cc0
Lingarao R <lingarao.r@vimukti.com>
2011-11-30 04:15:17 +0000

    Former-commit-id: d2ca1e9f76fb1c8f2c892bad3f2507c2014b134e

--- src/com/vimukti/accounter/mobile/preferences/CompanyInfoPreferenceCommand.java
+++ src/com/vimukti/accounter/mobile/preferences/CompanyInfoPreferenceCommand.java
@@ -6,12 +6,18 @@ import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
 import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.commands.NewCommand;
+import com.vimukti.accounter.mobile.commands.NewInvoiceCommand;
+import com.vimukti.accounter.mobile.commands.StateRequirement;
 import com.vimukti.accounter.mobile.commands.URLRequirement;
 import com.vimukti.accounter.mobile.requirements.BooleanRequirement;
 import com.vimukti.accounter.mobile.requirements.EmailRequirement;
 import com.vimukti.accounter.mobile.requirements.NameRequirement;
 import com.vimukti.accounter.mobile.requirements.PhoneRequirement;
+import com.vimukti.accounter.mobile.requirements.StringListRequirement;
+import com.vimukti.accounter.web.client.core.ClientAddress;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
+import com.vimukti.accounter.web.client.ui.CoreUtils;
 
 public class CompanyInfoPreferenceCommand extends
 		AbstractCompanyPreferencesCommand {
@@ -24,6 +30,12 @@ public class CompanyInfoPreferenceCommand extends
 	protected static final String EMAIL = "email";
 	protected static final String WEBSITE = "website";
 	protected static final String PHONE_NUMBER = "phonenumber";
+	private static final String ADDRESS1 = "address1";
+	private static final String ADDRESS2 = "address2";
+	private static final String CITY = "city";
+	private static final String POSTAL_CODE = "postalCode";
+	private static final String STATE = "stateOrProvinence";
+	private static final String COUNTRY = "countryOrRegion";
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
@@ -61,22 +73,45 @@ public class CompanyInfoPreferenceCommand extends
 			}
 		});
 
-		// list.add(new BooleanRequirement(IS_DIFFERENT_ADDRESS, true) {
-		//
-		// @Override
-		// protected String getTrueString() {
-		// return getMessages().getCompanyLegalCheckBoxText() + " "
-		// + getMessages().active();
-		// }
-		//
-		// @Override
-		// protected String getFalseString() {
-		// return getMessages().getCompanyLegalCheckBoxText() + " "
-		// + getMessages().inActive();
-		// }
-		// });
+		list.add(new NameRequirement(ADDRESS1, getMessages().pleaseEnter(
+				getMessages().address1()), getMessages().address1(), true, true));
+		list.add(new NameRequirement(ADDRESS2, getMessages().pleaseEnter(
+				getMessages().address2()), getMessages().address2(), true, true));
+		list.add(new NameRequirement(CITY, getMessages().pleaseEnter(
+				getMessages().city()), getMessages().city(), true, true));
+		list.add(new NameRequirement(POSTAL_CODE, getMessages().pleaseEnter(
+				getMessages().postalCode()), getMessages().postalCode(), true,
+				true));
+		list.add(new NameRequirement(COUNTRY, getMessages().pleaseEnter(
+				getMessages().country()), getMessages().country(), true, true));
 
-		// TODO address requirement
+		list.add(new StringListRequirement(STATE, "Please Enter state", STATE,
+				true, true, null) {
+
+			@Override
+			protected String getSetMessage() {
+				return "state has been selected";
+			}
+
+			@Override
+			protected String getSelectString() {
+				return "Select Account type";
+			}
+
+			@Override
+			protected List<String> getLists(Context context) {
+				String countryName = CompanyInfoPreferenceCommand.this.get(
+						COUNTRY).getValue();
+				List<String> statesAsListForCountry = CoreUtils
+						.getStatesAsListForCountry(countryName);
+				return statesAsListForCountry;
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return null;
+			}
+		});
 
 		list.add(new EmailRequirement(EMAIL, getMessages().pleaseEnter(
 				getMessages().email()), getMessages().email(), true, true));
@@ -87,9 +122,6 @@ public class CompanyInfoPreferenceCommand extends
 		list.add(new PhoneRequirement(PHONE_NUMBER, getMessages().pleaseEnter(
 				getMessages().phoneNumber()), getMessages().phoneNumber(),
 				true, true));
-		// list.add(new CompanyPreferencesAddressRequirment(COMPANY_ADDRESS,
-		// getMessages().pleaseEnter(getMessages().address()),
-		// getMessages().address(), true, true));
 
 	}
 
@@ -100,10 +132,13 @@ public class CompanyInfoPreferenceCommand extends
 		get(COMPANY_NAME).setValue(preferences.getTradingName());
 		get(IS_DIFFERENT_NAME).setValue(preferences.isShowLegalName());
 		get(LEGAL_NAME).setValue(preferences.getLegalName());
-		// get(IS_DIFFERENT_ADDRESS).setValue(
-		// preferences.isShowRegisteredAddress());
-		// get(COMPANY_ADDRESS).setValue(preferences.getTradingAddress());
-		// get(COMPANY_ADDRESS2).setValue(preferences.isShowRegisteredAddress());
+		ClientAddress tradingAddress = preferences.getTradingAddress();
+		get(ADDRESS1).setValue(tradingAddress.getAddress1());
+		get(ADDRESS2).setValue(tradingAddress.getStreet());
+		get(POSTAL_CODE).setValue(tradingAddress.getZipOrPostalCode());
+		get(COUNTRY).setValue(tradingAddress.getCountryOrRegion());
+		get(COUNTRY).setEditable(false);
+		get(STATE).setValue(tradingAddress.getStateOrProvinence());
 		get(EMAIL).setValue(preferences.getCompanyEmail());
 		get(WEBSITE).setValue(preferences.getWebSite());
 		get(PHONE_NUMBER).setValue(preferences.getPhone());
@@ -118,8 +153,6 @@ public class CompanyInfoPreferenceCommand extends
 		String name = get(COMPANY_NAME).getValue();
 		boolean isdiffname = get(IS_DIFFERENT_NAME).getValue();
 		String legelname = get(LEGAL_NAME).getValue();
-		// boolean isdiffadd = get(IS_DIFFERENT_ADDRESS).getValue();
-		// ClientAddress address = get(COMPANY_ADDRESS).getValue();
 		String email = get(EMAIL).getValue();
 		String website = get(WEBSITE).getValue();
 		String phno = get(PHONE_NUMBER).getValue();
@@ -135,14 +168,13 @@ public class CompanyInfoPreferenceCommand extends
 		preferences.setPhone(phno);
 		preferences.setCompanyEmail(email);
 		preferences.setWebSite(website);
-		// preferences.setTradingAddress(address);
-
-		// if (isdiffadd) {
-		// preferences.setShowRegisteredAddress(true);
-		// } else {
-		// preferences.setShowRegisteredAddress(false);
-		// }
-
+		ClientAddress address = new ClientAddress();
+		address.setAddress1((String) get(ADDRESS1).getValue());
+		address.setStreet((String) get(ADDRESS2).getValue());
+		address.setZipOrPostalCode((String) get(POSTAL_CODE).getValue());
+		address.setCountryOrRegion((String) get(COUNTRY).getValue());
+		address.setStateOrProvinence((String) get(STATE).getValue());
+		preferences.setTradingAddress(address);
 		savePreferences(context, preferences);
 		return null;
 	}
