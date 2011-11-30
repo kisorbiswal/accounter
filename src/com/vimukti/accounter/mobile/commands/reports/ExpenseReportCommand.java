87fc374dc5a18d65e2e48999bb4039a0fed142d1
Syamalamba P <syamalamba.p@vimukti.com>
2011-11-30 13:41:11 +0000

    Former-commit-id: c5be902f36754f3a533e66766674fedb66c07756

--- src/com/vimukti/accounter/mobile/commands/reports/ExpenseReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/ExpenseReportCommand.java
@@ -2,12 +2,18 @@ package com.vimukti.accounter.mobile.commands.reports;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.vimukti.accounter.core.Utility;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.ReportResultRequirement;
 import com.vimukti.accounter.mobile.requirements.StringListRequirement;
 import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.web.client.core.ClientTransaction;
@@ -25,12 +31,12 @@ public class ExpenseReportCommand extends NewAbstractReportCommand<ExpenseList> 
 
 			@Override
 			protected String getSetMessage() {
-				return "Expense type selected";
+				return null;
 			}
 
 			@Override
 			protected String getSelectString() {
-				return "Please select any one of the expense type";
+				return null;
 			}
 
 			@Override
@@ -47,11 +53,48 @@ public class ExpenseReportCommand extends NewAbstractReportCommand<ExpenseList> 
 			}
 		});
 		addDateRangeFromToDateRequirements(list);
+		list.add(new ReportResultRequirement<ExpenseList>() {
+
+			@Override
+			protected String onSelection(ExpenseList selection, String name) {
+				return "update transaction " + selection.getTransactionId();
+			}
+
+			@Override
+			protected void fillResult(Context context, Result makeResult) {
+				List<ExpenseList> records = getRecords();
+				Map<String, List<ExpenseList>> recordGroups = new HashMap<String, List<ExpenseList>>();
+				for (ExpenseList transactionDetailByAccount : records) {
+					String taxItemName = transactionDetailByAccount.getName();
+					List<ExpenseList> group = recordGroups.get(taxItemName);
+					if (group == null) {
+						group = new ArrayList<ExpenseList>();
+						recordGroups.put(taxItemName, group);
+					}
+					group.add(transactionDetailByAccount);
+				}
+
+				Set<String> keySet = recordGroups.keySet();
+				for (String accountName : keySet) {
+					List<ExpenseList> group = recordGroups.get(accountName);
+					addSelection(accountName);
+					double totalAmount = 0.0;
+					ResultList resultList = new ResultList(accountName);
+					resultList.setTitle(accountName);
+					for (ExpenseList rec : group) {
+						totalAmount += rec.getTotal();
+						Record createReportRecord = createReportRecord(rec);
+						resultList.add(createReportRecord);
+					}
+					makeResult.add(resultList);
+					makeResult.add("" + totalAmount);
+				}
+			}
+		});
 	}
 
 	protected Record createReportRecord(ExpenseList record) {
 		Record expenseRecord = new Record(record);
-
 		expenseRecord.add(getMessages().transactionName(),
 				Utility.getTransactionName(record.getTransactionType()));
 		expenseRecord.add(getMessages().transactionDate(),
