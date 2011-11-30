5583594ba268f11ea07a5f596fef841e604741d3
Syamalamba P <syamalamba.p@vimukti.com>
2011-11-30 07:31:15 +0000

    Former-commit-id: 5f8aaa01c3785cafee137f27c6d62a7f8e34a455

--- src/com/vimukti/accounter/mobile/commands/reports/TransactionDetailbyTaxItemReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/TransactionDetailbyTaxItemReportCommand.java
@@ -1,15 +1,20 @@
 package com.vimukti.accounter.mobile.commands.reports;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import com.vimukti.accounter.core.Utility;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 import com.vimukti.accounter.mobile.requirements.ReportResultRequirement;
 import com.vimukti.accounter.web.client.core.reports.TransactionDetailByTaxItem;
-import com.vimukti.accounter.web.client.ui.core.ReportUtility;
 import com.vimukti.accounter.web.server.FinanceTool;
 
 public class TransactionDetailbyTaxItemReportCommand extends
@@ -30,16 +35,47 @@ public class TransactionDetailbyTaxItemReportCommand extends
 			@Override
 			protected void fillResult(Context context, Result makeResult) {
 				List<TransactionDetailByTaxItem> records = getRecords();
+				Map<String, List<TransactionDetailByTaxItem>> recordGroups = new HashMap<String, List<TransactionDetailByTaxItem>>();
+				for (TransactionDetailByTaxItem transactionDetailByTaxItem : records) {
+					String taxItemName = transactionDetailByTaxItem
+							.getTaxItemName();
+					List<TransactionDetailByTaxItem> group = recordGroups
+							.get(taxItemName);
+					if (group == null) {
+						group = new ArrayList<TransactionDetailByTaxItem>();
+						recordGroups.put(taxItemName, group);
+					}
+					group.add(transactionDetailByTaxItem);
+				}
+
+				Set<String> keySet = recordGroups.keySet();
+				List<String> taxItems = new ArrayList<String>(keySet);
+				Collections.sort(taxItems);
+
+				for (String tax : taxItems) {
+					List<TransactionDetailByTaxItem> group = recordGroups
+							.get(tax);
+					double salesTaxTotal = 0.0;
+					double taxableAmount = 0.0;
+					addSelection(tax);
+					ResultList resultList = new ResultList(tax);
+					for (TransactionDetailByTaxItem rec : group) {
+						resultList.setTitle(rec.getTaxAgencyName() + "-" + tax);
+						resultList.add(createReportRecord(rec));
+						salesTaxTotal += rec.getSalesTaxAmount();
+						taxableAmount += rec.getTaxableAmount();
+					}
+					makeResult.add(resultList);
+					makeResult.add("Sales Tax Total: " + salesTaxTotal);
+					makeResult.add("Taxable amount Total: " + taxableAmount);
+				}
 			}
 		});
 	}
 
 	protected Record createReportRecord(TransactionDetailByTaxItem record) {
-
 		Record tdRecord = new Record(record);
-
-		tdRecord.add(ReportUtility.getTransactionName(record
-				.getTransactionType()));
+		tdRecord.add(Utility.getTransactionName(record.getTransactionType()));
 		tdRecord.add(getMessages().taxRate(), record.getRate());
 		tdRecord.add(getMessages().date(), record.getDate());
 		tdRecord.add(getMessages().number(), record.getNumber());
@@ -47,20 +83,13 @@ public class TransactionDetailbyTaxItemReportCommand extends
 		tdRecord.add(getMessages().memo(), record.getMemo());
 		tdRecord.add(getMessages().salesTax(), record.getSalesTaxAmount());
 		tdRecord.add(getMessages().taxable(), record.getTaxableAmount());
-
 		return tdRecord;
-	}
-
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	protected List<TransactionDetailByTaxItem> getRecords() {
 		ArrayList<TransactionDetailByTaxItem> transactionDetailByTaxItems = new ArrayList<TransactionDetailByTaxItem>();
 		try {
-			if (taxItemName == null && taxItemName.isEmpty()) {
+			if (taxItemName == null || taxItemName.isEmpty()) {
 				transactionDetailByTaxItems = new FinanceTool()
 						.getReportManager().getTransactionDetailByTaxItem(
 								getStartDate(), getEndDate(), getCompanyId());
@@ -85,6 +114,7 @@ public class TransactionDetailbyTaxItemReportCommand extends
 	@Override
 	protected String initObject(Context context, boolean isUpdate) {
 		String string = context.getString();
+		dateRangeChanged(getMessages().financialYearToDate());
 		if (string != null && !string.isEmpty()) {
 			String[] split = string.split(",");
 			context.setString(split[0]);
