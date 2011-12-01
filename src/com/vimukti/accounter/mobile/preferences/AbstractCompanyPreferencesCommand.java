d1abf8c6df4c524f83b85cb9bfd0a50fc3475f0e
Nagasuresh A <nagasuresh.a@vimukti.com>
2011-12-01 04:38:15 +0000

    Former-commit-id: 61bdc2c01f2f0d8e7f8f12b6e13831d902418436

--- src/com/vimukti/accounter/mobile/preferences/AbstractCompanyPreferencesCommand.java
+++ src/com/vimukti/accounter/mobile/preferences/AbstractCompanyPreferencesCommand.java
@@ -1,9 +1,6 @@
 package com.vimukti.accounter.mobile.preferences;
 
-import java.util.List;
-
 import com.vimukti.accounter.mobile.Context;
-import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.commands.NewAbstractCommand;
 import com.vimukti.accounter.web.client.core.ClientCompany;
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
@@ -12,25 +9,8 @@ import com.vimukti.accounter.web.server.FinanceTool;
 import com.vimukti.accounter.web.server.OperationContext;
 import com.vimukti.accounter.web.server.managers.CompanyManager;
 
-public class AbstractCompanyPreferencesCommand extends NewAbstractCommand {
-
-	@Override
-	protected String initObject(Context context, boolean isUpdate) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected String getWelcomeMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	protected String getDetailsMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public abstract class AbstractCompanyPreferencesCommand extends
+		NewAbstractCommand {
 
 	@Override
 	protected void setDefaultValues(Context context) {
@@ -39,21 +19,9 @@ public class AbstractCompanyPreferencesCommand extends NewAbstractCommand {
 	}
 
 	@Override
-	public String getSuccessMessage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String getId() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	protected void addRequirements(List<Requirement> list) {
-		// TODO Auto-generated method stub
-
 	}
 
 	protected void savePreferences(Context context,
