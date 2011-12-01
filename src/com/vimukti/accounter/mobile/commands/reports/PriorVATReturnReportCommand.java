74625f85f991d49fa3b1d8d11a2af69a9108a42e
Syamalamba P <syamalamba.p@vimukti.com>
2011-12-01 10:32:46 +0000

    Former-commit-id: 4b2c845034596f10c9a52eb0f5fee9b86d7c7128

--- src/com/vimukti/accounter/mobile/commands/reports/PriorVATReturnReportCommand.java
+++ src/com/vimukti/accounter/mobile/commands/reports/PriorVATReturnReportCommand.java
@@ -1,36 +1,34 @@
 package com.vimukti.accounter.mobile.commands.reports;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.vimukti.accounter.core.TAXAgency;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
-import com.vimukti.accounter.mobile.requirements.ChangeListner;
+import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
+import com.vimukti.accounter.mobile.requirements.ReportResultRequirement;
 import com.vimukti.accounter.mobile.requirements.TaxAgencyRequirement;
+import com.vimukti.accounter.web.client.core.reports.ExpenseList;
 import com.vimukti.accounter.web.client.core.reports.VATSummary;
 import com.vimukti.accounter.web.server.FinanceTool;
 
 public class PriorVATReturnReportCommand extends
 		NewAbstractReportCommand<VATSummary> {
-	private TAXAgency agency;
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
 		list.add(new TaxAgencyRequirement(TAX_AGENCY, getMessages()
-				.pleaseEnter(getMessages().taxAgencie()), getMessages()
-				.taxAgencie(), false, true, new ChangeListner<TAXAgency>() {
-
-			@Override
-			public void onSelection(TAXAgency value) {
-				agency = value;
-			}
-		}) {
+				.pleaseEnter(getMessages().taxAgency()), getMessages()
+				.taxAgency(), false, true, null) {
 
 			@Override
 			protected String getSetMessage() {
-				// TODO Auto-generated method stub
 				return null;
 			}
 
@@ -41,39 +39,69 @@ public class PriorVATReturnReportCommand extends
 
 			@Override
 			protected String getEmptyString() {
-				// TODO Auto-generated method stub
 				return null;
 			}
 
 			@Override
 			protected boolean filter(TAXAgency e, String name) {
-				// TODO Auto-generated method stub
 				return false;
 			}
 		});
 		addFromToDateRequirements(list);
+		list.add(new ReportResultRequirement<ExpenseList>() {
+
+			@Override
+			protected String onSelection(ExpenseList selection, String name) {
+				return "update transaction " + selection.getTransactionId();
+			}
+
+			@Override
+			protected void fillResult(Context context, Result makeResult) {
+				List<VATSummary> records = getRecords();
+				if (records.isEmpty()) {
+					makeResult.add("No Records to show");
+					return;
+				}
+				Map<String, List<VATSummary>> recordGroups = new HashMap<String, List<VATSummary>>();
+				for (VATSummary transactionDetailByAccount : records) {
+					String taxItemName = transactionDetailByAccount.getName();
+					List<VATSummary> group = recordGroups.get(taxItemName);
+					if (group == null) {
+						group = new ArrayList<VATSummary>();
+						recordGroups.put(taxItemName, group);
+					}
+					group.add(transactionDetailByAccount);
+				}
+
+				Set<String> keySet = recordGroups.keySet();
+				for (String accountName : keySet) {
+					List<VATSummary> group = recordGroups.get(accountName);
+					addSelection(accountName);
+					ResultList resultList = new ResultList(accountName);
+					resultList.setTitle(accountName);
+					for (VATSummary rec : group) {
+						resultList.add(createReportRecord(rec));
+					}
+					makeResult.add(resultList);
+				}
+			}
+		});
 	}
 
-	protected Record createReportRecord(VATSummary record) {
+	private Record createReportRecord(VATSummary record) {
 		Record vatRecord = new Record(record);
 		vatRecord.add(record.getName());
-		vatRecord.add(getEndDate() + "", record.getValue());
+		vatRecord.add("", record.getValue());
 		return vatRecord;
 	}
 
-	@Override
-	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	protected List<VATSummary> getRecords() {
+	private List<VATSummary> getRecords() {
 		ArrayList<VATSummary> vatSummaries = new ArrayList<VATSummary>();
+		TAXAgency agency = get(TAX_AGENCY).getValue();
 		try {
 			vatSummaries = new FinanceTool().getTaxManager()
 					.getPriorReturnVATSummary(agency, getEndDate(),
 							getCompanyId());
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -82,7 +110,6 @@ public class PriorVATReturnReportCommand extends
 
 	@Override
 	protected String initObject(Context context, boolean isUpdate) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
