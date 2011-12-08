e50003b70d260e1acf1e91b1201612ab02aea04f
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-08 12:47:30 +0000

    Former-commit-id: bae46a5e8201bc057d162a728e6921eca24be57b

--- src/com/vimukti/accounter/mobile/requirements/EstimatesAndSalesOrderTableRequirement.java
+++ src/com/vimukti/accounter/mobile/requirements/EstimatesAndSalesOrderTableRequirement.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import com.vimukti.accounter.core.Customer;
 import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Record;
 import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.Result;
+import com.vimukti.accounter.mobile.ResultList;
 import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.web.client.core.ClientTransaction;
 import com.vimukti.accounter.web.client.core.Lists.EstimatesAndSalesOrdersList;
@@ -17,7 +20,7 @@ public abstract class EstimatesAndSalesOrderTableRequirement extends
 
 	public EstimatesAndSalesOrderTableRequirement(String requirementName,
 			String enterString, String recordName) {
-		super(requirementName, enterString, recordName, false, false, true);
+		super(requirementName, enterString, recordName, false, true, true);
 		// setDefaultValue(new ArrayList<EstimatesAndSalesOrdersList>());
 	}
 
@@ -49,6 +52,17 @@ public abstract class EstimatesAndSalesOrderTableRequirement extends
 	@Override
 	protected void addCreateCommands(CommandList commandList) {
 
+	}
+
+	@Override
+	public Result run(Context context, Result makeResult, ResultList list,
+			ResultList actions) {
+		List<EstimatesAndSalesOrdersList> list2 = getList();
+		if (!list2.isEmpty()) {
+			return super.run(context, makeResult, list, actions);
+		} else {
+			return null;
+		}
 	}
 
 	@Override
