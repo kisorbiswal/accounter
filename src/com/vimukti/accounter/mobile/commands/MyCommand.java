e1e03a91eb170a09f13eb9958a100f3905036502
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-08 09:06:08 +0000

    Former-commit-id: aed3288be08d96ac089f704494c6b5dbcfc29622

--- src/com/vimukti/accounter/mobile/commands/MyCommand.java
+++ src/com/vimukti/accounter/mobile/commands/MyCommand.java
@@ -2,13 +2,9 @@ package com.vimukti.accounter.mobile.commands;
 
 import java.util.List;
 
-import com.vimukti.accounter.core.Address;
 import com.vimukti.accounter.mobile.Context;
 import com.vimukti.accounter.mobile.Requirement;
 import com.vimukti.accounter.mobile.Result;
-import com.vimukti.accounter.mobile.requirements.MultiRequirement;
-import com.vimukti.accounter.mobile.requirements.NameRequirement;
-import com.vimukti.accounter.mobile.requirements.StringRequirement;
 
 public class MyCommand extends NewAbstractCommand {
 
@@ -19,39 +15,7 @@ public class MyCommand extends NewAbstractCommand {
 
 	@Override
 	protected void addRequirements(List<Requirement> list) {
-		list.add(new NameRequirement("NAME", "PLease enter name", "Name",
-				false, true));
-		list.add(new MultiRequirement<Address>("Address", "Please Enter Addr",
-				"Address", true, true) {
 
-			@Override
-			protected void addRequirement(List<Requirement> list) {
-				list.add(new StringRequirement("Streat", "Please enter Streat",
-						"Streat", false, true));
-				list.add(new StringRequirement("City", "Please enter City",
-						"City", false, true));
-				list.add(new StringRequirement("Village",
-						"Please enter Village", "Village", true, true));
-			}
-
-			@Override
-			protected void setDefaultValues() {
-
-			}
-
-			@Override
-			protected String getDisplayValue() {
-				return getRequirement("Streat").getValue() + "-"
-						+ getRequirement("City").getValue() + "-"
-						+ getRequirement("Village").getValue();
-			}
-
-			@Override
-			protected Result finish(Context context) {
-				return null;
-			}
-
-		});
 	}
 
 	@Override
