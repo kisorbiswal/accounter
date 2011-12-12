d210cf4e729283c5b6af0a7c83d0c92c37466f6d
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-12 06:07:11 +0000

    Former-commit-id: bd9c6a01bae96ca3ea520e2104f9ead1f9137779

--- src/com/vimukti/accounter/main/AccounterSessionListner.java
+++ src/com/vimukti/accounter/main/AccounterSessionListner.java
@@ -4,28 +4,32 @@ import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+import org.apache.log4j.Logger;
+
 import com.vimukti.accounter.servlets.BaseServlet;
 import com.vimukti.accounter.web.server.CometManager;
 
-public class AccounterSessionListner implements HttpSessionListener
+public class AccounterSessionListner implements HttpSessionListener {
+	protected Logger log = Logger.getLogger(AccounterSessionListner.class);
 
-{
 	public void sessionCreated(HttpSessionEvent sessionEvent) {
 		HttpSession httpSession = sessionEvent.getSession();
 		System.out.println("session for corresponding id "
 				+ sessionEvent.getSession().getId());
-		httpSession.setMaxInactiveInterval(120);
+		httpSession.setMaxInactiveInterval(60);
 
 	}
 
 	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
 		// This method will be called just before session is to be destroyed
+		log.info("Session expired");
 
 		HttpSession httpSession = sessionEvent.getSession();
 		Long companyId = (Long) httpSession
 				.getAttribute(BaseServlet.COMPANY_ID);
 		String userID = (String) httpSession.getAttribute("userID");
 		if (userID != null) {
+			log.info("Session expired, Comet destroyed");
 			// session time out
 			CometManager.destroyStream(httpSession.getId(), companyId, userID);
 		}
