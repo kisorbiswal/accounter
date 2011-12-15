e7e27a3655ec160f2b4c2584c7fb04fe8f1e78d9
srinivas.g <srinivas.g@vimukti.com>
2011-12-15 13:07:28 +0000

    Former-commit-id: 394009015af086771295fe90a8815967dc08f9f3

--- src/com/vimukti/accounter/company/initialize/CompanyInitializer.java
+++ src/com/vimukti/accounter/company/initialize/CompanyInitializer.java
@@ -253,7 +253,7 @@ public abstract class CompanyInitializer {
 		BrandingTheme brandingTheme = new BrandingTheme("Standard",
 				SecureUtils.createID(), 1.35, 1.00, 1.00, "Times New Roman",
 				"10pt", "INVOICE", "CREDIT", "STATEMENT", "(None Added)", true,
-				"(None Added)", "(None Added)", "Classic Tempalate",
+				"(None Added)", "(None Added)", "Classic Template",
 				"Classic Template");
 		brandingTheme.setCompany(company);
 		session.save(brandingTheme);
