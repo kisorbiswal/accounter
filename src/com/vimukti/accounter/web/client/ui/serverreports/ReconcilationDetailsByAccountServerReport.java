ccb784a7b38f4bac8b5c76e042b5a458341f455f
Lingarao R <lingarao.r@vimukti.com>
2011-12-17 10:22:10 +0000

    Former-commit-id: a4f6d36174171903c4b2561ba6a749e2f32a002b

---
+++ src/com/vimukti/accounter/web/client/ui/serverreports/ReconcilationDetailsByAccountServerReport.java
@@ -0,0 +1,158 @@
+package com.vimukti.accounter.web.client.ui.serverreports;
+
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.client.core.reports.BaseReport;
+import com.vimukti.accounter.web.client.core.reports.ReconcilationItemList;
+import com.vimukti.accounter.web.client.ui.core.DecimalUtil;
+import com.vimukti.accounter.web.client.ui.core.ReportUtility;
+import com.vimukti.accounter.web.client.ui.reports.IFinanceReport;
+
+public class ReconcilationDetailsByAccountServerReport extends
+		AbstractFinaneReport<ReconcilationItemList> {
+	private String sectionName = "";
+
+	public ReconcilationDetailsByAccountServerReport(
+			IFinanceReport<ReconcilationItemList> reconcilationsReport,
+			long bankAccountId) {
+		this.reportView = reconcilationsReport;
+
+	}
+
+	public ReconcilationDetailsByAccountServerReport(long startDate,
+			long endDate, int generationType) {
+		super(startDate, endDate, generationType);
+	}
+
+	@Override
+	public String[] getDynamicHeaders() {
+		return new String[] { "", messages.date(), messages.name(),
+				messages.number(), messages.debit(), messages.credit() };
+	}
+
+	@Override
+	public String getTitle() {
+		return "ReconcilationDetailsByAccount";
+	}
+
+	@Override
+	public String[] getColunms() {
+		return new String[] { "", messages.date(), messages.name(),
+				messages.number(), messages.debit(), messages.credit() };
+	}
+
+	@Override
+	public int[] getColumnTypes() {
+
+		return new int[] { COLUMN_TYPE_TEXT, COLUMN_TYPE_DATE,
+				COLUMN_TYPE_TEXT, COLUMN_TYPE_NUMBER, COLUMN_TYPE_AMOUNT,
+				COLUMN_TYPE_AMOUNT };
+
+	}
+
+	@Override
+	public int getColumnWidth(int index) {
+		switch (index) {
+		case 0:
+			return 100;
+		case 1:
+			return 75;
+		case 3:
+			return 100;
+		case 4:
+			return 80;
+		case 5:
+			return 80;
+		case 6:
+			return 80;
+		default:
+			return -1;
+		}
+	}
+
+	@Override
+	public boolean isWiderReport() {
+		return true;
+	}
+
+	@Override
+	public void resetVariables() {
+		this.sectionDepth = 0;
+		this.sectionName = "";
+	}
+
+	@Override
+	public void processRecord(ReconcilationItemList record) {
+
+		if (sectionDepth == 0) {
+			this.sectionName = record.getBankAccountName();
+			addSection(new String[] { sectionName }, new String[] { "" },
+					new int[] { 4, 5 });
+		} else if (sectionDepth == 1) {
+			if (!sectionName.equals(record.getBankAccountName())) {
+				endSection();
+			} else {
+				return;
+			}
+		}
+		processRecord(record);
+	}
+
+	@Override
+	public Object getColumnData(ReconcilationItemList record, int columnIndex) {
+
+		switch (columnIndex) {
+		case 0:
+			return "";
+		case 1:
+			return getDateByCompanyType(record.getTransactionDate());
+		case 2:
+			return ReportUtility.getTransactionName(record.getTransationType());
+		case 3:
+			return record.getTransactionNo();
+		case 4:
+			if (DecimalUtil.isLessThan(record.getAmount(), 0.00D)) {
+				return Math.abs(record.getAmount());
+			}
+			return null;
+		case 5:
+			if (DecimalUtil.isGreaterThan(record.getAmount(), 0.00D)) {
+				return Math.abs(record.getAmount());
+			}
+			return null;
+		}
+		return null;
+
+	}
+
+	@Override
+	public ClientFinanceDate getStartDate(ReconcilationItemList obj) {
+		return obj.getStartDate();
+	}
+
+	@Override
+	public ClientFinanceDate getEndDate(ReconcilationItemList obj) {
+		return obj.getEndDate();
+	}
+
+	@Override
+	protected String getPreviousReportDateRange(Object object) {
+		return ((BaseReport) object).getDateRange();
+	}
+
+	@Override
+	protected ClientFinanceDate getPreviousReportStartDate(Object object) {
+		return ((BaseReport) object).getStartDate();
+	}
+
+	@Override
+	protected ClientFinanceDate getPreviousReportEndDate(Object object) {
+		return ((BaseReport) object).getEndDate();
+	}
+
+	@Override
+	public void makeReportRequest(long start, long end) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/vimukti/accounter/web/client/ui/serverreports/ReconcilationsServerReport.java
@@ -0,0 +1,92 @@
+package com.vimukti.accounter.web.client.ui.serverreports;
+
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
+import com.vimukti.accounter.web.client.core.Utility;
+import com.vimukti.accounter.web.client.core.reports.Reconciliation;
+import com.vimukti.accounter.web.client.ui.reports.IFinanceReport;
+
+public class ReconcilationsServerReport extends
+		AbstractFinaneReport<Reconciliation> {
+
+	public ReconcilationsServerReport(
+			IFinanceReport<Reconciliation> reconcilationsReport) {
+		this.reportView = reconcilationsReport;
+	}
+
+	public ReconcilationsServerReport(long startDate, long endDate,
+			int generationType) {
+		super(startDate, endDate, generationType);
+	}
+
+	@Override
+	public String[] getDynamicHeaders() {
+		return new String[] { messages.Account(), messages.accountType(),
+				messages.startDate(), "Last ReconcilationDate" };
+	}
+
+	@Override
+	public String getTitle() {
+		return "Reconciliations";
+	}
+
+	@Override
+	public int getColumnWidth(int index) {
+		if (index == 1)
+			return 160;
+		else if (index == 0)
+			return 70;
+		else if (index == 2)
+			return 70;
+		else if (index == 3)
+			return 100;
+		else
+			return -1;
+	}
+
+	@Override
+	public String[] getColunms() {
+		return new String[] { messages.Account(), messages.accountType(),
+				messages.startDate(), "Last ReconcilationDate" };
+	}
+
+	@Override
+	public int[] getColumnTypes() {
+		return new int[] { COLUMN_TYPE_NUMBER, COLUMN_TYPE_NUMBER,
+				COLUMN_TYPE_DATE, COLUMN_TYPE_DATE };
+	}
+
+	@Override
+	public void processRecord(Reconciliation record) {
+		addSection(new String[] {}, new String[] { "" }, new int[] {});
+	}
+
+	@Override
+	public Object getColumnData(Reconciliation record, int columnIndex) {
+		switch (columnIndex) {
+		case 0:
+			return record.getAccountName();
+		case 1:
+			return Utility.getAccountTypeString(record.getAccountType());
+		case 2:
+			return record.getStatementdate();
+		case 3:
+			return record.getReconcilationdate();
+		}
+		return null;
+	}
+
+	@Override
+	public ClientFinanceDate getStartDate(Reconciliation obj) {
+		return obj.getStartDate();
+	}
+
+	@Override
+	public ClientFinanceDate getEndDate(Reconciliation obj) {
+		return obj.getEndDate();
+	}
+
+	@Override
+	public void makeReportRequest(long start, long end) {
+
+	}
+}
