883c85f864d5d7e4188127cd1899491e34604d07
Praveena A <praveena.a@vimukti.com>
2011-12-19 07:36:16 +0000

    Former-commit-id: be5b1d67df09781c37800730c3fa69e18033061f

--- src/com/vimukti/accounter/web/client/ui/edittable/TextAreaEditColumn.java
+++ src/com/vimukti/accounter/web/client/ui/edittable/TextAreaEditColumn.java
@@ -1,7 +1,7 @@
 package com.vimukti.accounter.web.client.ui.edittable;
 
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBoxBase;
@@ -11,20 +11,31 @@ public abstract class TextAreaEditColumn<T> extends TextEditColumn<T> {
 	@Override
 	protected TextBoxBase createWidget() {
 		final TextArea textArea = new TextArea();
-		textArea.addKeyUpHandler(new KeyUpHandler() {
+		textArea.addKeyDownHandler(new KeyDownHandler() {
 
 			@Override
-			public void onKeyUp(KeyUpEvent event) {
+			public void onKeyDown(KeyDownEvent event) {
+				textArea.setHeight("2px");
 				updateHeight(textArea.getElement());
 			}
 		});
+		// textArea.addKeyUpHandler(new KeyUpHandler() {
+		//
+		// @Override
+		// public void onKeyUp(KeyUpEvent event) {
+		// updateHeight(textArea.getElement());
+		// }
+		//
+		// });
 
 		return textArea;
 	}
 
 	private native void updateHeight(Element text)/*-{
-		if (text.scrollHeight > text.clientHeight)
+		if (text.scrollHeight > text.clientHeight) {
 			text.style.height = text.scrollHeight + "px";
+		}
+
 	}-*/;
 
 }
