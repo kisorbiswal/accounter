2794d31c1c8ca3dae51b4b774edecda27b45af5c
Syamalamba P <syamalamba.p@vimukti.com>
2011-12-20 10:02:31 +0000

    Former-commit-id: 7f2628550e6a4350f2569ac7f420a325fcb95d1a

--- src/com/vimukti/accounter/mobile/commands/delete/CustomerGroupDeleteCommand.java
+++ src/com/vimukti/accounter/mobile/commands/delete/CustomerGroupDeleteCommand.java
@@ -1,13 +1,23 @@
 package com.vimukti.accounter.mobile.commands.delete;
 
 import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.web.client.core.AccounterCoreType;
+import com.vimukti.accounter.web.client.exception.AccounterException;
 
 public class CustomerGroupDeleteCommand extends AbstractDeleteCommand {
 
 	@Override
 	protected String initObject(Context context, boolean isUpdate) {
-		// TODO Auto-generated method stub
-		return null;
+		long customerGroupId = Long.parseLong(context.getString());
+
+		try {
+			delete(AccounterCoreType.CUSTOMER_GROUP, customerGroupId, context);
+		} catch (AccounterException e) {
+			addFirstMessage(context,
+					getMessages().payeeInUse(getMessages().account()));
+		}
+		return "customergroups";
+
 	}
 
 }
