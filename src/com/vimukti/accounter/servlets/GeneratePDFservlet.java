be3f74dcccccdb6e46224e3d06c580abc157f26d
srinivas.g <srinivas.g@vimukti.com>
2011-12-23 04:07:45 +0000

    Former-commit-id: 8d951cf7b4f9506e6e24e8e124e6fd4217a6f3c1

--- src/com/vimukti/accounter/servlets/GeneratePDFservlet.java
+++ src/com/vimukti/accounter/servlets/GeneratePDFservlet.java
@@ -9,7 +9,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -19,6 +23,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
 import org.zefer.pd4ml.PD4Constants;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfWriter;
 import com.vimukti.accounter.core.BrandingTheme;
 import com.vimukti.accounter.core.Company;
 import com.vimukti.accounter.core.CreditNotePDFTemplete;
@@ -216,6 +226,8 @@ public class GeneratePDFservlet extends BaseServlet {
 			switch (transactionType) {
 			// for invoice
 			case Transaction.TYPE_INVOICE:
+			case Transaction.TYPE_CUSTOMER_CREDIT_MEMO:
+			case Transaction.TYPE_ESTIMATE:
 
 				String output = outPutString.toString().replaceAll("</html>",
 						"");
@@ -227,36 +239,7 @@ public class GeneratePDFservlet extends BaseServlet {
 				InputStreamReader reader = new InputStreamReader(inputStream);
 				converter.generatePdfDocuments(printTemplete, sos, reader);
 				break;
-			// for credit note
-			case Transaction.TYPE_CUSTOMER_CREDIT_MEMO:
-				String creditOutput = outPutString.toString().replaceAll(
-						"</html>", "");
-				creditOutput = creditOutput.toString().replaceAll("<html>", "");
 
-				creditOutput = "<html>" + creditOutput + "</html>";
-
-				java.io.InputStream inputStr = new ByteArrayInputStream(
-						creditOutput.toString().getBytes());
-				InputStreamReader creditReader = new InputStreamReader(inputStr);
-				converter
-						.generatePdfDocuments(printTemplete, sos, creditReader);
-				break;
-			// for Estimate
-			case Transaction.TYPE_ESTIMATE:
-
-				String estimateOutput = outPutString.toString().replaceAll(
-						"</html>", "");
-				estimateOutput = estimateOutput.toString().replaceAll("<html>",
-						"");
-
-				output = "<html>" + estimateOutput + "</html>";
-				java.io.InputStream estimateInputStream = new ByteArrayInputStream(
-						output.getBytes());
-				InputStreamReader estimateReader = new InputStreamReader(
-						estimateInputStream);
-				converter.generatePdfDocuments(printTemplete, sos,
-						estimateReader);
-				break;
 			default:
 				// for generating pdf document for reports
 				converter.generatePdfReports(template, sos);
@@ -276,7 +259,7 @@ public class GeneratePDFservlet extends BaseServlet {
 		}
 	}
 
-	public void generateODT2PDF(HttpServletRequest request,
+	public void generateCustom2PDF(HttpServletRequest request,
 			HttpServletResponse response, String companyName) {
 		HashMap map = null;
 		ITemplate template = null;
@@ -285,6 +268,8 @@ public class GeneratePDFservlet extends BaseServlet {
 		int transactionType;
 		PrintTemplete printTemplete = null;
 		ServletOutputStream sos = null;
+		boolean isMultipleId = false;
+		List<String> fileNames = new ArrayList<String>();
 		try {
 
 			String footerImg = ("war" + File.separator + "images"
@@ -308,6 +293,7 @@ public class GeneratePDFservlet extends BaseServlet {
 
 				String objectId = request.getParameter("objectId");
 				if (request.getParameter("multipleIds") != null) {
+					isMultipleId = true;
 					objectId = request.getParameter("multipleIds");
 				}
 
@@ -345,7 +331,7 @@ public class GeneratePDFservlet extends BaseServlet {
 							fileName = "Invoice_" + invoice.getNumber();
 
 							map = Odt2PdfGeneration(invoice, company,
-									brandingTheme);
+									brandingTheme, isMultipleId, fileNames);
 
 						}
 						if (transactionType == Transaction.TYPE_CUSTOMER_CREDIT_MEMO) {
@@ -356,7 +342,7 @@ public class GeneratePDFservlet extends BaseServlet {
 											Long.parseLong(ids[i]));
 							fileName = "CreditNote" + memo.getNumber();
 							map = Odt2PdfGeneration(memo, company,
-									brandingTheme);
+									brandingTheme, isMultipleId, fileNames);
 						}
 						if (transactionType == Transaction.TYPE_ESTIMATE) {
 							Estimate estimate = (Estimate) financetool
@@ -365,7 +351,7 @@ public class GeneratePDFservlet extends BaseServlet {
 											Long.parseLong(ids[i]));
 							fileName = "Quote" + estimate.getNumber();
 							map = Odt2PdfGeneration(estimate, company,
-									brandingTheme);
+									brandingTheme, isMultipleId, fileNames);
 						}
 
 					}
@@ -393,13 +379,17 @@ public class GeneratePDFservlet extends BaseServlet {
 				case Transaction.TYPE_CUSTOMER_CREDIT_MEMO:
 				case Transaction.TYPE_ESTIMATE:
 
-					IContext context = (IContext) map.get("context");
-					IXDocReport report = (IXDocReport) map.get("report");
+					if (isMultipleId) {// for merging multiple custom pdf
+										// documents
+						mergePDFDocuments(fileNames, sos, true);
+					} else {
+						IContext context = (IContext) map.get("context");
+						IXDocReport report = (IXDocReport) map.get("report");
 
-					Options options = Options.getTo(ConverterTypeTo.PDF).via(
-							ConverterTypeVia.ITEXT);
-					report.convert(context, options, sos);
-
+						Options options = Options.getTo(ConverterTypeTo.PDF)
+								.via(ConverterTypeVia.ITEXT);
+						report.convert(context, options, sos);
+					}
 					break;
 
 				default:
@@ -479,7 +469,7 @@ public class GeneratePDFservlet extends BaseServlet {
 
 			if (brandingTheme.isCustomFile()) {
 				// for genearting reports using XdocReport
-				generateODT2PDF(request, response, companyName);
+				generateCustom2PDF(request, response, companyName);
 			} else {
 				// for generating reports using html templates
 				generateHtmlPDF(request, response, companyName);
@@ -487,7 +477,7 @@ public class GeneratePDFservlet extends BaseServlet {
 
 		} else {
 			// for all kinds of other reports
-			generateODT2PDF(request, response, companyName);
+			generateCustom2PDF(request, response, companyName);
 		}
 
 	}
@@ -567,7 +557,8 @@ public class GeneratePDFservlet extends BaseServlet {
 	}
 
 	private HashMap Odt2PdfGeneration(Transaction transaction, Company company,
-			BrandingTheme brandingTheme) {
+			BrandingTheme brandingTheme, boolean multipleIds,
+			List<String> fileNames) {
 
 		try {
 
@@ -575,6 +566,7 @@ public class GeneratePDFservlet extends BaseServlet {
 			InvoicePdfGeneration invoicePdfGeneration = null;
 			QuotePdfGeneration quotePdfGeneration = null;
 			String templeteName = null;
+			String fileName = null;
 
 			if (transaction instanceof Invoice) {
 				// for Invoice
@@ -589,6 +581,7 @@ public class GeneratePDFservlet extends BaseServlet {
 							+ "/" + brandingTheme.getID() + "/"
 							+ brandingTheme.getInvoiceTempleteName();
 				}
+				fileName = "Invoice_" + transaction.getNumber();
 				invoicePdfGeneration = new InvoicePdfGeneration(
 						(Invoice) transaction, company, brandingTheme);
 			}
@@ -605,6 +598,7 @@ public class GeneratePDFservlet extends BaseServlet {
 							+ "/" + brandingTheme.getID() + "/"
 							+ brandingTheme.getCreditNoteTempleteName();
 				}
+				fileName = "CreditNote_" + transaction.getNumber();
 				creditPdfGeneration = new CreditNotePdfGeneration(
 						(CustomerCreditMemo) transaction, company,
 						brandingTheme);
@@ -621,6 +615,7 @@ public class GeneratePDFservlet extends BaseServlet {
 							+ "/" + brandingTheme.getID() + "/"
 							+ brandingTheme.getQuoteTemplateName();
 				}
+				fileName = "Quote_" + transaction.getNumber();
 				quotePdfGeneration = new QuotePdfGeneration(
 						(Estimate) transaction, company, brandingTheme);
 			}
@@ -639,10 +634,23 @@ public class GeneratePDFservlet extends BaseServlet {
 			} else if (transaction instanceof Estimate) {
 				context = quotePdfGeneration.assignValues(context, report);
 			}
+
+			if (multipleIds) {
+				Options options = Options.getTo(ConverterTypeTo.PDF).via(
+						ConverterTypeVia.ITEXT);
+
+				File file = File.createTempFile(fileName.replace(" ", ""),
+						".pdf");
+				java.io.FileOutputStream fos = new java.io.FileOutputStream(
+						file);
+				report.convert(context, options, fos);
+				fileNames.add(file.getAbsolutePath());
+
+			}
+
 			HashMap objects = new HashMap();
 			objects.put("context", context);
 			objects.put("report", report);
-
 			return objects;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -651,4 +659,92 @@ public class GeneratePDFservlet extends BaseServlet {
 		return null;
 
 	}
+
+	/**
+	 * for merging custom PDF Documents in to Single Pdf Document
+	 * 
+	 * @param fileNames
+	 * @param outPutFileName
+	 * @param paginate
+	 */
+	private void mergePDFDocuments(List<String> fileNames,
+			OutputStream outputStream, boolean paginate) {
+		Document document = new Document();
+		try {
+			List<InputStream> pdfs = new ArrayList<InputStream>();
+
+			Iterator<String> fileIterator = fileNames.iterator();
+			while (fileIterator.hasNext()) {
+				String next = fileIterator.next();
+				pdfs.add(new FileInputStream(next));
+			}
+
+			List<PdfReader> readers = new ArrayList<PdfReader>();
+			int totalPages = 0;
+			Iterator<InputStream> iteratorPDFs = pdfs.iterator();
+
+			// Create Readers for the pdfs.
+			while (iteratorPDFs.hasNext()) {
+				InputStream pdf = iteratorPDFs.next();
+				PdfReader pdfReader = new PdfReader(pdf);
+				readers.add(pdfReader);
+				totalPages += pdfReader.getNumberOfPages();
+			}
+			// Create a writer for the outputstream
+			PdfWriter writer = PdfWriter.getInstance(document, outputStream);
+
+			document.open();
+			BaseFont bf = BaseFont.createFont(BaseFont.HELVETICA,
+					BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+			PdfContentByte cb = writer.getDirectContent(); // Holds the PDF
+			// data
+
+			PdfImportedPage page;
+			int currentPageNumber = 0;
+			int pageOfCurrentReaderPDF = 0;
+			Iterator<PdfReader> iteratorPDFReader = readers.iterator();
+
+			// Loop through the PDF files and add to the output.
+			while (iteratorPDFReader.hasNext()) {
+				PdfReader pdfReader = iteratorPDFReader.next();
+
+				// Create a new page in the target for each source page.
+				while (pageOfCurrentReaderPDF < pdfReader.getNumberOfPages()) {
+					document.newPage();
+					pageOfCurrentReaderPDF++;
+					currentPageNumber++;
+					page = writer.getImportedPage(pdfReader,
+							pageOfCurrentReaderPDF);
+					cb.addTemplate(page, 0, 0);
+
+					// Code for pagination.
+					if (paginate) {
+						cb.beginText();
+						cb.setFontAndSize(bf, 9);
+						cb.showTextAligned(PdfContentByte.ALIGN_CENTER, ""
+								+ currentPageNumber + " of " + totalPages, 520,
+								5, 0);
+						cb.endText();
+					}
+				}
+				pageOfCurrentReaderPDF = 0;
+			}
+
+			outputStream.flush();
+			document.close();
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (document.isOpen())
+				document.close();
+			try {
+				if (outputStream != null)
+					outputStream.close();
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
+
 }
