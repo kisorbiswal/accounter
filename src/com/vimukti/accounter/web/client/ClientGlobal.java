7aad971580db33b3344ede18a4b29cea163ff2de
Rajesh Akkineni <rajesh@vimukti.com>
2011-12-23 06:48:01 +0000

    Former-commit-id: 80b69fb73b6cce7950e411e03565196d2fe4f551

--- src/com/vimukti/accounter/web/client/ClientGlobal.java
+++ src/com/vimukti/accounter/web/client/ClientGlobal.java
@@ -2,6 +2,7 @@ package com.vimukti.accounter.web.client;
 
 import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.externalization.AccounterMessages;
+import com.vimukti.accounter.web.client.i18n.AccounterNumberFormat;
 import com.vimukti.accounter.web.client.ui.Accounter;
 
 public class ClientGlobal extends AbstractGlobal {
@@ -18,16 +19,8 @@ public class ClientGlobal extends AbstractGlobal {
 
 	@Override
 	public String toCurrencyFormat(double amount) {
-		// NumberFormat nf = NumberFormat.getCurrencyFormat();
-		// String test = nf.getPattern();
-		int decimalNumber = preferences().getDecimalNumber();
-		String pattern = "#,##0.";
-		for (int i = 0; i < decimalNumber; i++) {
-			pattern = pattern + "0";
-		}
-		com.google.gwt.i18n.client.NumberFormat format = com.google.gwt.i18n.client.NumberFormat
-				.getFormat(pattern);
-		return format.format(amount);
+		AccounterNumberFormat nf = AccounterNumberFormat.getCurrencyFormat();
+		return nf.format(amount, null);
 	}
 
 }
