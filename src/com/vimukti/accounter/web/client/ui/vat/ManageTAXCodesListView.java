06fb6b361f70a41ebbeb0a302222460832d0bcbf
Devaraju K <devaraju.k@vimukti.com>
2011-12-24 05:40:18 +0000

    Former-commit-id: 8a5444b35f77f502c66dcfb251576eae92790b36

--- src/com/vimukti/accounter/web/client/ui/vat/ManageTAXCodesListView.java
+++ src/com/vimukti/accounter/web/client/ui/vat/ManageTAXCodesListView.java
@@ -3,7 +3,9 @@
  */
 package com.vimukti.accounter.web.client.ui.vat;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.vimukti.accounter.web.client.core.ClientTAXCode;
 import com.vimukti.accounter.web.client.core.PaginationList;
@@ -108,6 +110,31 @@ public class ManageTAXCodesListView extends BaseListView<ClientTAXCode> {
 	@Override
 	public void printPreview() {
 		// NOTHING TO DO.
+	}
+
+	@Override
+	public Map<String, Object> saveView() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		// map.put("isActive", isActiveAccounts);
+		map.put("start", start);
+		return map;
+	}
+
+	@Override
+	public void restoreView(Map<String, Object> viewDate) {
+
+		if (viewDate == null || viewDate.isEmpty()) {
+			return;
+		}
+		// isActiveAccounts = (Boolean) viewDate.get("isActive");
+		start = (Integer) viewDate.get("start");
+		onPageChange(start, getPageSize());
+		// if (isActiveAccounts) {
+		// viewSelect.setComboItem(messages().active());
+		// } else {
+		// viewSelect.setComboItem(messages().inActive());
+		// }
+
 	}
 
 	@Override
