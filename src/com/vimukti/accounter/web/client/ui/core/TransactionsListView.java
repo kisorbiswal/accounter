5047ada59d0aacadeddd56b006f364e8fc2cb139
Devaraju K <devaraju.k@vimukti.com>
2011-12-24 06:40:03 +0000

    Former-commit-id: 54cbfd29e8ae1a4293ddd6a183e83333ea326c59

--- src/com/vimukti/accounter/web/client/ui/core/TransactionsListView.java
+++ src/com/vimukti/accounter/web/client/ui/core/TransactionsListView.java
@@ -1,7 +1,9 @@
 package com.vimukti.accounter.web.client.ui.core;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.ui.Accounter;
@@ -318,6 +320,34 @@ public abstract class TransactionsListView<T> extends BaseListView<T> {
 
 	protected void filterList(String selectedValue) {
 
+	}
+
+	@Override
+	public Map<String, Object> saveView() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("currentView", viewSelect.getValue().toString());
+		map.put("dateRange", dateRangeSelector.getValue().toString());
+		map.put("startDate", startDate);
+		map.put("endDate", endDate);
+		map.put("start", start);
+		return map;
+	}
+
+	@Override
+	public void restoreView(Map<String, Object> map) {
+		if (map == null || map.isEmpty()) {
+			return;
+		}
+		String currentView = (String) map.get("currentView");
+		viewSelect.setComboItem(currentView);
+		String dateRange1 = (String) map.get("dateRange");
+		dateRangeSelector.setComboItem(dateRange1);
+		ClientFinanceDate startDate1 = (ClientFinanceDate) map.get("startDate");
+		setStartDate(startDate1);
+		ClientFinanceDate endDate1 = (ClientFinanceDate) map.get("endDate");
+		setEndDate(endDate1);
+		start = (Integer) map.get("start");
+		onPageChange(start, getPageSize());
 	}
 
 	@Override
