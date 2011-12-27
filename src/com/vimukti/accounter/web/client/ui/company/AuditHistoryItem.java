e3e44f10f9ce6a6d34bcd7d37c6809b352c7a91d
srinivas.g <srinivas.g@vimukti.com>
2011-12-27 11:31:13 +0000

    Former-commit-id: feda29bb6880ac5cb7b045c6525b9bada5cea31b

--- src/com/vimukti/accounter/web/client/ui/company/AuditHistoryItem.java
+++ src/com/vimukti/accounter/web/client/ui/company/AuditHistoryItem.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import com.google.gwt.json.client.JSONArray;
 import com.google.gwt.json.client.JSONObject;
-import com.google.gwt.json.client.JSONString;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.HTML;
@@ -13,6 +12,7 @@ import com.google.gwt.user.client.ui.SimplePanel;
 
 public class AuditHistoryItem extends SimplePanel {
 
+	private static final String IS_MODIFIED = "isModified";
 	private final HTML html;
 
 	public AuditHistoryItem(JSONArray jsonArray) {
@@ -45,12 +45,18 @@ public class AuditHistoryItem extends SimplePanel {
 					sb.appendHtmlConstant("<td class='space'>&nbsp;</td><td class='space'>&nbsp;</td>");
 				} else {
 					value = obj.get(key);
+					boolean isModified = obj.get(IS_MODIFIED) != null ? obj
+							.get(IS_MODIFIED).isBoolean().booleanValue()
+							: false;
 					JSONArray list = value.isArray();
 					if (list == null) {
 						// It is just a key:value paid
 						sb.appendHtmlConstant("<td class='historyName'><b>");
 						sb.appendEscaped(key);
-						sb.appendHtmlConstant("</b></td><td class='historyValue'>");
+						sb.appendHtmlConstant("</b></td><td class='historyValue "
+								+ (isModified ? "activity-text-color" : "")
+								+ "'>");
+
 						sb.appendEscaped(toString(value));
 						sb.appendHtmlConstant("</td>");
 					} else if (list.size() > 0) {
@@ -68,14 +74,19 @@ public class AuditHistoryItem extends SimplePanel {
 							continue;
 						}
 
-						for (int z = 0; z < item.size(); z++) {
-							String columnName = getKey(item.get(z));
-							if (columnName == null) {
-								columns.add("");
-							} else {
-								columns.add(columnName);
+						// for getting all the columns in the list
+						for (int j = 0; j < list.size(); j++) {
+							JSONArray listItem = list.get(j).isArray();
+							for (int k = 0; k < listItem.size(); k++) {
+								String columnName = getKey(listItem.get(k));
+								if (columnName != null
+										&& (!columns.contains(columnName))) {
+
+									columns.add(columnName);
+								}
 							}
 						}
+
 						// we got all columns
 						// first create column headers
 						sb.appendHtmlConstant("<table width='100%' class='items audit_history_item' border='1px'>");
@@ -85,6 +96,7 @@ public class AuditHistoryItem extends SimplePanel {
 								sb.appendHtmlConstant("&nbsp;");
 							} else {
 								sb.appendHtmlConstant("<b>");
+
 								sb.appendEscaped(columnName);
 								sb.appendHtmlConstant("</b>");
 							}
@@ -97,22 +109,57 @@ public class AuditHistoryItem extends SimplePanel {
 							if (item == null) {
 								continue;
 							}
+							// int nextId = 0;
 							for (int w = 0; w < item.size(); w++) {
 								String columnName = columns.get(w);
-								JSONObject subObj = item.get(w).isObject();
+								// JSONObject subObj = item.get(w).isObject();
+								JSONObject subObj = getObjectByColumnName(
+										list.get(z), columnName);
 								if (subObj == null) {
-									continue;
+									subObj = new JSONObject();
 								}
 								sb.appendHtmlConstant("<td class='historyItemListCell'>");
 								if (columnName.equals("")) {
 									sb.appendHtmlConstant("&nbsp;");
 								} else {
-									JSONString string = subObj.get(columnName)
-											.isString();
-									if (string != null) {
-										sb.appendEscaped(string.stringValue());
-									} else {
-										sb.appendHtmlConstant("&nbsp;");
+									if (subObj != null) {
+										String string;
+										if (subObj.get(columnName) != null) {
+											string = subObj.get(columnName)
+													.isString() != null ? subObj
+													.get(columnName)
+													.isString()
+													.toString()
+													.substring(
+															1,
+															subObj.get(
+																	columnName)
+																	.toString()
+																	.length() - 1)
+													: subObj.get(columnName)
+															.toString();
+
+										} else {
+											string = " ";
+										}
+
+										if (string != null) {
+
+											boolean ismodifiedVal = subObj
+													.get(IS_MODIFIED) != null ? subObj
+													.get(IS_MODIFIED)
+													.isBoolean().booleanValue()
+													: false;
+											sb.appendHtmlConstant("<p  "
+													+ (ismodifiedVal ? " class='activity-text-color'"
+															: "") + ">");
+											sb.appendEscaped(string);
+
+											sb.appendHtmlConstant("</p>");
+
+										} else {
+											sb.appendHtmlConstant("&nbsp;");
+										}
 									}
 								}
 								sb.appendHtmlConstant("</td>");
@@ -134,6 +181,32 @@ public class AuditHistoryItem extends SimplePanel {
 		sb.appendHtmlConstant("</table>");
 
 		html.setHTML(sb.toSafeHtml());
+
+	}
+
+	/**
+	 * this method will return the JSONObject based on the columnname from the
+	 * itemArray
+	 * 
+	 * @param jsonValue
+	 * @param columnName
+	 */
+	private JSONObject getObjectByColumnName(JSONValue jsonValue,
+			String columnName) {
+		JSONArray array = jsonValue.isArray();
+		if (array != null) {
+			for (int i = 0; i < array.size(); i++) {
+				JSONObject object = array.get(i).isObject();
+				String key = getKey(object);
+				if (key.equals(columnName)) {
+					return object;
+				}
+
+			}
+		} else {
+			return jsonValue.isObject();
+		}
+		return null;
 
 	}
 
--- src/com/vimukti/accounter/web/client/ui/company/AuditHistoryView.java
+++ src/com/vimukti/accounter/web/client/ui/company/AuditHistoryView.java
@@ -3,9 +3,12 @@ package com.vimukti.accounter.web.client.ui.company;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.json.client.JSONArray;
+import com.google.gwt.json.client.JSONBoolean;
+import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -22,6 +25,7 @@ import com.vimukti.accounter.web.client.ui.core.ButtonBar;
 
 public class AuditHistoryView extends BaseView {
 
+	private static final String IS_MODIFIED = "isModified";
 	private final int objType;
 	private final long objID;
 	private final VerticalPanel mainVerticalPanel;
@@ -64,9 +68,6 @@ public class AuditHistoryView extends BaseView {
 					public void onSuccess(ArrayList<ClientActivity> result) {
 						if (result != null) {
 
-							if (result.size() > 1) {
-								compareHistoryString(result);
-							}
 							showItems(result);
 						}
 					}
@@ -92,25 +93,122 @@ public class AuditHistoryView extends BaseView {
 
 	}
 
-	private void compare(JSONArray array, JSONArray array2) {
-		int size = 0;
-		if (array.size() >= array2.size()) {
-			size = array.size();
-		} else {
-			size = array2.size();
-		}
-
-		int j = 0;
-
-		for (int i = 0; i < size; i++) {
-			if (array.get(j).toString().equals(array2.get(i).toString())) {
-				System.out.println("same" + array.get(j) + array2.get(i));
-				j++;
-
-			} else {
-				System.out.println("different" + array.get(j) + array2.get(i));
+	private JSONArray addIsModifiedParameter(JSONArray present) {
+		for (int i = 0; i < present.size(); i++) {
+			// for string, number, boolean type
+			JSONObject obj1 = present.get(i).isObject();
+			if (obj1 != null && obj1.toString().length() > 3) {
+				obj1.put(IS_MODIFIED, JSONBoolean.getInstance(true));
 			}
 		}
+
+		return present;
+	}
+
+	private void compare(JSONArray prev, JSONArray present) {
+
+		present = addIsModifiedParameter(present);
+
+		for (int i = 0; i < prev.size(); i++) {
+
+			JSONObject object1 = prev.get(i).isObject();
+			if (object1 != null) {
+				Set<String> keySet = object1.keySet();
+				// There should be only one key anyway
+				for (String key : keySet) {
+					JSONValue value1 = object1.get(key);
+					if (value1.isArray() == null) {
+						// for comparing , if it is string
+						JSONObject object2 = getObjectForKey(key, present);
+						if (object2 == null) {
+							continue;// nothing to compare
+						}
+						String value2 = object2.get(key).toString();
+						if (value1.toString().equals(value2)) {
+							object2.put(IS_MODIFIED,
+									JSONBoolean.getInstance(false));
+						}
+					} else {
+
+						// for checking , if it is an array
+						JSONArray itemArray1 = value1.isArray();
+						JSONObject object2 = getObjectForKey(key, present);
+						if (object2 == null) {
+							continue;// nothing to compare
+						}
+						JSONArray array2 = object2.get(key).isArray();
+						if (array2 == null) {
+							continue;// nothing to compare
+						}
+
+						for (int j = 0; j < itemArray1.size(); j++) {
+							JSONArray a1 = itemArray1.get(j).isArray();
+							JSONArray a2 = array2.get(j).isArray();
+
+							if (a1 == null || a2 == null) {
+								continue;// nothing to compare
+							}
+							// if sizes are equal then compare the arrays.
+							if ((a1.size() == a2.size())) {
+								compare(a1, a2);
+							}
+
+						}
+						// If we have more rows than before then make them all
+						// modified
+						for (int j = itemArray1.size(); j < array2.size(); j++) {
+							JSONArray a2 = array2.get(j).isArray();
+							if (a2 != null) {
+								addIsModifiedParameter(a2);
+							}
+						}
+					}
+				}
+
+			}
+		}
+	}
+
+	/**
+	 * this method is used to return matching array from the second array
+	 * 
+	 * @param a1
+	 * @param a2
+	 * @return
+	 */
+
+	private JSONArray getMatchingArray(JSONArray a1, JSONArray a2) {
+		String s1 = a1.toString();
+		for (int i = 0; i < a2.size(); i++) {
+			String s2 = a2.get(i).toString();
+			if (s1.equals(s2)) {
+				return a2.get(i).isArray();
+			}
+		}
+		return null;
+
+	}
+
+	/**
+	 * Return the object within this array which have the same key as we give
+	 * 
+	 * @param key
+	 * @param array2
+	 * @return
+	 */
+	private JSONObject getObjectForKey(String key, JSONArray array2) {
+		for (int i = 0; i < array2.size(); i++) {
+			JSONValue value = array2.get(i);
+			JSONObject object = value.isObject();
+			Set<String> keySet = object.keySet();
+			String objKey = keySet.size() > 0 ? keySet.iterator().next() : null;
+			if (objKey != null) {
+				if (objKey.equals(key)) {
+					return object;
+				}
+			}
+		}
+		return null;
 	}
 
 	protected void showItems(ArrayList<ClientActivity> activityList) {
@@ -125,16 +223,42 @@ public class AuditHistoryView extends BaseView {
 		mainVerticalPanel.add(pageNameLabel);
 		mainVerticalPanel.setWidth("100%");
 
-		for (ClientActivity activity : activityList) {
-			mainVerticalPanel.add(createControl(activity));
+		if (activityList.size() > 1) {
+			JSONValue val = JSONParser.parseLenient(activityList.get(0)
+					.getAuditHistory());
+			mainVerticalPanel.add(createControl(activityList.get(0), val));
+			for (int i = 0; i < activityList.size() - 1; i++) {
+
+				ClientActivity activity1 = activityList.get(i);
+				ClientActivity activity2 = activityList.get(i + 1);
+
+				String auditHistory1 = activity1.getAuditHistory();
+				String auditHistory2 = activity2.getAuditHistory();
+
+				JSONValue jSONValue1 = JSONParser.parseLenient(auditHistory1);
+				JSONValue jSONValue2 = JSONParser.parseLenient(auditHistory2);
+
+				compare(jSONValue1.isArray(), jSONValue2.isArray());
+
+				mainVerticalPanel.add(createControl(activityList.get(i + 1),
+						jSONValue2));
+
+			}
+		} else {
+			JSONValue val = JSONParser.parseLenient(activityList.get(0)
+					.getAuditHistory());
+			mainVerticalPanel.add(createControl(activityList.get(0), val));
+
 		}
 
 		this.add(mainVerticalPanel);
 		this.setCellHorizontalAlignment(mainVerticalPanel,
 				HasAlignment.ALIGN_LEFT);
+
 	}
 
-	private DisclosurePanel createControl(ClientActivity activity) {
+	private DisclosurePanel createControl(ClientActivity activity,
+			JSONValue jSONValue2) {
 
 		// here we are creating the proper format of time to show
 		DateTimeFormat datefmt = DateTimeFormat.getFormat(Accounter
@@ -150,7 +274,7 @@ public class AuditHistoryView extends BaseView {
 		DisclosurePanel panel = new DisclosurePanel(title);
 		panel.setWidth("100%");
 		JSONValue value = JSONParser.parseLenient(activity.getAuditHistory());
-		AuditHistoryItem item = new AuditHistoryItem(value.isArray());
+		AuditHistoryItem item = new AuditHistoryItem(jSONValue2.isArray());
 		panel.setContent(item);
 		return panel;
 	}
