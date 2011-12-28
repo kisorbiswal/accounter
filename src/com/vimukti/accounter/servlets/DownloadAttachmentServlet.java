4315b3552a45990de054760e029887ff8c6165c5
Rajesh Akkineni <rajesh@vimukti.com>
2011-12-28 05:48:13 +0000

    Former-commit-id: d49bac0523a26de309c1bb55e449f8269f070a03

--- src/com/vimukti/accounter/servlets/DownloadAttachmentServlet.java
+++ src/com/vimukti/accounter/servlets/DownloadAttachmentServlet.java
@@ -31,15 +31,14 @@ public class DownloadAttachmentServlet extends HttpServlet {
 		String name = req.getParameter("name");
 		try {
 			byte[] key = getEncryptionKey(req);
-			InputStream attachmentStream = AttachmentFileServer
+			InputStream in = AttachmentFileServer
 					.getAttachmentStream(attachmentId, key);
-			if (attachmentStream != null) {
+			if (in != null) {
 				int length = 0;
 				ServletOutputStream op = resp.getOutputStream();
 				resp.setHeader("Content-Disposition", "attachment; filename=\""
 						+ name + "\"");
 				byte[] bbuf = new byte[BUFSIZE];
-				DataInputStream in = new DataInputStream(attachmentStream);
 
 				while ((in != null) && ((length = in.read(bbuf)) != -1)) {
 					op.write(bbuf, 0, length);
