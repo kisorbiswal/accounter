95357b0f3a5652498c025076fcdd375eb0dd3b0e
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-28 07:22:14 +0000

    Former-commit-id: 74822b8dd7766805057f54a675f308d554c971ea

--- src/com/vimukti/accounter/web/client/ui/company/options/CurrencyFormatOption.java
+++ src/com/vimukti/accounter/web/client/ui/company/options/CurrencyFormatOption.java
@@ -1,5 +1,6 @@
 package com.vimukti.accounter.web.client.ui.company.options;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
@@ -13,14 +14,17 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.vimukti.accounter.web.client.core.ClientCompanyPreferences;
 import com.vimukti.accounter.web.client.core.ClientCurrency;
+import com.vimukti.accounter.web.client.i18n.AccounterNumberFormat;
+import com.vimukti.accounter.web.client.ui.Accounter;
 import com.vimukti.accounter.web.client.ui.CoreUtils;
 
 public class CurrencyFormatOption extends AbstractPreferenceOption {
 
 	private static CurrencyFormatOptionUiBinder uiBinder = GWT
 			.create(CurrencyFormatOptionUiBinder.class);
-
+	private static final char CURRENCY_SIGN = '\u00A4';
 	@UiField
 	TextBox positiveTextBox;
 
@@ -52,7 +56,7 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 	Label noOfDigitsAfterDecimalLabel;
 
 	@UiField
-	ListBox noOfDigitsAfterDecimalTextBox;
+	TextBox noOfDigitsAfterDecimalTextBox;
 
 	@UiField
 	Label digitGroupingSymbolLabel;
@@ -91,12 +95,10 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 
 	private int noOfDigitsAfterDecimal;
 
-	String[] groups = { "123456789", "123D456D789", "123456D789",
-			"12D34D56D789" };
+	String[] groups = { "12,34,56,789" };
 
 	String[] posForValue = { "S1D1", "1D1S", "S 1D1", "1D1 S" };
-
-	String[] postiveFormats = { "SN", "NS", "S N", "N S" };
+	String[] negForValue = { "(S1D1)", "-1D1S", "S-1D1", "1D1S-", "(1D1S)" };
 
 	interface CurrencyFormatOptionUiBinder extends
 			UiBinder<Widget, CurrencyFormatOption> {
@@ -111,12 +113,6 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 	@Override
 	public String getTitle() {
 		return messages.currencyFormat();
-	}
-
-	@Override
-	public void onSave() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -142,10 +138,7 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 		for (ClientCurrency currency : currenciesList) {
 			currencySymbolCombo.addItem(currency.getSymbol());
 		}
-		ClientCurrency currency = currenciesList.get(currencySymbolCombo
-				.getSelectedIndex());
-		initPositiveFormatValues(currency.getSymbol());
-		currencySymbolCombo.setSelectedIndex(0);
+
 		currencySymbolCombo.addChangeHandler(new ChangeHandler() {
 
 			@Override
@@ -153,43 +146,41 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 				ClientCurrency currency = currenciesList
 						.get(currencySymbolCombo.getSelectedIndex());
 				currencySymbol = currency.getSymbol();
-				initPositiveFormatValues(currencySymbol);
+				initPositiveFormatValues();
+				initNegativeFormatValues();
 				update();
 			}
 		});
-		for (int i = 0; i < 6; i++) {
-			noOfDigitsAfterDecimalTextBox.addItem(String.valueOf(i));
-		}
+		noOfDigitsAfterDecimalTextBox.addBlurHandler(new BlurHandler() {
+
+			@Override
+			public void onBlur(BlurEvent event) {
+				noOfDigitsAfterDecimal = Integer
+						.valueOf(noOfDigitsAfterDecimalTextBox.getValue());
+				update();
+			}
+		});
 
 		decimalSymbolTextBox.addBlurHandler(new BlurHandler() {
 
 			@Override
 			public void onBlur(BlurEvent event) {
 				decimalSymbol = decimalSymbolTextBox.getValue();
+				initPositiveFormatValues();
+				initNegativeFormatValues();
 				update();
 			}
 		});
-
-		noOfDigitsAfterDecimalTextBox.setSelectedIndex(0);
-		currencySymbolCombo.setSelectedIndex(0);
-
-		digitGroupingSymbolTextBox.setText(",");
-		digitGroupSymbol = ",";
-		initDigitGroupingValues();
 
 		digitGroupingSymbolTextBox.addBlurHandler(new BlurHandler() {
 
 			@Override
 			public void onBlur(BlurEvent event) {
-				int i = digitGroupingCombo.getSelectedIndex();
 				digitGroupSymbol = digitGroupingSymbolTextBox.getValue();
 				initDigitGroupingValues();
-				digitGroupingCombo.setSelectedIndex(i);
+				update();
 			}
 		});
-
-		positiveCurrencyFormatCombo.setSelectedIndex(0);
-		negativeCurrencyFormatCombo.setSelectedIndex(0);
 
 		positiveCurrencyFormatCombo.addChangeHandler(new ChangeHandler() {
 
@@ -217,34 +208,23 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 				update();
 			}
 		});
-
-		noOfDigitsAfterDecimalTextBox.addChangeHandler(new ChangeHandler() {
-
-			@Override
-			public void onChange(ChangeEvent event) {
-				noOfDigitsAfterDecimal = noOfDigitsAfterDecimalTextBox
-						.getSelectedIndex();
-				update();
-			}
-		});
-
 	}
 
 	protected void update() {
-		String posStr = postiveFormats[posNum];
-		String value = "";
-
-		value = posStr.replaceAll("S", currencySymbol);
-		String replaceAll = groups[digitGroupNum].replaceAll("D",
-				digitGroupSymbol);
-		value = posStr.replaceAll("N", replaceAll);
-
+		String value = groups[digitGroupNum].replaceAll(",", digitGroupSymbol);
 		value += decimalSymbol;
 		for (int i = 0; i < noOfDigitsAfterDecimal; i++) {
 			value += "0";
 		}
 
-		positiveTextBox.setValue(value);
+		String pValue = posForValue[posNum].replaceAll("S", currencySymbol)
+				.replaceAll("1D1", value);
+
+		String nValue = negForValue[negNum].replaceAll("S", currencySymbol)
+				.replaceAll("1D1", value);
+
+		positiveTextBox.setValue(pValue);
+		negativeTextBox.setValue(nValue);
 
 	}
 
@@ -252,24 +232,113 @@ public class CurrencyFormatOption extends AbstractPreferenceOption {
 		digitGroupingCombo.clear();
 		for (int i = 0; i < groups.length; i++) {
 			String value = groups[i];
-			value = value.replaceAll("D", digitGroupSymbol);
+			value = value.replaceAll(",", digitGroupSymbol);
 			digitGroupingCombo.addItem(value);
 		}
+		digitGroupingCombo.setSelectedIndex(digitGroupNum);
 	}
 
-	private void initPositiveFormatValues(String symbol) {
+	private void initPositiveFormatValues() {
 		positiveCurrencyFormatCombo.clear();
-		for (int i = 0; i < postiveFormats.length; i++) {
-			String value = postiveFormats[i];
-			value = value.replaceAll("S", symbol);
-			value = value.replaceAll("N", "1.1");
-			positiveCurrencyFormatCombo.addItem(value);
+		List<String> vals = getValues(posForValue);
+		for (String s : vals) {
+			positiveCurrencyFormatCombo.addItem(s);
 		}
+		positiveCurrencyFormatCombo.setSelectedIndex(posNum);
+	}
 
+	private List<String> getValues(String[] rawData) {
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < rawData.length; i++) {
+			String value = rawData[i];
+			value = value.replaceAll("S", currencySymbol);
+			value = value.replaceAll("D", decimalSymbol);
+			list.add(value);
+		}
+		return list;
+	}
+
+	private void initNegativeFormatValues() {
+		negativeCurrencyFormatCombo.clear();
+		List<String> vals = getValues(negForValue);
+		for (String s : vals) {
+			negativeCurrencyFormatCombo.addItem(s);
+		}
+		negativeCurrencyFormatCombo.setSelectedIndex(negNum);
+	}
+
+	@Override
+	public void onSave() {
+		ClientCompanyPreferences companyPreferences = getCompanyPreferences();
+		companyPreferences.setDecimalNumber(noOfDigitsAfterDecimal);
+		companyPreferences.setDecimalCharacte(decimalSymbol);
+		companyPreferences.setDigitGroupCharacter(digitGroupSymbol);
+
+		String value = "#,##";
+
+		value += getDecimalString();
+
+		String pValue = posForValue[posNum].replaceAll("S",
+				String.valueOf(CURRENCY_SIGN)).replaceAll("1D1", value);
+
+		String nValue = negForValue[negNum].replaceAll("S",
+				String.valueOf(CURRENCY_SIGN)).replaceAll("1D1", value);
+
+		String currencyPattern = pValue + ";" + nValue;
+		companyPreferences.setCurrencyFormat(currencyPattern);// @#,##0.00 S1D1
+		Accounter.setCurrencyFormat();
+	}
+
+	private String getDecimalString() {
+		String value = "0.";
+		for (int i = 0; i < noOfDigitsAfterDecimal; i++) {
+			value += "0";
+		}
+		return value;
 	}
 
 	@Override
 	public void initData() {
+		ClientCompanyPreferences companyPreferences = getCompanyPreferences();
+		currencySymbol = getCompanyPreferences().getPrimaryCurrency()
+				.getSymbol();
+		decimalSymbol = companyPreferences.getDecimalCharacter();
+		noOfDigitsAfterDecimal = companyPreferences.getDecimalNumber();
+		decimalSymbolTextBox.setValue(decimalSymbol);
+		noOfDigitsAfterDecimalTextBox.setValue(String
+				.valueOf(noOfDigitsAfterDecimal));
+		digitGroupSymbol = companyPreferences.getDigitGroupCharacter();
+		digitGroupingSymbolTextBox.setText(digitGroupSymbol);
 
+		String currencyFormat = companyPreferences.getCurrencyFormat();
+		String[] split = currencyFormat.split(";");
+		String positive = split[0];// ¤#,##0.00
+
+		String pv = positive.replaceAll(String.valueOf(CURRENCY_SIGN), "");// ¤#,##
+		pv = pv.replaceAll(" ", "");// #,##0.00
+
+		String pos = positive.replace(pv, "1D1");// ¤1.1
+		pos = pos.replace(CURRENCY_SIGN, 'S');
+		posNum = getIndex(posForValue, pos);
+
+		String negative = split[1];// (¤#,##0.00)
+		String neg = negative.replace(pv, "1D1");// (¤1.1)
+		neg = neg.replace(CURRENCY_SIGN, 'S');
+		negNum = getIndex(negForValue, neg);
+
+		initPositiveFormatValues();
+		initNegativeFormatValues();
+		initDigitGroupingValues();
+		update();
 	}
+
+	private int getIndex(String[] values, String pos) {
+		for (int i = 0; i < values.length; i++) {
+			if (values[i].endsWith(pos)) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
 }
