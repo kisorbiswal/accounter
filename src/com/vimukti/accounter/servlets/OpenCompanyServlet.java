b606143b731b61a2aa2df2141844ebdbdcef5af7
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-30 06:40:00 +0000

    Former-commit-id: beb002b813c119cf130e8ea39e6d5944ebd5c5e1

--- src/com/vimukti/accounter/servlets/OpenCompanyServlet.java
+++ src/com/vimukti/accounter/servlets/OpenCompanyServlet.java
@@ -1,17 +1,11 @@
 package com.vimukti.accounter.servlets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.MissingResourceException;
-import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
-import java.util.ResourceBundle.Control;
 import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
@@ -33,6 +27,7 @@ import com.vimukti.accounter.core.Company;
 import com.vimukti.accounter.core.User;
 import com.vimukti.accounter.main.ServerLocal;
 import com.vimukti.accounter.utils.HibernateUtil;
+import com.vimukti.accounter.utils.UTF8Control;
 import com.vimukti.accounter.web.server.FinanceTool;
 
 public class OpenCompanyServlet extends BaseServlet {
@@ -182,42 +177,6 @@ public class OpenCompanyServlet extends BaseServlet {
 		}
 
 		return result;
-	}
-
-	public class UTF8Control extends Control {
-		public ResourceBundle newBundle(String baseName, Locale locale,
-				String format, ClassLoader loader, boolean reload)
-				throws IllegalAccessException, InstantiationException,
-				IOException {
-			// The below is a copy of the default implementation.
-			String bundleName = toBundleName(baseName, locale);
-			String resourceName = toResourceName(bundleName, "properties");
-			ResourceBundle bundle = null;
-			InputStream stream = null;
-			if (reload) {
-				URL url = loader.getResource(resourceName);
-				if (url != null) {
-					URLConnection connection = url.openConnection();
-					if (connection != null) {
-						connection.setUseCaches(false);
-						stream = connection.getInputStream();
-					}
-				}
-			} else {
-				stream = loader.getResourceAsStream(resourceName);
-			}
-			if (stream != null) {
-				try {
-					// Only this line is changed to make it to read properties
-					// files as UTF-8.
-					bundle = new PropertyResourceBundle(new InputStreamReader(
-							stream, "UTF-8"));
-				} finally {
-					stream.close();
-				}
-			}
-			return bundle;
-		}
 	}
 
 	private String joinArray(String[] value) {
