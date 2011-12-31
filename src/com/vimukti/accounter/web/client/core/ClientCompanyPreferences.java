1dfd247b28d5578952637babdb5b104a6a33f882
Nagaraju P <nagaraju.p@vimukti.com>
2011-12-31 10:56:05 +0000

    Former-commit-id: 536dc6eba0a0c7c908d3bf9ec04954407f5f3256

--- src/com/vimukti/accounter/web/client/core/ClientCompanyPreferences.java
+++ src/com/vimukti/accounter/web/client/core/ClientCompanyPreferences.java
@@ -232,7 +232,7 @@ public class ClientCompanyPreferences implements IAccounterCore {
 	public ClientCompanyPreferences() {
 		this.preferencesFlag |= (USE_ACCOUNT_NO | IS_ACCURAL_BASIS
 				| SELL_SERVICES | SELL_SERVICES | ENTER_VAT_INFORMATION_NOW | REPORT_VAT_ON_ACURAL_BASIS);
-		this.currencyFormat = '\u00A4' + "#,##0.00;-" + '\u00A4' + "#,##0.00";
+		this.currencyFormat = '\u00A4' + " #,##0.00;-" + '\u00A4' + " #,##0.00";
 	}
 
 	public long getTrackFinanceDate() {
