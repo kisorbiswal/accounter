be032764064621699f10f509210f1e1c214cfa68
Devaraju K <devaraju.k@vimukti.com>
2012-01-10 14:07:38 +0000

    Former-commit-id: 5d851f28ee8a0b44398f96160c34cd4378039e89

--- src/com/vimukti/accounter/web/client/ui/vat/TAXAgencyListView.java
+++ src/com/vimukti/accounter/web/client/ui/vat/TAXAgencyListView.java
@@ -147,6 +147,8 @@ public class TAXAgencyListView extends BaseListView<PayeeList> {
 	public void onSuccess(PaginationList<PayeeList> result) {
 		if (result.isEmpty()) {
 			grid.removeAllRecords();
+			updateRecordsCount(result.getStart(), grid.getTableRowCount(),
+					result.getTotalCount());
 			grid.addEmptyMessage(messages.noRecordsToShow());
 			return;
 		}
