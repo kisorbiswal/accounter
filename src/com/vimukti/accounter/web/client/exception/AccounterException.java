0f75ce37f36f2e5b5e7dec190a87ee967aaef656
Devi G <devi.g@vimukti.com>
2012-01-11 13:03:37 +0000

    Former-commit-id: e52f4ac26b32295a8e7402fcd45bcabd9c037671

--- src/com/vimukti/accounter/web/client/exception/AccounterException.java
+++ src/com/vimukti/accounter/web/client/exception/AccounterException.java
@@ -145,6 +145,10 @@ public class AccounterException extends Exception implements IsSerializable {
 
 	public static final int ERROR_ALREADY_DELETED = 49;
 
+	public static final int ERROR_INVALID_DATE_OF_BIRTH = 50;
+
+	public static final int ERROR_DATE_OF_BIRTH_SHOULD_HAVE_18_YRS = 51;
+
 	protected int errorCode;
 
 	// private long id;
