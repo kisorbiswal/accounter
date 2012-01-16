e9c276e2f4aa3cacb32055485ceb22f99898b985
Udaykumar Ch <udaykumar.ch@vimukti.com>
2012-01-16 09:48:19 +0000

    Former-commit-id: 9c2ee9113e0883fba8d790be43c5ebf4c8f0b796

--- src/com/vimukti/accounter/web/client/ui/vat/PayTAXView.java
+++ src/com/vimukti/accounter/web/client/ui/vat/PayTAXView.java
@@ -43,7 +43,6 @@ import com.vimukti.accounter.web.client.ui.widgets.DateValueChangeHandler;
 public class PayTAXView extends AbstractTransactionBaseView<ClientPayTAX> {
 
 	private ArrayList<DynamicForm> listforms;
-	private DateField date;
 	private PayFromAccountsCombo payFromAccCombo;
 	private DateField billsDue;
 	private TAXAgencyCombo taxAgencyCombo;
@@ -742,29 +741,26 @@ public class PayTAXView extends AbstractTransactionBaseView<ClientPayTAX> {
 	@Override
 	public void onEdit() {
 		super.onEdit();
-		paymentMethodCombo.setDisabled(false);
-		billsDue.setDisabled(false);
-		payFromAccCombo.setDisabled(false);
-		grid.setCanEdit(true);
-		grid.setDisabled(false);
-
+		enableFormItems();
 	}
 
 	private void enableFormItems() {
 		setMode(EditMode.EDIT);
-		date.setDisabled(isInViewMode());
 		paymentMethodCombo.setDisabled(isInViewMode());
 		billsDue.setDisabled(isInViewMode());
 		taxAgencyCombo.setDisabled(isInViewMode());
 		payFromAccCombo.setDisabled(isInViewMode());
+		grid.setCanEdit(true);
+		grid.setDisabled(false);
 		if (printCheck.getValue().toString().equalsIgnoreCase("true")) {
 			checkNoText.setValue(messages.toBePrinted());
+		}
+		if (isMultiCurrencyEnabled()) {
+			currencyWidget.setDisabled(isInViewMode());
 		}
 		super.onEdit();
 
 		// fillGrid();
-		transaction = null;
-
 	}
 
 	@Override
