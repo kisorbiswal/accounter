244f7a7e4e2c8a78a65ee1b81b6a17c9c8151f47
Nagaraju P <nagaraju.p@vimukti.com>
2012-01-23 10:58:50 +0000

    Save as Drafts
    Former-commit-id: bf3348f9065d842c89d6ed5db09710ce15d8058f

--- src/com/vimukti/accounter/core/Invoice.java
+++ src/com/vimukti/accounter/core/Invoice.java
@@ -513,9 +513,9 @@ public class Invoice extends Transaction implements Lifecycle {
 		for (Estimate estimate : invoice.getEstimates()) {
 			estimate = (Estimate) session.get(Estimate.class, estimate.getID());
 
-			int executeUpdate = session.createSQLQuery(
-					"delete from invoice_estimates where estimate_id="
-							+ estimate.getID()).executeUpdate();
+			int executeUpdate = session
+					.getNamedQuery("delete.Estimate.from.drafts")
+					.setLong("estimateId", estimate.getID()).executeUpdate();
 
 			if (estimate != null) {
 
