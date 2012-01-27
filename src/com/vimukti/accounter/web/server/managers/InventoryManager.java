23d60866758745a8b0256a0b39c1d5e28ac05592
srinivas.g <srinivas.g@vimukti.com>
2012-01-27 11:19:01 +0000

    Reconcilation
    Former-commit-id: 9ab11ec535cd8d786922227a08ec6e995c6eee6e

--- src/com/vimukti/accounter/web/server/managers/InventoryManager.java
+++ src/com/vimukti/accounter/web/server/managers/InventoryManager.java
@@ -9,6 +9,7 @@ import org.hibernate.Session;
 
 import com.vimukti.accounter.core.ClientConvertUtil;
 import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.core.CustomerRefund;
 import com.vimukti.accounter.core.FinanceDate;
 import com.vimukti.accounter.core.ItemStatus;
 import com.vimukti.accounter.core.Measurement;
@@ -17,6 +18,7 @@ import com.vimukti.accounter.core.StockAdjustmentItem;
 import com.vimukti.accounter.core.StockTransfer;
 import com.vimukti.accounter.core.Transaction;
 import com.vimukti.accounter.core.Warehouse;
+import com.vimukti.accounter.core.WriteCheck;
 import com.vimukti.accounter.services.DAOException;
 import com.vimukti.accounter.utils.HibernateUtil;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
@@ -28,10 +30,13 @@ import com.vimukti.accounter.web.client.core.ClientStockTransferItem;
 import com.vimukti.accounter.web.client.core.ClientWarehouse;
 import com.vimukti.accounter.web.client.core.PaginationList;
 import com.vimukti.accounter.web.client.core.Lists.InvoicesList;
+import com.vimukti.accounter.web.client.core.Lists.TransactionsList;
 import com.vimukti.accounter.web.client.exception.AccounterException;
 import com.vimukti.accounter.web.client.ui.settings.StockAdjustmentList;
 
 public class InventoryManager extends Manager {
+
+	private static final Object Integer = null;
 
 	public PaginationList<InvoicesList> getInvoiceList(long companyId,
 			long fromDate, long toDate, int invoicesType, int viewType,
@@ -115,6 +120,233 @@ public class InventoryManager extends Manager {
 		} catch (DAOException e) {
 			throw (new DAOException(DAOException.DATABASE_EXCEPTION, e));
 		}
+
+	}
+
+	public PaginationList<TransactionsList> getReceivedTransactionsList(
+			long companyId, long fromDate, long toDate, int start, int length)
+			throws DAOException {
+
+		try {
+			Session session = HibernateUtil.getCurrentSession();
+			int total = 0;
+
+			PaginationList<TransactionsList> queryResult = new PaginationList<TransactionsList>();
+			Query query = session.getNamedQuery("getInvoicesList")
+					.setParameter("companyId", companyId)
+					.setLong("fromDate", fromDate).setLong("toDate", toDate)
+					.setParameter("viewType", 1)
+					.setParameter("todayDate", new FinanceDate().getDate());
+
+			List list = query.list();
+			Object[] object = null;
+			Iterator iterator;
+			if (list != null) {
+
+				iterator = list.iterator();
+
+				while ((iterator).hasNext()) {
+
+					TransactionsList transaction = new TransactionsList();
+					object = (Object[]) iterator.next();
+
+					if ((Integer) object[13] == null) {
+
+						long transactionNumber = (object[0] == null ? 0
+								: ((Long) object[0]));
+						transaction.setTransactionId(transactionNumber);
+						transaction.setType((Integer) object[1]);
+						transaction.setDate(new ClientFinanceDate(
+								(Long) object[2]));
+						transaction.setCustomerName((String) object[4]);
+						transaction.setReceivedAmount(object[8] == null ? null
+								: (Double) object[8]);
+						transaction.setCurrency((Long) object[11]);
+
+						queryResult.add(transaction);
+					}
+				}
+			}
+
+			// for allreceived payments
+			query = session.getNamedQuery("getAllCustomersPaymentsList")
+					.setParameter("companyId", companyId)
+					.setParameter("fromDate", fromDate)
+					.setParameter("toDate", toDate).setParameter("viewType", 0);
+			list = query.list();
+			if (list != null) {
+				object = null;
+				iterator = list.iterator();
+				while ((iterator).hasNext()) {
+					TransactionsList vendorPaymentsList = new TransactionsList();
+					object = (Object[]) iterator.next();
+					if ((Integer) object[12] == null) {
+						vendorPaymentsList
+								.setTransactionId((object[0] == null ? null
+										: ((Long) object[0])));
+						vendorPaymentsList.setType((Integer) object[1]);
+						vendorPaymentsList.setDate(new ClientFinanceDate(
+								object[2] == null ? null : ((Long) object[2])));
+						vendorPaymentsList.setNumber((object[3] == null ? null
+								: ((String) object[3])));
+						vendorPaymentsList.setCustomerName((String) object[4]);
+						vendorPaymentsList
+								.setReceivedAmount((Double) object[6]);
+						vendorPaymentsList.setCurrency((Long) object[10]);
+						queryResult.add(vendorPaymentsList);
+					}
+				}
+			}
+
+			return queryResult;
+
+		} catch (Exception e) {
+			throw (new DAOException(DAOException.DATABASE_EXCEPTION, e));
+		}
+	}
+
+	public PaginationList<TransactionsList> getSpentTransactionsList(
+			long companyId, long fromDate, long toDate, int start, int length)
+			throws DAOException {
+		int total = 0;
+		Session session = HibernateUtil.getCurrentSession();
+		Company company = getCompany(companyId);
+		PaginationList<TransactionsList> customerRefundsList = new PaginationList<TransactionsList>();
+
+		// for all customer payments
+		Query query = session.getNamedQuery("getCustomerRefund")
+				.setEntity("company", company)
+				.setParameter("fromDate", new FinanceDate(fromDate))
+				.setParameter("toDate", new FinanceDate(toDate));
+		List list = query.list();
+
+		if (list != null) {
+			Iterator i = list.iterator();
+			while (i.hasNext()) {
+				TransactionsList customerRefund = new TransactionsList();
+				CustomerRefund cr = (CustomerRefund) i.next();
+				if (cr.getStatementRecord() == null) {
+					customerRefund.setTransactionId(cr.getID());
+					customerRefund.setType(cr.getType());
+					customerRefund.setDate(new ClientFinanceDate(cr.getDate()
+							.getDate())); //
+					// customerRefund.setPaymentDate(new ClientFinanceDate(cr //
+					// .getDate().getDate())); //
+					// customerRefund.setIssueDate(new
+					// ClientFinanceDate(cr // .getDate().getDate()));
+					customerRefund.setDate(new ClientFinanceDate(cr.getDate()
+							.getDate()));
+					customerRefund
+							.setCustomerName(((cr.getPayTo() != null) ? cr
+									.getPayTo().getName() : null));
+					customerRefund.setSpentAmount(cr.getTotal());
+					customerRefund.setCurrency(cr.getCurrency().getID());
+
+					customerRefundsList.add(customerRefund);
+				}
+			}
+		}
+		query = session.getNamedQuery("getWriteCheck.by.payToType")
+				.setParameter("type", WriteCheck.TYPE_CUSTOMER)
+				.setEntity("company", company);
+		list = query.list();
+		if (list != null) {
+			Iterator i = list.iterator();
+			while (i.hasNext()) {
+
+				TransactionsList customerRefund = new TransactionsList();
+				WriteCheck wc = (WriteCheck) i.next();
+				if (wc.getStatementRecord() == null) {
+					customerRefund.setTransactionId(wc.getID());
+					customerRefund.setType(wc.getType());
+					customerRefund.setDate(new ClientFinanceDate(wc.getDate()
+							.getDate()));
+					customerRefund
+							.setCustomerName((wc.getCustomer() != null) ? wc
+									.getCustomer().getName()
+									: ((wc.getVendor() != null) ? wc
+											.getVendor().getName() : (wc
+											.getTaxAgency() != null ? wc
+											.getTaxAgency().getName() : null)));
+					customerRefund.setSpentAmount(wc.getAmount());
+					customerRefund.setCurrency(wc.getCurrency().getID());
+					customerRefundsList.add(customerRefund);
+				}
+			}
+		}
+
+		query = session.getNamedQuery("getBillsList")
+				.setParameter("companyId", companyId)
+				.setParameter("fromDate", fromDate)
+				.setParameter("toDate", toDate)
+				.setParameter("todayDate", new FinanceDate().getDate())
+				.setParameter("viewType", 0);
+
+		list = query.list();
+
+		if (list != null) {
+			Object[] object = null;
+			Iterator iterator = list.iterator();
+			while ((iterator).hasNext()) {
+
+				TransactionsList billsList = new TransactionsList();
+				object = (Object[]) iterator.next();
+				// comparing the balance amount, to determine this bill is paid
+				// or not.We need to display all unpaid bills
+				if ((Integer) object[13] == null && (Double) object[6] != 0) {
+					billsList.setTransactionId((Long) object[0]);
+					billsList.setType((Integer) object[1]);
+					billsList.setNumber((object[3] == null ? null
+							: ((String) object[3])));
+					billsList.setCustomerName((String) object[4]);
+					billsList.setSpentAmount((Double) object[5]);
+					billsList.setDate(new ClientFinanceDate((Long) object[9]));
+					billsList.setCurrency((Long) object[11]);
+
+					customerRefundsList.add(billsList);
+				}
+			}
+		}
+
+		// for VendorPaymentList
+		query = session.getNamedQuery("getVendorPaymentsList")
+				.setParameter("companyId", companyId)
+				.setParameter("fromDate", fromDate)
+				.setParameter("toDate", toDate).setParameter("viewType", 1000);
+		list = query.list();
+
+		if (list != null) {
+			Object[] object = null;
+			Iterator iterator = list.iterator();
+
+			while ((iterator).hasNext()) {
+
+				object = (Object[]) iterator.next();
+
+				String name = (String) object[6];
+				if ((Integer) object[14] == null) {
+					TransactionsList vendorPaymentsList = new TransactionsList();
+					vendorPaymentsList
+							.setTransactionId((object[0] == null ? null
+									: ((Long) object[0])));
+					vendorPaymentsList.setType((Integer) object[1]);
+					vendorPaymentsList.setDate(new ClientFinanceDate(
+							(Long) object[2]));
+					// vendorPaymentsList
+					// .setPaymentDate(new ClientFinanceDate(
+					// (Long) object[2]));
+					// vendorPaymentsList.setIssuedDate(new
+					// ClientFinanceDate(
+					// (Long) object[5]));
+					vendorPaymentsList.setCustomerName(name);
+					vendorPaymentsList.setSpentAmount((Double) object[8]);
+					vendorPaymentsList.setCurrency((Long) object[12]);
+					customerRefundsList.add(vendorPaymentsList);
+				}
+			}
+		}
+
+		return customerRefundsList;
 
 	}
 
