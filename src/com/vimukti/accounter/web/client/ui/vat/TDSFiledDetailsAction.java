ab5efd6dfb10efc990f5d515f7f32645144870f0
Varakumar Pjd <varakumar.pjd@vimukti.com>
2012-01-28 10:27:07 +0000

    Former-commit-id: cd43549d59df95c3b25f4e426f575cd10f54220c

---
+++ src/com/vimukti/accounter/web/client/ui/vat/TDSFiledDetailsAction.java
@@ -0,0 +1,95 @@
+package com.vimukti.accounter.web.client.ui.vat;
+
+import com.google.gwt.resources.client.ImageResource;
+import com.vimukti.accounter.web.client.ui.MainFinanceWindow;
+import com.vimukti.accounter.web.client.ui.core.AccounterAsync;
+import com.vimukti.accounter.web.client.ui.core.Action;
+import com.vimukti.accounter.web.client.ui.core.CreateViewAsyncCallback;
+
+public class TDSFiledDetailsAction extends Action {
+
+	private int formType;
+	private String ackNo;
+	private int financialYearStart;
+	private int financialYearEnd;
+	private int quater;
+	private long dateOfFiled;
+
+	public TDSFiledDetailsAction() {
+		super();
+		this.catagory = "TDS";
+	}
+
+	@Override
+	public String getText() {
+		return messages.tdsFiledDetails();
+	}
+
+	@Override
+	public void run() {
+		runAsync(data, isDependent);
+	}
+
+	public void runAsync(final Object data, final Boolean isDependent) {
+		AccounterAsync.createAsync(new CreateViewAsyncCallback() {
+
+			@Override
+			public void onCreated() {
+				TDSFiledDetailsView view = new TDSFiledDetailsView(formType,
+						ackNo, financialYearStart, financialYearEnd, quater,
+						dateOfFiled);
+				MainFinanceWindow.getViewManager().showView(view, data,
+						isDependent, TDSFiledDetailsAction.this);
+			}
+
+		});
+	}
+
+	@Override
+	public ImageResource getBigImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ImageResource getSmallImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryToken() {
+		return "tdsFiledDetails";
+	}
+
+	@Override
+	public String getHelpToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setFormType(int formType) {
+		this.formType = formType;
+	}
+
+	public void setAckNo(String ackNo) {
+		this.ackNo = ackNo;
+	}
+
+	public void setFinancialYearStart(int financialYearStart) {
+		this.financialYearStart = financialYearStart;
+	}
+
+	public void setFinancialYearEnd(int financialYearEnd) {
+		this.financialYearEnd = financialYearEnd;
+	}
+
+	public void setQuater(int quater) {
+		this.quater = quater;
+	}
+
+	public void setDateOfFiled(long dateOfFiled) {
+		this.dateOfFiled = dateOfFiled;
+	}
+
+}
