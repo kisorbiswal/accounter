4af65cd16bd937b3476739ccd7b66101362bda03
Syamalamba P <syamalamba.p@vimukti.com>
2012-01-28 11:36:27 +0000

    Former-commit-id: f264dc87b6fabaa71f099b44be1727bb3f1c1076

--- src/com/vimukti/accounter/servlets/Form16ApdfGenerationServlet.java
+++ src/com/vimukti/accounter/servlets/Form16ApdfGenerationServlet.java
@@ -6,24 +6,30 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.Session;
+
 import com.lowagie.text.FontFactory;
 import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.core.FinanceDate;
 import com.vimukti.accounter.core.Form16ApdfTemplate;
+import com.vimukti.accounter.core.TDSChalanDetail;
 import com.vimukti.accounter.core.TDSCoveringLetterTemplate;
+import com.vimukti.accounter.core.TDSDeductorMasters;
+import com.vimukti.accounter.core.TDSResponsiblePerson;
 import com.vimukti.accounter.core.TemplateBuilder;
 import com.vimukti.accounter.core.Vendor;
 import com.vimukti.accounter.main.CompanyPreferenceThreadLocal;
 import com.vimukti.accounter.servlets.GeneratePDFservlet.FontFactoryImpEx;
-import com.vimukti.accounter.web.client.core.ClientTDSDeductorMasters;
-import com.vimukti.accounter.web.client.core.ClientTDSResponsiblePerson;
+import com.vimukti.accounter.utils.HibernateUtil;
+import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.exception.AccounterException;
 import com.vimukti.accounter.web.server.FinanceTool;
 
@@ -67,45 +73,52 @@ public class Form16ApdfGenerationServlet extends BaseServlet {
 			String companyName) throws AccounterException {
 		try {
 			ServletOutputStream sos = null;
-			List<String> fileNames = new ArrayList<String>();
-
 			String vendorIDString = req.getParameter("vendorID");
 			String dateRangeString = req.getParameter("datesRange");
-			String placeString = req.getParameter("place");
-			String printDateString = req.getParameter("printDate");
+			String[] split = dateRangeString.split("-");
+			String fromDate = split[0];
+			String toDate = split[1];
+			String place = req.getParameter("place");
+			String date = req.getParameter("printDate");
 			String typeString = req.getParameter("type");
 
 			type = Integer.parseInt(typeString);
 
 			vendorID = Long.parseLong(vendorIDString);
 			companyID = (Long) req.getSession().getAttribute(COMPANY_ID);
+			String acknowledgementNo = req.getParameter("tdsCertificateNumber");
 			financetool = new FinanceTool();
 			TemplateBuilder.setCmpName(companyName);
 			company = financetool.getCompany(companyID);
 			CompanyPreferenceThreadLocal.set(financetool.getCompanyManager()
 					.getClientCompanyPreferences(company));
 
-			Set<Vendor> vendors = company.getVendors();
+			Session currentSession = HibernateUtil.getCurrentSession();
+			Vendor vendor = (Vendor) currentSession.get(Vendor.class, vendorID);
 			IXDocReport report = null;
 			IContext context = null;
+			TDSDeductorMasters tdsDeductorMasterDetails = company
+					.getTdsDeductor();
+			TDSResponsiblePerson tdsResposiblePerson = company
+					.getTdsResposiblePerson();
+			if (tdsDeductorMasterDetails == null || vendor == null
+					|| tdsResposiblePerson == null) {
+				return;
+			}
 			if (type == 0) {
-
 				fileName = "Form16A";
-				String templeteName = null;
-				templeteName = "templetes" + File.separator + "Form16a.docx";
+				String templeteName = "templetes" + File.separator
+						+ "Form16a.docx";
+				ClientFinanceDate cEndDate = new ClientFinanceDate(toDate);
+				ClientFinanceDate cStartDate = new ClientFinanceDate(fromDate);
+				FinanceDate startDate = new FinanceDate(cStartDate);
+				FinanceDate endDate = new FinanceDate(cEndDate);
+				ArrayList<TDSChalanDetail> chalanList = financetool
+						.getChalanList(startDate, endDate, acknowledgementNo,
+								company.getID());
 
-				ClientTDSDeductorMasters tdsDeductorMasterDetails = financetool
-						.getTDSDeductorMasterDetails(company.getID());
-				Form16ApdfTemplate form16APdfGeneration = null;
-				form16APdfGeneration = new Form16ApdfTemplate();
+				Form16ApdfTemplate form16APdfGeneration = new Form16ApdfTemplate();
 
-				for (Vendor vendor : vendors) {
-					if (vendor.getID() == vendorID) {
-						form16APdfGeneration.setVendorandCompanyData(vendor,
-								tdsDeductorMasterDetails);
-						break;
-					}
-				}
 				form16APdfGeneration.setDateYear(dateRangeString);
 
 				InputStream in = new BufferedInputStream(new FileInputStream(
@@ -119,30 +132,36 @@ public class Form16ApdfGenerationServlet extends BaseServlet {
 				} catch (XDocReportException e) {
 					e.printStackTrace();
 				}
+
+				form16APdfGeneration.setSummaryPaymentDetails(chalanList);
+
+				Map<Integer, ArrayList<TDSChalanDetail>> quartersList = new HashMap<Integer, ArrayList<TDSChalanDetail>>();
+				for (int i = 0; i < 4; i++) {
+					FinanceDate[] quarterDates = getQuarterDates(i);
+					ArrayList<TDSChalanDetail> quarterList = new ArrayList<TDSChalanDetail>();
+					if (quarterDates != null) {
+						quarterList = financetool.getChalanList(
+								quarterDates[0], quarterDates[1],
+								acknowledgementNo, company.getID());
+					}
+					quartersList.put(i, quarterList);
+				}
+				form16APdfGeneration.setSummaryOfTaxDetails(quartersList);
+				form16APdfGeneration.setVerificationDetails(
+						tdsResposiblePerson, place, date);
+				form16APdfGeneration.setVendorandCompanyData(vendor,
+						tdsDeductorMasterDetails);
 				context = report.createContext();
 				context = form16APdfGeneration.assignValues(context, report);
 
 			} else {
-
-				Vendor ven = null;
-				for (Vendor vendor : vendors) {
-					if (vendor.getID() == vendorID) {
-						ven = vendor;
-						fileName = "CoveringLetter" + vendor.getName();
-						break;
-					}
-				}
-
+				fileName = "CoveringLetter" + vendor.getName();
 				String templeteName = null;
 				templeteName = "templetes" + File.separator
 						+ "CoveringLetter.docx";
-
-				ClientTDSResponsiblePerson responsiblePersonDetails = financetool
-						.getResponsiblePersonDetails(company.getID());
-
 				TDSCoveringLetterTemplate coveringLetterTemp = new TDSCoveringLetterTemplate();
-				coveringLetterTemp.setValues(ven, company,
-						responsiblePersonDetails);
+				coveringLetterTemp.setValues(vendor, company,
+						tdsResposiblePerson);
 
 				InputStream in = new BufferedInputStream(new FileInputStream(
 						templeteName));
@@ -157,7 +176,6 @@ public class Form16ApdfGenerationServlet extends BaseServlet {
 				}
 				context = report.createContext();
 				context = coveringLetterTemp.assignValues(context, report);
-
 			}
 			FontFactory.setFontImp(new FontFactoryImpEx());
 			Options options = Options.getTo(ConverterTypeTo.PDF).via(
@@ -177,9 +195,40 @@ public class Form16ApdfGenerationServlet extends BaseServlet {
 		System.err.println("Pdf created");
 	}
 
+	private FinanceDate[] getQuarterDates(int quarter) {
+		int finYear = new ClientFinanceDate().getYear();
+		String frmDt = null;
+		String toDt = null;
+		switch (quarter) {
+		case 0:
+			frmDt = "04/01/" + Integer.toString(finYear);
+			toDt = "06/30/" + Integer.toString(finYear);
+			break;
+		case 1:
+			frmDt = "07/01/" + Integer.toString(finYear);
+			toDt = "09/30/" + Integer.toString(finYear);
+			break;
+		case 2:
+			frmDt = "10/01/" + Integer.toString(finYear);
+			toDt = "12/31/" + Integer.toString(finYear);
+			break;
+		case 3:
+			frmDt = "01/01/" + Integer.toString(finYear);
+			toDt = "31/03/" + Integer.toString(finYear);
+			break;
+		default:
+			break;
+
+		}
+		FinanceDate fromDate = new FinanceDate(frmDt);
+		FinanceDate toDate = new FinanceDate(toDt);
+		return new FinanceDate[] { fromDate, toDate };
+	}
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		doGet(req, resp);
 	}
+
 }
