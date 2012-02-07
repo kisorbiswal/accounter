09702d356f0357b93357c6ead99c991d065a78b4
Nagasuresh A <nagasuresh.a@vimukti.com>
2012-02-07 07:15:28 +0000

    Former-commit-id: 043f5f592a9f7e6119aac81769a3a7da52eeb6dc

---
+++ src/com/vimukti/accounter/core/Features.java
@@ -0,0 +1,26 @@
+package com.vimukti.accounter.core;
+
+/**
+ * 
+ * @author nagasuresh.a
+ *
+ */
+
+public class Features {
+	
+	public static final String CRESTE_COMPANY ="create company";
+	public static final String BRANDING_THEME ="brnading theme0";
+	public static final String IMPORT_BANK_STATEMENTS ="import bank statements";
+	public static final String USER_ACTIVITY ="user activity";
+	public static final String HISTORY ="history";
+	public static final String ATTACHMENTS ="attachments";
+	public static final String CLASS ="class";
+	public static final String LOCATION ="location";
+	public static final String BILLABLE_EXPENSE ="billable exenses";
+	public static final String CREDITS_CHARGES ="credtis and charges";
+	public static final String MERGING ="merging";
+	public static final String JOB_COSTING ="job costing";
+	public static final String ENCRYPTION ="encryption";
+	public static final String INVITE_USERS ="invite users";
+	
+}
