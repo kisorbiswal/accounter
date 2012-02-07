5889ce3f997010b370eb9f99da248703f7023533
Varakumar Pjd <varakumar.pjd@vimukti.com>
2012-02-07 10:18:32 +0000

    Former-commit-id: 70aca0461dca41053c2405626901c5bc592d5afc

--- src/com/vimukti/accounter/web/client/ui/combo/EmailCombo.java
+++ src/com/vimukti/accounter/web/client/ui/combo/EmailCombo.java
@@ -1,63 +1,75 @@
 package com.vimukti.accounter.web.client.ui.combo;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import com.vimukti.accounter.web.client.core.ClientEmailAccount;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.company.EmailAccountDialog;
+import com.vimukti.accounter.web.client.ui.core.ActionCallback;
 
-public class EmailCombo extends CustomCombo {
+public class EmailCombo extends CustomCombo<ClientEmailAccount> {
 
 	public EmailCombo(String title) {
 		super(title);
-
-		initCombo(getToAddress());
+		initCombo(getEmailAccounts());
 	}
 
 	public EmailCombo(String title, boolean isAddNewRequire) {
 		super(title, isAddNewRequire, 1);
-		initCombo(getToAddress());
+		initCombo(getEmailAccounts());
 	}
 
-	public ArrayList<String> getToAddress() {
-		String companyEmail = Accounter.getCompany().getCompanyEmail();
+	public List<ClientEmailAccount> getEmailAccounts() {
+		// String companyEmail = Accounter.getCompany().getCompanyEmail();
+		//
+		// ArrayList<String> toAdd = new ArrayList<String>();
+		// if (companyEmail != null && companyEmail.trim().length() != 0)
+		// toAdd.add(companyEmail);
+		//
+		// String userEmail = getCompany().getLoggedInUser().getEmail();
+		// if (userEmail != null && userEmail.trim().length() != 0)
+		// toAdd.add(userEmail);
 
-		ArrayList<String> toAdd = new ArrayList<String>();
-		if (companyEmail != null && companyEmail.trim().length() != 0)
-			toAdd.add(companyEmail);
-
-		String userEmail = getCompany().getLoggedInUser().getEmail();
-		if (userEmail != null && userEmail.trim().length() != 0)
-			toAdd.add(userEmail);
-
-		return toAdd;
-	}
-
-	@Override
-	protected String getDisplayName(Object object) {
-		if (object != null)
-			return object.toString() != null ? object.toString() : "";
-		else
-			return "";
+		return Accounter.getCompany().getEmailAccounts();
 	}
 
 	@Override
-	protected String getColumnData(Object object, int col) {
+	protected String getDisplayName(ClientEmailAccount object) {
+		return object.getEmailId() != null ? object.getEmailId() : "";
+	}
+
+	@Override
+	protected String getColumnData(ClientEmailAccount object, int col) {
 		switch (col) {
 		case 0:
-			return object.toString();
+			return object.getEmailId();
 		}
 		return null;
 	}
 
 	@Override
 	public String getDefaultAddNewCaption() {
-		// TODO Auto-generated method stub
-		return null;
+		return messages.email();
 	}
 
 	@Override
 	public void onAddNew() {
-		// TODO Auto-generated method stub
-
+		EmailAccountDialog dialog = new EmailAccountDialog(null);
+		dialog.setCallback(getActionCallBack());
+		dialog.center();
 	}
 
+	private ActionCallback<ClientEmailAccount> getActionCallBack() {
+		ActionCallback<ClientEmailAccount> callBack = new ActionCallback<ClientEmailAccount>() {
+
+			@Override
+			public void actionResult(ClientEmailAccount result) {
+				setComboItem(result);
+				if (getHandler() != null) {
+					getHandler().selectedComboBoxItem(result);
+				}
+			}
+		};
+		return callBack;
+	}
 }
