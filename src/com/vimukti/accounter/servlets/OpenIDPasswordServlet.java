37fdd4928e3526f7067644a3089a2a96f9a6ff6b
Nagaraju P <nagaraju.p@vimukti.com>
2012-02-08 10:43:42 +0000

    Former-commit-id: 7cea9a27f1d70c52e25c1ed79769c7a45e5145ec

--- src/com/vimukti/accounter/servlets/OpenIDPasswordServlet.java
+++ src/com/vimukti/accounter/servlets/OpenIDPasswordServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.vimukti.accounter.core.Client;
+import com.vimukti.accounter.utils.HexUtil;
+import com.vimukti.accounter.utils.Security;
+
 public class OpenIDPasswordServlet extends BaseServlet {
 
 	/**
@@ -29,7 +33,29 @@ public class OpenIDPasswordServlet extends BaseServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
+		HttpSession session = req.getSession();
+		String emailId = (String) session.getAttribute(EMAIL_ID);
+		if (session != null && emailId != null) {
+			String password = req.getParameter("password");
+			Client client = getClient(emailId);
+			if (client != null) {
+				password = HexUtil.bytesToHex(Security.makeHash(emailId
+						+ password.trim()));
+				if (!client.getPassword().equals(password)) {
+					req.setAttribute("error",
+							"Wrong password. Please enter you accounter passowrd");
+					dispatch(req, resp, VIEW);
+					return;
+				}
+				try {
+					NewLoginServlet.createD2(req, emailId, password);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				redirectExternal(req, resp, LOGIN_URL);
+			}
+		}
+		resp.sendRedirect(LOGIN_URL);
 	}
 
 }
