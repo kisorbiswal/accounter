95b4c326b8ed6941035708c90e264c5a96f34e2f
Nagasuresh A <nagasuresh.a@vimukti.com>
2012-02-08 05:26:21 +0000

    Former-commit-id: 6c40b9006fd029a0da943e7760db23cfe93b80b4

--- src/com/vimukti/accounter/web/client/ui/Accounter.java
+++ src/com/vimukti/accounter/web/client/ui/Accounter.java
@@ -158,7 +158,11 @@ public class Accounter implements EntryPoint {
 	}
 
 	public static ClientCompany getCompany() {
-		return company;
+		if (company != null) {
+			return company;
+		} else {
+			return null;
+		}
 	}
 
 	public static void setUser(ClientUser u) {
@@ -192,8 +196,8 @@ public class Accounter implements EntryPoint {
 	}
 
 	private native static void reloadMacMenu() /*-{
-		$wnd.MacReload();
-	}-*/;
+												$wnd.MacReload();
+												}-*/;
 
 	public static MainFinanceWindow getMainFinanceWindow() {
 		return mainWindow;
@@ -356,15 +360,15 @@ public class Accounter implements EntryPoint {
 	}
 
 	private static native void removeLoadingImage() /*-{
-		var parent = $wnd.document.getElementById('loadingWrapper');
-		var footer = $wnd.document.getElementById('mainFooter');
-		//		var feedbackimg = $wnd.document.getElementById('contact');
-		//		feedbackimg.style.visibility = 'visible';
-		//var header = $wnd.document.getElementById('mainHeader');
-		parent.style.visibility = 'hidden';
-		footer.style.visibility = 'visible';
-		//header.style.visibility = 'visible';
-	}-*/;
+													var parent = $wnd.document.getElementById('loadingWrapper');
+													var footer = $wnd.document.getElementById('mainFooter');
+													//		var feedbackimg = $wnd.document.getElementById('contact');
+													//		feedbackimg.style.visibility = 'visible';
+													//var header = $wnd.document.getElementById('mainHeader');
+													parent.style.visibility = 'hidden';
+													footer.style.visibility = 'visible';
+													//header.style.visibility = 'visible';
+													}-*/;
 
 	/**
 	 * 
