31835494580d7a74afde62a9995d7211be615949
Devaraju K <devaraju.k@vimukti.com>
2012-02-09 09:20:39 +0000

    Former-commit-id: 1eb3c8cc9eff33079e41ccd649025716dc9d31c4

--- src/com/vimukti/accounter/web/client/ui/reports/InventoryStockStatusByItemReport.java
+++ src/com/vimukti/accounter/web/client/ui/reports/InventoryStockStatusByItemReport.java
@@ -1,6 +1,5 @@
 package com.vimukti.accounter.web.client.ui.reports;
 
-import com.vimukti.accounter.core.ReportsGenerator;
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.reports.InventoryStockStatusDetail;
 import com.vimukti.accounter.web.client.ui.Accounter;
@@ -36,8 +35,7 @@ public class InventoryStockStatusByItemReport extends
 		UIUtils.generateReportPDF(
 				Integer.parseInt(String.valueOf(startDate.getDate())),
 				Integer.parseInt(String.valueOf(endDate.getDate())),
-				ReportsGenerator.REPORT_TYPE_INVENTORY_STOCK_STATUS_BYITEM, "",
-				"");
+				REPORT_TYPE_INVENTORY_STOCK_STATUS_BYITEM, "", "");
 	}
 
 	@Override
@@ -45,7 +43,6 @@ public class InventoryStockStatusByItemReport extends
 		UIUtils.exportReport(
 				Integer.parseInt(String.valueOf(startDate.getDate())),
 				Integer.parseInt(String.valueOf(endDate.getDate())),
-				ReportsGenerator.REPORT_TYPE_INVENTORY_STOCK_STATUS_BYITEM, "",
-				"");
+				REPORT_TYPE_INVENTORY_STOCK_STATUS_BYITEM, "", "");
 	}
 }
