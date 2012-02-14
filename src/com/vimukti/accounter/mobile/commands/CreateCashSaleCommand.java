76d4485c07a25902b0a49053f2c1a0f737effbdc
Syamalamba P <syamalamba.p@vimukti.com>
2012-02-14 06:21:35 +0000

    Former-commit-id: 1f4f7b43ea51115c50486ea24659d1387b510e8b

--- src/com/vimukti/accounter/mobile/commands/CreateCashSaleCommand.java
+++ src/com/vimukti/accounter/mobile/commands/CreateCashSaleCommand.java
@@ -289,14 +289,16 @@ public class CreateCashSaleCommand extends AbstractTransactionCommand {
 
 						@Override
 						public boolean filter(Account acc) {
-							return Arrays.asList(ClientAccount.TYPE_BANK,
-									ClientAccount.TYPE_CASH,
-									ClientAccount.TYPE_PAYPAL,
-									ClientAccount.TYPE_CREDIT_CARD,
-									ClientAccount.TYPE_OTHER_CURRENT_ASSET,
-									ClientAccount.TYPE_INVENTORY_ASSET,
-									ClientAccount.TYPE_FIXED_ASSET).contains(
-									acc.getType())
+							return acc.getIsActive()
+									&& Arrays
+											.asList(ClientAccount.TYPE_BANK,
+													ClientAccount.TYPE_CASH,
+													ClientAccount.TYPE_PAYPAL,
+													ClientAccount.TYPE_CREDIT_CARD,
+													ClientAccount.TYPE_OTHER_CURRENT_ASSET,
+													ClientAccount.TYPE_INVENTORY_ASSET,
+													ClientAccount.TYPE_FIXED_ASSET)
+											.contains(acc.getType())
 									&& acc.getID() != getCompany()
 											.getAccountsReceivableAccount()
 											.getID();
