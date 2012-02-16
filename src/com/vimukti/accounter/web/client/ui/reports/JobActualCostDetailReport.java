5652cb4e184d36db188d8f323aaa850b38fa3dff
Udaykumar Ch <udaykumar.ch@vimukti.com>
2012-02-16 05:51:39 +0000

    Former-commit-id: a0505c8d9c3cebdf14197c737015b1a4e88d48ae

--- src/com/vimukti/accounter/web/client/ui/reports/JobActualCostDetailReport.java
+++ src/com/vimukti/accounter/web/client/ui/reports/JobActualCostDetailReport.java
@@ -2,31 +2,71 @@ package com.vimukti.accounter.web.client.ui.reports;
 
 import com.vimukti.accounter.web.client.core.ClientFinanceDate;
 import com.vimukti.accounter.web.client.core.reports.JobActualCostDetail;
+import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.UIUtils;
 import com.vimukti.accounter.web.client.ui.serverreports.JobActualCostDetailServerReport;
 
 public class JobActualCostDetailReport extends
 		AbstractReportView<JobActualCostDetail> {
 
-	public JobActualCostDetailReport() {
-		this.serverReport = new JobActualCostDetailServerReport(this);
+	private boolean isActualcostDetail;
+	private long transactionId;
+	private long jobId;
+
+	public JobActualCostDetailReport(boolean isActualcostDetail,
+			long transactionId, long jobId) {
+		this.isActualcostDetail = isActualcostDetail;
+		this.transactionId = transactionId;
+		this.jobId = jobId;
+		this.serverReport = new JobActualCostDetailServerReport(this,
+				isActualcostDetail);
 	}
 
 	@Override
 	public void makeReportRequest(ClientFinanceDate start, ClientFinanceDate end) {
-		// TODO Auto-generated method stub
-
+		Accounter.createReportService().getJobActualCostOrRevenueDetails(start,
+				end, isActualcostDetail, transactionId, jobId, this);
 	}
 
 	@Override
 	public int getToolbarType() {
-		// TODO Auto-generated method stub
-		return 0;
+		return TOOLBAR_TYPE_DATE_RANGE;
+	}
+
+	@Override
+	public boolean canPrint() {
+		return true;
+	}
+
+	@Override
+	public boolean canExportToCsv() {
+		return true;
 	}
 
 	@Override
 	public void OnRecordClick(JobActualCostDetail record) {
-		// TODO Auto-generated method stub
-
+		record.setStartDate(toolbar.getStartDate());
+		record.setEndDate(toolbar.getEndDate());
+		record.setDateRange(toolbar.getSelectedDateRange());
+		ReportsRPC.openTransactionView(record.getType(),
+				record.getTransaction());
 	}
 
+	@Override
+	public void print() {
+		String customerName = this.data != null ? ((JobActualCostDetail) this.data)
+				.getCustomerName() : "";
+		UIUtils.generateReportPDF(
+				Integer.parseInt(String.valueOf(startDate.getDate())),
+				Integer.parseInt(String.valueOf(endDate.getDate())), 184, "",
+				"", customerName);
+	}
+
+	@Override
+	public void exportToCsv() {
+		UIUtils.exportReport(
+				Integer.parseInt(String.valueOf(startDate.getDate())),
+				Integer.parseInt(String.valueOf(endDate.getDate())), 184, "",
+				"");
+	}
 }
