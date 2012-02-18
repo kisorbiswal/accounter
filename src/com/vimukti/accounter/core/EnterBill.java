bbbce0f8ed4526d973d86b684f5881a0652feb35
lingarao <lingarao.r@vimukti.com>
2012-02-18 15:51:26 +0530

    jobs
    Former-commit-id: 5afe2467360846c28dc1eabeb365a23339c4c8e5

--- src/com/vimukti/accounter/core/EnterBill.java
+++ src/com/vimukti/accounter/core/EnterBill.java
@@ -1047,6 +1047,7 @@ public class EnterBill extends Transaction implements IAccounterServerCore {
 					estimate = new Estimate();
 					estimate.setCompany(getCompany());
 					estimate.setCustomer(newTransactionItem.getCustomer());
+					estimate.setJob(newTransactionItem.getJob());
 					estimate.setTransactionItems(new ArrayList<TransactionItem>());
 					estimate.setEstimateType(Estimate.BILLABLEEXAPENSES);
 					estimate.setType(Transaction.TYPE_ESTIMATE);
