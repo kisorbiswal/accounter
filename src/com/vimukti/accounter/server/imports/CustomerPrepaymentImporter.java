90d201adff4369dedeed65a4ad2296628d7281e1
devaraju <devaraju.k@vimukti.com>
2012-02-20 18:15:57 +0530

    Vendor Prepayment & Customer Prepayment importers completed.
    Former-commit-id: 4590bfb1e726084a4d3c53c77107ed1aabd983cc

---
+++ src/com/vimukti/accounter/server/imports/CustomerPrepaymentImporter.java
@@ -0,0 +1,45 @@
+package com.vimukti.accounter.server.imports;
+
+import java.util.List;
+
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.core.ClientCustomerPrePayment;
+import com.vimukti.accounter.web.client.core.ClientTransaction;
+import com.vimukti.accounter.web.client.imports.Field;
+
+public class CustomerPrepaymentImporter extends
+		TransactionImporter<ClientCustomerPrePayment> {
+
+	@Override
+	public ClientCustomerPrePayment getData() {
+		ClientTransaction custPrepayment = new ClientCustomerPrePayment();
+		getTransactionData(custPrepayment);
+		((ClientCustomerPrePayment) custPrepayment)
+				.setCustomer(getLong(messages.customer()));
+		((ClientCustomerPrePayment) custPrepayment)
+				.setDepositIn(getLong(messages.depositIn()));
+		((ClientCustomerPrePayment) custPrepayment).setTotal(getDouble(messages
+				.amount()));
+		((ClientCustomerPrePayment) custPrepayment)
+				.setPaymentMethod(getString(messages.paymentMethod()));
+		((ClientCustomerPrePayment) custPrepayment)
+				.setCheckNumber(getString(messages.checkNo()));
+		((ClientCustomerPrePayment) custPrepayment).setMemo(getString(messages
+				.memo()));
+		return ((ClientCustomerPrePayment) custPrepayment);
+	}
+
+	@Override
+	public List<Field<?>> getAllFields() {
+		List<Field<?>> list = super.getAllFields();
+		list.add(new Field<String>(messages.customer(), Global.get().Customer()));
+		list.add(new Field<String>(messages.depositIn(), messages.depositIn()));
+		list.add(new Field<Double>(messages.amount(), messages.amount()));
+		list.add(new Field<String>(messages.paymentMethod(), messages
+				.paymentMethod()));
+		list.add(new Field<String>(messages.checkNo(), messages.checkNo()));
+		list.add(new Field<String>(messages.memo(), messages.memo()));
+		return null;
+	}
+
+}
---
+++ src/com/vimukti/accounter/server/imports/VendorPrepaymentImporter.java
@@ -0,0 +1,43 @@
+package com.vimukti.accounter.server.imports;
+
+import java.util.List;
+
+import com.vimukti.accounter.web.client.core.ClientPayBill;
+import com.vimukti.accounter.web.client.core.ClientTransaction;
+import com.vimukti.accounter.web.client.imports.Field;
+
+public class VendorPrepaymentImporter extends
+		TransactionImporter<ClientPayBill> {
+
+	@Override
+	public ClientPayBill getData() {
+		ClientTransaction vendorPrepayment = new ClientPayBill();
+		getTransactionData(vendorPrepayment);
+		((ClientPayBill) vendorPrepayment)
+				.setVendor(getLong(messages.vendor()));
+		((ClientPayBill) vendorPrepayment).setPayFrom(getLong(messages
+				.payFrom()));
+		((ClientPayBill) vendorPrepayment)
+				.setTotal(getDouble(messages.amount()));
+		((ClientPayBill) vendorPrepayment).setPaymentMethod(getString(messages
+				.paymentMethod()));
+		((ClientPayBill) vendorPrepayment).setCheckNumber(getString(messages
+				.checkNo()));
+		((ClientPayBill) vendorPrepayment).setMemo(getString(messages.memo()));
+		return ((ClientPayBill) vendorPrepayment);
+	}
+
+	@Override
+	public List<Field<?>> getAllFields() {
+		List<Field<?>> list = super.getAllFields();
+		list.add(new Field<String>(messages.vendor(), messages.payTo()));
+		list.add(new Field<String>(messages.payFrom(), messages.payFrom()));
+		list.add(new Field<Double>(messages.amount(), messages.amount()));
+		list.add(new Field<String>(messages.paymentMethod(), messages
+				.paymentMethod()));
+		list.add(new Field<String>(messages.checkNo(), messages.checkNo()));
+		list.add(new Field<String>(messages.memo(), messages.memo()));
+		return null;
+	}
+
+}
