7065f0607e6aa65f9f2c684120289c9f97c2a9ea
Uday <udaykumar.ch@vimukti.com>
2012-02-27 16:19:46 +0530

    issue :- 3728
    Former-commit-id: 969495b142473dd0c4c39baf5372d61f64ae7224

--- src/com/vimukti/accounter/web/client/ui/serverreports/InventoryValuationDetailsServerReport.java
+++ src/com/vimukti/accounter/web/client/ui/serverreports/InventoryValuationDetailsServerReport.java
@@ -45,7 +45,7 @@ public class InventoryValuationDetailsServerReport extends
 	@Override
 	public int[] getColumnTypes() {
 		return new int[] { COLUMN_TYPE_TEXT, COLUMN_TYPE_DATE,
-				COLUMN_TYPE_TEXT, COLUMN_TYPE_TEXT, COLUMN_TYPE_AMOUNT,
+				COLUMN_TYPE_TEXT, COLUMN_TYPE_TEXT, COLUMN_TYPE_NUMBER,
 				COLUMN_TYPE_AMOUNT, COLUMN_TYPE_TEXT, COLUMN_TYPE_AMOUNT,
 				COLUMN_TYPE_AMOUNT };
 
