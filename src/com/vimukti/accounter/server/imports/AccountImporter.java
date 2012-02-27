72823c1e8a9b51ce24bd0d5ce6fb84039d8358f2
devaraju <devaraju.k@vimukti.com>
2012-02-27 09:25:10 +0530

    Crated AccountImporter class.
    Former-commit-id: d3693fbeb40c7e906cc24c05efa4cdb594cefa5d

---
+++ src/com/vimukti/accounter/server/imports/AccountImporter.java
@@ -0,0 +1,22 @@
+package com.vimukti.accounter.server.imports;
+
+import java.util.List;
+
+import com.vimukti.accounter.web.client.core.ClientAccount;
+import com.vimukti.accounter.web.client.core.ImportField;
+
+public class AccountImporter extends AbstractImporter<ClientAccount> {
+
+	@Override
+	public ClientAccount getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ImportField> getAllFields() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
