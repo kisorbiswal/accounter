5e1bab624b7a63c30f1f793ca0bb2dcf9bd49678
nagasuresh <vimukti22@vimukti22-PC.(none)>
2012-03-05 16:50:45 +0530

    Solved the issues in the subscription management issue .
    Former-commit-id: fdc940442c9f515a17d9b6d7cbd747d9d4735f60

--- src/com/vimukti/accounter/servlets/SubscriptionManagementServlet.java
+++ src/com/vimukti/accounter/servlets/SubscriptionManagementServlet.java
@@ -43,6 +43,11 @@ public class SubscriptionManagementServlet extends BaseServlet {
 
 		Session session = HibernateUtil.getCurrentSession();
 		org.hibernate.Transaction transaction = session.beginTransaction();
+		HttpSession session2 = req.getSession();
+		if (session2 == null) {
+			resp.sendRedirect(LOGIN_URL);
+			return;
+		}
 		String emailId = req.getSession().getAttribute(EMAIL_ID).toString();
 		Client client = getClient(emailId);
 		if (client == null) {
@@ -70,7 +75,7 @@ public class SubscriptionManagementServlet extends BaseServlet {
 		saveEntry(clientSubscription);
 		transaction.commit();
 
-		redirectExternal(req, resp, "/main/subscription/thankyou");
+		resp.sendRedirect(COMPANIES_URL);
 	}
 
 	private void mergeUsers(Client client, Set<String> oldMembers,
@@ -155,21 +160,18 @@ public class SubscriptionManagementServlet extends BaseServlet {
 			Set<String> members = client.getClientSubscription().getMembers();
 			List<String> createdUsers = getExistedUsers(members);
 			boolean isFirst = true;
-			if (!members.isEmpty()) {
-				finalString = "[";
-				for (String string2 : members) {
-					if (isFirst) {
-						finalString += getDict(string2,
-								createdUsers.contains(string2));
-						isFirst = false;
-					} else {
-						finalString += ","
-								+ getDict(string2,
-										createdUsers.contains(string2));
-					}
+			finalString = "[";
+			for (String string2 : members) {
+				if (isFirst) {
+					finalString += getDict(string2,
+							createdUsers.contains(string2));
+					isFirst = false;
+				} else {
+					finalString += ","
+							+ getDict(string2, createdUsers.contains(string2));
 				}
-				finalString += "]";
 			}
+			finalString += "]";
 			req.setAttribute("userIdsList", finalString);
 			dispatch(req, resp, view);
 		} else {
