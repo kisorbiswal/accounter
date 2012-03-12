f221c062281338f4b404571313de2a00f2a90241
Nagaraju P <nagaraju.p@vimukti.com>
2012-03-12 19:14:07 +0530

    Changed calling of delete method in finance tool
    Former-commit-id: 974877ad5cd715cf4fcec07e6c0069cb7966e196

--- src/com/vimukti/accounter/servlets/SubscriptionManagementServlet.java
+++ src/com/vimukti/accounter/servlets/SubscriptionManagementServlet.java
@@ -97,29 +97,23 @@ public class SubscriptionManagementServlet extends BaseServlet {
 		saveEntry(clientSubscription);
 
 		try {
-			int deletedUsers = mergeUsers(client, oldMembers, existedUsers,
-					members);
-			if (deletedUsers == 0) {
-				transaction.commit();
-			}
-
+			mergeUsers(client, oldMembers, existedUsers, members);
+			transaction.commit();
 		} catch (AccounterException e) {
+			transaction.rollback();
 			e.printStackTrace();
 		}
 		resp.sendRedirect(COMPANIES_URL);
 	}
 
-	private int mergeUsers(Client client, Set<String> oldMembers,
+	private void mergeUsers(Client client, Set<String> oldMembers,
 			List<String> existedUsers, Set<String> members)
 			throws AccounterException {
-		int deletedUsers = 0;
 		for (String o : oldMembers) {
 			if (!members.contains(o) && existedUsers.contains(o)) {
 				SubscryptionTool.deleteUser(client, o);
-				deletedUsers++;
 			}
 		}
-		return deletedUsers;
 	}
 
 	private boolean checkTotalMembers(Set<String> members, int type) {
