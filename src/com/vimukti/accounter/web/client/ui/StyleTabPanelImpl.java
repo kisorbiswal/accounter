255f60c31e48b9192244010272f6730d65b08a08
s <syamalamba.p@vimukti.com>
2012-03-17 12:04:07 +0530

    Added style names for the closed and opened panels.
    Former-commit-id: 2975061d07e237939d498d09e5a84d2606d83280

--- src/com/vimukti/accounter/web/client/ui/StyleTabPanelImpl.java
+++ src/com/vimukti/accounter/web/client/ui/StyleTabPanelImpl.java
@@ -1,24 +1,33 @@
 package com.vimukti.accounter.web.client.ui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
-public class StyleTabPanelImpl extends GwtTabPanel{
-
+public class StyleTabPanelImpl extends GwtTabPanel {
 
 	private StyledPanel tabPanel;
 
 	public StyleTabPanelImpl() {
 		tabPanel = new StyledPanel("tabPanel");
 	}
-	
+
 	@Override
 	public void add(StyledPanel generalTab, String general) {
-		
+		final StyledPanel groupPanel = new StyledPanel("groupPanel");
 		Label tabTitle = new Label(general);
 		tabTitle.setStyleName("tabTitle");
-		tabPanel.add(tabTitle);
-		tabPanel.add(generalTab);
+		tabTitle.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				selectTab(tabPanel.getWidgetIndex(groupPanel));
+			}
+		});
+		groupPanel.add(tabTitle);
+		groupPanel.add(generalTab);
+		tabPanel.add(groupPanel);
 	}
 
 	@Override
@@ -26,6 +35,16 @@ public class StyleTabPanelImpl extends GwtTabPanel{
 		return tabPanel;
 	}
 
-
+	@Override
+	public void selectTab(int index) {
+		for (int i = 0; i < tabPanel.getWidgetCount(); i++) {
+			StyledPanel panel = (StyledPanel) tabPanel.getWidget(i);
+			panel.removeStyleName("openedTabPabel");
+			panel.addStyleName("closedTabPabel");
+		}
+		StyledPanel groupPanel = (StyledPanel) tabPanel.getWidget(index);
+		groupPanel.removeStyleName("closedTabPabel");
+		groupPanel.addStyleName("openedTabPabel");
+	}
 
 }
