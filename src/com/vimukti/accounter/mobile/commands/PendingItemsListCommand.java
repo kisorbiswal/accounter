907153c8d779848ca8b238f0f74bd5607e4f86d2
varakumar <varakumar.pjd@vimukti.com>
2012-03-24 15:47:52 +0530

    Fixedassets List commands
    Former-commit-id: 68f87b427a09b7748fb5e6c29fa4952b0270dc36

---
+++ src/com/vimukti/accounter/mobile/commands/PendingItemsListCommand.java
@@ -0,0 +1,123 @@
+package com.vimukti.accounter.mobile.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import com.vimukti.accounter.core.FixedAsset;
+import com.vimukti.accounter.mobile.CommandList;
+import com.vimukti.accounter.mobile.Context;
+import com.vimukti.accounter.mobile.Record;
+import com.vimukti.accounter.mobile.Requirement;
+import com.vimukti.accounter.mobile.requirements.ShowListRequirement;
+
+public class PendingItemsListCommand extends AbstractCommand {
+
+	@Override
+	public String getId() {
+		return null;
+	}
+
+	@Override
+	protected void addRequirements(List<Requirement> list) {
+
+		list.add(new ShowListRequirement<FixedAsset>(getMessages()
+				.fixedAssets(), getMessages().pleaseSelect(
+				getMessages().fixedAsset()), 20) {
+
+			@Override
+			protected String onSelection(FixedAsset value) {
+				return "updateItem " + value.getID();
+			}
+
+			@Override
+			protected String getShowMessage() {
+				return getMessages().pendingItemsList();
+			}
+
+			@Override
+			protected String getEmptyString() {
+				return getMessages().noRecordsToShow();
+			}
+
+			@Override
+			protected Record createRecord(FixedAsset value) {
+				return PendingItemsListCommand.this.createRecord(value);
+			}
+
+			@Override
+			protected void setCreateCommand(CommandList list) {
+				PendingItemsListCommand.this.setCreateCommand(list);
+			}
+
+			@Override
+			protected boolean filter(FixedAsset e, String name) {
+
+				return e.getName().startsWith(name)
+						|| String.valueOf(e.getID()).startsWith(
+								"" + getNumberFromString(name));
+			}
+
+			@Override
+			protected List<FixedAsset> getLists(Context context) {
+				Set<FixedAsset> assets = getFixedAssets(context);
+				List<FixedAsset> result = new ArrayList<FixedAsset>();
+				for (FixedAsset asset : assets) {
+					if (asset.getStatus() == FixedAsset.STATUS_PENDING)
+						result.add(asset);
+				}
+				return result;
+			}
+
+		});
+
+	}
+
+	protected Record createRecord(FixedAsset value) {
+		Record record = new Record(value);
+		record.add(getMessages().item(), value.getName());
+		record.add(getMessages().assetNumber(), value.getAssetNumber());
+		record.add(getMessages().account(), value.getAssetAccount().getName());
+		record.add(getMessages().purchaseDate(), value.getPurchaseDate());
+		record.add(
+				getMessages().purchasePrice(),
+				getPreferences().getPrimaryCurrency().getSymbol() + " "
+						+ value.getPurchasePrice());
+		return record;
+	}
+
+	protected void setCreateCommand(CommandList list) {
+		list.add("createNewFixedAsset");
+		return;
+	}
+
+	protected Set<FixedAsset> getFixedAssets(Context context) {
+		return context.getCompany().getFixedAssets();
+	}
+
+	@Override
+	protected String initObject(Context context, boolean isUpdate) {
+		return null;
+	}
+
+	@Override
+	protected String getWelcomeMessage() {
+		return null;
+	}
+
+	@Override
+	protected String getDetailsMessage() {
+		return null;
+	}
+
+	@Override
+	protected void setDefaultValues(Context context) {
+
+	}
+
+	@Override
+	public String getSuccessMessage() {
+		return getMessages().success();
+	}
+
+}
