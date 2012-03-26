78576444f65c87fdb15f7308f12490784e79f0f4
Nagaraju P <nagaraju.p@vimukti.com>
2012-03-26 14:27:40 +0530

    Migrator5 for features.
    Former-commit-id: 4041c40e285d43ba0405f2244dc5a2a3c15ac2a5

--- src/com/vimukti/accounter/core/Company.java
+++ src/com/vimukti/accounter/core/Company.java
@@ -41,7 +41,7 @@ public class Company implements IAccounterServerCore {
 
 	public static final String OTHER = "Other";
 
-	public static final int CURRENT_VERSION = 4;
+	public static final int CURRENT_VERSION = 5;
 
 	private long id;
 	private int version;
