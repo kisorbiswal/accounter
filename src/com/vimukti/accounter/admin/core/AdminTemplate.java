f18734d3f9527d82ad694f42b37ddef520723a5b
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2012-03-30 14:54:34 +0530

    Added AdminTemplate class and Mapping
    Former-commit-id: c4ef26c9c30cf7649c7ec96a7bfda2598b5fe59c

---
+++ src/com/vimukti/accounter/admin/core/AdminTemplate.java
@@ -0,0 +1,58 @@
+package com.vimukti.accounter.admin.core;
+
+import com.vimukti.accounter.core.CreatableObject;
+
+public class AdminTemplate extends CreatableObject {
+
+	private String name;
+
+	private String subject;
+
+	private String body;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject
+	 *            the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	/**
+	 * @return the body
+	 */
+	public String getBody() {
+		return body;
+	}
+
+	/**
+	 * @param body
+	 *            the body to set
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+}
