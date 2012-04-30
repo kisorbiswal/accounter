7c99bc4ee0204af68a4f634f9e607e0b8a789c69
Prasanna Kumar G <prasannakumar.g@vimukti.com>
2012-04-30 14:46:22 +0530

    Disabled InlineImages.
    Former-commit-id: d18dbc6b95c2fcad884b4bb70701bb06a540099f

--- src/com/vimukti/accounter/web/client/ui/ImageButton.java
+++ src/com/vimukti/accounter/web/client/ui/ImageButton.java
@@ -3,28 +3,28 @@ package com.vimukti.accounter.web.client.ui;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Image;
 import com.vimukti.accounter.web.client.Global;
 import com.vimukti.accounter.web.client.externalization.AccounterMessages;
 
 public class ImageButton extends Button {
 
-	private ImageResource res;
-	protected static AccounterMessages messages=Global.get().messages();
+	protected static AccounterMessages messages = Global.get().messages();
 
 	public ImageButton(ImageResource res) {
-		this.res = res;
+		setImage(res);
 		setStyleName("Empty-text");
 	}
 
 	public ImageButton(String title, ImageResource res) {
 		super(title);
-		this.res = res;
+		setImage(res);
 		setStyleName("image-button");
 	}
 
 	public ImageButton(String title, ImageResource res, ClickHandler handler) {
 		super(title, handler);
-		this.res = res;
+		setImage(res);
 	}
 
 	/**
@@ -33,32 +33,13 @@ public class ImageButton extends Button {
 	 * @param res
 	 */
 	private void setImage(ImageResource res) {
-		// Element closeseparator = DOM.createSpan();
-		// closeseparator.addClassName("button-separator");
-		// closeseparator.setInnerText("|");
-		// DOM.appendChild(this.getElement(), closeseparator);
-
-		// Element closeimage = DOM.createSpan();
-		// closeimage.addClassName("button-image");
-		// DOM.appendChild(this.getElement(), closeimage);
-		//
-		// ThemesUtil.addDivToButton(this, res, "image-button-right-image");
-		this.getElement().getStyle()
-				.setBackgroundImage("url(" + res.getURL() + ")");
+		Image image = new Image(res);
+		getElement().insertFirst(image.getElement());
 	}
 
-	@Override
-	protected void onLoad() {
-		super.onLoad();
-		setImage(res);
-	}
-	
 	@Override
 	public void setTabIndex(int index) {
 		super.setTabIndex(index);
 	}
-
-	
-	
 
 }
