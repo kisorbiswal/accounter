d39b25ccdf5be6c4c3a72e395b93f8fd4b0a53a7
udaykumar.ch <udaykumar.ch@vimukti.com>
2012-05-03 10:54:16 +0530

    issue :- 5187
    Former-commit-id: c7ade01facc454a15fde476bbd56044d5c370e22

--- src/com/vimukti/accounter/web/client/ui/company/BuildAssembliesGrid.java
+++ src/com/vimukti/accounter/web/client/ui/company/BuildAssembliesGrid.java
@@ -1,9 +1,15 @@
 package com.vimukti.accounter.web.client.ui.company;
 
+import com.vimukti.accounter.web.client.AccounterAsyncCallback;
+import com.vimukti.accounter.web.client.core.AccounterCoreType;
 import com.vimukti.accounter.web.client.core.ClientBuildAssembly;
 import com.vimukti.accounter.web.client.core.ClientItem;
 import com.vimukti.accounter.web.client.core.ClientTransaction;
+import com.vimukti.accounter.web.client.core.IAccounterCore;
+import com.vimukti.accounter.web.client.exception.AccounterException;
 import com.vimukti.accounter.web.client.ui.Accounter;
+import com.vimukti.accounter.web.client.ui.Accounter.AccounterType;
+import com.vimukti.accounter.web.client.ui.core.ErrorDialogHandler;
 import com.vimukti.accounter.web.client.ui.grids.BaseListGrid;
 import com.vimukti.accounter.web.client.ui.grids.ListGrid;
 import com.vimukti.accounter.web.client.ui.reports.ReportsRPC;
@@ -39,6 +45,25 @@ public class BuildAssembliesGrid extends BaseListGrid<ClientBuildAssembly> {
 	@Override
 	protected void executeDelete(ClientBuildAssembly object) {
 
+		if (!Accounter.getUser().getUserRole()
+				.equalsIgnoreCase(messages.readOnly())) {
+			AccounterAsyncCallback<ClientBuildAssembly> callback = new AccounterAsyncCallback<ClientBuildAssembly>() {
+
+				@Override
+				public void onException(AccounterException caught) {
+				}
+
+				@Override
+				public void onResultSuccess(ClientBuildAssembly result) {
+					if (result != null) {
+						deleteObject(result);
+					}
+				}
+
+			};
+			Accounter.createGETService().getObjectById(
+					AccounterCoreType.BUILD_ASSEMBLY, object.getID(), callback);
+		}
 	}
 
 	@Override
@@ -65,8 +90,10 @@ public class BuildAssembliesGrid extends BaseListGrid<ClientBuildAssembly> {
 
 	@Override
 	public void onDoubleClick(ClientBuildAssembly obj) {
-		ReportsRPC.openTransactionView(ClientTransaction.TYPE_BUILD_ASSEMBLY,
-				obj.getID());
+		if (isCanOpenTransactionView(0, IAccounterCore.STOCK_ADJUSTMENT)) {
+			ReportsRPC.openTransactionView(
+					ClientTransaction.TYPE_BUILD_ASSEMBLY, obj.getID());
+		}
 	}
 
 	@Override
@@ -74,6 +101,46 @@ public class BuildAssembliesGrid extends BaseListGrid<ClientBuildAssembly> {
 		return new String[] { messages.assemblyItem(), messages.description(),
 				messages.buildQuantity(), messages.quantityOnHand(),
 				messages.reorderPoint(), messages.delete() };
+	}
+
+	@Override
+	protected void onClick(ClientBuildAssembly obj, int row, int col) {
+		if (!isCanOpenTransactionView(0, IAccounterCore.STOCK_ADJUSTMENT)) {
+			return;
+		}
+		switch (col) {
+		case 5:
+			showWarnDialog(obj);
+			break;
+		default:
+			break;
+		}
+	}
+
+	@Override
+	protected void showWarnDialog(final ClientBuildAssembly object) {
+
+		Accounter.showWarning(
+				messages.doyouwanttoDeleteObj(messages.This()
+						+ object.getName() + " " + messages.stockAdjustment()),
+				AccounterType.WARNING, new ErrorDialogHandler() {
+
+					@Override
+					public boolean onYesClick() {
+						executeDelete(object);
+						return true;
+					}
+
+					@Override
+					public boolean onNoClick() {
+						return true;
+					}
+
+					@Override
+					public boolean onCancelClick() {
+						return false;
+					}
+				});
 	}
 
 	@Override
