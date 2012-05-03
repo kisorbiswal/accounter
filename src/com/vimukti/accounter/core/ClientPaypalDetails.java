fd99be6b0b7db21d88e394adcee413f0df30258e
nagasuresh <nagasuresh.a@vimukti.com>
2012-05-03 16:18:13 +0530

    saving all details of paypal client
    Former-commit-id: ee4f49714192dd1c50ac0e9e0b774a8cced54216

--- src/com/vimukti/accounter/core/ClientPaypalDetails.java
+++ src/com/vimukti/accounter/core/ClientPaypalDetails.java
@@ -12,6 +12,34 @@ public class ClientPaypalDetails {
 	private String mcCurrency;
 	private String addressCountry;
 	private String clinetEmailId;
+	private String mcGross;
+	private String protectionEligibility;
+	private String payerId;
+	private String paymentDate;
+	private String PDTpaymentSstatus;
+	private String charset;
+	private String noptionSelection1;
+	private String nmcFee;
+	private String notifyVersion;
+	private String subscrId;
+	private String custom;
+	private String payerStatus;
+	private String business;
+	private String verifySign;
+	private String optionName1;
+	private String txnId;
+	private String paymentType;
+	private String payerBusinessName;
+	private String btnId;
+	private String receiverEmail;
+	private String paymentFee;
+	private String receiverId;
+	private String txnType;
+	private String itemName;
+	private String itemNumber;
+	private String residenceCountry;
+	private String transactionSubject;
+	private String ipnTrackId;
 
 	public ClientPaypalDetails() {
 
@@ -81,12 +109,236 @@ public class ClientPaypalDetails {
 		this.addressCountry = addressCountry;
 	}
 
-	public String getClinetEmailId(){
+	public String getClinetEmailId() {
 		return clinetEmailId;
 	}
 
 	public void setClinetEmailId(String clinetEmailId) {
 		this.clinetEmailId = clinetEmailId;
+	}
+
+	public String getMcGross() {
+		return mcGross;
+	}
+
+	public void setMcGross(String mcGross) {
+		this.mcGross = mcGross;
+	}
+
+	public String getProtectionEligibility() {
+		return protectionEligibility;
+	}
+
+	public void setProtectionEligibility(String protectionEligibility) {
+		this.protectionEligibility = protectionEligibility;
+	}
+
+	public String getPayerId() {
+		return payerId;
+	}
+
+	public void setPayerId(String payerId) {
+		this.payerId = payerId;
+	}
+
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public String getPDTpaymentSstatus() {
+		return PDTpaymentSstatus;
+	}
+
+	public void setPDTpaymentSstatus(String pDTpaymentSstatus) {
+		PDTpaymentSstatus = pDTpaymentSstatus;
+	}
+
+	public String getCharset() {
+		return charset;
+	}
+
+	public void setCharset(String charset) {
+		this.charset = charset;
+	}
+
+	public String getNoptionSelection1() {
+		return noptionSelection1;
+	}
+
+	public void setNoptionSelection1(String noptionSelection1) {
+		this.noptionSelection1 = noptionSelection1;
+	}
+
+	public String getNmcFee() {
+		return nmcFee;
+	}
+
+	public void setNmcFee(String nmcFee) {
+		this.nmcFee = nmcFee;
+	}
+
+	public String getNotifyVersion() {
+		return notifyVersion;
+	}
+
+	public void setNotifyVersion(String notifyVersion) {
+		this.notifyVersion = notifyVersion;
+	}
+
+	public String getSubscrId() {
+		return subscrId;
+	}
+
+	public void setSubscrId(String subscrId) {
+		this.subscrId = subscrId;
+	}
+
+	public String getCustom() {
+		return custom;
+	}
+
+	public void setCustom(String custom) {
+		this.custom = custom;
+	}
+
+	public String getPayerStatus() {
+		return payerStatus;
+	}
+
+	public void setPayerStatus(String payerStatus) {
+		this.payerStatus = payerStatus;
+	}
+
+	public String getBusiness() {
+		return business;
+	}
+
+	public void setBusiness(String business) {
+		this.business = business;
+	}
+
+	public String getVerifySign() {
+		return verifySign;
+	}
+
+	public void setVerifySign(String verifySign) {
+		this.verifySign = verifySign;
+	}
+
+	public String getOptionName1() {
+		return optionName1;
+	}
+
+	public void setOptionName1(String optionName1) {
+		this.optionName1 = optionName1;
+	}
+
+	public String getTxnId() {
+		return txnId;
+	}
+
+	public void setTxnId(String txnId) {
+		this.txnId = txnId;
+	}
+
+	public String getPaymentType() {
+		return paymentType;
+	}
+
+	public void setPaymentType(String paymentType) {
+		this.paymentType = paymentType;
+	}
+
+	public String getPayerBusinessName() {
+		return payerBusinessName;
+	}
+
+	public void setPayerBusinessName(String payerBusinessName) {
+		this.payerBusinessName = payerBusinessName;
+	}
+
+	public String getBtnId() {
+		return btnId;
+	}
+
+	public void setBtnId(String btnId) {
+		this.btnId = btnId;
+	}
+
+	public String getReceiverEmail() {
+		return receiverEmail;
+	}
+
+	public void setReceiverEmail(String receiverEmail) {
+		this.receiverEmail = receiverEmail;
+	}
+
+	public String getPaymentFee() {
+		return paymentFee;
+	}
+
+	public void setPaymentFee(String paymentFee) {
+		this.paymentFee = paymentFee;
+	}
+
+	public String getReceiverId() {
+		return receiverId;
+	}
+
+	public void setReceiverId(String receiverId) {
+		this.receiverId = receiverId;
+	}
+
+	public String getTxnType() {
+		return txnType;
+	}
+
+	public void setTxnType(String txnType) {
+		this.txnType = txnType;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemNumber() {
+		return itemNumber;
+	}
+
+	public void setItemNumber(String itemNumber) {
+		this.itemNumber = itemNumber;
+	}
+
+	public String getResidenceCountry() {
+		return residenceCountry;
+	}
+
+	public void setResidenceCountry(String residenceCountry) {
+		this.residenceCountry = residenceCountry;
+	}
+
+	public String getTransactionSubject() {
+		return transactionSubject;
+	}
+
+	public void setTransactionSubject(String transactionSubject) {
+		this.transactionSubject = transactionSubject;
+	}
+
+	public String getIpnTrackId() {
+		return ipnTrackId;
+	}
+
+	public void setIpnTrackId(String ipnTrackId) {
+		this.ipnTrackId = ipnTrackId;
 	}
 
 }
