442f1aa35b6ca6ad5f5b5dcee03e57dcd1442eda
Rajesh Akkineni <rajesh@vimukti.com>
2012-08-02 17:45:33 +0530

    Changed Win8 Project
    Former-commit-id: 468760fd10348ae73ebf1cc8a8c08113c1a59fef

--- platforms/windows 8/Accounter/Accounter/js/default.js
+++ platforms/windows 8/Accounter/Accounter/js/default.js
@@ -1,13 +1,33 @@
-﻿(function () {
-    'use strict';
-    // Uncomment the following line to enable first chance exceptions.
-    Debug.enableFirstChanceException(true);
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
 
-    WinJS.Application.onmainwindowactivated = function (e) {
-        if (e.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.launch) {
-           
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+    WinJS.strictProcessing();
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
         }
-    }
+    };
 
-    WinJS.Application.start();
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
 })();
