b86272e02ed3382e31453e4b1fbcddcd646731de
lingarao <lingarao.r@vimukti.com>
2013-03-30 14:54:40 +0530

    Mail Commands
    Former-commit-id: 085a6e1216cd7194e3baa6872be191ae9db58f0d

---
+++ src/com/vimukti/accounter/text/commands/AccountCommand.java
@@ -0,0 +1,20 @@
+package com.vimukti.accounter.text.commands;
+
+import com.vimukti.accounter.text.ITextData;
+import com.vimukti.accounter.text.ITextResponse;
+
+public class AccountCommand extends CreateOrUpdateCommand {
+
+	@Override
+	public void parse(ITextData data, ITextResponse respnse) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void process(ITextResponse respnse) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
