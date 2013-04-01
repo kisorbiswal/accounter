32d24dcef4f27d951baf0f93b0b15326e3813ff3
lingarao <lingarao.r@vimukti.com>
2013-04-01 11:40:35 +0530

    Profit and Loss Command
    Former-commit-id: f7e56c6528b2205eb12fba732e0486e179647908

--- src/com/vimukti/accounter/text/commands/AbstractReportCommand.java
+++ src/com/vimukti/accounter/text/commands/AbstractReportCommand.java
@@ -1,5 +1,26 @@
 package com.vimukti.accounter.text.commands;
 
+import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.web.server.FinanceTool;
 
 public abstract class AbstractReportCommand implements ITextCommand {
+	/**
+	 * Get the Company
+	 * 
+	 * @return
+	 */
+	protected Company getCompany() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Get Finance Tool
+	 * 
+	 * @return
+	 */
+	protected FinanceTool getFinanceTool() {
+		// TODO
+		return null;
+	}
 }
--- src/com/vimukti/accounter/text/commands/reports/ProfitAndLossCommand.java
+++ src/com/vimukti/accounter/text/commands/reports/ProfitAndLossCommand.java
@@ -1,21 +1,61 @@
 package com.vimukti.accounter.text.commands.reports;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.vimukti.accounter.core.FinanceDate;
 import com.vimukti.accounter.text.ITextData;
 import com.vimukti.accounter.text.ITextResponse;
 import com.vimukti.accounter.text.commands.AbstractReportCommand;
+import com.vimukti.accounter.web.client.core.ReportInput;
+import com.vimukti.accounter.web.client.exception.AccounterException;
+import com.vimukti.accounter.web.server.managers.ExportManager;
 
+/**
+ * Profit and Loss Command
+ * 
+ * @author Lingarao.R
+ * 
+ */
 public class ProfitAndLossCommand extends AbstractReportCommand {
+
+	private FinanceDate startDate;
+	private FinanceDate endDate;
 
 	@Override
 	public boolean parse(ITextData data, ITextResponse respnse) {
-		// TODO Auto-generated method stub
-		return false;
+
+		// START DATE
+		if (!data.isDate()) {
+			respnse.addError("Invalid Date format for date field");
+		}
+		// if next date is null,then set the default present date
+		startDate = data.nextDate(new FinanceDate());
+
+		// END DATE
+		if (!data.isDate()) {
+			respnse.addError("Invalid Date format for date field");
+		}
+		// if next date is null,then set the default present date
+		endDate = data.nextDate(new FinanceDate());
+
+		return true;
 	}
 
 	@Override
 	public void process(ITextResponse respnse) {
-		// TODO Auto-generated method stub
-
+		ExportManager manager = getFinanceTool().getExportManager();
+		try {
+			List<String> exportReportToFile = manager.exportReportToFile(
+					getCompany().getId(), ReportInput.REPORT_EXPORT_TYPE_PDF,
+					111, startDate.getDate(), endDate.getDate(),
+					new ArrayList<ReportInput>());
+			// adding File name to response
+			for (String file : exportReportToFile) {
+				respnse.addFile(file);
+			}
+		} catch (AccounterException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
