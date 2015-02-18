7e12f24db6d6f09a9206517b026b5efb2582b7ba
prabhakar <prabhakar.gaali@vimukti.com>
2015-02-18 15:29:52 +0530

    AccountMigrator
    Former-commit-id: 39414d1177657504bdfba4ad3fc2f17c8332d042

--- src/com/vimukti/accounter/migration/AccountMigrator.java
+++ src/com/vimukti/accounter/migration/AccountMigrator.java
@@ -1,11 +1,52 @@
 package com.vimukti.accounter.migration;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.vimukti.accounter.core.Account;
+import com.vimukti.accounter.core.Company;
+import com.vimukti.accounter.core.Currency;
+import com.vimukti.accounter.core.FinanceDate;
+import com.vimukti.accounter.core.User;
 
-public class AccountMigrator { 
-	public static JSONObject migrate(Account account) {
-		return null;
+public class AccountMigrator {
+
+	public static JSONObject migrate(Account account) throws JSONException {
+
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("number", account.getNumber());
+
+		jsonObject.put("name", account.getName());
+
+		jsonObject.put("asOf", account.getAsOf());
+
+		jsonObject.put("type", account.getType());
+
+		jsonObject.put("currency", account.getCurrency());
+
+		jsonObject.put("inactive", account.getIsActive());
+
+		jsonObject.put("description", account.getComment());
+
+		jsonObject.put("creditLimit", account.getCreditLimit());
+
+		jsonObject.put("cardLimit", account.getCardOrLoanNumber());
+
+		jsonObject.put("currentBalance", account.getCurrentBalance());
+
+		jsonObject.put("totalBalance", account.getTotalBalance());
+
+		jsonObject.put("paypalEmail", account.getPaypalEmail());
+
+		jsonObject.put("lastCheckNum", account.getLastCheckNum());
+
+		jsonObject.put("cashFlowCategory", account.getCashFlowCategory());
+
+		jsonObject.put("openingBalance", account.getOpeningBalance());
+
+		jsonObject.put("currencyFactor", account.getCurrencyFactor());
+
+		return jsonObject;
 	}
 }
