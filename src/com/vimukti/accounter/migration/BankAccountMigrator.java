564f81a141759bb53ca55707a3943c9ec97c7794
sivakumar <sivakumar.p@vimukti.com>
2015-02-19 10:53:29 +0530

    BanA/C, SalesPerson, Shipping Method,Terms
    Former-commit-id: af59201083c8dd7b9ad2d3a78446e22efe91cd4f

--- src/com/vimukti/accounter/migration/BankAccountMigrator.java
+++ src/com/vimukti/accounter/migration/BankAccountMigrator.java
@@ -4,8 +4,15 @@ import org.json.JSONObject;
 
 import com.vimukti.accounter.core.BankAccount;
 
-public class BankAccountMigrator { 
-	public static JSONObject migrate(BankAccount bankaccount) {
-		return null;
+public class BankAccountMigrator implements IMigrator<BankAccount> { 
+	
+	@Override
+	public JSONObject migrate(BankAccount bankAccount, MigratorContext context) throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("bankName",bankAccount.getBank().getName());
+		jsonObject.put("bankAccountType",bankAccount.getBankAccountType());
+		jsonObject.put("bankAccountNumber",bankAccount.getBankAccountNumber());
+		return jsonObject;
 	}
 }
