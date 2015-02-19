c218dddbcb2026f59d380e13b42df0e95e288f62
raju <raju.p@vimukti.com>
2015-02-19 16:15:24 +0530

    Accounter migrator classes
    Former-commit-id: efcbf4cf5422c28789d8461ed86b1c8593527835

--- src/com/vimukti/accounter/migration/WarehouseMigrator.java
+++ src/com/vimukti/accounter/migration/WarehouseMigrator.java
@@ -1,11 +1,36 @@
 package com.vimukti.accounter.migration;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.vimukti.accounter.core.Address;
+import com.vimukti.accounter.core.Contact;
 import com.vimukti.accounter.core.Warehouse;
 
-public class WarehouseMigrator { 
-	public static JSONObject migrate(Warehouse warehouse) {
-		return null;
+public class WarehouseMigrator implements IMigrator<Warehouse> {
+
+	@Override
+	public JSONObject migrate(Warehouse obj, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("warehouseCode", obj.getWarehouseCode());
+		jsonObject.put("warehouseName", obj.getName());
+		Contact contact = obj.getContact();
+		jsonObject.put("contactName", contact.getName());
+		jsonObject.put("contactNumber", contact.getBusinessPhone());
+		jsonObject.put("mobileNumber", obj.getMobileNumber());
+		jsonObject.put("dDInumber", obj.getDDINumber());
+		jsonObject.put("defaultWarehouse", obj.isDefaultWarehouse());
+		//Setting Address reference
+		Address address = obj.getAddress();
+		JSONObject jsonAddress = new JSONObject();
+		jsonAddress.put("street", address.getStreet());
+		jsonAddress.put("city", address.getCity());
+		jsonAddress.put("stateOrProvince", address.getStateOrProvinence());
+		jsonAddress.put("zipOrPostalCode", address.getZipOrPostalCode());
+		jsonAddress.put("country", address.getCountryOrRegion());
+		jsonObject.put("address", jsonAddress);
+		// TODO WareHouseItems
+		return jsonObject;
 	}
 }
