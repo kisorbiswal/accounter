84996b7ecd10cc8f507a972de7090babaf27bf16
prabhakar <prabhakar.gaali@vimukti.com>
2015-02-19 19:21:32 +0530

    accounter migration
    Former-commit-id: efba65bdd6a81dc61abbac82e22fc227e5565913

--- src/com/vimukti/accounter/migration/PayHeadMigrator.java
+++ src/com/vimukti/accounter/migration/PayHeadMigrator.java
@@ -1,11 +1,36 @@
 package com.vimukti.accounter.migration;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.vimukti.accounter.core.PayHead;
+import com.vimukti.accounter.web.client.core.ClientPayHead;
 
-public class PayHeadMigrator { 
-	public static JSONObject migrate(PayHead payhead) {
-		return null;
+public class PayHeadMigrator {
+	public static JSONObject migrate(PayHead payhead, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", payhead.getName());
+		jsonObject.put(
+				"payHeadType",
+				context.getPickListContext().get("PayHeadType",
+						ClientPayHead.getPayHeadType(payhead.getType())));
+		jsonObject.put("isAffectNetSalary", payhead.isAffectNetSalary());
+		jsonObject.put("expenseAccount", payhead.getAccount());
+		jsonObject.put("calculationType", payhead.getCalculationType());
+		jsonObject.put("paySlipName", payhead.getNameToAppearInPaySlip());
+		jsonObject.put("isDeduction", payhead.isDeduction());
+		jsonObject.put("isEarning", payhead.isEarning());
+		jsonObject.put("assetAccount",
+				context.get("Account", payhead.getAssetAccount().getID()));
+		jsonObject.put("statutoryLiabilityAccount",
+				context.get("Account", payhead.getLiabilityAccount().getID()));
+		// TODO PayHead.obj has
+		// isFromTimeSheet,formulaItems,lastComputedValuer,otherPayHead,
+		// earningDeductionOn
+		// ,calculationPeriod,attendanceLeaveWithPay,productionType,
+		// perDayCalculationBasis,userDefinedCalendar,computeOn,computationSlabs
+		// not in PayHead.java
+		return jsonObject;
 	}
 }
--- src/com/vimukti/accounter/migration/ReconciliationMigrator.java
+++ src/com/vimukti/accounter/migration/ReconciliationMigrator.java
@@ -1,11 +1,44 @@
 package com.vimukti.accounter.migration;
 
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.vimukti.accounter.core.Reconciliation;
+import com.vimukti.accounter.core.ReconciliationItem;
 
-public class ReconciliationMigrator { 
-	public static JSONObject migrate(Reconciliation reconciliation) {
-		return null;
+public class ReconciliationMigrator implements IMigrator<Reconciliation> {
+	@Override
+	public JSONObject migrate(Reconciliation reconciliation,
+			MigratorContext context) throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("name", reconciliation.getName());
+		jsonObject.put("account",
+				context.get("Account", reconciliation.getAccount().getID()));
+		jsonObject.put("startDate", reconciliation.getStartDate()
+				.getAsDateObject());
+		jsonObject
+				.put("endDate", reconciliation.getEndDate().getAsDateObject());
+		jsonObject.put("reconciliationDate", reconciliation
+				.getReconcilationDate().getAsDateObject());
+		jsonObject.put("closingBalance", reconciliation.getClosingBalance());
+		Set<ReconciliationItem> items = reconciliation.getItems();
+		JSONArray array = new JSONArray();
+		for (ReconciliationItem item : items) {
+			JSONObject jsonObject1 = new JSONObject();
+			jsonObject1.put("transactionNumber", item.getTransactionNo());
+			jsonObject1.put("transactionDate", item.getTransactionDate()
+					.getAsDateObject());
+			jsonObject1.put("memo", item.getTransctionMemo());
+			jsonObject1.put("amount", item.getAmount());
+			array.put(jsonObject1);
+			// TODO accountTransaction type is AccountTransaction in obj. not
+			// available in java file
+		}
+		jsonObject.put("reconciliationItems", array);
+		// TODO clearedAmount,difference are not available in java file
+		return jsonObject;
 	}
 }
