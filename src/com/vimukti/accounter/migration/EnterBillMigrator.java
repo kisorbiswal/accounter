36e3c409503883d09592f9893312316a031b46d1
sivakumar <sivakumar.p@vimukti.com>
2015-02-20 15:13:35 +0530

    Transaction Migrator
    Former-commit-id: 34f60bc66969eb4db6364a32890a8e5aa713ae29

--- src/com/vimukti/accounter/migration/EnterBillMigrator.java
+++ src/com/vimukti/accounter/migration/EnterBillMigrator.java
@@ -1,42 +1,20 @@
 package com.vimukti.accounter.migration;
 
-import java.util.List;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.vimukti.accounter.core.AccounterClass;
 import com.vimukti.accounter.core.Address;
 import com.vimukti.accounter.core.EnterBill;
-import com.vimukti.accounter.core.Location;
-import com.vimukti.accounter.core.Quantity;
-import com.vimukti.accounter.core.TAXCode;
-import com.vimukti.accounter.core.TransactionItem;
-import com.vimukti.accounter.core.Unit;
+import com.vimukti.accounter.core.PaymentTerms;
 
-public class EnterBillMigrator implements IMigrator<EnterBill> {
+public class EnterBillMigrator extends TransactionMigrator<EnterBill> {
 
 	@Override
 	public JSONObject migrate(EnterBill obj, MigratorContext context)
 			throws JSONException {
-		JSONObject enterBill = new JSONObject();
-		enterBill.put("date", obj.getDate());
-		enterBill.put("Number", obj.getNumber());
+		JSONObject enterBill = super.migrate(obj, context);
 		enterBill.put("payee", context.get("Vendor", obj.getVendor().getID()));
 		enterBill.put("contact", obj.getContact());
-		AccounterClass accounterClass = obj.getAccounterClass();
-		if (accounterClass != null) {
-			enterBill.put("accountClass",
-					context.get("AccountClass", accounterClass.getID()));
-		}
-		Location location = obj.getLocation();
-		if (location != null) {
-			enterBill
-					.put("location", context.get("Location", location.getID()));
-		}
-		enterBill.put("currencyFactor", obj.getCurrencyFactor());
-		enterBill.put("notes", obj.getMemo());
 		enterBill.put("isReconciled", "");
 		enterBill.put("dueDate", obj.getDueDate().getAsDateObject());
 		enterBill.put("phone", obj.getPhone());
@@ -52,61 +30,12 @@ public class EnterBillMigrator implements IMigrator<EnterBill> {
 			addressJSON.put("country", vendorAddress.getCountryOrRegion());
 			enterBill.put("billTo", vendorAddress);
 		}
-		enterBill.put("paymentTerm",
-				context.get("PaymentTerm", obj.getPaymentTerm().getID()));
+		PaymentTerms paymentTerm = obj.getPaymentTerm();
+		if (paymentTerm != null) {
+			enterBill.put("paymentTerm",
+					context.get("PaymentTerm", paymentTerm.getID()));
+		}
 		enterBill.put("deliveryDate", obj.getDeliveryDate().getAsDateObject());
-		List<TransactionItem> transactionItems = obj.getTransactionItems();
-		JSONArray billItems = new JSONArray();
-		for (TransactionItem transactionItem : transactionItems) {
-			JSONObject billItem = new JSONObject();
-			if (transactionItem.getType() == TransactionItem.TYPE_ACCOUNT) {
-				billItem.put("account", context.get("Account", transactionItem
-						.getItem().getID()));
-			} else {
-				billItem.put("item",
-						context.get("Item", transactionItem.getItem().getID()));
-				{
-					Quantity quantity = transactionItem.getQuantity();
-					JSONObject quantityJSON = new JSONObject();
-					quantityJSON.put("value", quantity.getValue());
-					Unit unit = quantity.getUnit();
-					quantityJSON.put("unitFactor", unit.getFactor());
-					quantityJSON.put("unit", unit);
-					billItem.put("quantityItem", quantityJSON);
-				}
-			}
-			billItem.put("description", transactionItem.getDescription());
-			billItem.put("unitPrice", transactionItem.getUnitPrice());
-			AccounterClass classCategory = transactionItem.getAccounterClass();
-			if (classCategory != null) {
-				billItem.put("accountClass",
-						context.get("AccountClass", classCategory.getID()));
-			}
-			billItem.put("taxCode", context.get("TaxCode", transactionItem
-					.getTaxCode().getID()));
-			billItem.put("taxable", transactionItem.isTaxable());
-			billItem.put("discount", transactionItem.getDiscount());
-			billItems.put(billItem);
-		}
-		enterBill.put("amountIncludesTax", transactionItems.get(0)
-				.isAmountIncludeTAX());
-		boolean taxPerDetailLine = context.getCompany().getPreferences()
-				.isTaxPerDetailLine();
-		if (!taxPerDetailLine) {
-			TAXCode taxCode = transactionItems.get(0).getTaxCode();
-			if (taxCode != null) {
-				enterBill.put("taxCode",
-						context.get("TaxCode", taxCode.getID()));
-			}
-		}
-		boolean discountPerDetailLine = context.getCompany().getPreferences()
-				.isDiscountPerDetailLine();
-		if (!discountPerDetailLine) {
-			Double discount = transactionItems.get(0).getDiscount();
-			if (discount != null) {
-				enterBill.put("discount", discount);
-			}
-		}
 		return enterBill;
 	}
 }
--- src/com/vimukti/accounter/migration/TransactionMigrator.java
+++ src/com/vimukti/accounter/migration/TransactionMigrator.java
@@ -1,11 +1,94 @@
 package com.vimukti.accounter.migration;
 
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.vimukti.accounter.core.AccounterClass;
+import com.vimukti.accounter.core.Location;
+import com.vimukti.accounter.core.Quantity;
+import com.vimukti.accounter.core.TAXCode;
 import com.vimukti.accounter.core.Transaction;
+import com.vimukti.accounter.core.TransactionItem;
+import com.vimukti.accounter.core.Unit;
 
-public class TransactionMigrator { 
-	public static JSONObject migrate(Transaction transaction) {
-		return null;
+public class TransactionMigrator<T extends Transaction> implements IMigrator<T> {
+
+	@Override
+	public JSONObject migrate(T obj, MigratorContext context)
+			throws JSONException {
+		JSONObject transaction = new JSONObject();
+		transaction.put("date", obj.getDate());
+		transaction.put("number", obj.getNumber());
+		AccounterClass accounterClass = obj.getAccounterClass();
+		if (accounterClass != null) {
+			transaction.put("accountClass",
+					context.get("AccountClass", accounterClass.getID()));
+		}
+		Location location = obj.getLocation();
+		if (location != null) {
+			transaction.put("location",
+					context.get("Location", location.getID()));
+		}
+		transaction.put("currencyFactor", obj.getCurrencyFactor());
+		transaction.put("notes", obj.getMemo());
+		List<TransactionItem> transactionItems = obj.getTransactionItems();
+		JSONArray tItems = new JSONArray();
+		for (TransactionItem transactionItem : transactionItems) {
+			JSONObject tItem = new JSONObject();
+			if (transactionItem.getType() == TransactionItem.TYPE_ACCOUNT) {
+				tItem.put("account", context.get("Account", transactionItem
+						.getItem().getID()));
+			} else {
+				tItem.put("item",
+						context.get("Item", transactionItem.getItem().getID()));
+				{
+					Quantity quantity = transactionItem.getQuantity();
+					JSONObject quantityJSON = new JSONObject();
+					quantityJSON.put("value", quantity.getValue());
+					Unit unit = quantity.getUnit();
+					quantityJSON.put("unitFactor", unit.getFactor());
+					quantityJSON.put("unit", unit);
+					tItem.put("quantityItem", quantityJSON);
+				}
+			}
+			tItem.put("description", transactionItem.getDescription());
+			tItem.put("unitPrice", transactionItem.getUnitPrice());
+			AccounterClass classCategory = transactionItem.getAccounterClass();
+			if (classCategory != null) {
+				tItem.put("accountClass",
+						context.get("AccountClass", classCategory.getID()));
+			}
+			tItem.put("taxCode", context.get("TaxCode", transactionItem
+					.getTaxCode().getID()));
+			tItem.put("taxable", transactionItem.isTaxable());
+			tItem.put("discount", transactionItem.getDiscount());
+			tItems.put(tItem);
+		}
+		if (!transactionItems.isEmpty()) {
+			transaction.put("transactionItems", tItems);
+			transaction.put("amountIncludesTax", transactionItems.get(0)
+					.isAmountIncludeTAX());
+			boolean taxPerDetailLine = context.getCompany().getPreferences()
+					.isTaxPerDetailLine();
+			if (!taxPerDetailLine) {
+				TAXCode taxCode = transactionItems.get(0).getTaxCode();
+				if (taxCode != null) {
+					transaction.put("taxCode",
+							context.get("TaxCode", taxCode.getID()));
+				}
+			}
+			boolean discountPerDetailLine = context.getCompany()
+					.getPreferences().isDiscountPerDetailLine();
+			if (!discountPerDetailLine) {
+				Double discount = transactionItems.get(0).getDiscount();
+				if (discount != null) {
+					transaction.put("discount", discount);
+				}
+			}
+		}
+		return transaction;
 	}
 }
