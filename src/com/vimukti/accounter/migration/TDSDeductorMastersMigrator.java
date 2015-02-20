eaf3d8c0ff29efa0abaee47a08148d4327cb41f5
sivakumar <sivakumar.p@vimukti.com>
2015-02-20 10:44:37 +0530

    TDS Migrator
    Former-commit-id: 7bb71053453709a09ba78f18310d0e212143bc0f

--- src/com/vimukti/accounter/migration/TDSDeductorMastersMigrator.java
+++ src/com/vimukti/accounter/migration/TDSDeductorMastersMigrator.java
@@ -1,11 +1,48 @@
 package com.vimukti.accounter.migration;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.vimukti.accounter.core.TDSDeductorMasters;
+import com.vimukti.accounter.web.client.core.Utility;
 
-public class TDSDeductorMastersMigrator {
-	public static JSONObject migrate(TDSDeductorMasters tdsdeductormasters) {
-		return null;
+public class TDSDeductorMastersMigrator implements
+		IMigrator<TDSDeductorMasters> {
+
+	@Override
+	public JSONObject migrate(TDSDeductorMasters obj, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("deductorName", obj.getDeductorName());
+		jsonObject.put("branch", obj.getBranch());
+		jsonObject.put("flatNo", obj.getFlatNo());
+		jsonObject.put("buildingName", obj.getBuildingName());
+		jsonObject.put("area", obj.getArea());
+		jsonObject.put("city", obj.getCity());
+		jsonObject.put("state", obj.getState());
+		jsonObject.put("pinCode", obj.getPinCode());
+		jsonObject.put("addressdChanged", obj.isAddressdChanged());
+		jsonObject.put("telephoneNumber", obj.getTelephoneNumber());
+		jsonObject.put("faxNo", obj.getFaxNo());
+		jsonObject.put(
+				"deductorType",
+				context.getPickListContext().get("DeductorType",
+						obj.getDeductorType()));
+		jsonObject.put("govtState", obj.getGovtState());
+		jsonObject.put("paoCode", obj.getPaoCode());
+		jsonObject.put("paoRegistration", obj.getPaoRegistration());
+		jsonObject.put("ddoCode", obj.getDdoCode());
+		jsonObject.put("ddoRegistration", obj.getDdoRegistration());
+		jsonObject.put("ministryDeptName", obj.getMinistryDeptName());
+		jsonObject.put("ministryDeptOtherName", obj.getMinistryDeptOtherName());
+		jsonObject.put("tanNumber", obj.getTanNumber());
+		jsonObject.put("panNumber", obj.getPanNumber());
+		jsonObject.put("stdCode", obj.getStdCode());
+		jsonObject.put("isAddressSameForResopsiblePerson",
+				obj.isAddressSameForResopsiblePerson());
+		jsonObject.put("taxOfficeAddress", obj.getTaxOfficeAddress());
+
+		return jsonObject;
 	}
 }
--- src/com/vimukti/accounter/migration/TDSResponsiblePersonMigrator.java
+++ src/com/vimukti/accounter/migration/TDSResponsiblePersonMigrator.java
@@ -1,11 +1,53 @@
 package com.vimukti.accounter.migration;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.vimukti.accounter.core.TDSResponsiblePerson;
+import com.vimukti.accounter.web.client.Global;
+import com.vimukti.accounter.web.client.externalization.AccounterMessages;
 
-public class TDSResponsiblePersonMigrator { 
-	public static JSONObject migrate(TDSResponsiblePerson tdsresponsibleperson) {
-		return null;
+public class TDSResponsiblePersonMigrator implements
+		IMigrator<TDSResponsiblePerson> {
+
+	@Override
+	public JSONObject migrate(TDSResponsiblePerson obj, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = new JSONObject();
+
+		jsonObject.put("responsibleName", obj.getResponsibleName());
+		jsonObject.put("designation", obj.getDesignation());
+		jsonObject.put("branch", obj.getBranch());
+		jsonObject.put("flatNo", obj.getFlatNo());
+		jsonObject.put("buildingName", obj.getBuildingName());
+		jsonObject.put("street", obj.getStreet());
+		jsonObject.put("area", obj.getArea());
+		jsonObject.put("city", obj.getCity());
+		jsonObject.put("stateName", obj.getStateName());
+		jsonObject.put("pinCode", obj.getPinCode());
+		jsonObject.put("addressChanged", obj.isAddressChanged());
+		jsonObject.put("telephoneNo", obj.getTelephoneNumber());
+		jsonObject.put("faxNo", obj.getFaxNo());
+		jsonObject.put("emailAddress", obj.getEmailAddress());
+		jsonObject.put("financialYear", obj.getFinancialYear());
+		jsonObject.put("assesmentYear", obj.getAssesmentYear());
+		jsonObject.put(
+				"retutnType",
+				context.getPickListContext().get("RetutnType",
+						getReturnTypeString(obj.getReturnType())));
+		jsonObject.put("existingTDSassesses", obj.isExistingTDSassesse());
+		jsonObject.put("panNumber", obj.getPanNumber());
+		jsonObject.put("panRegistrationNumber", obj.getPanRegistrationNumber());
+		jsonObject.put("mobilenumber", obj.getMobileNumber());
+		jsonObject.put("stdCode", obj.getStdCode());
+		return jsonObject;
+	}
+
+	public String getReturnTypeString(int returnType) {
+		AccounterMessages messages = Global.get().messages();
+		if (returnType == 1) {
+			return messages.electronic();
+		}
+		return messages.digital();
 	}
 }
