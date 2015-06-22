bc28fe244a916b3fdf4f366c9c0eda15a1d9d3c4
nagarjuna.j <nagarjuna.j@vimukti.com>
2015-06-22 11:42:37 +0530

    Migration
    Former-commit-id: c2366cb864bab213bb695c5f447be68f4f3fd156

---
+++ src/com/vimukti/accounter/migration/FileTaxMigrator.java
@@ -0,0 +1,35 @@
+package com.vimukti.accounter.migration;
+
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.vimukti.accounter.core.TAXAgency;
+import com.vimukti.accounter.core.TAXReturn;
+import com.vimukti.accounter.core.TAXReturnEntry;
+
+public class FileTaxMigrator extends TransactionMigrator<TAXReturn> {
+
+	@Override
+	public JSONObject migrate(TAXReturn obj, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = super.migrate(obj, context);
+		TAXAgency taxAgency = obj.getTaxAgency();
+		if (taxAgency != null) {
+			jsonObject.put("taxAgency",
+					context.get("TaxAgency", taxAgency.getID()));
+		}
+		List<TAXReturnEntry> taxReturnEntries = obj.getTaxReturnEntries();
+		JSONArray array = new JSONArray();
+		for (TAXReturnEntry taxReturnEntry : taxReturnEntries) {
+			JSONObject transactionJson = new JSONObject();
+			// TODO TaxRateCalculation Object is not there in account
+			// FileTaxItem Completely depends on that.
+			array.put(transactionJson);
+		}
+		jsonObject.put("taxItems", array);
+		return jsonObject;
+	}
+}
---
+++ src/com/vimukti/accounter/migration/TaxRefundMigrator.java
@@ -0,0 +1,47 @@
+package com.vimukti.accounter.migration;
+
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.vimukti.accounter.core.ReceiveVAT;
+import com.vimukti.accounter.core.ReceiveVATEntries;
+import com.vimukti.accounter.core.TAXAgency;
+
+public class TaxRefundMigrator extends TransactionMigrator<ReceiveVAT> {
+
+	@Override
+	public JSONObject migrate(ReceiveVAT obj, MigratorContext context)
+			throws JSONException {
+		JSONObject jsonObject = super.migrate(obj, context);
+		jsonObject.put("depositIn",
+				context.get("Account", obj.getDepositIn().getID()));
+		jsonObject.put("filterbyTAXreturnendDate", obj
+				.getReturnsDueOnOrBefore().getAsDateObject());
+		TAXAgency taxAgency = obj.getTaxAgency();
+		if (taxAgency != null) {
+			jsonObject.put("involvedPayee",
+					context.get("TaxAgency", taxAgency.getID()));
+		}
+		JSONArray array = new JSONArray();
+		Set<ReceiveVATEntries> receiveVATEntriesList = obj
+				.getReceiveVATEntriesList();
+		for (ReceiveVATEntries vatEntries : receiveVATEntriesList) {
+			JSONObject itemJson = new JSONObject();
+			TAXAgency taxAgency2 = vatEntries.getTAXAgency();
+			if (taxAgency2 != null) {
+				itemJson.put("taxAgency",
+						context.get("TaxAgency", taxAgency2.getID()));
+			}
+			itemJson.put("fileTax",
+					context.get("FileTax", vatEntries.getTransaction().getID()));
+			itemJson.put("taxDue", vatEntries.getBalance());
+			itemJson.put("amountToReceive", vatEntries.getAmount());
+			array.put(itemJson);
+		}
+		jsonObject.put("taxRefundItems", array);
+		return jsonObject;
+	}
+}
