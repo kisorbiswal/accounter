9b1c2747f15a6b5cb68c969d00ec81b39bc45bff
raju <raju.p@vimukti.com>
2015-06-22 11:57:52 +0530

    Account migration
    Former-commit-id: 46095cd8ba9a2447b092aa5563e05be6883b6d36

--- src/com/vimukti/accounter/migration/CreditsMigrator.java
+++ src/com/vimukti/accounter/migration/CreditsMigrator.java
@@ -3,7 +3,6 @@ package com.vimukti.accounter.migration;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.vimukti.accounter.core.Contact;
 import com.vimukti.accounter.core.Estimate;
 
 public class CreditsMigrator extends TransactionMigrator<Estimate> {
@@ -11,20 +10,11 @@ public class CreditsMigrator extends TransactionMigrator<Estimate> {
 	public JSONObject migrate(Estimate obj, MigratorContext context)
 			throws JSONException {
 		JSONObject jsonObj = super.migrate(obj, context);
-
-		Contact contact = obj.getContact();
-		if (contact != null) {
-			JSONObject jsonContact = new JSONObject();
-			jsonContact.put("isPrimary", contact.isPrimary());
-			jsonContact.put("contactName", contact.getName());
-			jsonContact.put("title", contact.getTitle());
-			jsonContact.put("businessPhone", contact.getBusinessPhone());
-			jsonContact.put("email", contact.getEmail());
-			jsonObj.put("contact", jsonContact);
+		if (obj.getContact() != null) {
+			jsonObj.put("contact",
+					context.get("Contact", obj.getContact().getID()));
 		}
-		jsonObj.put("payeee",
-				context.get("Customer", obj.getCustomer().getID()));
-
+		jsonObj.put("payee", context.get("Customer", obj.getCustomer().getID()));
 		return jsonObj;
 	}
 }
